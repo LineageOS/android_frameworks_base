6afcf2bdd1e2ba1be9cc2073af189bbeec797d5e
Konsta <konsta09@gmail.com>
2012-08-20 10:39:57 +0300

    Framework: Volume key cursor control
    This feature is moved to framework so it also works with third
    party keyboards.
    
    Change-Id: I8e20240e7bee5351ab20bb3d701eb95a5fd3e112

--- core/java/android/inputmethodservice/InputMethodService.java
+++ core/java/android/inputmethodservice/InputMethodService.java
@@ -715,6 +715,11 @@ public class InputMethodService extends AbstractInputMethodService {
     @Nullable
     private ImeTracker.Token mCurStatsToken;
 
+    int mVolumeKeyCursorControl;
+    private static final int VOLUME_CURSOR_OFF = 0;
+    private static final int VOLUME_CURSOR_ON = 1;
+    private static final int VOLUME_CURSOR_ON_REVERSE = 2;
+
     final ViewTreeObserver.OnComputeInternalInsetsListener mInsetsComputer = info -> {
         onComputeInsets(mTmpInsets);
         mNavigationBarController.updateInsets(mTmpInsets);
@@ -1636,6 +1641,9 @@ public class InputMethodService extends AbstractInputMethodService {
             service.getContentResolver().registerContentObserver(
                     Settings.Secure.getUriFor(Settings.Secure.SHOW_IME_WITH_HARD_KEYBOARD),
                     false, observer);
+            service.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.VOLUME_KEY_CURSOR_CONTROL),
+                    false, observer);
             return observer;
         }
 
@@ -1676,6 +1684,9 @@ public class InputMethodService extends AbstractInputMethodService {
                 // state as if configuration was changed.
                 mService.resetStateForNewConfiguration();
             }
+
+            mService.mVolumeKeyCursorControl = Settings.System.getInt(mService.getContentResolver(),
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0);
         }
 
         @Override
@@ -1746,6 +1757,8 @@ public class InputMethodService extends AbstractInputMethodService {
         // cache preference so we don't have to read ContentProvider when IME is requested to be
         // shown the first time (cold start).
         mSettingsObserver.shouldShowImeWithHardKeyboard();
+        mVolumeKeyCursorControl = Settings.System.getInt(getContentResolver(),
+                Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0);
 
         final boolean hideNavBarForKeyboard = getApplicationContext().getResources().getBoolean(
                 com.android.internal.R.bool.config_hideNavBarForKeyboard);
@@ -3679,6 +3692,23 @@ public class InputMethodService extends AbstractInputMethodService {
             }
         }
 
+        if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP) {
+            if (isInputViewShown() && mVolumeKeyCursorControl != VOLUME_CURSOR_OFF) {
+                sendDownUpKeyEvents(mVolumeKeyCursorControl == VOLUME_CURSOR_ON_REVERSE
+                        ? KeyEvent.KEYCODE_DPAD_RIGHT : KeyEvent.KEYCODE_DPAD_LEFT);
+                return true;
+            }
+            return false;
+        }
+        if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            if (isInputViewShown() && mVolumeKeyCursorControl != VOLUME_CURSOR_OFF) {
+                sendDownUpKeyEvents(mVolumeKeyCursorControl == VOLUME_CURSOR_ON_REVERSE
+                        ? KeyEvent.KEYCODE_DPAD_LEFT : KeyEvent.KEYCODE_DPAD_RIGHT);
+                return true;
+            }
+            return false;
+        }
+
         return doMovementKey(keyCode, event, MOVEMENT_DOWN);
     }
 
@@ -3752,6 +3782,11 @@ public class InputMethodService extends AbstractInputMethodService {
             if (event.isTracking() && !event.isCanceled()) {
                 return true;
             }
+        }
+
+        if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP
+                 || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            return isInputViewShown() && mVolumeKeyCursorControl != VOLUME_CURSOR_OFF;
         }
 
         return doMovementKey(keyCode, event, MOVEMENT_UP);
