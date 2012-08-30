64de6f3b987b5a040cced348c9ada5e8e02dab19
Timo Wendt <timo@tjwendt.de>
2012-08-30 12:18:41 +0300

    Runtime toggle of navbar
    This adds the framework support for enabling the Navigation bar
    on devices with hardware keys. It is toggled from Settings, and
    depends on device-specific support for the KeyDisabler hardware
    control
    
    Change-Id: I88fecb2ca1e8613591c327a93f53909b00239cd8
    
    wm: Nullify hardkey function assignments if enabling the navbar
    
    This caused erroneous (and sometimes duplicate) events being generated
    due to the regular key function assignments. The navbar does its
    own action management, so don't try to derive from the actions
    usually present in hard keys.
    
    Change-Id: I82866e24547f8145cac4f07820ae90aacce09281
    
    Update DEV_FORCE_SHOW_NAVBAR constant.
    
    Change-Id: Ie5b4317162c514d22276956f81007e064a3d0f32
    
    Settings: Move DEV_FORCE_SHOW_NAVBAR load to loadSecureSettings.
    
    Change-Id: I6ac53b8c9f7fce6f9ca6b4ad7bf31a1c1e896863

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -6387,6 +6387,11 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public boolean needsNavigationBar() {
+        return mPolicy.needsNavigationBar();
+    }
+
+    @Override
     public void lockNow(Bundle options) {
         mPolicy.lockNow(options);
     }
