3022c08aca582d3e04627fdc5d437dcf81ada5d3
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-11-23 14:23:16 +0000

    fw/b: Button backlight brightness
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Danny Baumann <dannybaumann@web.de>
    Co-authored-by: nadlabak <pavel@doshaska.net>
    Co-authored-by: Steve Kondik <steve@cyngn.com>
    Change-Id: I5176a2028c18408c17bac7f25e62b5612fd6c227

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -96,6 +96,8 @@ import com.android.server.display.utils.SensorUtils;
 import com.android.server.display.whitebalance.DisplayWhiteBalanceController;
 import com.android.server.display.whitebalance.DisplayWhiteBalanceFactory;
 import com.android.server.display.whitebalance.DisplayWhiteBalanceSettings;
+import com.android.server.lights.LightsManager;
+import com.android.server.lights.LogicalLight;
 import com.android.server.policy.WindowManagerPolicy;
 
 import lineageos.providers.LineageSettings;
@@ -243,6 +245,9 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
     // Battery stats.
     @Nullable
     private final IBatteryStats mBatteryStats;
+
+    // The lights manager.
+    private final LightsManager mLights;
 
     // The sensor manager.
     private final SensorManager mSensorManager;
@@ -525,6 +530,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         mTag = TAG + "[" + mDisplayId + "]";
         mThermalBrightnessThrottlingDataId =
                 logicalDisplay.getDisplayInfoLocked().thermalBrightnessThrottlingDataId;
+        mLights = LocalServices.getService(LightsManager.class);
         mDisplayDevice = mLogicalDisplay.getPrimaryDisplayDeviceLocked();
         mUniqueDisplayId = logicalDisplay.getPrimaryDisplayDeviceLocked().getUniqueId();
         mDisplayStatsId = mUniqueDisplayId.hashCode();
@@ -1358,6 +1364,15 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                 state, /* reason= */ stateAndReason.second,
                 mDisplayStateController.shouldPerformScreenOffTransition());
         state = mPowerState.getScreenState();
+
+        // Disable button lights when screen off or dozing
+        if (state == Display.STATE_OFF || state == Display.STATE_DOZE ||
+                state == Display.STATE_DOZE_SUSPEND) {
+            LogicalLight buttonsLight = mLights.getLight(LightsManager.LIGHT_ID_BUTTONS);
+            if (buttonsLight != null) {
+                buttonsLight.setBrightness(PowerManager.BRIGHTNESS_OFF_FLOAT);
+            }
+        }
 
         DisplayBrightnessState displayBrightnessState = mDisplayBrightnessController
                 .updateBrightness(mPowerRequest, state, mDisplayOffloadSession);
