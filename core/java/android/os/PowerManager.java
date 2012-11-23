4f73701fe9d7aa9d48673b1aee812ee10fae0495
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-11-23 14:23:16 +0000

    fw/b: Button backlight brightness
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Danny Baumann <dannybaumann@web.de>
    Co-authored-by: nadlabak <pavel@doshaska.net>
    Co-authored-by: Steve Kondik <steve@cyngn.com>
    Change-Id: I5176a2028c18408c17bac7f25e62b5612fd6c227

--- core/java/android/os/PowerManager.java
+++ core/java/android/os/PowerManager.java
@@ -565,7 +565,8 @@ public final class PowerManager {
             BRIGHTNESS_CONSTRAINT_TYPE_MAXIMUM,
             BRIGHTNESS_CONSTRAINT_TYPE_DEFAULT,
             BRIGHTNESS_CONSTRAINT_TYPE_DIM,
-            BRIGHTNESS_CONSTRAINT_TYPE_DOZE
+            BRIGHTNESS_CONSTRAINT_TYPE_DOZE,
+            BRIGHTNESS_CONSTRAINT_TYPE_DEFAULT_BUTTON
     })
     @Retention(RetentionPolicy.SOURCE)
     public @interface BrightnessConstraint{}
@@ -598,6 +599,12 @@ public final class PowerManager {
      * @hide
      */
     public static final int BRIGHTNESS_CONSTRAINT_TYPE_DOZE = 4;
+
+    /**
+     * Brightness constraint type: minimum allowed value.
+     * @hide
+     */
+    public static final int BRIGHTNESS_CONSTRAINT_TYPE_DEFAULT_BUTTON = 8;
 
     /**
      * @hide
