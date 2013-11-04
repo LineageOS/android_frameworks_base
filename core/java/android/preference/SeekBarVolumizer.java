44f2bb9ca1cbe8a0f527d095feb58dea41fc95a6
Adnan Begovic <adnan@cyngn.com>
2013-11-04 00:58:57 +0000

    frameworks: Add unlinked ringtone and notification volumes
    Port over linked volumes from 12.1
    
    Implement linked volumes and add upgrade path.
    
    Change-Id: Ibbeff656fcaaa4a2ff79de530b48b216df271b21
    
    frameworks: read VOLUME_LINK_NOTIFICATION from proper table
    
    Change-Id: I15e9caa18f307a51a732c7a59013bbbb234a7477
    Signed-off-by: Roman Birg <roman@cyngn.com>
    (cherry picked from commit 1b7cfa1716017e0d03c9693428c21d8e11c5dc0a)
    
    fix Notification volume slider linking
    
    Change-Id: I0f74485f5c8c2aa7698cf9ba1c60c2c8c967798d
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Improve SeekBarVolumizer behavior with unlinked streams
    
    - Fix notification stream enabling at 0 volume
    - Fix the notification slider being set when moving ringer stream
    
    Change-Id: If3705cc833c07f726f8ad7b1ec79a42e86e8ff22
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    base: Fix notification sounds for wifi only devices
    
    The linking of voice & notification doesn't take into account the
    fact that a device may not actually have voice capabilities.  Whenever
    checking whether or not to link notification and ring volumes, also
    verify that the device really is voice capable.
    
    Change-Id: Iae49302d3b5934d36b182e289e4975f5dd85b834
    
    frameworks: fix setting up linked notification stream on boot
    
    updateStreamVolumeAlias() was previously responsible for checking the
    linked notification variable and mapping the notification stream, but
    the setting to read whether the notifications were linked was read in
    after that method had run which was responsible for linking them.
    Extract the logic and re-run it again after reading the persisted
    settings.
    
    Change-Id: Ia62bf3ca072def54ec6ebf4f9e865d9c913e3bc4
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    fixup volume stream linking
    
    - Make the SeekBarVolumizer move notification and ringer streams when
     linked and moving ringer.
    
    - Fix initial state causing volume stream seek bar to be set to 0
      initially
    
    - Clean up AudioService initialization code
    
    Change-Id: Ia11a7a392e5eb3ef862147843e365d5f20eaf90a
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: Fix volume panel inconsistencies
    
    * Use xml drawables everywhere
    * Add muted notification icon
    * Use AOSP naming scheme for drawables
    
    Change-Id: I14a3af9a7aecf4828f81a8e39855608e452848d6
    
    Misc fixes
    
    Change-Id: Ia94b0a6655e52f5949f031360f525c871cd8df14
    Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>
    
    Use STREAM_RING on non-voice capable devices
    
    * That's what Google does.
    
    Change-Id: Iac88d161eb1dccbbb5c538d5e04d8d1fc43edef2
    (cherry picked from commit 981e89eb6b99c058a9b3bf533e6494ab44a4b241)
    
    VolumeDialog: Fix mismerge
    
    Fixes not disappearing cast pickers
    
    * Commit 568585b introduced a new LinearLayout for the volume rows
    * Remove unused booleans
    
    Change-Id: I0660d10872cc1098fdd67b618a20ea0f8e926e68
    
    Change-Id: Ia11a7a392e5eb3ef862147843e365d5f20eaf90a

--- core/java/android/preference/SeekBarVolumizer.java
+++ core/java/android/preference/SeekBarVolumizer.java
@@ -62,6 +62,7 @@ public class SeekBarVolumizer implements OnSeekBarChangeListener, Handler.Callba
     private final NotificationManager mNotificationManager;
     private final int mStreamType;
     private final int mMaxStreamVolume;
+    private final boolean mVoiceCapable;
     private boolean mAffectedByRingerMode;
     private boolean mNotificationOrRing;
     private final Receiver mReceiver = new Receiver();
@@ -129,6 +130,8 @@ public class SeekBarVolumizer implements OnSeekBarChangeListener, Handler.Callba
             }
         }
         mDefaultUri = defaultUri;
+        mVoiceCapable = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_voice_capable);
     }
 
     private static boolean isNotificationOrRing(int stream) {
@@ -141,6 +144,11 @@ public class SeekBarVolumizer implements OnSeekBarChangeListener, Handler.Callba
 
     private static boolean isMediaStream(int stream) {
         return stream == AudioManager.STREAM_MUSIC;
+    }
+
+    private boolean isNotificationStreamLinked() {
+        return mVoiceCapable && Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.VOLUME_LINK_NOTIFICATION, 1) == 1;
     }
 
     public void setSeekBar(SeekBar seekBar) {
@@ -170,11 +178,17 @@ public class SeekBarVolumizer implements OnSeekBarChangeListener, Handler.Callba
             mSeekBar.setProgress(mLastAudibleStreamVolume, true);
         } else if (mNotificationOrRing && mRingerMode == AudioManager.RINGER_MODE_VIBRATE) {
             mSeekBar.setProgress(0, true);
+            mSeekBar.setEnabled(isSeekBarEnabled());
         } else if (mMuted) {
             mSeekBar.setProgress(0, true);
         } else {
+            mSeekBar.setEnabled(isSeekBarEnabled());
             mSeekBar.setProgress(mLastProgress > -1 ? mLastProgress : mOriginalStreamVolume, true);
         }
+    }
+
+    private boolean isSeekBarEnabled() {
+        return !(mStreamType == AudioManager.STREAM_NOTIFICATION && isNotificationStreamLinked());
     }
 
     @Override
@@ -287,7 +301,7 @@ public class SeekBarVolumizer implements OnSeekBarChangeListener, Handler.Callba
     }
 
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
-        if (fromTouch) {
+        if (fromTouch && isSeekBarEnabled()) {
             postSetVolume(progress);
         }
         if (mCallback != null) {
@@ -463,7 +477,8 @@ public class SeekBarVolumizer implements OnSeekBarChangeListener, Handler.Callba
         }
 
         private void updateVolumeSlider(int streamType, int streamValue) {
-            final boolean streamMatch = mNotificationOrRing ? isNotificationOrRing(streamType)
+            final boolean streamMatch = mNotificationOrRing && isNotificationStreamLinked()
+                    ? isNotificationOrRing(streamType)
                     : (streamType == mStreamType);
             if (mSeekBar != null && streamMatch && streamValue != -1) {
                 final boolean muted = mAudioManager.isStreamMute(mStreamType)
