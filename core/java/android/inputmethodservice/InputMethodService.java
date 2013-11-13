06adfcacb3c448359d545528be5bb5c53dde6378
BigBrother1984 <carlosavignano@aospa.co>
2013-11-13 20:03:22 +0100

    InputMethodService: Disable fullscreen keyboard.
    * Hardcode the keyboard in landscape mode avoiding going full screen.
    
    
    Change-Id: I290d17571204c753382873ca15b98a3ab5917765

--- core/java/android/inputmethodservice/InputMethodService.java
+++ core/java/android/inputmethodservice/InputMethodService.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2007-2008 The Android Open Source Project
+ * This code has been modified. Portions copyright (C) 2013, ParanoidAndroid Project.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -1232,7 +1233,7 @@ public class InputMethodService extends AbstractInputMethodService {
      * is currently running in fullscreen mode.
      */
     public void updateFullscreenMode() {
-        boolean isFullscreen = mShowInputRequested && onEvaluateFullscreenMode();
+        boolean isFullscreen = false;
         boolean changed = mLastShowInputRequested != mShowInputRequested;
         if (mIsFullscreen != isFullscreen || !mFullscreenApplied) {
             changed = true;
