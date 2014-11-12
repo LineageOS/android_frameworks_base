e81a884c69e9d3e230abaf1c0007851be9acda91
htchoi1995 <htchoi1995@gmail.com>
2014-11-12 14:07:51 -0500

    Disable/Enable screenshot sound [1/2]
    Change-Id: I9f32d0026833f9350a2e8e4bedaff3801f508e76

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -58,6 +58,7 @@ import android.os.Process;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.Slog;
 import android.view.Display;
@@ -767,6 +768,7 @@ class GlobalScreenshot {
             @Override
             public void run() {
                 // Play the shutter sound to notify that we've taken a screenshot
+                if (Settings.System.getInt(mContext.getContentResolver(), Settings.System.SCREENSHOT_SOUND, 1) == 1)
                 mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
 
                 mScreenshotView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
