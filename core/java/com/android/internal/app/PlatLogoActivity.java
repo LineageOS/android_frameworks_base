86b1a0e4e58f636306c79bdbbbdfbeb86c35f685
Michael Bestas <mikeioannina@gmail.com>
2014-11-13 21:22:46 +0200

    PlatLogo: Show lineage platlogo when tapping on LineageOS version
    Credit: Asher Simonds
    
    Depends on the following commit in Settings:
    f99a8b72245325f743998ce7219ac7141b1a6148 Add Modver to Settings.
    
    Change-Id: Ie2b092bafd3588270177648003dafed8226cf298
    Signed-off-by: Joey Rizzoli <joey@lineageos.it>

--- core/java/com/android/internal/app/PlatLogoActivity.java
+++ core/java/com/android/internal/app/PlatLogoActivity.java
@@ -86,7 +86,9 @@ public class PlatLogoActivity extends Activity {
 
         im.setBackground(new RippleDrawable(
                 ColorStateList.valueOf(0xFFFFFFFF),
-                getDrawable(com.android.internal.R.drawable.platlogo),
+                getDrawable(getIntent().getBooleanExtra("is_lineage", false)
+                        ? com.android.internal.R.drawable.platlogo_lineage
+                        : com.android.internal.R.drawable.platlogo),
                 null));
 //        im.setOutlineProvider(new ViewOutlineProvider() {
 //            @Override
