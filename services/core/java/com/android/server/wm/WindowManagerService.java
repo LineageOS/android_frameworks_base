61a1b8c21f682f57751c261430fd09db4a175e8c
Kyrylo Mikos <kiril.mik.os@gmail.com>
2014-11-17 13:56:05 +0200

    Implement expanded desktop feature
    This squashes the following commits:
    
    Bringup expanded desktop and implement per-user configuration.
    
    Change-Id: I90d834d788b3050a3f4c11dda9b6d1a7d79546cc
    
    Create configurable default expanded desktop style.
    
    Change-Id: I90c7f8544cda9b91fb9b700fcf146f4e58b15a26
    
    Remove unused POLICY_CONTROL_SELECTED. Unbreak public api.
    
    Change-Id: Ib0900349115442274fb08eb172de2567a8b00295

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -3212,6 +3212,11 @@ public class WindowManagerService extends IWindowManager.Stub
         return mPointerEventDispatcher != null;
     }
 
+    @Override
+    public void addSystemUIVisibilityFlag(int flags) {
+        mLastStatusBarVisibility |= flags;
+    }
+
     // Called by window manager policy. Not exposed externally.
     @Override
     public int getLidState() {
