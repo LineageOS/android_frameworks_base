8cc2ac1c99198535f9ec1edaadb803cc09aa7207
Michael Gernoth <michael@gernoth.net>
2014-11-18 19:43:45 +0100

    keyboard: re-add keyboard brightness handling
    This is a squash of the following commits:
    
    keyboard: re-add code to detect lid state
    
    This was lost in the cm-11 merge. Without this, a change in lid state
    isn't generated to light up the keyboard backlight
    
    Author: Dave Daynard <nardholio@gmail.com>
    Change-Id: Ief35cebcb62da13afe1ae7531d69ab58e196be9e
    
    Keyboard light: Fix the physical keyboard not light when it's visible.
    
    Author: YuanQY <yuanqingyun@gmail.com>
    Change-Id: I595afd3cb6b422a17ae0f6ec20aa51979db13810
    
    keyboard: adding functional alt/shift lights
    
    This is useful for devices that has QWERTY keyboard
    and leds for the Alt/Shift (Fn/Caps) keys, like
    htc doubleshot.
    
    Author: tbalden <illespal@gmail.com>
    
    keyboard: remove special fn/caps handling
    
    There are only a handful of devices with physical
    keyboards on nougat, and it's unlikely that
    any of them will get ported to oreo.
    
    Remove the special handling to clean up some of the
    logic and code amount.
    
    Author: Demon000 <demonsingur@gmail.com>
    
    Change-Id: I66ebc2d881438f5b51db77eaa885421e65a7da0d

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -239,6 +239,7 @@ public final class PowerManagerService extends SystemService
     private DreamManagerInternal mDreamManager;
     private Light mAttentionLight;
     private Light mButtonsLight;
+    private Light mKeyboardLight;
 
     private final Object mLock = LockGuard.installNewLock(LockGuard.INDEX_POWER);
 
@@ -635,6 +636,8 @@ public final class PowerManagerService extends SystemService
     private static native void nativeSendPowerHint(int hintId, int data);
     private static native void nativeSetFeature(int featureId, int data);
 
+    private boolean mKeyboardVisible = false;
+
     public PowerManagerService(Context context) {
         super(context);
         mContext = context;
@@ -747,6 +750,7 @@ public final class PowerManagerService extends SystemService
             mLightsManager = getLocalService(LightsManager.class);
             mAttentionLight = mLightsManager.getLight(LightsManager.LIGHT_ID_ATTENTION);
             mButtonsLight = mLightsManager.getLight(LightsManager.LIGHT_ID_BUTTONS);
+            mKeyboardLight = mLightsManager.getLight(LightsManager.LIGHT_ID_KEYBOARD);
 
             // Initialize display power management.
             mDisplayManagerInternal.initPowerManagement(
@@ -1935,8 +1939,10 @@ public final class PowerManagerService extends SystemService
                     if (now < nextTimeout) {
                         if (now > mLastUserActivityTime + BUTTON_ON_DURATION) {
                             mButtonsLight.setBrightness(0);
+                            mKeyboardLight.setBrightness(0);
                         } else {
                             mButtonsLight.setBrightness(mDisplayPowerRequest.screenBrightness);
+                            mKeyboardLight.setBrightness(mKeyboardVisible ? mDisplayPowerRequest.screenBrightness : 0);
                             nextTimeout = now + BUTTON_ON_DURATION;
                         }
                         mUserActivitySummary = USER_ACTIVITY_SCREEN_BRIGHT;
@@ -4248,6 +4254,21 @@ public final class PowerManagerService extends SystemService
                 userActivityInternal(eventTime, event, flags, uid);
             } finally {
                 Binder.restoreCallingIdentity(ident);
+            }
+        }
+
+        @Override // Binder call
+        public void setKeyboardVisibility(boolean visible) {
+            synchronized (mLock) {
+                if (DEBUG_SPEW) {
+                    Slog.d(TAG, "setKeyboardVisibility: " + visible);
+                }
+                if (mKeyboardVisible != visible) {
+                    mKeyboardVisible = visible;
+                    if (!visible) {
+                        mKeyboardLight.turnOff();
+                    }
+                }
             }
         }
 
