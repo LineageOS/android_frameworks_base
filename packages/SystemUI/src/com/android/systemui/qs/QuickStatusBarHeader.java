f56331ce6ae14e31ff2c94f08b10d4a68b88b8af
Roman Birg <roman@cyngn.com>
2014-12-01 14:05:52 -0800

    SystemUI: Make time clickable in statusbar
    Clicking clock opens alarms.
    
    Change-Id: I622527c1f144a8867aa543348b70a4fefe3f6403
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -15,10 +15,12 @@
 package com.android.systemui.qs;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Rect;
+import android.provider.AlarmClock;
 import android.support.annotation.VisibleForTesting;
 import android.util.AttributeSet;
 import android.view.View;
@@ -41,6 +43,7 @@ public class QuickStatusBarHeader extends RelativeLayout {
     private ActivityStarter mActivityStarter;
 
     private QSPanel mQsPanel;
+    private View mClock;
 
     private boolean mExpanded;
     private boolean mListening;
@@ -60,6 +63,15 @@ public class QuickStatusBarHeader extends RelativeLayout {
         mHeaderQsPanel = findViewById(R.id.quick_qs_panel);
         mHeaderQsPanel.setVisibility(res.getBoolean(R.bool.config_showQuickSettingsRow)
                 ? VISIBLE : GONE);
+
+        mClock = findViewById(R.id.clock);
+        mClock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mActivityStarter.startActivity(new Intent(AlarmClock.ACTION_SHOW_ALARMS),
+                        true /* dismissShade */);
+            }
+        });
 
         // RenderThread is doing more harm than good when touching the header (to expand quick
         // settings), so disable it for this view
