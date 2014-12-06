c8a0b790a0bf8866159916f4be3fea83fb3804e7
Jon Haus <haus.xda@gmail.com>
2014-12-06 17:19:23 +0100

    SystemUI: Network Traffic [1/3]
     * Fix visibility when all data is disabled
     * Added vector drawables (courtesy of kover)
    
    Changes from original cm-14.1 commit:
    
    *) Forward-ported to Oreo and adapted to Lineage SDK (bgcngm)
    
    *) Implement DarkReceiver to ensure the network traffic ui
       has the correct tint. (bgcngm)
    
    *) Increase refresh interval from 1s to 2s. (sam3000)
    
    *) Don't generate messages when the statusbar isn't visible.
       Tested with youtube, netflix, amazon video and screen off.
       (sam3000)
    
    *) Set the unit to Mbps by default. Allow for Kbps later but
       will need settings code changes. (sam3000)
    
    *) Move resources to lineage-sdk in preparation for later
       moving most of the code too.  (sam3000)
    
    *) Various other misc clean ups / simplifications. More to
       come. (sam3000)
    
    *) Add logging for development purposes (will be removed later).
       (sam3000)
    
    Change-Id: Ib947832860970a3bccfac70d27a4761f6164d3d5

---
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -0,0 +1,359 @@
+/**
+ * Copyright (C) 2017 The LineageOS project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.statusbar.policy;
+
+import android.animation.ArgbEvaluator;
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.net.ConnectivityManager;
+import android.net.TrafficStats;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.os.Message;
+import android.os.SystemClock;
+import android.text.format.Formatter;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.View.OnSystemUiVisibilityChangeListener;
+import android.widget.TextView;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.statusbar.policy.DarkIconDispatcher.DarkReceiver;
+
+import lineageos.providers.LineageSettings;
+
+import org.lineageos.platform.internal.R;
+
+public class NetworkTraffic extends TextView {
+    private static final String TAG = "NetworkTraffic";
+
+    private static final int MODE_DISABLED = 0;
+    private static final int MODE_UPSTREAM_ONLY = 1;
+    private static final int MODE_DOWNSTREAM_ONLY = 2;
+    private static final int MODE_UPSTREAM_AND_DOWNSTREAM = 3;
+    private static final int REFRESH_INTERVAL = 2000;
+
+    private static final int MESSAGE_TYPE_PERIODIC_REFRESH = 0;
+    private static final int MESSAGE_TYPE_UPDATE_VIEW = 1;
+
+    private static final int BANDWIDTH_UNIT_KBPS = 1;
+    private static final int BANDWIDTH_UNIT_MBPS = 2;
+
+    private int mBwUnit = BANDWIDTH_UNIT_MBPS;
+
+    private int mMode = MODE_DISABLED;
+    private boolean mAttached;
+    private boolean mVisible;
+    private long mTotalRxBytes;
+    private long mTotalTxBytes;
+    private long mLastUpdateTime;
+    private int mTextSizeSingle;
+    private int mTextSizeMulti;
+    private boolean mAutoHide;
+    private int mAutoHideThreshold;
+    private int mDarkModeFillColor;
+    private int mLightModeFillColor;
+    private int mIconTint = Color.WHITE;
+    private SettingsObserver mObserver;
+    private Drawable mDrawable;
+
+    public NetworkTraffic(Context context) {
+        this(context, null);
+    }
+
+    public NetworkTraffic(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public NetworkTraffic(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        final Resources resources = getResources();
+        mTextSizeSingle = resources.getDimensionPixelSize(R.dimen.net_traffic_single_text_size);
+        mTextSizeMulti = resources.getDimensionPixelSize(R.dimen.net_traffic_multi_text_size);
+
+        mDarkModeFillColor = context.getColor(
+                com.android.systemui.R.color.dark_mode_icon_color_dual_tone_fill);
+        mLightModeFillColor = context.getColor(
+                com.android.systemui.R.color.light_mode_icon_color_dual_tone_fill);
+
+        mObserver = new SettingsObserver(mTrafficHandler);
+
+        setOnSystemUiVisibilityChangeListener(mSystemUiVisibilityChangeListener);
+        mAttached = false;
+        mVisible = false;
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Log.d(TAG, "in onAttachedToWindow()");
+        if (!mAttached) {
+            mAttached = true;
+            mContext.registerReceiver(mIntentReceiver,
+                    new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+            mObserver.observe();
+            Dependency.get(DarkIconDispatcher.class).addDarkReceiver(mDarkReceiver);
+        }
+        updateSettings();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        Log.d(TAG, "in onDetachedFromWindow()");
+        if (mAttached) {
+            mContext.unregisterReceiver(mIntentReceiver);
+            mObserver.unobserve();
+            Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(mDarkReceiver);
+            mAttached = false;
+        }
+    }
+
+    @Override
+    public void onVisibilityAggregated(boolean isVisible) {
+        Log.d(TAG, "in onVisibilityAggregated, isVisible = " + isVisible);
+        super.onVisibilityAggregated(isVisible);
+        if (mVisible != isVisible) {
+            mVisible = isVisible;
+            mTrafficHandler.sendEmptyMessage(MESSAGE_TYPE_UPDATE_VIEW);
+        }
+    }
+
+    private View.OnSystemUiVisibilityChangeListener mSystemUiVisibilityChangeListener =
+            new View.OnSystemUiVisibilityChangeListener() {
+        @Override
+        public void onSystemUiVisibilityChange(int visibility) {
+            Log.d(TAG, "in onSystemUiVisibilityChange, visibility = " + visibility);
+            final boolean isVisible =
+                    (visibility & (SYSTEM_UI_FLAG_FULLSCREEN | SYSTEM_UI_FLAG_LOW_PROFILE)) == 0;
+            if (mVisible != isVisible) {
+                mVisible = isVisible;
+                mTrafficHandler.sendEmptyMessage(MESSAGE_TYPE_UPDATE_VIEW);
+            }
+        }
+    };
+
+    private DarkReceiver mDarkReceiver = new DarkReceiver() {
+        @Override
+        public void onDarkChanged(Rect area, float darkIntensity, int tint) {
+            Log.d(TAG, "in onDarkChanged()");
+            mIconTint = (int) ArgbEvaluator.getInstance().evaluate(darkIntensity,
+                    mLightModeFillColor, mDarkModeFillColor);
+            setTextColor(mIconTint);
+            updateTrafficDrawableColor();
+        }
+    };
+
+    private Handler mTrafficHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Log.d(TAG, "in handleMessage()");
+
+            long now = SystemClock.elapsedRealtime();
+            long timeDelta = now - mLastUpdateTime;
+            mLastUpdateTime = now;
+
+            long txBytes = TrafficStats.getTotalTxBytes() - mTotalTxBytes;
+            long rxBytes = TrafficStats.getTotalRxBytes() - mTotalRxBytes;
+            mTotalRxBytes += rxBytes;
+            mTotalTxBytes += txBytes;
+
+            if (shouldHide(txBytes, rxBytes, timeDelta)) {
+                setText("");
+                setVisibility(GONE);
+            } else if (!isConnectionAvailable()) {
+                clearHandlerCallbacks();
+                setVisibility(GONE);
+            } else if (timeDelta != 0) {
+                // Get information for uplink ready so the line return can be added
+                StringBuilder output = new StringBuilder();
+                if (shouldShowUpstream()) {
+                    output.append(formatOutput(timeDelta, txBytes));
+                }
+
+                // Ensure text size is where it needs to be
+                int textSize;
+                if (mMode == MODE_UPSTREAM_AND_DOWNSTREAM) {
+                    output.append("\n");
+                    textSize = mTextSizeMulti;
+                } else {
+                    textSize = mTextSizeSingle;
+                }
+
+                // Add information for downlink if it's called for
+                if (shouldShowDownstream()) {
+                    output.append(formatOutput(timeDelta, rxBytes));
+                }
+
+                // Update view if there's anything new to show
+                if (!output.toString().contentEquals(getText())) {
+                    setTextSize(TypedValue.COMPLEX_UNIT_PX, (float) textSize);
+                    setText(output.toString());
+                }
+                setVisibility(VISIBLE);
+            }
+
+            // Post delayed message to refresh every REFRESH_INTERVAL
+            if (mVisible) {
+                mTrafficHandler.sendEmptyMessageDelayed(MESSAGE_TYPE_PERIODIC_REFRESH,
+                        REFRESH_INTERVAL);
+            }
+        }
+
+        private boolean shouldShowUpstream() {
+            return mMode == MODE_UPSTREAM_ONLY || mMode == MODE_UPSTREAM_AND_DOWNSTREAM;
+        }
+
+        private boolean shouldShowDownstream() {
+            return mMode == MODE_DOWNSTREAM_ONLY || mMode == MODE_UPSTREAM_AND_DOWNSTREAM;
+        }
+
+        private String formatOutput(long timeDelta, long bytes) {
+            final float kbps = 8 * (float) bytes / (timeDelta / 1000f) / 1000f;
+            final float value;
+            final String unit;
+            if (mBwUnit == BANDWIDTH_UNIT_KBPS) {
+                value = kbps;
+                unit = mContext.getString(R.string.kilobitspersecond_short);
+            } else {
+                value = kbps / 1000f;
+                unit = mContext.getString(R.string.megabitspersecond_short);
+            }
+            return mContext.getString(R.string.network_traffic_format_with_unit, value, unit);
+        }
+
+        private boolean shouldHide(long txBytes, long rxBytes, long timeDelta) {
+            if (!mAutoHide) {
+                return false;
+            }
+            long speedTxKB = (long) (txBytes / (timeDelta / 1000f)) / 1024;
+            long speedRxKB = (long) (rxBytes / (timeDelta / 1000f)) / 1024;
+
+            if (shouldShowUpstream() && speedTxKB > mAutoHideThreshold) {
+                return false;
+            }
+            if (shouldShowDownstream() && speedRxKB > mAutoHideThreshold) {
+                return false;
+            }
+            return true;
+        }
+    };
+
+    private final BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ConnectivityManager.CONNECTIVITY_ACTION.equals(action)) {
+                updateViewState();
+            }
+        }
+    };
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(LineageSettings.Secure.getUriFor(
+                    LineageSettings.Secure.NETWORK_TRAFFIC_MODE),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(LineageSettings.Secure.getUriFor(
+                    LineageSettings.Secure.NETWORK_TRAFFIC_AUTOHIDE),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(LineageSettings.Secure.getUriFor(
+                    LineageSettings.Secure.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        void unobserve() {
+            mContext.getContentResolver().unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateSettings();
+        }
+    }
+
+    private boolean isConnectionAvailable() {
+        ConnectivityManager cm =
+                (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null;
+    }
+
+    private void updateSettings() {
+        ContentResolver resolver = mContext.getContentResolver();
+
+        mMode = LineageSettings.Secure.getIntForUser(resolver,
+                LineageSettings.Secure.NETWORK_TRAFFIC_MODE, 0, UserHandle.USER_CURRENT);
+        mAutoHide = LineageSettings.Secure.getIntForUser(resolver,
+                LineageSettings.Secure.NETWORK_TRAFFIC_AUTOHIDE, 0, UserHandle.USER_CURRENT) == 1;
+        mAutoHideThreshold = LineageSettings.Secure.getIntForUser(resolver,
+                LineageSettings.Secure.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 10,
+                UserHandle.USER_CURRENT);
+
+        if (mMode != MODE_DISABLED) {
+            updateTrafficDrawable();
+        }
+        updateViewState();
+    }
+
+    private void updateViewState() {
+        mTrafficHandler.sendEmptyMessage(MESSAGE_TYPE_UPDATE_VIEW);
+    }
+
+    private void clearHandlerCallbacks() {
+        mTrafficHandler.removeMessages(MESSAGE_TYPE_PERIODIC_REFRESH);
+        mTrafficHandler.removeMessages(MESSAGE_TYPE_UPDATE_VIEW);
+    }
+
+    private void updateTrafficDrawable() {
+        final int drawableResId;
+        if (mMode == MODE_UPSTREAM_AND_DOWNSTREAM) {
+            drawableResId = R.drawable.stat_sys_network_traffic_updown;
+        } else if (mMode == MODE_UPSTREAM_ONLY) {
+            drawableResId = R.drawable.stat_sys_network_traffic_up;
+        } else if (mMode == MODE_DOWNSTREAM_ONLY) {
+            drawableResId = R.drawable.stat_sys_network_traffic_down;
+        } else {
+            drawableResId = 0;
+        }
+        mDrawable = drawableResId != 0 ? getResources().getDrawable(drawableResId) : null;
+        setCompoundDrawablesWithIntrinsicBounds(null, null, mDrawable, null);
+        updateTrafficDrawableColor();
+    }
+
+    private void updateTrafficDrawableColor() {
+        if (mDrawable != null) {
+            mDrawable.setColorFilter(mIconTint, PorterDuff.Mode.SRC_ATOP);
+        }
+    }
+}
