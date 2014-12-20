4a577930c84950c6cb935537ae612b3eb2834cf5
nadlabak <pavel@doshaska.net>
2014-12-20 23:56:55 +0200

    Forward port 'Swap volume buttons' (1/3)
    [mikeioannina]: Adjust for 5.0 changes
    [bgcngm]: Adjust for 13.0 changes
    [LuK1337]: Adjust for 14.0 changes
    
    Change-Id: I1b45da63b815aa1b3ddf7cda2b7afb0872ab433f

--- services/core/java/com/android/server/input/NativeInputManagerService.java
+++ services/core/java/com/android/server/input/NativeInputManagerService.java
@@ -131,6 +131,8 @@ interface NativeInputManagerService {
 
     void setShowTouches(boolean enabled);
 
+    void setVolumeKeysRotation(int mode);
+
     void setInteractive(boolean interactive);
 
     void reloadCalibration();
@@ -362,6 +364,9 @@ interface NativeInputManagerService {
 
         @Override
         public native void setShowTouches(boolean enabled);
+
+        @Override
+        public native void setVolumeKeysRotation(int mode);
 
         @Override
         public native void setInteractive(boolean interactive);
