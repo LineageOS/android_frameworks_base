ce92cdfa52bcca3a1ef804b064d109bfe82d3bfc
Shashi Shekar Shankar <ssbang@codeaurora.org>
2015-01-05 17:40:39 +0530

    Performance: Memory Optimizations.
    The following optimizations are squashed in this change.
    
    1. Enable Aggressive trim settings.
    
    This change will enable aggressive trim settings for targets
    up to 1GB. The change can be turned on/off from system properties.
    By default, the properties are set for targets up to 1GB
    
    Changes spread in to : ActivityManagerConstants.java
    
    2. Propagate B-services to higher adj.
    
    Depending on the inactivity of a service, move the B-services to
    highest adj 906. Under memory pressure, these services will
    be killed first ahead of cached apps which results in better
    concurrency numbers with bg apps. Inactivity time and minumum
    no of services to be maintained are configurable as system properties.
    Improves concurrency.
    
    Changes spread in to : ActivityManagerService.java
    
    3. Postpone service restart during app launch.
    
    In the android framework, when the service gets killed,
    it will be rescheduled. Postpone the service restart if
    the app is in process of startup. By Default this feature
    is disabled. It can be enabled from build.prop of target.
    
    Changes spread in to : ActiveServices.java
    
    4. Performance: Make Cached apps limit configurable.
    
    Set it via a target specific property
    file. Allows flexibility to set the limit
    based on device config.
    
    Normally, setting a smaller value for
    low-end device helps with performance.
    
    Changes spread in to : ActivityManagerConstants.java
    
    5. am: Add new flag to detect activity "launching" state
    
    Use this flag to determine service re-schdelue delay and
    exclude persistent services from delay.
    
    CRs-Fixed: 783020 792122 879756 911145 2111356
    
    Squash of following change-IDs:
        Change-Id: I233dddbff07e7ec1fe2ee96402fe1d411903beb5
        Change-Id: Ied6cfcc3d11951f32f18de680b0e3483db8e163e
        Change-Id: Ia86edf027e20df2c7aa6f34e3aa293f7a8e6f1fa
        Change-Id: I90a733d7134d86efffadf6815ce3d2944dd09419
        Change-Id: Id38d536ff375b57d69f5ca73265f62e85740fc04
    
    Change-Id: I3b2110743a6e0dd1379aa3b7703b3a897db3f6e6

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1951,6 +1951,16 @@ public class ActivityManagerService extends IActivityManager.Stub
     CompatModeDialog mCompatModeDialog;
     long mLastMemUsageReportTime = 0;
 
+    // Min aging threshold in milliseconds to consider a B-service
+    int mMinBServiceAgingTime =
+            SystemProperties.getInt("ro.vendor.qti.sys.fw.bservice_age", 5000);
+    // Threshold for B-services when in memory pressure
+    int mBServiceAppThreshold =
+            SystemProperties.getInt("ro.vendor.qti.sys.fw.bservice_limit", 5);
+    // Enable B-service aging propagation on memory pressure.
+    boolean mEnableBServicePropagation =
+            SystemProperties.getBoolean("ro.vendor.qti.sys.fw.bservice_enable", false);
+
     /**
      * Flag whether the current user is a "monkey", i.e. whether
      * the UI is driven by a UI automation tool.
@@ -25064,6 +25074,9 @@ public class ActivityManagerService extends IActivityManager.Stub
         int nextCachedAdj = curCachedAdj+1;
         int curEmptyAdj = ProcessList.CACHED_APP_MIN_ADJ;
         int nextEmptyAdj = curEmptyAdj+2;
+        ProcessRecord selectedAppRecord = null;
+        long serviceLastActivity = 0;
+        int numBServices = 0;
 
         boolean retryCycles = false;
 
@@ -25074,6 +25087,34 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
         for (int i=N-1; i>=0; i--) {
             ProcessRecord app = mLruProcesses.get(i);
+            if (mEnableBServicePropagation && app.serviceb
+                    && (app.curAdj == ProcessList.SERVICE_B_ADJ)) {
+                numBServices++;
+                for (int s = app.services.size() - 1; s >= 0; s--) {
+                    ServiceRecord sr = app.services.valueAt(s);
+                    if (DEBUG_OOM_ADJ) Slog.d(TAG,"app.processName = " + app.processName
+                            + " serviceb = " + app.serviceb + " s = " + s + " sr.lastActivity = "
+                            + sr.lastActivity + " packageName = " + sr.packageName
+                            + " processName = " + sr.processName);
+                    if (SystemClock.uptimeMillis() - sr.lastActivity
+                            < mMinBServiceAgingTime) {
+                        if (DEBUG_OOM_ADJ) {
+                            Slog.d(TAG,"Not aged enough!!!");
+                        }
+                        continue;
+                    }
+                    if (serviceLastActivity == 0) {
+                        serviceLastActivity = sr.lastActivity;
+                        selectedAppRecord = app;
+                    } else if (sr.lastActivity < serviceLastActivity) {
+                        serviceLastActivity = sr.lastActivity;
+                        selectedAppRecord = app;
+                    }
+                }
+            }
+            if (DEBUG_OOM_ADJ && selectedAppRecord != null) Slog.d(TAG,
+                    "Identified app.processName = " + selectedAppRecord.processName
+                    + " app.pid = " + selectedAppRecord.pid);
             if (!app.killedByAm && app.thread != null) {
                 app.procStateChanged = false;
                 computeOomAdjLocked(app, ProcessList.UNKNOWN_ADJ, TOP_APP, true, now);
@@ -25227,6 +25268,14 @@ public class ActivityManagerService extends IActivityManager.Stub
                     numTrimming++;
                 }
             }
+        }
+        if (numBServices > mBServiceAppThreshold && true == mAllowLowerMemLevel
+                && selectedAppRecord != null) {
+            ProcessList.setOomAdj(selectedAppRecord.pid, selectedAppRecord.info.uid,
+                    ProcessList.CACHED_APP_MAX_ADJ);
+            selectedAppRecord.setAdj = selectedAppRecord.curAdj;
+            if (DEBUG_OOM_ADJ) Slog.d(TAG,"app.processName = " + selectedAppRecord.processName
+                        + " app.pid = " + selectedAppRecord.pid + " is moved to higher adj");
         }
 
         incrementProcStateSeqAndNotifyAppsLocked();
