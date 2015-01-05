0dcf5bcc99db37a1fa20eee9298b5faa96262f9c
Shashi Shekar Shankar <ssbang@codeaurora.org>
2015-01-05 17:40:39 +0530

    Performance: Memory Optimizations
    The following optimizations are squashed in this change.
    
    1. Enable Aggressive trim settings.
    
    This change will enable aggressive trim settings for targets
    up to 1GB. The change can be turned on/off from system properties.
    By default, the properties are set for targets up to 1GB.
    
    Changes spread in to : ActivityManagerConstants.java
    CRs-Fixed: 783020
    
    2. Propagate B-services to higher adj.
    
    Depending on the inactivity of a service, move the B-services to
    highest adj 906. Under memory pressure, these services will
    be killed first ahead of cached apps which results in better
    concurrency numbers with bg apps. Inactivity time and minumum
    no of services to be maintained are configurable as system properties.
    Improves concurrency.
    
    Changes spread in to : ActivityManagerService.java
    CRs-Fixed: 792122
    
    3. Postpone service restart during app launch.
    
    In the android framework, when the service gets killed,
    it will be rescheduled. Postpone the service restart if
    the app is in process of startup. By Default this feature
    is disabled. It can be enabled from build.prop of target.
    
    Changes spread in to : ActiveServices.java
    CRs-Fixed: 879756 911145
    
    4. Performance: Make Cached apps limit configurable.
    
    Set it via a target specific property
    file. Allows flexibility to set the limit
    based on device config.
    
    Normally, setting a smaller value for
    low-end device helps with performance.
    
    Changes spread in to : ActivityManagerConstants.java
    
    Squash of following change-IDs:
        Change-Id: I233dddbff07e7ec1fe2ee96402fe1d411903beb5
        Change-Id: Ied6cfcc3d11951f32f18de680b0e3483db8e163e
        Change-Id: Ia86edf027e20df2c7aa6f34e3aa293f7a8e6f1fa
        Change-Id: I90a733d7134d86efffadf6815ce3d2944dd09419
    
    Change-Id: I3b2110743a6e0dd1379aa3b7703b3a897db3f6e6

--- services/core/java/com/android/server/am/ActiveServices.java
+++ services/core/java/com/android/server/am/ActiveServices.java
@@ -114,6 +114,10 @@ public final class ActiveServices {
     // at the same time.
     final int mMaxStartingBackground;
 
+    // Flag to reschedule the services during app launch. Disable by default.
+    private static final boolean SERVICE_RESCHEDULE
+            = SystemProperties.getBoolean("ro.vendor.qti.am.reschedule_service", false);
+
     final SparseArray<ServiceMap> mServiceMap = new SparseArray<>();
 
     /**
@@ -1911,6 +1915,14 @@ public final class ActiveServices {
                         r.pendingStarts.add(0, si);
                         long dur = SystemClock.uptimeMillis() - si.deliveredTime;
                         dur *= 2;
+                        if (SERVICE_RESCHEDULE && DEBUG_DELAYED_SERVICE) {
+                            Slog.w(TAG,"Can add more delay !!!"
+                               +" si.deliveredTime "+si.deliveredTime
+                               +" dur "+dur
+                               +" si.deliveryCount "+si.deliveryCount
+                               +" si.doneExecutingCount "+si.doneExecutingCount
+                               +" allowCancel "+allowCancel);
+                        }
                         if (minDuration < dur) minDuration = dur;
                         if (resetTime < dur) resetTime = dur;
                     } else {
@@ -1923,6 +1935,13 @@ public final class ActiveServices {
             }
 
             r.totalRestartCount++;
+            if (SERVICE_RESCHEDULE && DEBUG_DELAYED_SERVICE) {
+                Slog.w(TAG,"r.name "+r.name+" N "+N+" minDuration "+minDuration
+                       +" resetTime "+resetTime+" now "+now
+                       +" r.restartDelay "+r.restartDelay
+                       +" r.restartTime+resetTime "+(r.restartTime+resetTime)
+                       +" allowCancel "+allowCancel);
+            }
             if (r.restartDelay == 0) {
                 r.restartCount++;
                 r.restartDelay = minDuration;
@@ -1947,6 +1966,14 @@ public final class ActiveServices {
             }
 
             r.nextRestartTime = now + r.restartDelay;
+            if (SERVICE_RESCHEDULE && DEBUG_DELAYED_SERVICE) {
+                Slog.w(TAG,"r.name "+r.name+" N "+N+" minDuration "+minDuration
+                       +" resetTime "+resetTime+" now "+now
+                       +" r.restartDelay "+r.restartDelay
+                       +" r.restartTime+resetTime "+(r.restartTime+resetTime)
+                       +" r.nextRestartTime "+r.nextRestartTime
+                       +" allowCancel "+allowCancel);
+            }
 
             // Make sure that we don't end up restarting a bunch of services
             // all at the same time.
@@ -1988,6 +2015,15 @@ public final class ActiveServices {
         r.nextRestartTime = SystemClock.uptimeMillis() + r.restartDelay;
         Slog.w(TAG, "Scheduling restart of crashed service "
                 + r.shortName + " in " + r.restartDelay + "ms");
+
+        if (SERVICE_RESCHEDULE && DEBUG_DELAYED_SERVICE) {
+            for (int i=mRestartingServices.size()-1; i>=0; i--) {
+                ServiceRecord r2 = mRestartingServices.get(i);
+                Slog.w(TAG,"Restarting list - i "+i+" r2.nextRestartTime "
+                           +r2.nextRestartTime+" r2.name "+r2.name);
+            }
+        }
+
         EventLog.writeEvent(EventLogTags.AM_SCHEDULE_SERVICE_RESTART,
                 r.userId, r.shortName, r.restartDelay);
 
@@ -2008,7 +2044,31 @@ public final class ActiveServices {
             return;
         }
         try {
-            bringUpServiceLocked(r, r.intent.getIntent().getFlags(), r.createdFromFg, true, false);
+            if(SERVICE_RESCHEDULE) {
+                boolean shouldDelay = false;
+                ActivityRecord top_rc = null;
+                ActivityStack stack = mAm.getFocusedStack();
+                if(stack != null) {
+                    top_rc = stack.topRunningActivityLocked();
+                }
+                if(top_rc != null) {
+                    if(!top_rc.nowVisible && !r.shortName.contains(top_rc.packageName)) {
+                        shouldDelay = true;
+                    }
+                }
+                if(!shouldDelay) {
+                    bringUpServiceLocked(r, r.intent.getIntent().getFlags(), r.createdFromFg, true, false);
+                } else {
+                    if (DEBUG_DELAYED_SERVICE) {
+                        Slog.v(TAG, "Reschedule service restart due to app launch"
+                              +" r.shortName "+r.shortName+" r.app = "+r.app);
+                    }
+                    r.resetRestartCounter();
+                    scheduleServiceRestartLocked(r, true);
+                }
+            } else {
+                bringUpServiceLocked(r, r.intent.getIntent().getFlags(), r.createdFromFg, true, false);
+            }
         } catch (TransactionTooLargeException e) {
             // Ignore, it's been logged and nothing upstack cares.
         }
@@ -2249,6 +2309,11 @@ public final class ActiveServices {
                 if (newService) {
                     app.services.remove(r);
                     r.app = null;
+                    if (SERVICE_RESCHEDULE && DEBUG_DELAYED_SERVICE) {
+                    Slog.w(TAG, " Failed to create Service !!!! ."
+                           +"This will introduce huge delay...  "
+                           +r.shortName + " in " + r.restartDelay + "ms");
+                    }
                 }
 
                 // Retry.
