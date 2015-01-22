33feffc8d6e4b1ff4919759bd91e3d1bfcb9f7cd
Roman Birg <roman@cyngn.com>
2015-01-22 11:21:56 -0800

    SystemUI: Add Profiles tile
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Jan 22 11:21:56 2015 -0800
    
        System Profiles in QS Tiles
    
        Change-Id: I2a438af301212241533b969bf2c6c8390ef09cbc
    
    Author: Joey Rizzoli <joey@lineageos.org>
    Date:   Fri Oct 13 20:18:03 2017 +0200
    
        SystemUI: Profiles tile should require authentication
    
        A profile with the option to disable secure lockscreen could be
        used to bypass the lockscreen security.
        Require the user to unlock their device when tapping on the profile
        quick tile.
    
        BUGBASH-1095
    
        Change-Id: I9d63fcd9b1e3f47c9ca3374806d5ef05fe9ff32b
        Signed-off-by: Joey Rizzoli <joey@lineageos.org>
    
    Author: Michael W <baddaemon87@gmail.com>
    Date:   Sat Aug 4 18:00:34 2018 +0200
    
        Tiles: SystemProfiles: Adapt behaviour
    
        * Opening the detail view should enable profiles
        * Disabling the switch should also close the details
        * The tile's title should just be "System profiles"
          when disabled, we also don't say "Wifi off" on wifi tile
        * Set dualTarget to true so the tile indicates expandability
    
        Change-Id: I4eb463bc84ad78fcabb8acef1829c990ab04f19b
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Mon Sep 10 11:21:58 2018 +0100
    Edit:   Port and revamp to match P style
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Tue Jul 23 22:26:19 2019 +0200
    
        ProfilesTile: Enable authentication for handleClick()
    
        * This disallows enabling profile on secure lockscreen which
          could allow enabling last profile that could potentially
          have a disabled lockscreen.
        * Fixes : https://gitlab.com/LineageOS/issues/android/issues/965
    
        Change-Id: Ic9a845f1b6e4a408134c4a9ad31810d88375a99b
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Thu Oct 8 00:13:34 2020 +0100
    Edit:   Update to Android 11 APIs
    
    Change-Id: I2dd93cd93b5a99d3d5fda9c3d937009f792bdc2e

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -46,6 +46,7 @@ import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.PowerShareTile;
+import com.android.systemui.qs.tiles.ProfilesTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
@@ -96,6 +97,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<HeadsUpTile> mHeadsUpTileProvider;
     private final Provider<LiveDisplayTile> mLiveDisplayTileProvider;
     private final Provider<PowerShareTile> mPowerShareTileProvider;
+    private final Provider<ProfilesTile> mProfilesTileProvider;
     private final Provider<ReadingModeTile> mReadingModeTileProvider;
     private final Provider<SyncTile> mSyncTileProvider;
     private final Provider<UsbTetherTile> mUsbTetherTileProvider;
@@ -132,6 +134,7 @@ public class QSFactoryImpl implements QSFactory {
             Provider<HeadsUpTile> headsUpTileProvider,
             Provider<LiveDisplayTile> liveDisplayTileProvider,
             Provider<PowerShareTile> powerShareTileProvider,
+            Provider<ProfilesTile> profilesTileProvider,
             Provider<ReadingModeTile> readingModeTileProvider,
             Provider<SyncTile> syncTileProvider,
             Provider<UsbTetherTile> usbTetherTileProvider,
@@ -164,6 +167,7 @@ public class QSFactoryImpl implements QSFactory {
         mHeadsUpTileProvider = headsUpTileProvider;
         mLiveDisplayTileProvider = liveDisplayTileProvider;
         mPowerShareTileProvider = powerShareTileProvider;
+        mProfilesTileProvider = profilesTileProvider;
         mReadingModeTileProvider = readingModeTileProvider;
         mSyncTileProvider = syncTileProvider;
         mUsbTetherTileProvider = usbTetherTileProvider;
@@ -233,6 +237,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mLiveDisplayTileProvider.get();
             case "powershare":
                 return mPowerShareTileProvider.get();
+            case "profiles":
+                return mProfilesTileProvider.get();
             case "reading_mode":
                 return mReadingModeTileProvider.get();
             case "sync":
