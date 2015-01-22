dc77d1949458d634482136848a8f5acab9d4f7df
Roman Birg <roman@cyngn.com>
2015-01-22 11:21:56 -0800

    SystemUI: Add Profiles tile
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Jan 22 11:21:56 2015 -0800
    
        System Profiles in QS Tiles
    
        Change-Id: I2a438af301212241533b969bf2c6c8390ef09cbc
    
    Author: Joey Rizzoli <joey@lineageos.org>
    Date:   Fri Oct 13 20:18:03 2017 +0200
    
        SystemUI: Profiles tile should require authentication
    
        A profile with the option to disable secure lockscreen could be
        used to bypass the lockscreen security.
        Require the user to unlock their device when tapping on the profile
        quick tile.
    
        BUGBASH-1095
    
        Change-Id: I9d63fcd9b1e3f47c9ca3374806d5ef05fe9ff32b
        Signed-off-by: Joey Rizzoli <joey@lineageos.org>
    
    Author: Michael W <baddaemon87@gmail.com>
    Date:   Sat Aug 4 18:00:34 2018 +0200
    
        Tiles: SystemProfiles: Adapt behaviour
    
        * Opening the detail view should enable profiles
        * Disabling the switch should also close the details
        * The tile's title should just be "System profiles"
          when disabled, we also don't say "Wifi off" on wifi tile
        * Set dualTarget to true so the tile indicates expandability
    
        Change-Id: I4eb463bc84ad78fcabb8acef1829c990ab04f19b
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Mon Sep 10 11:21:58 2018 +0100
    Edit:   Port and revamp to match P style
    
    Change-Id: If561205566128d91d5ba1aa894a3463c64f4aa25

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -43,6 +43,7 @@ import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.ProfilesTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SyncTile;
@@ -119,6 +120,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new HeadsUpTile(mHost);
             case "livedisplay":
                 return new LiveDisplayTile(mHost);
+            case "profiles":
+                return new ProfilesTile(mHost);
             case "reading_mode":
                 return new ReadingModeTile(mHost);
             case "sync":
