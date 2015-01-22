a5e8479f1368591c454aa9ceef8a6d41453cb974
Roman Birg <roman@cyngn.com>
2015-01-22 11:21:56 -0800

    System Profiles in QS Tiles
    Change-Id: I2a438af301212241533b969bf2c6c8390ef09cbc

--- packages/SystemUI/src/com/android/systemui/qs/QSTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSTile.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -559,6 +560,8 @@ public abstract class QSTile<TState extends State> {
     }
 
     public static class State {
+        public boolean visible;
+        public boolean enabled = true;
         public Icon icon;
         public CharSequence label;
         public CharSequence contentDescription;
@@ -573,7 +576,9 @@ public abstract class QSTile<TState extends State> {
         public boolean copyTo(State other) {
             if (other == null) throw new IllegalArgumentException();
             if (!other.getClass().equals(getClass())) throw new IllegalArgumentException();
-            final boolean changed = !Objects.equals(other.icon, icon)
+            final boolean changed = other.visible != visible
+                    || !Objects.equals(other.enabled, enabled)
+                    || !Objects.equals(other.icon, icon)
                     || !Objects.equals(other.label, label)
                     || !Objects.equals(other.contentDescription, contentDescription)
                     || !Objects.equals(other.autoMirrorDrawable, autoMirrorDrawable)
@@ -587,6 +592,8 @@ public abstract class QSTile<TState extends State> {
                     expandedAccessibilityClassName)
                     || !Objects.equals(other.disabledByPolicy, disabledByPolicy)
                     || !Objects.equals(other.enforcedAdmin, enforcedAdmin);
+            other.visible = visible;
+            other.enabled = enabled;
             other.icon = icon;
             other.label = label;
             other.contentDescription = contentDescription;
@@ -613,6 +620,8 @@ public abstract class QSTile<TState extends State> {
 
         protected StringBuilder toStringBuilder() {
             final StringBuilder sb = new StringBuilder(getClass().getSimpleName()).append('[');
+            sb.append("visible=").append(visible);
+            sb.append(",enabled=").append(enabled);
             sb.append(",icon=").append(icon);
             sb.append(",label=").append(label);
             sb.append(",contentDescription=").append(contentDescription);
