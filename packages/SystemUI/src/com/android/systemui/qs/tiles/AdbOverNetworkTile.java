af8a599aa4d6ad99193c7d072fbeab07ccc91860
Danesh M <daneshm90@gmail.com>
2015-01-23 11:57:52 -0800

    SystemUI: Add ADB over network tile
    Author: Danesh M <daneshm90@gmail.com>
    Date:   Fri Jan 23 11:57:52 2015 -0800
    
        SystemUI: Add adb over network tile
    
        Change-Id: Ib1fa0c87562ff3a077bf9c7283ce81fc4a8271db
    
    Author: Joey <joey@lineageos.org>
    Date:   Mon Jun 25 23:47:39 2018 +0200
    
        SystemUI: require auth for adb wireless
    
        Change-Id: I246a0310f0015862b750aa73ed2b546ef6658c8b
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Jason Edson <jaysonedson@gmail.com>
    Date:   Sun Sep 2 08:59:39 2018 +0100
    Edit:   Updated drawable to make it look like stock AOSP
            and added the host address as secondary text.
    
    Change-Id: I30256bdec4379b870129070e1a9adc93ee7d4043

---
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/AdbOverNetworkTile.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.net.NetworkUtils;
+import android.net.Uri;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.policy.KeyguardMonitor;
+
+import lineageos.providers.LineageSettings;
+import org.lineageos.internal.logging.LineageMetricsLogger;
+
+import java.net.InetAddress;
+
+/** Quick settings tile: AdbOverNetwork **/
+public class AdbOverNetworkTile extends QSTileImpl<BooleanState> {
+
+    private boolean mListening;
+    private final KeyguardMonitor mKeyguardMonitor;
+    private final KeyguardMonitorCallback mCallback = new KeyguardMonitorCallback();
+
+    private static final Intent SETTINGS_DEVELOPMENT =
+            new Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS);
+
+    public AdbOverNetworkTile(QSHost host) {
+        super(host);
+        mKeyguardMonitor = Dependency.get(KeyguardMonitor.class);
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleClick() {
+        if (mKeyguardMonitor.isSecure() && !mKeyguardMonitor.canSkipBouncer()) {
+            Dependency.get(ActivityStarter.class)
+                    .postQSRunnableDismissingKeyguard(this::toggleAction);
+        } else {
+            toggleAction();
+        }
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return SETTINGS_DEVELOPMENT;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.value = isAdbNetworkEnabled();
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_network_adb);
+        state.label = mContext.getString(R.string.quick_settings_network_adb_label);
+        if (state.value) {
+            WifiManager wifiManager = mContext.getSystemService(WifiManager.class);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+
+            if (wifiInfo != null) {
+                // If wifiInfo is not null, set secondary label to "hostAddress"
+                InetAddress address = NetworkUtils.intToInetAddress(wifiInfo.getIpAddress());
+                state.secondaryLabel = address.getHostAddress();
+            } else {
+                // If wifiInfo is null, disable secondary label
+                state.secondaryLabel = null;
+            }
+            state.state = Tile.STATE_ACTIVE;
+        } else {
+            // Otherwise set the disabled label and icon
+            state.secondaryLabel = null;
+            state.state = Tile.STATE_INACTIVE;
+        }
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_network_adb_label);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return LineageMetricsLogger.TILE_ADB_OVER_NETWORK;
+    }
+
+    private boolean isAdbEnabled() {
+        return Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.ADB_ENABLED, 0) > 0;
+    }
+
+    private boolean isAdbNetworkEnabled() {
+        return LineageSettings.Secure.getInt(mContext.getContentResolver(),
+                LineageSettings.Secure.ADB_PORT, 0) > 0;
+    }
+
+    private void toggleAction() {
+        LineageSettings.Secure.putIntForUser(mContext.getContentResolver(),
+                LineageSettings.Secure.ADB_PORT, getState().value ? -1 : 5555,
+                UserHandle.USER_CURRENT);
+    }
+
+    private ContentObserver mObserver = new ContentObserver(mHandler) {
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            refreshState();
+        }
+    };
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        if (mListening != listening) {
+            mListening = listening;
+            if (listening) {
+                mContext.getContentResolver().registerContentObserver(
+                        LineageSettings.Secure.getUriFor(LineageSettings.Secure.ADB_PORT),
+                        false, mObserver);
+                mContext.getContentResolver().registerContentObserver(
+                        Settings.Global.getUriFor(Settings.Global.ADB_ENABLED),
+                        false, mObserver);
+                mKeyguardMonitor.addCallback(mCallback);
+            } else {
+                mContext.getContentResolver().unregisterContentObserver(mObserver);
+                mKeyguardMonitor.removeCallback(mCallback);
+            }
+        }
+    }
+
+    private class KeyguardMonitorCallback implements KeyguardMonitor.Callback {
+        @Override
+        public void onKeyguardShowingChanged() {
+            refreshState();
+        }
+    }
+}
