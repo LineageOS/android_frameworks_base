94f4eb87fdf09e8cbf6c37e2f7584e9cd91c42ac
Danesh M <daneshm90@gmail.com>
2015-01-23 11:57:52 -0800

    SystemUI: ADB over network tile
    Author: Danesh M <daneshm90@gmail.com>
    Date:   Fri Jan 23 11:57:52 2015 -0800
    
        SystemUI: Add adb over network tile
    
        Change-Id: Ib1fa0c87562ff3a077bf9c7283ce81fc4a8271db
    
    Author: Joey <joey@lineageos.org>
    Date:   Mon Jun 25 23:47:39 2018 +0200
    
        SystemUI: require auth for adb wireless
    
        Change-Id: I246a0310f0015862b750aa73ed2b546ef6658c8b
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Jason Edson <jaysonedson@gmail.com>
    Date:   Sun Sep 2 08:59:39 2018 +0100
    Edit:   Udated drawable to make it look like stock AOSP
            and added the host address as secondary text.
    
    Change-Id: I30256bdec4379b870129070e1a9adc93ee7d4043

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -24,6 +24,7 @@ import com.android.systemui.R;
 import com.android.systemui.plugins.qs.*;
 import com.android.systemui.plugins.qs.QSTileView;
 import com.android.systemui.qs.external.CustomTile;
+import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
@@ -104,6 +105,8 @@ public class QSFactoryImpl implements QSFactory {
             case "nfc":
                 return new NfcTile(mHost);
             // Custom tiles.
+            case "adb_network":
+                return new AdbOverNetworkTile(mHost);
             case "caffeine":
                 return new CaffeineTile(mHost);
             case "heads_up":
