cb5e5cf8d40d119dd2f9d1b5e8728dc70bf854e5
Michael Bestas <mikeioannina@gmail.com>
2015-04-03 14:33:37 -0700

    Forward port CM Screen Security settings (1/2)
     * Variable size pattern lockscreen
     * Toggle dots/error pattern visibility
    
    Change-Id: Ie109e82c1fb2fd96b07e977e1cd76ae3acb865ff
    
    Fix pattern visibility settings (1/2)
    
    Change-Id: Ic627953c5df854c442671a98b5da539b994da18b
    
    LockPatternUtils: Use the actual user id to set pattern size
    
    Ticket: CYNGNOS-2462
    Change-Id: Ia68e26ec2dfc23317135d933bc25204c1380bb02
    
    LockSettings: fix build
    
    Change-Id: Ic65b1a2af398faffb83776ee4013d47f79ab6619
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- core/tests/coretests/src/com/android/internal/widget/LockPatternViewTest.java
+++ core/tests/coretests/src/com/android/internal/widget/LockPatternViewTest.java
@@ -148,7 +148,7 @@ public class LockPatternViewTest {
                 MotionEvent.obtain(0, 0, MotionEvent.ACTION_DOWN, mDot1x, mDot1y, 1));
         mLockPatternView.onTouchEvent(
                 MotionEvent.obtain(0, 0, MotionEvent.ACTION_UP, mDot1x, mDot1y, 1));
-        verify(mPatternListener).onPatternDetected(any());
+        verify(mPatternListener).onPatternDetected(any(), LockPatternUtils.PATTERN_SIZE_DEFAULT);
     }
 
     @UiThreadTest
@@ -183,7 +183,8 @@ public class LockPatternViewTest {
         mLockPatternView.onTouchEvent(
                 MotionEvent.obtain(0, 3, MotionEvent.ACTION_UP, mDot2x, mDot2y, 1));
 
-        verify(mPatternListener).onPatternDetected(mCellsArgumentCaptor.capture());
+        verify(mPatternListener).onPatternDetected(mCellsArgumentCaptor.capture(),
+                LockPatternUtils.PATTERN_SIZE_DEFAULT);
         List<LockPatternView.Cell> patternCells = mCellsArgumentCaptor.getValue();
         assertThat(patternCells, hasSize(2));
         assertThat(patternCells,
@@ -200,7 +201,8 @@ public class LockPatternViewTest {
         mLockPatternView.onTouchEvent(
                 MotionEvent.obtain(0, 3, MotionEvent.ACTION_UP, mDot5x, mDot5y, 1));
 
-        verify(mPatternListener).onPatternDetected(mCellsArgumentCaptor.capture());
+        verify(mPatternListener).onPatternDetected(mCellsArgumentCaptor.capture(),
+                LockPatternUtils.PATTERN_SIZE_DEFAULT);
         List<LockPatternView.Cell> patternCells = mCellsArgumentCaptor.getValue();
         assertThat(patternCells, hasSize(2));
         assertThat(patternCells,
@@ -220,7 +222,8 @@ public class LockPatternViewTest {
                 MotionEvent.obtain(0, 0, MotionEvent.ACTION_UP, mViewSize - mDefaultError,
                         mViewSize - mDefaultError, 1));
 
-        verify(mPatternListener).onPatternDetected(mCellsArgumentCaptor.capture());
+        verify(mPatternListener).onPatternDetected(mCellsArgumentCaptor.capture(),
+                LockPatternUtils.PATTERN_SIZE_DEFAULT);
         List<LockPatternView.Cell> patternCells = mCellsArgumentCaptor.getValue();
         assertThat(patternCells, hasSize(7));
         assertThat(patternCells,
