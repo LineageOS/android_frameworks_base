49552cf2b84ffff1a3787d6a6a12fc85322784a8
Michael Bestas <mikeioannina@gmail.com>
2015-04-03 14:33:37 -0700

    Forward port CM Screen Security settings (1/2)
     * Variable size pattern lockscreen
     * Toggle dots/error pattern visibility
    
    Change-Id: Ie109e82c1fb2fd96b07e977e1cd76ae3acb865ff
    
    Fix pattern visibility settings (1/2)
    
    Change-Id: Ic627953c5df854c442671a98b5da539b994da18b
    
    LockPatternUtils: Use the actual user id to set pattern size
    
    Ticket: CYNGNOS-2462
    Change-Id: Ia68e26ec2dfc23317135d933bc25204c1380bb02
    
    LockSettings: fix build
    
    Change-Id: Ic65b1a2af398faffb83776ee4013d47f79ab6619
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- services/core/java/com/android/server/locksettings/LockSettingsService.java
+++ services/core/java/com/android/server/locksettings/LockSettingsService.java
@@ -1559,6 +1559,11 @@ public class LockSettingsService extends ILockSettings.Stub {
                 && !getSeparateProfileChallengeEnabledInternal(userId);
     }
 
+
+    public byte getLockPatternSize(int userId) {
+        return mStorage.getLockPatternSize(userId);
+    }
+
     /**
      * Send credentials for user {@code userId} to {@link RecoverableKeyStoreManager} during an
      * unlock operation.
