135cdc3461ba7d1df14024083567cccefb8a78f4
Michael Bestas <mikeioannina@gmail.com>
2015-04-03 14:33:37 -0700

    Forward port CM Screen Security settings (1/2)
     * Variable size pattern lockscreen
     * Toggle dots/error pattern visibility
    
    Change-Id: Ie109e82c1fb2fd96b07e977e1cd76ae3acb865ff
    
    Fix pattern visibility settings (1/2)
    
    Change-Id: Ic627953c5df854c442671a98b5da539b994da18b
    
    LockPatternUtils: Use the actual user id to set pattern size
    
    Ticket: CYNGNOS-2462
    Change-Id: Ia68e26ec2dfc23317135d933bc25204c1380bb02
    
    LockSettings: fix build
    
    Change-Id: Ic65b1a2af398faffb83776ee4013d47f79ab6619
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/bouncer/ui/viewmodel/PatternBouncerViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/bouncer/ui/viewmodel/PatternBouncerViewModel.kt
@@ -45,9 +45,11 @@ class PatternBouncerViewModel(
     ) {
 
     /** The number of columns in the dot grid. */
-    val columnCount = 3
+    val columnCount: Byte
+        get() = interactor.patternSize.value
     /** The number of rows in the dot grid. */
-    val rowCount = 3
+    val rowCount: Byte
+        get() = interactor.patternSize.value
 
     private val _selectedDots = MutableStateFlow<LinkedHashSet<PatternDotViewModel>>(linkedSetOf())
     /** The dots that were selected by the user, in the order of selection. */
