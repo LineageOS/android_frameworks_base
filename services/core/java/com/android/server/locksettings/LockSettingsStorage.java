5497838eabcf748551aa50ccaca37527c47020ee
Michael Bestas <mikeioannina@gmail.com>
2015-04-03 14:33:37 -0700

    Forward port CM Screen Security settings (1/2)
     * Variable size pattern lockscreen
     * Toggle dots/error pattern visibility
    
    Change-Id: Ie109e82c1fb2fd96b07e977e1cd76ae3acb865ff
    
    Fix pattern visibility settings (1/2)
    
    Change-Id: Ic627953c5df854c442671a98b5da539b994da18b
    
    LockPatternUtils: Use the actual user id to set pattern size
    
    Ticket: CYNGNOS-2462
    Change-Id: Ia68e26ec2dfc23317135d933bc25204c1380bb02
    
    LockSettings: fix build
    
    Change-Id: Ic65b1a2af398faffb83776ee4013d47f79ab6619
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- services/core/java/com/android/server/locksettings/LockSettingsStorage.java
+++ services/core/java/com/android/server/locksettings/LockSettingsStorage.java
@@ -30,6 +30,7 @@ import android.os.Environment;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.os.storage.StorageManager;
+import android.provider.Settings;
 import android.util.ArrayMap;
 import android.util.Log;
 import android.util.Slog;
@@ -441,6 +442,14 @@ class LockSettingsStorage {
         }
         writeFile(getLockPasswordFilename(userId), passwordHash);
         writeFile(getLockPatternFilename(userId), patternHash);
+    }
+
+    public byte getLockPatternSize(int userId) {
+        long size = Long.valueOf(readKeyValue(Settings.Secure.LOCK_PATTERN_SIZE, "-1", userId));
+        if (size > 0 && size < 128) {
+            return (byte) size;
+        }
+        return LockPatternUtils.PATTERN_SIZE_DEFAULT;
     }
 
     @VisibleForTesting
