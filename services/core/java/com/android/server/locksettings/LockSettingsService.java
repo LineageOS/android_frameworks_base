d00128296df98007d10635a7c35828b705c35527
Michael Bestas <mikeioannina@gmail.com>
2015-04-03 14:33:37 -0700

    Forward port CM Screen Security settings (1/2)
     * Variable size pattern lockscreen
     * Toggle dots/error pattern visibility
    
    Change-Id: Ie109e82c1fb2fd96b07e977e1cd76ae3acb865ff
    
    Fix pattern visibility settings (1/2)
    
    Change-Id: Ic627953c5df854c442671a98b5da539b994da18b
    
    LockPatternUtils: Use the actual user id to set pattern size
    
    Ticket: CYNGNOS-2462
    Change-Id: Ia68e26ec2dfc23317135d933bc25204c1380bb02
    
    LockSettings: fix build
    
    Change-Id: Ic65b1a2af398faffb83776ee4013d47f79ab6619
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- services/core/java/com/android/server/locksettings/LockSettingsService.java
+++ services/core/java/com/android/server/locksettings/LockSettingsService.java
@@ -1292,6 +1292,10 @@ public class LockSettingsService extends ILockSettings.Stub {
                 && mLockPatternUtils.isSeparateProfileChallengeEnabled(userId);
     }
 
+    public byte getLockPatternSize(int userId) {
+        return mStorage.getLockPatternSize(userId);
+    }
+
     // This method should be called by LockPatternUtil only, all internal methods in this class
     // should call setLockCredentialInternal.
     @Override
@@ -1740,7 +1744,10 @@ public class LockSettingsService extends ILockSettings.Stub {
         if (storedHash.version == CredentialHash.VERSION_LEGACY) {
             final byte[] hash;
             if (storedHash.type == LockPatternUtils.CREDENTIAL_TYPE_PATTERN) {
-                hash = LockPatternUtils.patternToHash(LockPatternUtils.stringToPattern(credential));
+                final byte lockPatternSize = getLockPatternSize(userId);
+                hash = LockPatternUtils.patternToHash(
+                        LockPatternUtils.stringToPattern(credential, lockPatternSize),
+                        lockPatternSize);
             } else {
                 hash = mLockPatternUtils.legacyPasswordToHash(credential, userId)
                         .getBytes(StandardCharsets.UTF_8);
@@ -2119,7 +2126,10 @@ public class LockSettingsService extends ILockSettings.Stub {
             Secure.LOCK_PATTERN_ENABLED,
             Secure.LOCK_BIOMETRIC_WEAK_FLAGS,
             Secure.LOCK_PATTERN_VISIBLE,
-            Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED
+            Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED,
+            Secure.LOCK_PATTERN_SIZE,
+            Secure.LOCK_DOTS_VISIBLE,
+            Secure.LOCK_SHOW_ERROR_PATH
     };
 
     // Reading these settings needs the contacts permission
