b14be75a4038872a9d7e778e4cba98b02e1efa0d
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    SystemUI: Add heads up tile
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I0e3cff73752c59d804dcec43b86524c2e8df4f57

--- packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
+++ packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
@@ -18,6 +18,7 @@ package com.android.systemui.lineage
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.CaffeineTile
+import com.android.systemui.qs.tiles.HeadsUpTile
 
 import dagger.Binds
 import dagger.Module
@@ -31,4 +32,10 @@ interface LineageModule {
     @IntoMap
     @StringKey(CaffeineTile.TILE_SPEC)
     fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
+
+    /** Inject HeadsUpTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(HeadsUpTile.TILE_SPEC)
+    fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
 }
