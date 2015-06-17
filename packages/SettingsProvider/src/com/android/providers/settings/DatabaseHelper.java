b1f34f883ee5c9b078fc52618f1aefb03ed4a537
eray orçunus <erayorcunus@gmail.com>
2015-06-17 16:12:19 +0300

    lockscreen: Add support for showing unlock screen directly
    Squash of the following changes, updated for the new SDK:
    
      From: eray orçunus <erayorcunus@gmail.com>
      Date: Wed, 17 Jun 2015 16:12:19 +0300
    
          lockscreen: Add option for showing unlock screen directly
    
           * Option appears on PIN, pattern and password methods
           * User can press back button to see swipe-up-to-unlock screen
           * Instantly hide keyguard if Smart Lock has unlocked phone
    
          Change-Id: I2ceeed348d7e61632b8344f35431f30cba62ca4f
          Ticket-Id: CYNGNOS-1873
          Signed-off-by: eray orçunus <erayorcunus@gmail.com>
    
      From: Danesh M <daneshm90@gmail.com>
      Date: Tue, 17 Nov 2015 20:09:38 -0800
    
          SettingsProvider: Add missing platform sdk dependency
    
          Change-Id: I4844eaa0a989bf634a2251e95dc84a505037c012
    
    Change-Id: I0cfb43bfb631fce532dfacdf56455309a04ddfa2

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -51,6 +51,8 @@ import com.android.internal.util.XmlUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.internal.widget.LockPatternView;
 
+import lineageos.providers.LineageSettings;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -718,6 +720,7 @@ class DatabaseHelper extends SQLiteOpenHelper {
                    Secure.LOCK_PATTERN_SIZE,
                    Secure.LOCK_DOTS_VISIBLE,
                    Secure.LOCK_SHOW_ERROR_PATH,
+                   LineageSettings.Secure.LOCK_PASS_TO_SECURITY_VIEW,
                    "lockscreen.password_type",
                    "lockscreen.lockoutattemptdeadline",
                    "lockscreen.patterneverchosen",
