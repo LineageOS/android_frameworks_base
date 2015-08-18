1f5022de51299b284f19cc44a007d2cd672c0d64
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2015-08-18 12:27:39 +0530

    SystemUI: Add visualizer feature
    This is a squash of the following commits:
    
    Author: Utkarsh Gupta <utkarsh.eminem@gmail.com>
    Date:   Tue Aug 18 12:27:39 2015 +0530
    
        SystemUI: use new visualizer from Eleven
    
        Change-Id: I4441440868cac71caa32c91a0875f00a630fe596
        Signed-off-by: Utkarsh Gupta <utkarsh.eminem@gmail.com>
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Mikalacki Sava <mikalackis@gmail.com>
    Date:   Thu Sep 3 23:10:35 2015 +0200
    
        SystemUI: fix NPE crash when Visualizer is not initialized
    
        Change-Id: Ic3774011acfae0312f1642d88c7f5bf2abc9f5a7
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Fri Sep 4 15:55:39 2015 -0700
    
        SystemUI: hide visualizer when keyguard is occluded
    
        Change-Id: Ia943fd829f7a379fee0f657bb08c34b8c6f36caa
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Jan 7 19:27:38 2016 +0100
    
        SystemUI: Make lockscreen visualizer battery friendly
    
        In case of offloaded playback, the device is allowed to sleep.
        However, if the lockscreen visualizer is enabled, the device
        hardly enters deep sleep, even if the screen is off. To prevent
        this, enable the visualizer only when the lockscreen is shown.
    
        Change-Id: I484270694b734c9b53e61a7c0ad74391b54fe8cd
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Mar 17 09:18:56 2016 -0700
    
        SystemUI: more consistent visualizer logic
    
        - only call setVisible from screen on and screen off methods
        - eagerly unregister self to make sure we don't get added twice as a
          listener
        - Add some debugging under the DEBUG flag
    
        Change-Id: Iada13058f87d4c8d8c8b60f6eebf055652ff2c8d
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Mon Apr 4 15:29:34 2016 -0700
    
        SystemUI: show visualizer in shade-locked view
    
        This brings back the behavior we had in 12.1 - visualizer would still be
        visible in the shade locked state.
    
        Also with the following improvements:
            * use less alpha
            * always attach/detach in a background thread (Async task)
            * use the statusbar state instead keyguard view state listener
    
        Change-Id: I6b8a57f9f0bba6ba0591de0cb1f94a0eb904cc1c
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Dan Pasanen <invisiblek@cyanogenmod.org>
    Date:   Wed May 18 20:27:47 2016 -0500
    
        Visualizer: Let visualizer fill the entire available screen
    
        * This will allow it to be more dynamic (landscape lock screen for instance)
    
        Change-Id: I2ffd6b0ca8832812faaa4cc0ec4cdd6071fbec8c
    
    Author: Matthias Yzusqui <myzb.dev@gmail.com>
    Date:   Wed Sep 21 15:25:00 2016 +0200
    
        SystemUI: fix navbar drawing glitch
    
        Change-Id: I58e255517d35675c043061f2edb398dab0d16ba8
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Fri Oct 7 08:56:25 2016 +0800
    
        SystemUI: Use Tuner API for CM settings
    
        Get rid of all the excess code by implementing TunerService.Tunable
        and observing any changes made to the settings through it.
    
        Remove UserContentObserver as the Tuner API handles user switches.
    
        Also remove some unused imports that were introduced in earlier
        CM commits, but were never removed since.
    
        Change-Id: Iecafafabdaec82b3b3c72293bea865de48f0e90a
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Sat Dec 2 15:10:28 2017 +0000
    Edit:   Forward-port visualizer feature to O
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Sat Aug 25 01:42:06 2018 +0100
    Edit:   Forward-port visualizer feature to P
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Sat Sep 21 14:56:13 2019 +0100
    Edit:   Forward-port visualizer feature to Q
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Thu Oct 1 10:26:13 2020 +0100
    Edit:   Forward-port visualizer feature to R
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Mon Dec 27 22:35:13 2021 +0100
    Edit:   Forward-port visualizer feature to S
    
    Change-Id: I556a233f70f53f08084fd2cca319e3fb05f47243

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -24,6 +24,7 @@ import android.annotation.MainThread;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.app.Notification;
+import android.app.WallpaperManager;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -72,6 +73,7 @@ import com.android.systemui.statusbar.phone.ScrimController;
 import com.android.systemui.statusbar.phone.ScrimState;
 import com.android.systemui.statusbar.phone.StatusBar;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
+import com.android.systemui.statusbar.VisualizerView;
 import com.android.systemui.util.DeviceConfigProxy;
 import com.android.systemui.util.Utils;
 import com.android.systemui.util.concurrency.DelayableExecutor;
@@ -135,6 +137,7 @@ public class NotificationMediaManager implements Dumpable {
     private final Lazy<StatusBar> mStatusBarLazy;
     private final MediaArtworkProcessor mMediaArtworkProcessor;
     private final Set<AsyncTask<?, ?, ?>> mProcessArtworkTasks = new ArraySet<>();
+    private final WallpaperManager mWallpaperManager;
 
     protected NotificationPresenter mPresenter;
     private MediaController mMediaController;
@@ -157,6 +160,8 @@ public class NotificationMediaManager implements Dumpable {
                     clearCurrentMediaNotification();
                 }
                 findAndUpdateMediaNotifications();
+                mStatusBarLazy.get().getVisualizerView()
+                        .setPlaying(state.getState() == PlaybackState.STATE_PLAYING);
             }
         }
 
@@ -196,6 +201,8 @@ public class NotificationMediaManager implements Dumpable {
         // in session state
         mMediaSessionManager = (MediaSessionManager) mContext.getSystemService(
                 Context.MEDIA_SESSION_SERVICE);
+        mWallpaperManager = (WallpaperManager) mContext.getSystemService(
+                Context.WALLPAPER_SERVICE);
         // TODO: use KeyguardStateController#isOccluded to remove this dependency
         mStatusBarLazy = statusBarLazy;
         mNotificationShadeWindowController = notificationShadeWindowController;
@@ -680,8 +687,9 @@ public class NotificationMediaManager implements Dumpable {
         }
         boolean hasMediaArtwork = artworkDrawable != null;
         boolean allowWhenShade = false;
+        Bitmap lockWallpaper = null;
         if (ENABLE_LOCKSCREEN_WALLPAPER && artworkDrawable == null) {
-            Bitmap lockWallpaper =
+            lockWallpaper =
                     mLockscreenWallpaper != null ? mLockscreenWallpaper.getBitmap() : null;
             if (lockWallpaper != null) {
                 artworkDrawable = new LockscreenWallpaper.WallpaperDrawable(
@@ -700,6 +708,27 @@ public class NotificationMediaManager implements Dumpable {
         mColorExtractor.setHasMediaArtwork(hasMediaArtwork);
         if (mScrimController != null) {
             mScrimController.setHasBackdrop(hasArtwork);
+        }
+
+        if (mStatusBarStateController.getState() != StatusBarState.SHADE) {
+            VisualizerView visualizerView = mStatusBarLazy.get().getVisualizerView();
+            if (!mKeyguardStateController.isKeyguardFadingAway() &&
+                    !mStatusBarLazy.get().isScreenFullyOff()) {
+                visualizerView.setPlaying(getMediaControllerPlaybackState(mMediaController) ==
+                        PlaybackState.STATE_PLAYING);
+            }
+
+            if (artworkDrawable instanceof BitmapDrawable) {
+                // always use current backdrop to color eq
+                visualizerView.setBitmap(((BitmapDrawable) artworkDrawable).getBitmap());
+            } else if (lockWallpaper instanceof Bitmap) {
+                // use lockscreen wallpaper in case user set one
+                visualizerView.setBitmap(lockWallpaper.getConfig() == Bitmap.Config.HARDWARE ?
+                        lockWallpaper.copy(Bitmap.Config.ARGB_8888, false) : lockWallpaper);
+            } else {
+                // use regular wallpaper
+                visualizerView.setBitmap(mWallpaperManager.getBitmap(false));
+            }
         }
 
         if ((hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
