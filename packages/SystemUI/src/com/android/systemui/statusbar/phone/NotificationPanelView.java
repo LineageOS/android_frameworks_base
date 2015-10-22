b8ecc1adc0b5b9f6ccb5b3b2e9db4d015639649d
Danesh M <daneshm90@gmail.com>
2015-10-22 10:51:02 -0700

    SystemUI : Clock position support + am/pm styles
    Based on Dave Kessler's work from cm-12.0
    
    Change-Id: I88e630fcfcffd8a795f1abbeecd71a258d0e8e81
    
    SystemUI: recalculate clock position on removing empty shade views
    
    Change-Id: I70cc2dd3e1da99d4467cd43fc81018ccf135cd77
    Signed-off-by: Roman Birg <roman@cyngn.com>
    (cherry picked from commit 4f965c58bcfb8c657cf9bd26a30fe3eeb9c058ef)
    
    SystemUI: fix clock jumping when expanding QS from lockscreen
    
    commit 4f965c5 fixed recalculating clock position from shade_locked but
    it didn't account for other scenarios such as when the QS is expanding,
    causing the clock to jump when expanding QS from lockscreen.
    
    Change-Id: I0e4f473fbc500f7dcdfa015cbedda07761c3cdb2
    Signed-off-by: Roman Birg <roman@cyngn.com>
    (cherry picked from commit 6831615aa4db003fc7b3a64e22083c6cb69f3fa4)
    
    SystemUI : Fix left/center clock visibility
    
    Change-Id: I5bee0927ceaf93ea4cc0751bcb866aaaed64356b
    
    SystemUI : Make clock/navigation multi-user compatible
    
    Change-Id: Ie6a1552aeb31e138221143d548f7cd4cbb5bde7e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2154,6 +2154,9 @@ public class NotificationPanelView extends PanelView implements
 
         // Hide "No notifications" in QS.
         mNotificationStackScroller.updateEmptyShadeView(mShadeEmpty && !mQsExpanded);
+        if (mStatusBarState == StatusBarState.KEYGUARD) {
+            positionClockAndNotifications();
+        }
     }
 
     public void setQsScrimEnabled(boolean qsScrimEnabled) {
