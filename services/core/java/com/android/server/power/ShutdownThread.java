bd01ad999c91dc1c828affb0a7e433112ad102a8
Jorge Ruesga <jorge@ruesga.com>
2015-10-23 19:55:12 +0200

    base: don't show reboot options for safe mode
    In a user long press in "power off" menu the system lead it to "reboot to safe mode", but if
    the our advance power menu is enabled, we are showing the reboot options instead of the
    safe mode advice message. Since reboot options doesn't made sense here is better to
    respect the original behavior.
    
    Change-Id: I45e7f7a780655ed79d442f583fd9ffa6fba4e447
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -217,7 +217,7 @@ public final class ShutdownThread extends Thread {
                                     ? com.android.internal.R.string.reboot_title
                                     : com.android.internal.R.string.power_off);
 
-            if (!advancedReboot) {
+            if (!advancedReboot || mRebootSafeMode) {
                 confirmDialogBuilder.setMessage(resourceId);
             } else {
                 confirmDialogBuilder
@@ -229,7 +229,7 @@ public final class ShutdownThread extends Thread {
                     new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            if (advancedReboot) {
+                            if (!mRebootSafeMode && advancedReboot) {
                                 boolean softReboot = false;
                                 ListView reasonsList = ((AlertDialog)dialog).getListView();
                                 int selected = reasonsList.getCheckedItemPosition();
