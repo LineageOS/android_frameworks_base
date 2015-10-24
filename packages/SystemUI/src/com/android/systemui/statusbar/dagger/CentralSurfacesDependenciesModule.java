ee8dce19e86e21ae5e38e22080b390785ff34dac
beanstown106 <nbenis106@gmail.com>
2015-10-23 21:07:35 -0400

    SystemUI: Allow to enable Lockscreen Media Art
    This is a squash of the following commits:
    
    Author: beanstown106 <nbenis106@gmail.com>
    Date:   Fri Oct 23 21:07:35 2015 -0400
    
        SystemUI: Allow to enable Lockscreen Media Art
    
        Change-Id: Id93e115649df5d6dc680101a8c852644b217c28a
    
    Author: Ezio Lacandia Bijelkic <brabus84@gmail.com>
    Date:   Fri Nov 20 00:41:04 2020 +0200
    
        SystemUI: Hide lockscreen media art if media is not playing
    
        it's annoying to get the artwork even if the notification gets
        swiped out and media is paused, because the media session is still alive
    
        Change-Id: Ie65932127e47c463ef9a8b763298fc4dae20f8ae
    
    Change-Id: Idd3033b52b4aa1cf07eb694ea0c9c1c934248f7c

--- packages/SystemUI/src/com/android/systemui/statusbar/dagger/CentralSurfacesDependenciesModule.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/dagger/CentralSurfacesDependenciesModule.java
@@ -80,6 +80,7 @@ import com.android.systemui.statusbar.phone.ongoingcall.OngoingCallLogger;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.RemoteInputUriController;
 import com.android.systemui.statusbar.window.StatusBarWindowController;
+import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 import com.android.systemui.util.time.SystemClock;
 
@@ -114,7 +115,8 @@ public interface CentralSurfacesDependenciesModule {
             RemoteInputControllerLogger remoteInputControllerLogger,
             NotificationClickNotifier clickNotifier,
             ActionClickLogger actionClickLogger,
-            DumpManager dumpManager) {
+            DumpManager dumpManager,
+            TunerService tunerService) {
         return new NotificationRemoteInputManager(
                 context,
                 notifPipelineFlags,
@@ -149,7 +151,8 @@ public interface CentralSurfacesDependenciesModule {
             KeyguardStateController keyguardStateController,
             DumpManager dumpManager,
             WallpaperManager wallpaperManager,
-            DisplayManager displayManager) {
+            DisplayManager displayManager,
+            TunerService tunerService) {
         return new NotificationMediaManager(
                 context,
                 centralSurfacesOptionalLazy,
@@ -166,7 +169,8 @@ public interface CentralSurfacesDependenciesModule {
                 keyguardStateController,
                 dumpManager,
                 wallpaperManager,
-                displayManager);
+                displayManager,
+                tunerService);
     }
 
     /** */
