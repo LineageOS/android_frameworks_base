ea712fe3a77f5a02ea2052206a14424b6bd6fdcd
Adnan Begovic <adnan@cyngn.com>
2015-10-27 14:43:27 -0700

    admin: Restore requireSecureKeyguard interface.
    Change-Id: I3c0533bafdae77df953d5bff457a4efdb94167e7

--- core/java/android/app/admin/DevicePolicyManager.java
+++ core/java/android/app/admin/DevicePolicyManager.java
@@ -8109,4 +8109,24 @@ public class DevicePolicyManager {
             throw re.rethrowFromSystemServer();
         }
     }
+
+    /**
+     * Lineage: check if secure keyguard is required
+     * @hide
+     */
+    public boolean requireSecureKeyguard() {
+        return requireSecureKeyguard(UserHandle.myUserId());
+    }
+
+    /** @hide */
+    public boolean requireSecureKeyguard(int userHandle) {
+        if (mService != null) {
+            try {
+                return mService.requireSecureKeyguard(userHandle);
+            } catch (RemoteException e) {
+                Log.w(TAG, "Failed to get secure keyguard requirement");
+            }
+        }
+        return true;
+    }
 }
