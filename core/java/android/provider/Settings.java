9b32396128b387c51a0a2cf28afd02288a3bee11
Adnan Begovic <adnan@cyngn.com>
2015-11-11 12:05:59 -0800

    fw: Move DOUBLE_TAP_SLEEP_GESTURE to CMSettings.
    Change-Id: I8274b7c241cef6835a1114a702e68c95b6d2e036

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3761,12 +3761,6 @@ public final class Settings {
          */
 
         /**
-         *  Enable statusbar double tap gesture on to put device to sleep
-         * @hide
-         */
-        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
-
-        /**
           * Volume keys control cursor in text fields (default is 0)
           * 0 - Disabled
           * 1 - Volume up/down moves cursor left/right
