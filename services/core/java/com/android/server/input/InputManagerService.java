ad36a42d8a117ba45a13d1f9e448d7dcd3332191
Danesh M <daneshm90@gmail.com>
2015-11-12 10:52:11 -0800

    Framework : Move System settings to CMSettings
    Change-Id: I4e9fb06db7b9ba05e4a7bbe11916bb2271af4727

--- services/core/java/com/android/server/input/InputManagerService.java
+++ services/core/java/com/android/server/input/InputManagerService.java
@@ -114,6 +114,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 
+import cyanogenmod.providers.CMSettings;
+
 import libcore.io.IoUtils;
 import libcore.io.Streams;
 import libcore.util.Objects;
@@ -1686,7 +1688,7 @@ public class InputManagerService extends IInputManager.Stub
 
     public void registerVolumeKeysRotationSettingObserver() {
         mContext.getContentResolver().registerContentObserver(
-                Settings.System.getUriFor(Settings.System.SWAP_VOLUME_KEYS_ON_ROTATION), false,
+                CMSettings.System.getUriFor(CMSettings.System.SWAP_VOLUME_KEYS_ON_ROTATION), false,
                 new ContentObserver(mHandler) {
                     @Override
                     public void onChange(boolean selfChange) {
@@ -1698,9 +1700,9 @@ public class InputManagerService extends IInputManager.Stub
     private int getVolumeKeysRotationSetting(int defaultValue) {
         int result = defaultValue;
         try {
-            result = Settings.System.getIntForUser(mContext.getContentResolver(),
-                    Settings.System.SWAP_VOLUME_KEYS_ON_ROTATION, UserHandle.USER_CURRENT);
-        } catch (SettingNotFoundException snfe) {
+            result = CMSettings.System.getIntForUser(mContext.getContentResolver(),
+                    CMSettings.System.SWAP_VOLUME_KEYS_ON_ROTATION, UserHandle.USER_CURRENT);
+        } catch (CMSettings.CMSettingNotFoundException snfe) {
         }
         return result;
     }
