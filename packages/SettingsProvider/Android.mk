94a7fcc27f4e1a57fbdeaaa15ec267480b0323ba
d34d <clark@cyngn.com>
2015-11-15 13:29:07 -0800

    settings: Allow accessing LineageSettings via settings command
    This patch allows the settings command line tool to be used with
    the LineageSettings provider by passing in --lineage
    
    Change-Id: Ie0906c9957e9de8418164d7cc84983d57fa4a015

--- packages/SettingsProvider/Android.mk
+++ packages/SettingsProvider/Android.mk
@@ -7,7 +7,11 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src) \
     src/com/android/providers/settings/EventLogTags.logtags
 
 LOCAL_JAVA_LIBRARIES := telephony-common ims-common
-LOCAL_STATIC_JAVA_LIBRARIES := junit
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    junit \
+    android-support-v14-preference \
+    org.lineageos.platform.internal
 
 LOCAL_PACKAGE_NAME := SettingsProvider
 LOCAL_PRIVATE_PLATFORM_APIS := true
