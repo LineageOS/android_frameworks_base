0a9dbfd514e81e84bf8a80b63e514b61dd8157de
dhacker29 <dhackerdvm@gmail.com>
2015-11-24 01:53:47 -0500

    Core: Use ro.build.date to signal mIsUpgrade
    M: We use a static fingerprint that is only changed when a new OEM build is released, so every flash shows Android is starting instead of upgrading.  This will fix that.
    
    N: even though we dont have the dexopt sceen on N, this is still needed to delete the correct caches, and grant/deny specific runtime permissions like a true oem update would do
    
    Updated for Nougat By: BeansTown106
    
    Change-Id: I0e3ed5c8f0351e48944432ae6a0c5194ddeff1fa

--- services/core/java/com/android/server/pm/Settings.java
+++ services/core/java/com/android/server/pm/Settings.java
@@ -343,7 +343,7 @@ final class Settings {
         public void forceCurrent() {
             sdkVersion = Build.VERSION.SDK_INT;
             databaseVersion = CURRENT_DATABASE_VERSION;
-            fingerprint = Build.FINGERPRINT;
+            fingerprint = Build.DATE;
         }
     }
 
@@ -3192,7 +3192,7 @@ final class Settings {
         // on update drop the files before loading them.
         if (PackageManagerService.CLEAR_RUNTIME_PERMISSIONS_ON_UPGRADE) {
             final VersionInfo internal = getInternalVersion();
-            if (!Build.FINGERPRINT.equals(internal.fingerprint)) {
+            if (!Build.DATE.equals(internal.fingerprint)) {
                 for (UserInfo user : users) {
                     mRuntimePermissionsPersistence.deleteUserRuntimePermissionsFile(user.id);
                 }
@@ -5257,7 +5257,7 @@ final class Settings {
         }
 
         public void onDefaultRuntimePermissionsGrantedLPr(int userId) {
-            mFingerprints.put(userId, Build.FINGERPRINT);
+            mFingerprints.put(userId, Build.DATE);
             writePermissionsForUserAsyncLPr(userId);
         }
 
@@ -5420,7 +5420,7 @@ final class Settings {
                 serializer.endDocument();
                 destination.finishWrite(out);
 
-                if (Build.FINGERPRINT.equals(fingerprint)) {
+                if (Build.DATE.equals(fingerprint)) {
                     mDefaultPermissionsGranted.put(userId, true);
                 }
             // Any error while writing is fatal.
@@ -5532,7 +5532,7 @@ final class Settings {
                     case TAG_RUNTIME_PERMISSIONS: {
                         String fingerprint = parser.getAttributeValue(null, ATTR_FINGERPRINT);
                         mFingerprints.put(userId, fingerprint);
-                        final boolean defaultsGranted = Build.FINGERPRINT.equals(fingerprint);
+                        final boolean defaultsGranted = Build.DATE.equals(fingerprint);
                         mDefaultPermissionsGranted.put(userId, defaultsGranted);
                     } break;
 
