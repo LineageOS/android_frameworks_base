b3efdafa93b45a45fddfe5515bf3088fead40a04
Adnan Begovic <adnan@cyngn.com>
2015-12-01 16:34:10 -0800

    SystemUI: Introduce custom QS tile management
    This squashes the following commits from CM 13.0:
    cf6674d766fe683e52e5c58e63152e9abd6ece89 SystemUi: Reintroduce custom qs tiles management.
    552ab56f722a02008fd808a71ec5e8e99ec910cf qs: use external resource package for custom tiles
    f20ea48c6eee4bf011c441a53502f05878b43a52 SystemUi: Nuke DONT_TRACK label, and introduce CMMetricsLogger constants.
    
    Change-Id: I88dbf3911d4783de7c3f231aa57c51ce43bc8ae6

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/UserSwitcherController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/UserSwitcherController.java
@@ -58,6 +58,8 @@ import com.android.systemui.qs.tiles.UserDetailView;
 import com.android.systemui.statusbar.phone.ActivityStarter;
 import com.android.systemui.statusbar.phone.SystemUIDialog;
 
+import cyanogenmod.app.StatusBarPanelCustomTile;
+
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.lang.ref.WeakReference;
@@ -797,6 +799,11 @@ public class UserSwitcherController {
         @Override
         public Intent getSettingsIntent() {
             return USER_SETTINGS_INTENT;
+        }
+
+        @Override
+        public StatusBarPanelCustomTile getCustomTile() {
+            return null;
         }
 
         @Override
