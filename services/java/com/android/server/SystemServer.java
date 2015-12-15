2d7ccd21e33654c63f69d145b8d831c47dc4e445
Ethan Chen <intervigil@gmail.com>
2015-12-15 09:59:08 -0800

    Use custom ADB over wifi property
    * Use a custom system property to trigger the real one, so we avoid
      running afoul of any SELinux CTS requirements.
    
    Change-Id: I22b6ce63cbfa92cb0ca24f302489376a3fd5dca5

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -282,7 +282,7 @@ public final class SystemServer {
             int adbPort = LineageSettings.Secure.getInt(mContentResolver,
                 LineageSettings.Secure.ADB_PORT, 0);
             // setting this will control whether ADB runs on TCP/IP or USB
-            SystemProperties.set("service.adb.tcp.port", Integer.toString(adbPort));
+            SystemProperties.set("adb.network.port", Integer.toString(adbPort));
         }
     }
 
