237179d005baa90561edfc7ad0cbeec604deb0c0
Shufeng Hou <shou@codeaurora.org>
2015-12-17 17:25:54 +0800

    SystemUI: Fix the unlock sound played repeatedly
    Sometimes keyguardDone() may be invoked from PhoneWindowManager many times
    in a short time, especially when app works with FLAG_DISMISS_KEYGUARD.
    
    Make sure the unlock sound is only played when keyguard is showing and the
    screen is on.
    
    Change-Id: I396588579a3be3b3210e619179d2a57211904644
    CRs-Fixed: 900840

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1776,7 +1776,9 @@ public class KeyguardViewMediator extends SystemUI {
             // only play "unlock" noises if not on a call (since the incall UI
             // disables the keyguard)
             if (TelephonyManager.EXTRA_STATE_IDLE.equals(mPhoneState)) {
-                playSounds(false);
+                if (mShowing && mDeviceInteractive) {
+                    playSounds(false);
+                }
             }
 
             mWakeAndUnlocking = false;
