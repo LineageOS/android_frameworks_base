7ffc23be73419e45021f6e9e1d6fce65776df749
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-20 00:56:47 +0200

    SystemUI: Don't vibrate on touchscreen camera gesture
    * This is usually handled in device-specific keyhandler,
      where vibration is optional and has the same duration
      in all gestures
    
    Change-Id: I879671c9d6f785137d63b8b70de35f5d5020748d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5657,7 +5657,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             pm.wakeUp(SystemClock.uptimeMillis(), "com.android.systemui:CAMERA_GESTURE");
             mStatusBarKeyguardViewManager.notifyDeviceWakeUpRequested();
         }
-        vibrateForCameraGesture();
+        if (source != StatusBarManager.CAMERA_LAUNCH_SOURCE_SCREEN_GESTURE) {
+            vibrateForCameraGesture();
+        }
         if (!mStatusBarKeyguardViewManager.isShowing()) {
             startActivityDismissingKeyguard(KeyguardBottomAreaView.INSECURE_CAMERA_INTENT,
                     false /* onlyProvisioned */, true /* dismissShade */,
