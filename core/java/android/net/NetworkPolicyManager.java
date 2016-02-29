55ef1366751366326160caf9254f3537a6573e00
Oliver Scott <olivercscott@gmail.com>
2016-02-29 10:02:34 -0800

    Firewall: Transport-based toggle support (1/3)
    Update Connectivity with the network transports that UIDs are allowed
    to use, e.g. Wi-Fi or Cellular.
    
    Needs corresponding Connectivity and netd changes.
    
    Also includes squashed changes:
    
    (constants from NetworkPolicyManager.java)
    Author: Oliver Scott <olivercscott@gmail.com>
    Date:   Mon Feb 29 10:02:34 2016 -0800
    
        fw/b: Add support for allowing/disallowing apps on cellular, vpn and wifi networks
    
        Dynamically add/remove apps from restricted networking mode's allowlist based on policy and network transport changes
    
        Change-Id: I526d0058cda71a9e93046d116c0d79093390a85b
    
    Author: Tommy Webb <tommy@calyxinstitute.org>
    Date:   Wed Apr 12 13:43:18 2023 -0400
    
        NPMS: Check if UID is disallowed by transports
    
        The isUidNetworkingBlocked method now also consults Connectivity as to
        whether the UID is currently disallowed from accessing networks.
    
        Requires: I2729b61c349ec2812a74d7d1c04b90a58b0f5b88
        Change-Id: I1cf17b837af5f62c97d99127e1811aa58ef734bf
    
    Author: Tommy Webb <tommy@calyxinstitute.org>
    Date:   Wed Sep 20 16:26:11 2023 -0400
    
        Notify about changes to allowed transports
    
        Add a method to NetworkPolicyCallback to be notified when a UID's
        allowed transports have changed.
    
        Requires Connectivity change of the same Change-Id.
    
        Squash with: Ifeb8342698a8ba4ce2453d64dc32590aebca5fe5
        "Track and send allowed transports to Connectivity"
    
        Change-Id: Ie476f23684b00397197184e965201d6823b28de2
    
    Author: Tommy Webb <tommy@calyxinstitute.org>
    Date:   Wed Sep 20 16:38:07 2023 -0400
    
        Allow Connectivity to report denylist changes
    
        Rather than calling an API provided by Connectivity to check for
        a UID's presence on the denylist, provide an API that Connectivity
        itself can use to update NetworkPolicyManagerService with the latest
        denied UIDs.
    
        Requires Connectivity change of the same Change-Id.
    
        Squash with: I1cf17b837af5f62c97d99127e1811aa58ef734bf
        "NPMS: Check if UID is disallowed by transports"
    
        Change-Id: I3c3593f110753a3ce02af3739f600190f22e9663
    
    Requires: I79342edbec92090cca20853ba50ea7fd48ec81c2
    Change-Id: Ifeb8342698a8ba4ce2453d64dc32590aebca5fe5

--- core/java/android/net/NetworkPolicyManager.java
+++ core/java/android/net/NetworkPolicyManager.java
@@ -400,6 +400,17 @@ public class NetworkPolicyManager {
     }
 
     /** @hide */
+    @SystemApi(client = SystemApi.Client.MODULE_LIBRARIES)
+    @RequiresPermission(NetworkStack.PERMISSION_MAINLINE_NETWORK_STACK)
+    public void notifyDenylistChanged(@NonNull int[] uidsAdded, @NonNull int[] uidsRemoved) {
+        try {
+            mService.notifyDenylistChanged(uidsAdded, uidsRemoved);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /** @hide */
     @UnsupportedAppUsage(maxTargetSdk = Build.VERSION_CODES.P, trackingBug = 115609023)
     public void registerListener(INetworkPolicyListener listener) {
         try {
@@ -983,6 +994,11 @@ public class NetworkPolicyManager {
          */
         @SystemApi(client = SystemApi.Client.MODULE_LIBRARIES)
         default void onUidBlockedReasonChanged(int uid, int blockedReasons) {}
+
+        /** @hide */
+        @SystemApi(client = SystemApi.Client.MODULE_LIBRARIES)
+        default void onUidsAllowedTransportsChanged(@NonNull int[] uids,
+                @NonNull long[] allowedTransports) {}
     }
 
     /** @hide */
@@ -1002,6 +1018,11 @@ public class NetworkPolicyManager {
                 dispatchOnUidBlockedReasonChanged(mExecutor, mCallback, uid, newBlockedReasons);
             }
         }
+
+        @Override
+        public void onAllowedTransportsChanged(int[] uids, long[] allowedTransports) {
+            dispatchOnUidsAllowedTransportsChanged(mExecutor, mCallback, uids, allowedTransports);
+        }
     }
 
     private static void dispatchOnUidBlockedReasonChanged(@Nullable Executor executor,
@@ -1012,6 +1033,17 @@ public class NetworkPolicyManager {
             executor.execute(PooledLambda.obtainRunnable(
                     NetworkPolicyCallback::onUidBlockedReasonChanged,
                     callback, uid, blockedReasons).recycleOnUse());
+        }
+    }
+
+    private static void dispatchOnUidsAllowedTransportsChanged(@Nullable Executor executor,
+            @NonNull NetworkPolicyCallback callback, int[] uids, long[] allowedTransports) {
+        if (executor == null) {
+            callback.onUidsAllowedTransportsChanged(uids, allowedTransports);
+        } else {
+            executor.execute(PooledLambda.obtainRunnable(
+                    NetworkPolicyCallback::onUidsAllowedTransportsChanged,
+                    callback, uids, allowedTransports).recycleOnUse());
         }
     }
 
@@ -1071,5 +1103,6 @@ public class NetworkPolicyManager {
         @Override public void onSubscriptionPlansChanged(int subId, SubscriptionPlan[] plans) { }
         @Override public void onBlockedReasonChanged(int uid,
                 int oldBlockedReasons, int newBlockedReasons) { }
+        @Override public void onAllowedTransportsChanged(int[] uids, long[] allowedTransports) { }
     }
 }
