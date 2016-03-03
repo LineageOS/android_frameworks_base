45d6a456a176092694d4312aa29933b8aa9d81e6
Wei Huang <wei.x.huang@sonymobile.com>
2016-03-03 21:07:41 +0800

    Support VoLTE icon on status bar
    - Add notification logic for following events in TelephonyRegistry.
      (1) IMS registration state
      (2) IMS feature capability
    
    - Display VoLTE icon when following conditions met.
      (1) IMS is registered
      (2) IMS VoLTE feature supported
    
    Bug: 28806100
    Change-Id: I85e29937c59d0c1362f213e336f3fe788e36f4d2

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -798,6 +798,10 @@ public class NetworkControllerImpl extends BroadcastReceiver
                             : Math.min(Integer.parseInt(level), icons[0].length - 1);
                     controller.getState().connected = controller.getState().level >= 0;
                 }
+                String volte = args.getString("volte");
+                if (volte != null) {
+                    controller.getState().volteAvailable = volte.equals("show");
+                }
                 controller.getState().enabled = show;
                 controller.notifyListeners();
             }
@@ -863,6 +867,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
         boolean show4gForLte = false;
         boolean hideLtePlus = false;
         boolean hspaDataDistinguishable;
+        boolean showVolteIcon = false;
 
         static Config readConfig(Context context) {
             Config config = new Config();
@@ -875,6 +880,8 @@ public class NetworkControllerImpl extends BroadcastReceiver
             config.hspaDataDistinguishable =
                     res.getBoolean(R.bool.config_hspa_data_distinguishable);
             config.hideLtePlus = res.getBoolean(R.bool.config_hideLtePlus);
+            config.showVolteIcon =
+                    res.getBoolean(R.bool.config_showVolteIcon);
             return config;
         }
     }
