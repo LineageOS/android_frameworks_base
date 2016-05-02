9aebbcdce0ca9dd7b4e241780864c7fcdfa0e06e
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-05-02 09:34:21 -0500

    SystemUI: Fix toggling lockscreen rotation [1/3]
    Change-Id: I28fea0124c91b8efc206a940761e77fa61d1fefc

--- packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowControllerImpl.java
@@ -27,12 +27,15 @@ import android.app.IActivityManager;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.database.ContentObserver;
 import android.graphics.PixelFormat;
 import android.graphics.Region;
 import android.os.Binder;
 import android.os.Build;
+import android.os.Handler;
 import android.os.RemoteException;
 import android.os.Trace;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
@@ -76,6 +79,8 @@ import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.user.domain.interactor.SelectedUserInteractor;
 
 import dagger.Lazy;
+
+import lineageos.providers.LineageSettings;
 
 import java.io.PrintWriter;
 import java.lang.ref.Reference;
@@ -256,6 +261,17 @@ public class NotificationShadeWindowControllerImpl implements NotificationShadeW
         }
     }
 
+    private boolean shouldEnableKeyguardScreenRotation() {
+        boolean enableAccelerometerRotation =
+                Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.ACCELEROMETER_ROTATION, 0) != 0;
+        boolean enableLockScreenRotation =
+                LineageSettings.System.getInt(mContext.getContentResolver(),
+                LineageSettings.System.LOCKSCREEN_ROTATION, 0) != 0;
+        return mKeyguardStateController.isKeyguardScreenRotationAllowed()
+                && (enableLockScreenRotation && enableAccelerometerRotation);
+    }
+
     /**
      * Adds the notification shade view to the window manager.
      */
@@ -302,6 +318,8 @@ public class NotificationShadeWindowControllerImpl implements NotificationShadeW
         }
 
         mLpChanged.copyFrom(mLp);
+        SettingsObserver observer = new SettingsObserver(new Handler());
+        observer.observe(mContext);
         onThemeChanged();
 
         // Make the state consistent with KeyguardViewMediator#setupLocked during initialization.
@@ -424,7 +442,7 @@ public class NotificationShadeWindowControllerImpl implements NotificationShadeW
 
     private void adjustScreenOrientation(NotificationShadeWindowState state) {
         if (state.bouncerShowing || state.isKeyguardShowingAndNotOccluded() || state.dozing) {
-            if (mKeyguardStateController.isKeyguardScreenRotationAllowed()) {
+            if (shouldEnableKeyguardScreenRotation()) {
                 mLpChanged.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_USER;
             } else {
                 mLpChanged.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_NOSENSOR;
@@ -1030,4 +1048,29 @@ public class NotificationShadeWindowControllerImpl implements NotificationShadeW
             apply(mCurrentState);
         }
     };
+
+    private class SettingsObserver extends ContentObserver {
+        public SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        public void observe(Context context) {
+            context.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.ACCELEROMETER_ROTATION),
+                    false, this);
+            context.getContentResolver().registerContentObserver(
+                    LineageSettings.System.getUriFor(LineageSettings.System.LOCKSCREEN_ROTATION),
+                    false, this);
+        }
+
+        public void unobserve(Context context) {
+            context.getContentResolver().unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            // update the state
+            apply(mCurrentState);
+        }
+    }
 }
