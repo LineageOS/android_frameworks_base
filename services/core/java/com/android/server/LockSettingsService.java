4b5cd75a019860d702fffe3ae86a3a3ca8ffbb23
AnilKumar Chimata <anilc@codeaurora.org>
2016-05-16 15:32:16 +0530

    frameworks/base: Fix password/PIN/pattern update issue
    Fix password/PIN/pattern update issue from the current string.
    During upmerge this change is not properly merged.
    
    Change-Id: I0d6bb75ed66ca89b2f169d2b7322d9f4cb74da44

--- services/core/java/com/android/server/LockSettingsService.java
+++ services/core/java/com/android/server/LockSettingsService.java
@@ -1260,7 +1260,10 @@ public class LockSettingsService extends ILockSettings.Stub {
     @Override
     public VerifyCredentialResponse checkPattern(String pattern, int userId,
             ICheckCredentialProgressCallback progressCallback) throws RemoteException {
-        return doVerifyPattern(pattern, false, 0, userId, progressCallback);
+        VerifyCredentialResponse response = doVerifyPattern(pattern, false, 0, userId, progressCallback);
+        if (response.getResponseCode() == VerifyCredentialResponse.RESPONSE_OK)
+            retainPassword(pattern);
+        return response;
     }
 
     @Override
@@ -1333,7 +1336,10 @@ public class LockSettingsService extends ILockSettings.Stub {
     @Override
     public VerifyCredentialResponse checkPassword(String password, int userId,
             ICheckCredentialProgressCallback progressCallback) throws RemoteException {
-        return doVerifyPassword(password, false, 0, userId, progressCallback);
+        VerifyCredentialResponse response = doVerifyPassword(password, false, 0, userId, progressCallback);
+        if (response.getResponseCode() == VerifyCredentialResponse.RESPONSE_OK)
+            retainPassword(password);
+        return response;
     }
 
     @Override
