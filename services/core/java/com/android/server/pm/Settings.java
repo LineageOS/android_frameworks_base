75c02e7603fd1c4be1c7c1c1c42e0a4f23bbc7dc
Mårten Kongstad <marten.kongstad@sonymobile.com>
2016-06-02 09:35:31 +0200

    XOMS7-N: Integrate OverlayManagerService into framework [3/11]
    Hand over ownership of overlays to OverlayManagerService.
    
    Changes to a package's overlays are propagated using the activity life
    cycle. Affected activities will be recreated as needed. This provides a
    well-defined point to modify an application's assets while the
    application is paused.
    
    Consolidate how overlays targeting the system and overlays targeting
    regular applications are handled. Previously, system overlays were
    handled as a special case. Now, everything is handled identically. As a
    side effect, the call to idmap --scan during Zygote boot has become
    obsolete and is removed.
    
    Deprecate and remove use of SCAN_TRUSTED_OVERLAY from
    PackageManagerService. Previously, the flag was used to restrict what
    paths overlays were allowed to be installed in. Now, overlay packages
    are first class packages and it is up to the OverlayManagerService to
    decide whether to use the overlay or not.
    
    Information on what overlays to use is recorded in
    ApplicationInfo.resourceDirs. The PackageManagerService is responsible
    for the creation of ApplicationInfo objects. The OverlayManagerService
    is responsible for informing the PackageManagerService in advance about
    what resourceDirs to use.
    
    When launching an application, the ApplicationInfo is already populated
    with up-to-date information about overlays.
    
    When enabling or disabling an overlay for a running application, the
    OverlayManagerService first notifies the OverlayManagerService about the
    updated resourceDirs. It then tells the ActivityManagerService to push
    the new ApplicationInfo object to the application's ActivityThread.
    Finally the application requests its ResourcesManager to create new
    ResourcesImpl objects based on the updated paths.
    
    Change-Id: If0b1eaa690c38f9c33f7c8dc981314205a73fa9c

--- services/core/java/com/android/server/pm/Settings.java
+++ services/core/java/com/android/server/pm/Settings.java
@@ -811,7 +811,7 @@ final class Settings {
                                     false, // suspended
                                     null, null, null,
                                     false, // blockUninstall
-                                    INTENT_FILTER_DOMAIN_VERIFICATION_STATUS_UNDEFINED, 0,
+                                    INTENT_FILTER_DOMAIN_VERIFICATION_STATUS_UNDEFINED, 0, null,
                                     null,
                                     null
                                     );
@@ -1615,7 +1615,8 @@ final class Settings {
                                 false,  // suspended
                                 null, null, null,
                                 false, // blockUninstall
-                                INTENT_FILTER_DOMAIN_VERIFICATION_STATUS_UNDEFINED, 0,
+
+                                INTENT_FILTER_DOMAIN_VERIFICATION_STATUS_UNDEFINED, 0, null,
                                 null,
                                 null
                                 );
@@ -1726,7 +1727,7 @@ final class Settings {
 
                     ps.setUserState(userId, ceDataInode, enabled, installed, stopped, notLaunched,
                             hidden, suspended, enabledCaller, enabledComponents, disabledComponents,
-                            blockUninstall, verifState, linkGeneration,
+                            blockUninstall, verifState, linkGeneration, null,
                             protectedComponents, visibleComponents);
                 } else if (tagName.equals("preferred-activities")) {
                     readPreferredActivitiesLPw(parser, userId);
