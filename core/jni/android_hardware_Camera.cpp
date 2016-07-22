73abf4ea647ed34950b3e5a11ad79f57bab00f77
Sauhard Pande <sauhardp@codeaurora.org>
2016-07-21 18:23:21 -0700

    Camera: Expose Aux camera to apps present in the whitelist
    Issue:
    3rd party apk cannot handle additional aux camera and may cause failure,
    Mono camera doesnt support all capabilities of HAL3 causing CTS issue.
    
    Fix:
    1. Expose aux camera to apps present in the whitelist
    2. ignore the availabe/unavailable status update for aux camera
    3. returning exception for open request for bad cameraid.
    
    CRs-Fixed: 1086937
    
    Change-Id: I15910154c6df205e6d4e00bfad30a00c9e3d5bee

--- core/jni/android_hardware_Camera.cpp
+++ core/jni/android_hardware_Camera.cpp
@@ -1090,7 +1090,7 @@ static void android_hardware_Camera_enableFocusMoveCallback(JNIEnv *env, jobject
 //-------------------------------------------------
 
 static const JNINativeMethod camMethods[] = {
-  { "getNumberOfCameras",
+  { "_getNumberOfCameras",
     "()I",
     (void *)android_hardware_Camera_getNumberOfCameras },
   { "_getCameraInfo",
