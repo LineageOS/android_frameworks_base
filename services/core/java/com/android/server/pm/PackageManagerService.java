628ffeac005894b25a7edd74627da52e9d87d8df
Steve Kondik <steve@cyngn.com>
2016-09-01 22:43:04 -0700

    Revert "framework: Add permission dialog"
     * Conflicts with Privacy Guard
    
    This reverts commit cfd88000aa4efc90353a4f7dcbfdce962dfdab7f.
    
    Change-Id: I2b30acf4ccdbfd316389b1f6cf4c4ef20012ff0a

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -309,7 +309,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
-import android.os.SystemProperties;
 
 /**
  * Keep track of all those APKs everywhere.
@@ -1785,14 +1784,6 @@ public class PackageManagerService extends IPackageManager.Stub {
                     res.removedInfo.args.doPostDeleteLI(true);
                 }
             }
-            //Show permission control menu after installation
-            if(isStrictOpEnable()){
-                Intent intent = new Intent(Intent.ACTION_MANAGE_APP_PERMISSIONS);
-                intent.putExtra(Intent.EXTRA_PACKAGE_NAME, packageName);
-                intent.putExtra("hideInfoButton", true);// used by the start activity
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); //outside of Activity context need :FLAG_ACTIVITY_NEW_TASK .
-                mContext.startActivity(intent);
-            }
         }
 
         // If someone is watching installs - notify them
@@ -1805,10 +1796,6 @@ public class PackageManagerService extends IPackageManager.Stub {
                 Slog.i(TAG, "Observer no longer exists.");
             }
         }
-    }
-
-    private boolean isStrictOpEnable() {
-        return SystemProperties.getBoolean("persist.sys.strict_op_enable", false);
     }
 
     private void grantRuntimePermissionsGrantedToDisabledPrivSysPackageParentLPw(
