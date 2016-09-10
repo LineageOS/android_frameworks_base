5d614e0e66c3361a651d6d99e002dbda748ce4f3
Matthias Yzusqui <myzb.dev@gmail.com>
2016-09-10 13:04:19 +0200

    Revert "Emergency call button on Swipe lock Screen."
    This feature is (1) buggy and thus perma enabled on MSIM and (2)
    UX wise poorly implemented. Kill this CAF feature and the padding
    introduced along with it to return to the AOSP look.
    
    This reverts commit e7b6415c010c269e34cce09277148f7b2f91bcf5.
    
    Change-Id: I7abb68ad20bdca949ab8aa2af99b94d15e83802c

--- packages/Keyguard/src/com/android/keyguard/EmergencyButton.java
+++ packages/Keyguard/src/com/android/keyguard/EmergencyButton.java
@@ -24,7 +24,6 @@ import android.content.res.Configuration;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.SystemClock;
-import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.telecom.TelecomManager;
 import android.telephony.ServiceState;
@@ -196,7 +195,7 @@ public class EmergencyButton extends Button {
         }
     }
 
-    public void updateEmergencyCallButton() {
+    private void updateEmergencyCallButton() {
         boolean visible = false;
         if (mIsVoiceCapable) {
             // Emergency calling requires voice capability.
@@ -210,8 +209,7 @@ public class EmergencyButton extends Button {
                     visible = mEnableEmergencyCallWhileSimLocked;
                 } else {
                     // Only show if there is a secure screen (pin/pattern/SIM pin/SIM puk);
-                    visible = mLockPatternUtils.isSecure(KeyguardUpdateMonitor.getCurrentUser()) ||
-                              SystemProperties.getBoolean("persist.radio.emgcy_btn_onswipe", false);
+                    visible = mLockPatternUtils.isSecure(KeyguardUpdateMonitor.getCurrentUser());
                 }
 
                 if (mContext.getResources().getBoolean(R.bool.kg_hide_emgcy_btn_when_oos)) {
