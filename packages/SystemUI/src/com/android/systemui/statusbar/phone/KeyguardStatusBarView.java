adb67550689bcdcf7a9550e7afd894b6a712a4bd
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-09-25 00:11:18 +0800

    Revert "SystemUI: Show percentage of battery"
    CM has its own battery customization options.
    Revert the original CAF commit as it is unnecessary.
    
    This reverts commit 296a682a69a4be8fb99fa6c3e82cd5833d594b80.
    
    Change-Id: I1e806d89cfa56ff18f91cc8ae7b7f65954f5f92b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -167,9 +167,7 @@ public class KeyguardStatusBarView extends RelativeLayout
             }
         }
 
-        boolean showBatteryLevel = getResources().getBoolean(R.bool.config_showBatteryPercentage);
-        mBatteryLevel.setVisibility(
-                mBatteryCharging || showBatteryLevel ? View.VISIBLE : View.GONE);
+        mBatteryLevel.setVisibility(mBatteryCharging ? View.VISIBLE : View.GONE);
     }
 
     private void updateSystemIconsLayoutParams() {
