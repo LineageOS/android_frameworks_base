43796781875863451f0d2d1407f752d7d1184bb3
Steve Kondik <steve@cyngn.com>
2016-10-12 03:29:21 -0700

    perf: Add missing activity resumed trigger
     * Lost in merge.
    
    Change-Id: Ibd241e437d5225371738a2e999c2acc793551fcd

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -2255,6 +2255,11 @@ final class ActivityStack {
             return false;
         }
 
+        // Some activities may want to alter the system power management
+        if (mStackSupervisor.mService.mPerf != null) {
+            mStackSupervisor.mService.mPerf.activityResumed(next.intent);
+        }
+
         // The activity may be waiting for stop, but that is no longer
         // appropriate for it.
         mStackSupervisor.mStoppingActivities.remove(next);
