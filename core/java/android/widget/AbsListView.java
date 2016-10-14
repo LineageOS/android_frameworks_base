53148dc64f5c7afd3218a0165ef53523822007c0
Jake Weinstein <xboxlover360@gmail.com>
2016-10-13 23:56:02 -0400

    base: set scrolling to 0.006f
    Change-Id: I18a52ac84ba9c8274adc757e41ca2ca995d514f6
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -4608,6 +4608,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
 
         FlingRunnable() {
             mScroller = new OverScroller(getContext());
+            mScroller.setFriction(0.006f);
         }
 
         void start(int initialVelocity) {
