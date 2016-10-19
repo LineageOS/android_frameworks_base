d917c7fdb02835016cc2820ee4fa7e63be622a27
Danny Baumann <dannybaumann@web.de>
2016-10-19 13:38:06 +0200

    Support enforcing a minimum delay between notification sounds of an app.
    Useful e.g. for messenger apps.
    
    Change-Id: If8e8cc9e2f02d70537c1f9dc14f22bbd0ec1e9a6

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -272,6 +272,7 @@ import android.util.Slog;
 import android.util.SparseArray;
 import android.util.SparseBooleanArray;
 import android.util.StatsEvent;
+import android.util.TimeUtils;
 import android.util.Xml;
 import android.util.proto.ProtoOutputStream;
 import android.view.Display;
@@ -356,6 +357,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Set;
@@ -618,6 +620,7 @@ public class NotificationManagerService extends SystemService {
     @GuardedBy("mToastQueue")
     private final Set<Integer> mToastRateLimitingDisabledUids = new ArraySet<>();
     final ArrayMap<String, NotificationRecord> mSummaryByGroupKey = new ArrayMap<>();
+    final ArrayMap<String, Long> mLastSoundTimestamps = new ArrayMap<>();
 
     // True if the toast that's on top of the queue is being shown at the moment.
     @GuardedBy("mToastQueue")
@@ -3575,6 +3578,19 @@ public class NotificationManagerService extends SystemService {
             handleSavePolicyFile();
         }
 
+        @Override
+        public void setNotificationSoundTimeout(String pkg, int uid, long timeout) {
+            checkCallerIsSystem();
+            mPreferencesHelper.setNotificationSoundTimeout(pkg, uid, timeout);
+            handleSavePolicyFile();
+        }
+
+        @Override
+        public long getNotificationSoundTimeout(String pkg, int uid) {
+            checkCallerIsSystem();
+            return mPreferencesHelper.getNotificationSoundTimeout(pkg, uid);
+        }
+
         /**
          * Updates the enabled state for notifications for the given package (and uid).
          * Additionally, this method marks the app importance as locked by the user, which
@@ -6381,6 +6397,14 @@ public class NotificationManagerService extends SystemService {
                 pw.println("\n  Usage Stats:");
                 mUsageStats.dump(pw, "    ", filter);
             }
+
+            long now = SystemClock.elapsedRealtime();
+            pw.println("\n  Last notification sound timestamps:");
+            for (Map.Entry<String, Long> entry : mLastSoundTimestamps.entrySet()) {
+                pw.print("    " + entry.getKey() + " -> ");
+                TimeUtils.formatDuration(entry.getValue(), now, pw);
+                pw.println(" ago");
+            }
         }
     }
 
@@ -8295,7 +8319,8 @@ public class NotificationManagerService extends SystemService {
                 }
                 hasValidVibrate = vibration != null;
                 boolean hasAudibleAlert = hasValidSound || hasValidVibrate;
-                if (hasAudibleAlert && !shouldMuteNotificationLocked(record)) {
+                if (hasAudibleAlert && !shouldMuteNotificationLocked(record)
+                        && !isInSoundTimeoutPeriod(record)) {
                     if (!sentAccessibilityEvent) {
                         sendAccessibilityEvent(record);
                         sentAccessibilityEvent = true;
@@ -8366,6 +8391,10 @@ public class NotificationManagerService extends SystemService {
         } else if (wasShowLights) {
             updateLightsLocked();
         }
+        if (buzz || beep) {
+            mLastSoundTimestamps.put(generateLastSoundTimeoutKey(record),
+                    SystemClock.elapsedRealtime());
+        }
         final int buzzBeepBlink = (buzz ? 1 : 0) | (beep ? 2 : 0) | (blink ? 4 : 0);
         if (buzzBeepBlink > 0) {
             // Ignore summary updates because we don't display most of the information.
@@ -8394,6 +8423,24 @@ public class NotificationManagerService extends SystemService {
         }
         record.setAudiblyAlerted(buzz || beep);
         return buzzBeepBlink;
+    }
+
+    private boolean isInSoundTimeoutPeriod(NotificationRecord record) {
+        long timeoutMillis = mPreferencesHelper.getNotificationSoundTimeout(
+                record.getSbn().getPackageName(), record.getSbn().getUid());
+        if (timeoutMillis == 0) {
+            return false;
+        }
+
+        Long value = mLastSoundTimestamps.get(generateLastSoundTimeoutKey(record));
+        if (value == null) {
+            return false;
+        }
+        return SystemClock.elapsedRealtime() - value < timeoutMillis;
+    }
+
+    private String generateLastSoundTimeoutKey(NotificationRecord record) {
+        return record.getSbn().getPackageName() + "|" + record.getSbn().getUid();
     }
 
     @GuardedBy("mNotificationLock")
--- services/core/java/com/android/server/notification/PreferencesHelper.java
+++ services/core/java/com/android/server/notification/PreferencesHelper.java
@@ -142,6 +142,7 @@ public class PreferencesHelper implements RankingConfig {
     private static final String ATT_SENT_INVALID_MESSAGE = "sent_invalid_msg";
     private static final String ATT_SENT_VALID_MESSAGE = "sent_valid_msg";
     private static final String ATT_USER_DEMOTED_INVALID_MSG_APP = "user_demote_msg_app";
+    private static final String ATT_SOUND_TIMEOUT = "sound-timeout";
 
     private static final int DEFAULT_PRIORITY = Notification.PRIORITY_DEFAULT;
     private static final int DEFAULT_VISIBILITY = NotificationManager.VISIBILITY_NO_OVERRIDE;
@@ -163,6 +164,8 @@ public class PreferencesHelper implements RankingConfig {
      */
     private static final int DEFAULT_LOCKED_APP_FIELDS = 0;
     private final SysUiStatsEvent.BuilderFactory mStatsEventBuilderFactory;
+
+    private static final int DEFAULT_SOUND_TIMEOUT = 0;
 
     /**
      * All user-lockable fields for a given application.
@@ -308,6 +311,8 @@ public class PreferencesHelper implements RankingConfig {
             r.hasSentValidMessage = parser.getAttributeBoolean(null, ATT_SENT_VALID_MESSAGE, false);
             r.userDemotedMsgApp = parser.getAttributeBoolean(
                     null, ATT_USER_DEMOTED_INVALID_MSG_APP, false);
+            r.soundTimeout = parser.getAttributeLong(
+                    null, ATT_SOUND_TIMEOUT, DEFAULT_SOUND_TIMEOUT);
 
             final int innerDepth = parser.getDepth();
             int type;
@@ -596,6 +601,9 @@ public class PreferencesHelper implements RankingConfig {
                 }
                 if (r.visibility != DEFAULT_VISIBILITY) {
                     out.attributeInt(null, ATT_VISIBILITY, r.visibility);
+                }
+                if (r.soundTimeout != DEFAULT_SOUND_TIMEOUT) {
+                    out.attributeLong(null, ATT_SOUND_TIMEOUT, r.soundTimeout);
                 }
                 if (r.bubblePreference != DEFAULT_BUBBLE_PREFERENCE) {
                     out.attributeInt(null, ATT_ALLOW_BUBBLE, r.bubblePreference);
@@ -1776,6 +1784,21 @@ public class PreferencesHelper implements RankingConfig {
     }
 
     /**
+     * @hide
+     */
+    public long getNotificationSoundTimeout(String packageName, int uid) {
+        return getOrCreatePackagePreferencesLocked(packageName, uid).soundTimeout;
+    }
+
+    /**
+     * @hide
+     */
+    public void setNotificationSoundTimeout(String packageName, int uid, long timeout) {
+        getOrCreatePackagePreferencesLocked(packageName, uid).soundTimeout = timeout;
+        updateConfig();
+    }
+
+    /**
      * Returns the delegate for a given package, if it's allowed by the package and the user.
      */
     public @Nullable String getNotificationDelegate(String sourcePkg, int sourceUid) {
@@ -2209,6 +2232,9 @@ public class PreferencesHelper implements RankingConfig {
                         if (r.showBadge != DEFAULT_SHOW_BADGE) {
                             PackagePreferences.put("showBadge", Boolean.valueOf(r.showBadge));
                         }
+                        if (r.soundTimeout != DEFAULT_SOUND_TIMEOUT) {
+                            PackagePreferences.put("soundTimeout", r.soundTimeout);
+                        }
                         JSONArray channels = new JSONArray();
                         for (NotificationChannel channel : r.channels.values()) {
                             channels.put(channel.toJson());
@@ -2480,6 +2506,7 @@ public class PreferencesHelper implements RankingConfig {
                 p.groups = new ArrayMap<>();
                 p.delegate = null;
                 p.lockedAppFields = DEFAULT_LOCKED_APP_FIELDS;
+                p.soundTimeout = DEFAULT_SOUND_TIMEOUT;
                 p.bubblePreference = DEFAULT_BUBBLE_PREFERENCE;
                 p.importance = DEFAULT_IMPORTANCE;
                 p.priority = DEFAULT_PRIORITY;
@@ -2714,6 +2741,7 @@ public class PreferencesHelper implements RankingConfig {
         boolean showBadge = DEFAULT_SHOW_BADGE;
         int bubblePreference = DEFAULT_BUBBLE_PREFERENCE;
         int lockedAppFields = DEFAULT_LOCKED_APP_FIELDS;
+        long soundTimeout = DEFAULT_SOUND_TIMEOUT;
         // these fields are loaded on boot from a different source of truth and so are not
         // written to notification policy xml
         boolean defaultAppLockedImportance = DEFAULT_APP_LOCKED_IMPORTANCE;
