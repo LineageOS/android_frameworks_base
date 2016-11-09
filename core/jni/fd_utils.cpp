4f00db0b7b7ae22cba61b1d3376afbc01367689a
Steve Kondik <steve@cyngn.com>
2016-11-08 22:26:54 -0800

    Add lineage sdk resource APK to Zygote FD whitelist
    Change-Id: Ica91b23e467db61f142b183316db4afee2a2a283

--- core/jni/fd_utils.cpp
+++ core/jni/fd_utils.cpp
@@ -39,6 +39,7 @@ static const char* kPathWhitelist[] = {
   "/dev/socket/webview_zygote",
   "/sys/kernel/debug/tracing/trace_marker",
   "/system/framework/framework-res.apk",
+  "/system/framework/org.lineageos.platform-res.apk",
   "/dev/urandom",
   "/dev/ion",
   "/dev/dri/renderD129", // Fixes b/31172436
