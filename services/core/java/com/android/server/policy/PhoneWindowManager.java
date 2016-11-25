603931d06619b1696e9ff17d4c715d47faf29905
jrior001 <jriordan001@gmail.com>
2016-11-24 22:34:25 -0500

    Allow lid to send a generic COVER_CHANGED broadcast
    This intent will be used to launch a common app for devices
    with a view window flip cover(LG, Asus..)  or dotcase(HTC).
    
    Change-Id: I8ce163d0d3f71e40680f42348847e548b41a0045

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -8018,6 +8018,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         synchronized (mLock) {
             updateWakeGestureListenerLp();
         }
+        sendLidChangeBroadcast();
+    }
+
+    private void sendLidChangeBroadcast() {
+        Log.d(TAG, "Sending cover change broadcast, mLidState=" + mLidState);
+        Intent intent = new Intent(cyanogenmod.content.Intent.ACTION_LID_STATE_CHANGED);
+        intent.putExtra(cyanogenmod.content.Intent.EXTRA_LID_STATE, mLidState);
+        intent.setFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+        mContext.sendBroadcastAsUser(intent, UserHandle.SYSTEM);
     }
 
     void updateUiMode() {
