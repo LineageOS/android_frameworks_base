d8198b23bed54202806ee2b82ce625c82f19e1d3
Simao Gomes Viana <xdevs23@outlook.com>
2016-11-30 21:01:10 +0100

    Disable overall debugging
    Idk if this is a good idea, but let this exist.
    
    Change-Id: If88aee265cdf985cef8231e6dc4c87437a582638
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- packages/SystemUI/src/com/android/systemui/qs/external/TileServiceManager.java
+++ packages/SystemUI/src/com/android/systemui/qs/external/TileServiceManager.java
@@ -50,7 +50,7 @@ public class TileServiceManager {
     private static final long MIN_BIND_TIME = 5000;
     private static final long UNBIND_DELAY = 30000;
 
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
 
     private static final String TAG = "TileServiceManager";
 
--- services/core/java/com/android/server/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/fingerprint/FingerprintService.java
@@ -95,7 +95,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public class FingerprintService extends SystemService implements IHwBinder.DeathRecipient {
     static final String TAG = "FingerprintService";
-    static final boolean DEBUG = true;
+    static final boolean DEBUG = false;
     private static final boolean CLEANUP_UNUSED_FP = false;
     private static final String FP_DATA_DIR = "fpdata";
     private static final int MSG_USER_SWITCHING = 10;
