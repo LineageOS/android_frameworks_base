1a65290e4a49c743a190a75195820c90b4889ba9
PMS22 <prathams99@rediff.com>
2016-12-15 13:18:00 +0000

    EasterEgg: We like cats
    - Logically no one names their cats giving a #number
    - Random func is used to generate a random name for cats from 30 given names
    - Nmes taken form wikipedia
    - Thanks to @The-Pyrox and @xdevs23 for helping and fixes
    
    Change-Id: Id30c39f3897d1b385342392bf34904400fd279e2
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- packages/EasterEgg/src/com/android/egg/neko/Cat.java
+++ packages/EasterEgg/src/com/android/egg/neko/Cat.java
@@ -33,6 +33,7 @@ import com.android.internal.logging.MetricsLogger;
 
 public class Cat extends Drawable {
     public static final long[] PURR = {0, 40, 20, 40, 20, 40, 20, 40, 20, 40, 20, 40};
+    public static final String[] CAT_NAMES = null;
 
     private Random mNotSoRandom;
     private Bitmap mBitmap;
@@ -139,11 +140,16 @@ public class Cat extends Drawable {
     }
 
     public Cat(Context context, long seed) {
+        if(CAT_NAMES == null)
+            CAT_NAMES = context.getResources().getStringArray(R.array.cat_names);
+        final Random rand;
+        final int i;
         D = new CatParts(context);
         mSeed = seed;
-
+        rand = new Random();
+        i = rand.nextInt(CAT_NAMES.length);
         setName(context.getString(R.string.default_cat_name,
-                String.valueOf(mSeed % 1000)));
+                String.valueOf(CAT_NAMES[i])));
 
         final Random nsr = notSoRandom(seed);
 
