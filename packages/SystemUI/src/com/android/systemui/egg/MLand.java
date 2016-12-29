9395d02b1769419fd875143ced9ec7ae823b4c97
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-12-29 18:21:23 +0800

    Revert "Add CM PlatLogo (1/2)"
    As part of the rebrand to LineageOS.
    
    This reverts commit aed50cbe3c3dbe175dac695764df586d8dda14c9.
    
    Change-Id: I7ee6125f6c8d4ec8e57e5a1673bf2e095a11cb76

--- packages/SystemUI/src/com/android/systemui/egg/MLand.java
+++ packages/SystemUI/src/com/android/systemui/egg/MLand.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2015 The Android Open Source Project
- * Copyright (C) 2014-2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -210,8 +209,6 @@ public class MLand extends FrameLayout {
 
         // we assume everything will be laid out left|top
         setLayoutDirection(LAYOUT_DIRECTION_LTR);
-
-        Player.eggPlayer = getEggPlayer();
 
         setupPlayers(DEFAULT_PLAYERS);
 
@@ -1010,7 +1007,7 @@ public class MLand extends FrameLayout {
         public void step(long t_ms, long dt_ms, float t, float dt);
     }
 
-    protected static class Player extends ImageView implements GameView {
+    private static class Player extends ImageView implements GameView {
         public float dv;
         public int color;
         private MLand mLand;
@@ -1019,8 +1016,6 @@ public class MLand extends FrameLayout {
         private boolean mAlive;
         private int mScore;
         private TextView mScoreField;
-
-        protected static int eggPlayer;
 
         private final int[] sColors = new int[] {
                 //0xFF78C557,
@@ -1093,7 +1088,7 @@ public class MLand extends FrameLayout {
         public Player(Context context) {
             super(context);
 
-            setBackgroundResource(eggPlayer);
+            setBackgroundResource(R.drawable.android);
             getBackground().setTintMode(PorterDuff.Mode.SRC_ATOP);
             color = sColors[(sNextColor++%sColors.length)];
             getBackground().setTint(color);
@@ -1442,9 +1437,5 @@ public class MLand extends FrameLayout {
             w = h = irand(PARAMS.STAR_SIZE_MIN, PARAMS.STAR_SIZE_MAX);
             v = z = 0;
         }
-    }
-
-    protected int getEggPlayer() {
-        return R.drawable.android;
     }
 }
