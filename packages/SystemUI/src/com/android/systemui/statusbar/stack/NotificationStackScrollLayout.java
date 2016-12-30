5f6c8e1facee6a4d505ee454982bb63ec0f755c6
dongwan0605.kim <dongwan0605.kim@lge.com>
2016-12-30 15:46:50 +0900

    Fix wrong peek height of the notification panel
    From N, the topmost notification slides in expanded from the top, not collapsed.
    (https://android.googlesource.com/platform/frameworks/base/+/d1ad9ab3121cb54387c5d2b71b48708dcaed5c43)
    But when peek from status bar, the peek height was measured with collapsed height.
    Therefore fix the peek height with expanded height of the topmost notification.
    
    Test: manual - Test peeking from status bar with expandable topmost notification.
    Change-Id: I46173c9e82a1221b543133c38ec6aaf746244011
    (cherry picked from commit c2ed273ca592e008917e74ad1f3ce9c9ccddd041)
    (cherry picked from commit 09b798ab2041d792056a64562f73c8b11997b42e)

--- packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
@@ -768,7 +768,7 @@ public class NotificationStackScrollLayout extends ViewGroup
 
     public int getFirstItemMinHeight() {
         final ExpandableView firstChild = getFirstChildNotGone();
-        return firstChild != null ? firstChild.getMinHeight() : mCollapsedSize;
+        return firstChild != null ? firstChild.getIntrinsicHeight() : mCollapsedSize;
     }
 
     public int getBottomStackPeekSize() {
@@ -2210,10 +2210,7 @@ public class NotificationStackScrollLayout extends ViewGroup
     }
 
     public int getPeekHeight() {
-        final ExpandableView firstChild = getFirstChildNotGone();
-        final int firstChildMinHeight = firstChild != null ? firstChild.getCollapsedHeight()
-                : mCollapsedSize;
-        return mIntrinsicPadding + firstChildMinHeight + mBottomStackPeekSize
+        return mIntrinsicPadding + getFirstItemMinHeight() + mBottomStackPeekSize
                 + mBottomStackSlowDownHeight;
     }
 
