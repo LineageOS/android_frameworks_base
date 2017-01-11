984a36851ffc5a0bdd102c87a7874ecbce01f0f3
Sam Mortimer <sam@mortimer.me.uk>
2017-01-10 22:21:45 -0800

    Reset all package signatures on boot
    Can be used to make a special build that resets all
    package signatures without wiping any data.
    
    Change-Id: Iff65f2ed3c9d0f80be7221ff90803eda4732cac0

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -369,6 +369,8 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
 
     static final boolean HIDE_EPHEMERAL_APIS = false;
 
+    private static final boolean RESET_ALL_PACKAGE_SIGNATURES_ON_BOOT = true;
+
     static final String PRECOMPILE_LAYOUTS = "pm.precompile_layouts";
 
     private static final int RADIO_UID = Process.PHONE_UID;
@@ -706,6 +708,7 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
     final ProtectedPackages mProtectedPackages;
 
     private boolean mFirstBoot;
+    boolean mResetSignatures;
 
     final boolean mIsEngBuild;
     private final boolean mIsUserDebugBuild;
@@ -2343,12 +2346,16 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
             mShouldStopSystemPackagesByDefault = mContext.getResources()
                     .getBoolean(R.bool.config_stopSystemPackagesByDefault);
 
+            mResetSignatures = RESET_ALL_PACKAGE_SIGNATURES_ON_BOOT;
+
             final int[] userIds = mUserManager.getUserIds();
             PackageParser2 packageParser = mInjector.getScanningCachingPackageParser();
             mOverlayConfig = mInitAppsHelper.initSystemApps(packageParser, packageSettings, userIds,
                     startTime);
             mInitAppsHelper.initNonSystemApps(packageParser, userIds, startTime);
             packageParser.close();
+
+            mResetSignatures = false;
 
             mRequiredVerifierPackages = getRequiredButNotReallyRequiredVerifiersLPr(computer);
             mRequiredInstallerPackage = getRequiredInstallerLPr(computer);
