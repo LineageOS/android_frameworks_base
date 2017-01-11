0072711c4d2585fa6142cd87b2b043f29d15d85b
Sam Mortimer <sam@mortimer.me.uk>
2017-01-10 22:21:45 -0800

    [DO NOT SUBMIT]Reset all package signatures on boot
    Can be used to make a special build that resets all
    package signatures without wiping any data.
    
    Change-Id: Iff65f2ed3c9d0f80be7221ff90803eda4732cac0

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -375,6 +375,8 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     static final boolean CLEAR_RUNTIME_PERMISSIONS_ON_UPGRADE = false;
 
+    private static final boolean RESET_ALL_PACKAGE_SIGNATURES_ON_BOOT = true;
+
     private static final boolean DISABLE_EPHEMERAL_APPS = false;
     private static final boolean HIDE_EPHEMERAL_APIS = true;
 
@@ -650,6 +652,8 @@ public class PackageManagerService extends IPackageManager.Stub {
     final ProtectedPackages mProtectedPackages;
 
     boolean mFirstBoot;
+
+    private boolean mResetSignatures;
 
     // System configuration read by SystemConfig.
     final int[] mGlobalGids;
@@ -2335,6 +2339,8 @@ public class PackageManagerService extends IPackageManager.Stub {
                 }
             }
 
+            mResetSignatures = RESET_ALL_PACKAGE_SIGNATURES_ON_BOOT;
+
             // Collect vendor overlay packages.
             // (Do this before scanning any apps.)
             // For security and version matching reason, only consider
@@ -2574,6 +2580,8 @@ public class PackageManagerService extends IPackageManager.Stub {
                 }
             }
             mExpectingBetter.clear();
+
+            mResetSignatures = false;
 
             // Resolve the storage manager.
             mStorageManagerPackage = getStorageManagerPackageName();
@@ -8479,7 +8487,13 @@ public class PackageManagerService extends IPackageManager.Stub {
 
             pkg.applicationInfo.uid = pkgSetting.appId;
             pkg.mExtras = pkgSetting;
-            if (shouldCheckUpgradeKeySetLP(pkgSetting, scanFlags)) {
+            if (mResetSignatures) {
+                Slog.d(TAG, "resetting signatures on package " + pkg.packageName);
+                pkgSetting.signatures.mSignatures = pkg.mSignatures;
+                if (pkgSetting.sharedUser != null) {
+                    pkgSetting.sharedUser.signatures.mSignatures = pkg.mSignatures;
+                }
+            } else if (shouldCheckUpgradeKeySetLP(pkgSetting, scanFlags)) {
                 if (checkUpgradeKeySetLP(pkgSetting, pkg)) {
                     // We just determined the app is signed correctly, so bring
                     // over the latest parsed certs.
