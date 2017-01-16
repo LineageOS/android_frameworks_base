b13b6c6544ea7009cd6f79c662f1a7fa250ac6e8
Arne Coucheron <arco68@gmail.com>
2017-01-16 20:37:50 +0100

    Revert "SystemUI: Fix for display datatype icon while in roaming"
    This reverts commit bc63ed800dfad538e5f55b6d8c9f28fa6eb33b51.
    
    Change-Id: I26f9c9af42fc6918e63c105fdc00ace9d30d5acc

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -113,9 +113,6 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
                 : R.drawable.ic_qs_signal_no_signal;
         state.icon = ResourceIcon.get(iconId);
         state.isOverlayIconWide = cb.isDataTypeIconWide;
-        state.isShowRoaming = (!(cb.noSim || !cb.enabled || cb.airplaneModeEnabled)
-                && cb.mobileSignalIconId > 0);
-        state.subId = cb.subId;
         state.autoMirrorDrawable = !cb.noSim;
         state.overlayIconId = cb.enabled && (cb.dataTypeIconId > 0) ? cb.dataTypeIconId : 0;
         state.filter = iconId != R.drawable.ic_qs_no_sim;
@@ -186,7 +183,6 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
         String enabledDesc;
         boolean noSim;
         boolean isDataTypeIconWide;
-        int subId;
     }
 
     private final class CellSignalCallback extends SignalCallbackAdapter {
@@ -216,7 +212,6 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
             mInfo.activityOut = activityOut;
             mInfo.enabledDesc = description;
             mInfo.isDataTypeIconWide = qsType != 0 && isWide;
-            mInfo.subId = subId;
             refreshState(mInfo);
         }
 
