e37944cf379bf5caf033a10deb94810a1c10353d
Arne Coucheron <arco68@gmail.com>
2017-01-17 01:16:42 +0100

    Revert "IMS: Show VoWIFI icon based on IMS capabilities."
    This reverts commit 4404f4a28f851a444c8ce0b28b33d9b17237ece6.
    
    Change-Id: Ie608fce50e9e9f5f1587ca870c4a75d9f37518a1

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/CallbackHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/CallbackHandler.java
@@ -148,8 +148,7 @@ public class CallbackHandler extends Handler implements EmergencyListener, Signa
             final boolean activityOut, final int dataActivityId, final int mobileActivityId,
             final int stackedDataIcon, final int stackedVoiceIcon,
             final String typeContentDescription, final String description, final boolean isWide,
-            final int subId, final int dataNetworkTypeInRoamingId, final int embmsIconId,
-            final int imsIconId, final boolean isImsOverWifi) {
+            final int subId, final int dataNetworkTypeInRoamingId, final int embmsIconId) {
         post(new Runnable() {
             @Override
             public void run() {
@@ -160,7 +159,7 @@ public class CallbackHandler extends Handler implements EmergencyListener, Signa
                                 activityOut, dataActivityId, mobileActivityId,
                                 stackedDataIcon, stackedVoiceIcon, typeContentDescription,
                                 description, isWide, subId, dataNetworkTypeInRoamingId,
-                                embmsIconId, imsIconId, isImsOverWifi);
+                                embmsIconId);
                     } else {
                         signalCluster.setMobileDataIndicators(statusIcon, qsIcon, statusType,
                                 qsType, activityIn, activityOut, dataActivityId,
