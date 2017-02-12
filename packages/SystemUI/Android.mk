eedd1837f5e8f694f7b546f62c657efb9a09d2d3
Ethan Chen <intervigil@gmail.com>
2017-02-12 14:03:42 -0800

    Revert "SystemUI: Use mContext.getDrawable for battery icon"
    This reverts commit a9dd79baefc9a1a3354411ec6544410bb1bf112a.
    
    Revert "SystemUI: Add VectorDrawable battery styles"
    
    * I actually dislike this new look a lot.
    
    This reverts commit 26d3f0b1ab0c54293d89495d79e1f75065ba9f81.
    
    Change-Id: If03e537e3256e4a6157cdb7bebc013deaa8f520e

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -31,8 +31,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4 \
     framework-protos \
     SystemUI-proto-tags \
-    org.cyanogenmod.platform.internal \
-    uicommon
+    org.cyanogenmod.platform.internal
 
 LOCAL_JAVA_LIBRARIES := telephony-common
 LOCAL_FULL_LIBS_MANIFEST_FILES := $(LOCAL_PATH)/AndroidManifest_cm.xml
