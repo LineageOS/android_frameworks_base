6b0f629cce6ebbbf81ab6a7a079450361bd5b07c
Gabriele M <moto.falcon.git@gmail.com>
2017-03-13 22:49:18 +0100

    SystemUI: Dismiss keyguard on boot if disabled by current profile
    Properly dismiss keyguard on boot if the current profile has it
    disabled, otherwise we will just show a black screen with just the
    status bar and a navbar with only the back button.
    
    BUGBASH-87
    
    Change-Id: I14ed10ae9db40c32be22c1fb43f8a1719787d91b

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -2249,6 +2249,8 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
         if (mLockPatternUtils.isLockScreenDisabled(KeyguardUpdateMonitor.getCurrentUser())
                 && !lockedOrMissing && !forceShow) {
             if (DEBUG) Log.d(TAG, "doKeyguard: not showing because lockscreen is off");
+            setShowingLocked(false, mAodShowing);
+            hideLocked();
             return;
         }
 
