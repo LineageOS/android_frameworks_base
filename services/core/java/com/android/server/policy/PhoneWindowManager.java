85a5b6e3a17e1e11dd31459e9baac8846164ce02
Christopher R. Palmer <crpalmer@gmail.com>
2017-04-10 14:35:44 +0300

    base: Fix proximity check on power key To test, enable "Settings >> Display >> Prevent accidental wakeup", hold your hand over the proximity sensor and turn your phone on.
    Prior to this change, it will wake up the phone and after the change
    it will not.
    
    Change-Id: I0d8643878f3d88774e0b9a97d998011a5c1fb7c5

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -7267,7 +7267,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private void wakeUpFromPowerKey(long eventTime) {
-        wakeUp(eventTime, mAllowTheaterModeWakeFromPowerKey, "android.policy:POWER");
+        wakeUp(eventTime, mAllowTheaterModeWakeFromPowerKey, "android.policy:POWER", true);
     }
 
     private boolean wakeUp(long wakeTime, boolean wakeInTheaterMode, String reason) {
