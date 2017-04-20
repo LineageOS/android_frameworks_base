31b97db018cd5a66a8e725341d45e91502270508
Luca Stefani <luca020400@lineageos.org>
2017-04-20 17:43:20 +0200

    Revert "SystemUI: [RJIL new function] AutoBrightness"
    This reverts commit cc5b481c5af50ee4751cef295ebd64751b3b0a56.
    
    Change-Id: I857febde70bf58b1fbafae81abece2e6340814d1

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -27,7 +27,6 @@ import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import com.android.internal.logging.MetricsLogger;
@@ -101,8 +100,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
 
         mBrightnessController = new BrightnessController(getContext(),
                 (ImageView) findViewById(R.id.brightness_icon),
-                (ToggleSlider) findViewById(R.id.brightness_slider),
-                (CheckBox) findViewById(R.id.brightness_auto));
+                (ToggleSlider) findViewById(R.id.brightness_slider));
 
     }
 
@@ -111,9 +109,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
                 R.layout.qs_paged_tile_layout, this, false);
         mTileLayout.setListening(mListening);
         addView((View) mTileLayout);
-        if (getResources().getBoolean(R.bool.config_show_auto_brightness)) {
-            ((CheckBox) findViewById(R.id.brightness_auto)).setVisibility(View.VISIBLE);
-        }
     }
 
     public boolean isShowingCustomize() {
