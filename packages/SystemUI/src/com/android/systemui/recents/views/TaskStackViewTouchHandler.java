3ef6327fc28dfc5397cd54a4dd5f2e74fba1c141
Anas Karbila <anaskarbila@gmail.com>
2017-05-01 02:20:07 +0900

    SystemUI: Squashed support for recents task locking
    Author: Anas Karbila <anaskarbila@gmail.com>
    Date:   Mon May 1 02:20:07 2017 +0900
    
        SystemUI: Support recents task locking
    
        This adds the ability to lock task in recents so they won't be cleared when removing all tasks
    
        Ticket: NOUGAT-1
    
        Drawables from material.io
    
        Change-Id: I06ffe411e72d4e837cfec7b9b77fb2519e417bc7
    
    Author: Joey Rizzoli <joey@lineageos.org>
    Date:   Sat Jun 3 17:00:43 2017 +0200
    
        SystemUI: animate icon when locking a task
    
        Change-Id: Ic153cb3707a14dba96d8889b11f149a339fb55e0
        Signed-off-by: Joey Rizzoli <joey@lineageos.org>
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Tue Jun 20 01:28:27 2017 +0200
    
        SystemUI: Fix task lock & freeform buttons placement
    
        * Get rid of hardcoded margin
        * Use proper gravity flags for task lock button
    
        Change-Id: Ib864b23f405ad34a56c37e2d9b9d0d391cf4dc8e
    
    Author: thecrazyskull <anaskarbila@gmail.com>
    Date:   Sat Sep 9 17:03:18 2017 +0200
    
        SystemUI: locked tasks should have a valid view
    
        If the view doesn't exist, the card won't be able to receive touch
        events. We need those touch events to have a bounce-back animation
        when trying to swipe away a locked card.
    
        Change-Id: If7b3c5e5094d2752926eb306dd54264da0ceaa3e
    
    Change-Id: I882f741ba518090202203e4326a5cf7205ebc098

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskStackViewTouchHandler.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskStackViewTouchHandler.java
@@ -439,7 +439,8 @@ class TaskStackViewTouchHandler implements SwipeHelper.Callback {
     @Override
     public View getChildAtPosition(MotionEvent ev) {
         TaskView tv = findViewAtPoint((int) ev.getX(), (int) ev.getY());
-        if (tv != null && canChildBeDismissed(tv)) {
+        if (tv != null && (canChildBeDismissed(tv)
+                || Recents.sLockedTasks.contains(tv.getTask()))) {
             return tv;
         }
         return null;
@@ -450,8 +451,8 @@ class TaskStackViewTouchHandler implements SwipeHelper.Callback {
         // Disallow dismissing an already dismissed task
         TaskView tv = (TaskView) v;
         Task task = tv.getTask();
-        return !mSwipeHelperAnimations.containsKey(v) &&
-                (mSv.getStack().indexOfStackTask(task) != -1);
+        return !mSwipeHelperAnimations.containsKey(v) && !Recents.sLockedTasks.contains(task)
+                && (mSv.getStack().indexOfStackTask(task) != -1);
     }
 
     /**
