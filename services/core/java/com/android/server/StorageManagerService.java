6531b2e089d10ca6b6d69eae4e5f5b4cb5f5efe1
AnilKumar Chimata <anilc@codeaurora.org>
2017-05-18 12:35:20 -0700

    frameworks: base: Port password retention feature
    Port password retention feature for HW FDE.
    
    This patch also include these changes:
       - Fix clearing of retained password
       - keyguard: Fix password doesnot sanitize after verification
       - LockSettings: fix the get password issue during boot up
       - frameworks/base: Fix pattern update issue
    
    Change-Id: I2def56b14c10229b72feccd1c97b281cad65f282

--- services/core/java/com/android/server/StorageManagerService.java
+++ services/core/java/com/android/server/StorageManagerService.java
@@ -111,6 +111,7 @@ import com.android.server.NativeDaemonConnector.SensitiveArg;
 import com.android.server.pm.PackageManagerException;
 import com.android.server.pm.PackageManagerService;
 import com.android.server.storage.AppFuseBridge;
+import com.android.internal.widget.ILockSettings;
 
 import libcore.io.IoUtils;
 import libcore.util.EmptyArray;
@@ -2700,9 +2701,23 @@ class StorageManagerService extends IStorageManager.Stub
             Slog.i(TAG, "changing encryption password...");
         }
 
+        ILockSettings lockSettings = ILockSettings.Stub.asInterface(
+                        ServiceManager.getService("lock_settings"));
+        String currentPassword="default_password";
+        try {
+            currentPassword = lockSettings.getPassword();
+        } catch (RemoteException e) {
+            Slog.e(TAG, "Couldn't get password" + e);
+        }
+
         try {
             NativeDaemonEvent event = mCryptConnector.execute("cryptfs", "changepw", CRYPTO_TYPES[type],
-                        new SensitiveArg(password));
+                        new SensitiveArg(currentPassword), new SensitiveArg(password));
+            try {
+                lockSettings.sanitizePassword();
+            } catch (RemoteException e) {
+                Slog.e(TAG, "Couldn't sanitize password" + e);
+            }
             return Integer.parseInt(event.getMessage());
         } catch (NativeDaemonConnectorException e) {
             // Encryption failed
