091ee968599061feb8842500cb2d3a8bd44a8643
Danny Baumann <dannybaumann@web.de>
2017-05-18 14:00:39 +0200

    Add support for page theme color to WebView and WebChromeClient.
    Will only be implemented by system WebView, so users will have to call
    the isThemeColorSupported() method in order to check whether the current
    WebView implementation supports this.
    
    Change-Id: Iaf4c3b8e4b8b7d8a74303b32d1c17c836dc744da

--- core/java/android/webkit/WebView.java
+++ core/java/android/webkit/WebView.java
@@ -24,6 +24,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Picture;
 import android.graphics.Rect;
@@ -58,6 +59,8 @@ import android.widget.AbsoluteLayout;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
@@ -337,6 +340,8 @@ public class WebView extends AbsoluteLayout
     // build target is JB MR2 or newer. Defaults to false, and is
     // set in the WebView constructor.
     private static volatile boolean sEnforceThreadChecking = false;
+
+    private Method mGetThemeColorMethod;
 
     /**
      *  Transportation object for returning WebView across thread boundaries.
@@ -633,6 +638,13 @@ public class WebView extends AbsoluteLayout
         checkThread();
 
         ensureProviderCreated();
+
+        try {
+            mGetThemeColorMethod = mProvider.getClass().getMethod("getThemeColor");
+        } catch (Exception e) {
+            // ignored, no theme color support
+        }
+
         mProvider.init(javaScriptInterfaces, privateBrowsing);
         // Post condition of creating a webview is the CookieSyncManager.getInstance() is allowed.
         CookieSyncManager.setGetInstanceIsAllowed();
@@ -1485,6 +1497,37 @@ public class WebView extends AbsoluteLayout
     public int getProgress() {
         checkThread();
         return mProvider.getProgress();
+    }
+
+    /**
+     * Checks whether the WebView implementation has support for fetching
+     * the theme color set by the page.
+     *
+     * @return true if the WebView supports the getThemeColor() method
+     * @hide
+     */
+    public boolean isThemeColorSupported() {
+        return mGetThemeColorMethod != null;
+    }
+
+    /**
+     * Gets the theme color set by the page.
+     *
+     * The returned color may not be fully opaque. If the page didn't set
+     * any theme color, Color.TRANSPARENT is returned.
+     *
+     * @return theme color set by the page
+     * @hide
+     */
+    public int getThemeColor() {
+        if (mGetThemeColorMethod != null) {
+            try {
+                return (Integer) mGetThemeColorMethod.invoke(mProvider);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                // ignored, fall back to returning transparent
+            }
+        }
+        return Color.TRANSPARENT;
     }
 
     /**
