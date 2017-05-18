545dd7014fd5a1af35769c1a814994644677a286
AnilKumar Chimata <anilc@codeaurora.org>
2017-05-18 12:35:20 -0700

    frameworks: base: Port password retention feature
    Port password retention feature for HW FDE.
    
    This patch also include these changes:
       - Fix clearing of retained password
       - keyguard: Fix password doesnot sanitize after verification
       - LockSettings: fix the get password issue during boot up
       - frameworks/base: Fix pattern update issue
    
    CRs-Fixed: 2210986
    Change-Id: I2def56b14c10229b72feccd1c97b281cad65f282

--- services/core/java/com/android/server/StorageManagerService.java
+++ services/core/java/com/android/server/StorageManagerService.java
@@ -125,6 +125,7 @@ import com.android.internal.util.Preconditions;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.server.pm.PackageManagerService;
 import com.android.server.storage.AppFuseBridge;
+import com.android.internal.widget.ILockSettings;
 
 import libcore.io.IoUtils;
 import libcore.util.EmptyArray;
@@ -2309,8 +2310,22 @@ class StorageManagerService extends IStorageManager.Stub
             Slog.i(TAG, "changing encryption password...");
         }
 
+        ILockSettings lockSettings = ILockSettings.Stub.asInterface(
+                        ServiceManager.getService("lock_settings"));
+        String currentPassword="default_password";
         try {
-            mVold.fdeChangePassword(type, password);
+            currentPassword = lockSettings.getPassword();
+        } catch (Exception e) {
+            Slog.wtf(TAG, "Couldn't get password" + e);
+        }
+
+        try {
+            mVold.fdeChangePassword(type, currentPassword, password);
+            try {
+                lockSettings.sanitizePassword();
+            } catch (Exception e) {
+                Slog.wtf(TAG, "Couldn't sanitize password" + e);
+            }
             return 0;
         } catch (Exception e) {
             Slog.wtf(TAG, e);
