6d48756919db9b238e891f8ad91af63a434d9342
Alberto97 <albertop2197@gmail.com>
2017-05-29 21:07:05 +0200

    SystemUI: Specify user while changing brightness mode
    Allow any user changing brightness mode,
    without this only the main one is able to do that.
    
    BUGBASH-598
    
    Change-Id: Ib51e9a45c42837cabb1cfb0d48334165b023a827

--- packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
@@ -300,10 +300,11 @@ public class BrightnessController implements ToggleSlider.Listener {
         mIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.putIntForUser(mContext.getContentResolver(),
                     Settings.System.SCREEN_BRIGHTNESS_MODE,
                     !mAutomatic ? Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC
-                    : Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+                    : Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL,
+                    UserHandle.USER_CURRENT);
             }
         });
     }
