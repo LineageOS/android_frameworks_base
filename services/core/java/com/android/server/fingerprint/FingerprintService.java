7670a03b999168bd63cdc20ab0c4cb24a22095d1
Ícaro Hoff <icarohoff@gmail.com>
2017-06-04 12:17:51 -0300

    fingerprint: handle PerformanceStats NULL pointers
    bcc100a has added support for this feature, but the NULL checks
    weren't complete, only "handleAcquired" was checking if PerformanceStats
    was supported before calling the API.
    Extend the NULL checks to "handleAuthenticated" and "handleFailedAttempt"
    calls as well.
    
    Change-Id: I3cc9b35ea6b81dc0c503b9feab940873b344323e
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- services/core/java/com/android/server/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/fingerprint/FingerprintService.java
@@ -460,10 +460,12 @@ public class FingerprintService extends SystemService implements IHwBinder.Death
         if (client != null && client.onAuthenticated(fingerId, groupId)) {
             removeClient(client);
         }
-        if (fingerId != 0) {
-            mPerformanceStats.accept++;
-        } else {
-            mPerformanceStats.reject++;
+        if (mPerformanceStats != null) {
+            if (fingerId != 0) {
+                mPerformanceStats.accept++;
+            } else {
+                mPerformanceStats.reject++;
+            }
         }
     }
 
@@ -895,10 +897,12 @@ public class FingerprintService extends SystemService implements IHwBinder.Death
                 mFailedAttempts.put(currentUser, mFailedAttempts.get(currentUser, 0) + 1);
                 mTimedLockoutCleared.put(ActivityManager.getCurrentUser(), false);
                 final int lockoutMode = getLockoutMode();
-                if (lockoutMode == AuthenticationClient.LOCKOUT_PERMANENT) {
-                    mPerformanceStats.permanentLockout++;
-                } else if (lockoutMode == AuthenticationClient.LOCKOUT_TIMED) {
-                    mPerformanceStats.lockout++;
+                if (mPerformanceStats != null) {
+                    if (lockoutMode == AuthenticationClient.LOCKOUT_PERMANENT) {
+                        mPerformanceStats.permanentLockout++;
+                    } else if (lockoutMode == AuthenticationClient.LOCKOUT_TIMED) {
+                        mPerformanceStats.lockout++;
+                    }
                 }
 
                 // Failing multiple times will continue to push out the lockout time
