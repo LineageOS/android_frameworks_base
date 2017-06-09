ef214d126450e32372f1387c5cd9f19d681d65f6
Mohan Thadikamalla <mohant@nvidia.com>
2017-06-09 14:03:16 +0530

    frameworks/base: Wifi application property support
    feature: NvPropSupport
    Bug 200316146
    
    In order to optimize the wifi scan algorithms, wpa_supplicant
    needs to know the state of the applications. So add wifi
    application property WiFi Manager.
    
    Bug 1400711
    
    Change-Id: I8d434e1b9fe785b4a619d1faafe0482d89a334fe
    Reviewed-on: http://git-master/r/1212008
    (cherry picked from commit cb26f117631173976e22d66fe7e94567d077fe73)
    Reviewed-on: http://git-psac/r/10412
    Reviewed-by: Mohan Thadikamalla <mohant@nvidia.com>
    Tested-by: Mohan Thadikamalla <mohant@nvidia.com>
    Reviewed-by: Srinivas Ramachandran <srinivasra@nvidia.com>
    Reviewed-by: David Pearce <dpearce@nvidia.com>
    Tested-by: David Pearce <dpearce@nvidia.com>
    Reviewed-on: http://git-psac.nvidia.com/r/13451
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bibhay Ranjan <bibhayr@nvidia.com>
    Tested-by: Bibhay Ranjan <bibhayr@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1552641
    Reviewed-by: Niranjan Artal <nartal@nvidia.com>
    Tested-by: Niranjan Artal <nartal@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1585021

--- wifi/java/android/net/wifi/WifiManager.java
+++ wifi/java/android/net/wifi/WifiManager.java
@@ -4850,6 +4850,42 @@ public class WifiManager {
             mService.stopDppSession();
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
+
+    /**
+     * Class to access wifi application properties
+     * @hide
+     */
+    public class WifiAppPropertyManager {
+        private final IBinder mBinder;
+
+        private WifiAppPropertyManager() {
+            mBinder = new Binder();
+        }
+
+        public String getProperty(String key) {
+            try {
+                return mService.getAppProperty(key);
+            } catch (RemoteException e) {
+                return null;
+            }
+        }
+
+        public int setProperty(String key, String value) {
+            try {
+                return mService.setAppProperty(key, value);
+            } catch (RemoteException e) {
+                return -1;
+            }
+        }
+
+        public int setPropertyOnDeath(String key, String value) {
+            synchronized (mBinder) {
+                try {
+                    return mService.setAppPropertyOnDeath(mBinder, key, value);
+                } catch (RemoteException e) {
+                    return -1;
+                }
+            }
         }
     }
 
@@ -5010,5 +5046,12 @@ public class WifiManager {
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
         }
+
+    /**
+     * API to get wifi application property manager
+     * @hide
+     */
+    public WifiAppPropertyManager getWifiAppPropertyManager() {
+        return new WifiAppPropertyManager();
     }
 }
