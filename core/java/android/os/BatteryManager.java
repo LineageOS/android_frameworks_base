96d828ad695a22aede2d19d40339c01713470910
Erfan Abdi <erfangplus@gmail.com>
2017-06-09 05:06:31 +0430

    Battery: add Battery Moto Mod Support
    Change-Id: I1d7a7f7dafeed3afaee9584f1eea8ca9e9e29a15

--- core/java/android/os/BatteryManager.java
+++ core/java/android/os/BatteryManager.java
@@ -135,6 +135,11 @@ public class BatteryManager {
      */
     public static final String EXTRA_OEM_FAST_CHARGER = "oem_fast_charger";
 
+    public static final String EXTRA_MOD_LEVEL = "mod_level";
+    public static final String EXTRA_MOD_STATUS = "mod_status";
+    public static final String EXTRA_MOD_TYPE = "mod_type";
+    public static final String EXTRA_MOD_FLAG = "mod_flag";
+
     // values for "status" field in the ACTION_BATTERY_CHANGED Intent
     public static final int BATTERY_STATUS_UNKNOWN = Constants.BATTERY_STATUS_UNKNOWN;
     public static final int BATTERY_STATUS_CHARGING = Constants.BATTERY_STATUS_CHARGING;
