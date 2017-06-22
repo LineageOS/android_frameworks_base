a7963f5a90b02ef77e167391ae8182b348e3046e
Dan Pasanen <dan.pasanen@gmail.com>
2017-06-22 13:26:32 -0500

    [1/3] SystemUI: Custom quick settings tile columns
    Change-Id: I196b37ca684311c2bf1b3e33361a0fb6eafdee0c

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -36,6 +36,7 @@ import android.util.ArraySet;
 
 import com.android.internal.util.ArrayUtils;
 import com.android.systemui.DemoMode;
+import com.android.systemui.qs.QSPanel;
 import com.android.systemui.qs.QSTileHost;
 import com.android.systemui.settings.CurrentUserTracker;
 import com.android.systemui.statusbar.phone.ClockController;
@@ -73,6 +74,7 @@ public class TunerServiceImpl extends TunerService {
             ClockController.CLOCK_POSITION,
             NotificationPanelView.DOUBLE_TAP_SLEEP_GESTURE,
             NotificationPanelView.STATUS_BAR_QUICK_QS_PULLDOWN,
+            QSPanel.STATUS_BAR_QS_TILE_COLUMNS,
             QSTileHost.TILES_SETTING,
             Settings.Secure.DOZE_ALWAYS_ON,
             StatusBar.FORCE_SHOW_NAVBAR,
