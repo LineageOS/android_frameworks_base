d5abfd016b7a6e6e09f38cd04f37abec4db12dd2
Umashankar Godachi <umasha@codeaurora.org>
2017-06-27 15:35:39 +0530

    Use proper coded alphabet when a contact is written into EF_ADN of USIM.
    Based on the description in "3GPP TS 31.102 clause 4.4.2.3" shown below,
    Alpha Identifier of EF_ADN of USIM is defined in default 7-bit coded
    alphabet if bit8 is set to 0 otherwise, it is defined in UCS2 coded.
    
    Change-Id: Ifbe06e1dd1cf29692f1201ab27e71250de30b3df
    CRs-Fixed: 2219692

--- telephony/java/com/android/internal/telephony/uicc/IccUtils.java
+++ telephony/java/com/android/internal/telephony/uicc/IccUtils.java
@@ -22,9 +22,11 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.telephony.Rlog;
 
+import com.android.internal.telephony.EncodeException;
 import com.android.internal.telephony.GsmAlphabet;
 
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 
 /**
  * Various methods, useful for dealing with SIM data.
@@ -882,5 +884,31 @@ public class IccUtils {
             return (byte) (c - 0x57);
         }
         return 0;
+    }
+
+    static byte[]
+    stringToAdnStringField(String alphaTag) {
+        boolean isUcs2 = false;
+        try {
+           for(int i = 0; i < alphaTag.length(); i++) {
+               GsmAlphabet.countGsmSeptets(alphaTag.charAt(i), true);
+           }
+        } catch (EncodeException e) {
+            isUcs2 = true;
+        }
+        return stringToAdnStringField(alphaTag, isUcs2);
+    }
+
+    static byte[]
+    stringToAdnStringField(String alphaTag, boolean isUcs2) {
+        if (!isUcs2) {
+            return GsmAlphabet.stringToGsm8BitPacked(alphaTag);
+        }
+        byte[] alphaTagBytes = alphaTag.getBytes(Charset.forName("UTF-16BE"));
+        byte[] ret = new byte[1 + alphaTagBytes.length];
+        ret[0] = (byte)0x80;
+        System.arraycopy(alphaTagBytes, 0, ret, 1, alphaTagBytes.length);
+
+        return ret;
     }
 }
