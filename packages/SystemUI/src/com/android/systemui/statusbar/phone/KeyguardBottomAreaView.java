83e34b2cab502fb230c2bbafbe42339a70392486
Tony Mak <tonymak@google.com>
2017-07-28 11:53:56 +0100

    Enforce policy for camera gesture in keyguard
    Test:
    1. Set lock screen, set keyguard policy. Lock the device.
       Observe that double tap is not showing camera
    
    2. Set lock screen, unset the keyguard policy. Lock the device.
       Observe that double tap is showing camera
    
    3. Unset lock screen (swipe), set the keyguard policy. Lock the device.
       Observe that double tap is showing camera.
    
    4. Unset lock screen (swipe), unset the keyguard policy. Lock the device.
       Observe that double tap is showing camera.
    
    Bug: 63787722
    Merged-In: I104688eaad719528376e2851f837d5956a6a1169
    
    Change-Id: I104688eaad719528376e2851f837d5956a6a1169
    (cherry picked from commit 65f02e8ba7a9f013d6971b3d6d1bd95f1785cb3d)
    CVE-2017-0822

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -419,7 +419,10 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
                 visible = !mShortcutHelper.isTargetEmpty(Shortcuts.RIGHT_SHORTCUT);
             } else {
                 ResolveInfo resolved = resolveCameraIntent();
-                visible = !isCameraDisabledByDpm() && resolved != null
+                boolean isCameraDisabled =
+                        (mPhoneStatusBar != null) && !mPhoneStatusBar.isCameraAllowedByAdmin();
+                visible = !isCameraDisabled
+                        && resolved != null
                         && getResources().getBoolean(R.bool.config_keyguardShowCameraAffordance);
             }
         }
@@ -459,24 +462,6 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         PackageManager pm = mContext.getPackageManager();
         return pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)
                 && pm.resolveActivity(PHONE_INTENT, 0) != null;
-    }
-
-    private boolean isCameraDisabledByDpm() {
-        final DevicePolicyManager dpm =
-                (DevicePolicyManager) getContext().getSystemService(Context.DEVICE_POLICY_SERVICE);
-        if (dpm != null && mPhoneStatusBar != null) {
-            try {
-                final int userId = ActivityManagerNative.getDefault().getCurrentUser().id;
-                final int disabledFlags = dpm.getKeyguardDisabledFeatures(null, userId);
-                final  boolean disabledBecauseKeyguardSecure =
-                        (disabledFlags & DevicePolicyManager.KEYGUARD_DISABLE_SECURE_CAMERA) != 0
-                                && mPhoneStatusBar.isKeyguardSecure();
-                return dpm.getCameraDisabled(null) || disabledBecauseKeyguardSecure;
-            } catch (RemoteException e) {
-                Log.e(TAG, "Can't get userId", e);
-            }
-        }
-        return false;
     }
 
     private void watchForCameraPolicyChanges() {
