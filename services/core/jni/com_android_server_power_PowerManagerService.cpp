9d7064591c362737315e4b01f18048dae367369e
Corinna Vinschen <xda@vinschen.de>
2017-08-12 13:42:58 +0200

    PowerManager: Allow to distinguish different keypresses
    * Use keypress info to exclude pressing volume keys from
      illuminating HW buttons in config_buttonLightOnKeypressOnly
      mode.
    
    Change-Id: I6bfc7ddd075e12e1ad10c3663a63e80c8d7f983d
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- services/core/jni/com_android_server_power_PowerManagerService.cpp
+++ services/core/jni/com_android_server_power_PowerManagerService.cpp
@@ -37,6 +37,7 @@
 #include <hardware/power.h>
 #include <hardware_legacy/power.h>
 #include <suspend/autosuspend.h>
+#include <android/keycodes.h>
 
 #include "com_android_server_power_PowerManagerService.h"
 
@@ -168,7 +169,8 @@ static void sendPowerHint(PowerHint hintId, uint32_t data) {
     }
 }
 
-void android_server_PowerManagerService_userActivity(nsecs_t eventTime, int32_t eventType) {
+void android_server_PowerManagerService_userActivity(nsecs_t eventTime, int32_t eventType,
+        int32_t keyCode) {
     if (gPowerManagerServiceObj) {
         // Throttle calls into user activity by event type.
         // We're a little conservative about argument checking here in case the caller
@@ -190,9 +192,14 @@ void android_server_PowerManagerService_userActivity(nsecs_t eventTime, int32_t 
 
         JNIEnv* env = AndroidRuntime::getJNIEnv();
 
+        int flags = 0;
+        if (keyCode == AKEYCODE_VOLUME_UP || keyCode == AKEYCODE_VOLUME_DOWN) {
+            flags |= USER_ACTIVITY_FLAG_NO_BUTTON_LIGHTS;
+        }
+
         env->CallVoidMethod(gPowerManagerServiceObj,
                 gPowerManagerServiceClassInfo.userActivityFromNative,
-                nanoseconds_to_milliseconds(eventTime), eventType, 0);
+                nanoseconds_to_milliseconds(eventTime), eventType, flags);
         checkAndClearExceptionFromCallback(env, "userActivityFromNative");
     }
 }
