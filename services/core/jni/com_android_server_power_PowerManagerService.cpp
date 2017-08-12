f9c98f61661db018a0ecdf1f6925112b7dd05953
Corinna Vinschen <xda@vinschen.de>
2017-08-12 13:42:58 +0200

    PowerManager: Allow to distinguish different keypresses
    * Use keypress info to exclude pressing volume keys from
      illuminating HW buttons in config_buttonLightOnKeypressOnly
      mode.
    
    Change-Id: I6bfc7ddd075e12e1ad10c3663a63e80c8d7f983d
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- services/core/jni/com_android_server_power_PowerManagerService.cpp
+++ services/core/jni/com_android_server_power_PowerManagerService.cpp
@@ -24,6 +24,7 @@
 #include <aidl/android/hardware/power/Mode.h>
 #include <aidl/android/system/suspend/ISystemSuspend.h>
 #include <aidl/android/system/suspend/IWakeLock.h>
+#include <android/keycodes.h>
 #include <android-base/chrono_utils.h>
 #include <android/binder_manager.h>
 #include <android/system/suspend/ISuspendControlService.h>
@@ -99,7 +100,8 @@ static bool setPowerMode(Mode mode, bool enabled) {
 }
 
 void android_server_PowerManagerService_userActivity(nsecs_t eventTime, int32_t eventType,
-                                                     ui::LogicalDisplayId displayId) {
+                                                     ui::LogicalDisplayId displayId,
+                                                     int32_t keyCode) {
     if (gPowerManagerServiceObj) {
         // Throttle calls into user activity by event type.
         // We're a little conservative about argument checking here in case the caller
@@ -121,9 +123,15 @@ void android_server_PowerManagerService_userActivity(nsecs_t eventTime, int32_t 
 
         JNIEnv* env = AndroidRuntime::getJNIEnv();
 
+        int flags = 0;
+        if (keyCode == AKEYCODE_VOLUME_UP || keyCode == AKEYCODE_VOLUME_DOWN) {
+            flags |= USER_ACTIVITY_FLAG_NO_BUTTON_LIGHTS;
+        }
+
         env->CallVoidMethod(gPowerManagerServiceObj,
                             gPowerManagerServiceClassInfo.userActivityFromNative,
-                            nanoseconds_to_milliseconds(eventTime), eventType, displayId.val(), 0);
+                            nanoseconds_to_milliseconds(eventTime), eventType, displayId.val(),
+                            flags);
         checkAndClearExceptionFromCallback(env, "userActivityFromNative");
     }
 }
