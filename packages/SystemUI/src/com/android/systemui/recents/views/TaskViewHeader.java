660b78d426c19f34d7548f4bc7515b8be5416458
Harry Youd <harry@harryyoud.co.uk>
2017-08-27 21:29:57 +0200

    Recents: Add accessibility descriptions for lock ImageViews
    Change-Id: I09ce342ce135017294f5a4197b7e52dded3fb17e

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskViewHeader.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskViewHeader.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -454,6 +455,21 @@ public class TaskViewHeader extends FrameLayout
      */
     public void setShouldDarkenBackgroundColor(boolean flag) {
         mShouldDarkenBackgroundColor = flag;
+    }
+
+    private void updateLockTaskDrawable() {
+        if (Recents.sLockedTasks.contains(mTask)) {
+            mLockTaskButton.setImageDrawable(mTask.useLightOnPrimaryColor ?
+                    mLightLockedDrawable : mDarkLockedDrawable);
+            mLockTaskButton.setContentDescription(
+                    getResources().getString(R.string.task_locked_desc, mTask.title));
+        } else {
+            mLockTaskButton.setImageDrawable(mTask.useLightOnPrimaryColor ?
+                    mLightUnlockedDrawable : mDarkUnlockedDrawable);
+            mLockTaskButton.setContentDescription(
+                    getResources().getString(R.string.task_unlocked_desc, mTask.title));
+        }
+        ((AnimatedVectorDrawable) mLockTaskButton.getDrawable()).start();
     }
 
     /**
