9547278d3ce0f2c410c36a13aefc0ee817110103
maxwen <max.weninger@gmail.com>
2017-09-05 20:36:26 +0200

    base: add CPU info overlay
    Change-Id: I3c75a9c2837c9293822a33a95380cc1edb7f2b1a
    
    Signed-off-by: xyyx <xyyx@mail.ru>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10323,6 +10323,11 @@ public final class Settings {
                 "storage_settings_clobber_threshold";
 
         /**
+         * @hide
+         */
+        public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
