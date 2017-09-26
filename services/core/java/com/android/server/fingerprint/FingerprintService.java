86c79bf4bf77e914806014eaad6abef242467a47
xyyx <xyyx@mail.ru>
2017-09-26 10:51:30 +0800

    Fingerprints: Increase MAX_FAILED_ATTEMPTS

--- services/core/java/com/android/server/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/fingerprint/FingerprintService.java
@@ -119,7 +119,7 @@ public class FingerprintService extends SystemService implements IHwBinder.Death
             Collections.synchronizedMap(new HashMap<>());
     private final AppOpsManager mAppOps;
     private static final long FAIL_LOCKOUT_TIMEOUT_MS = 30*1000;
-    private static final int MAX_FAILED_ATTEMPTS_LOCKOUT_TIMED = 5;
+    private static final int MAX_FAILED_ATTEMPTS_LOCKOUT_TIMED = 10;
     private static final int MAX_FAILED_ATTEMPTS_LOCKOUT_PERMANENT = 20;
 
     private static final long CANCEL_TIMEOUT_LIMIT = 3000; // max wait for onCancel() from HAL,in ms
