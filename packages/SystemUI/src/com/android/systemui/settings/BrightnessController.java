44907ceb7bba89d1a42ba9f3fde016de2699931f
Alberto97 <albertop2197@gmail.com>
2017-10-14 20:41:23 +0200

    SystemUI: Port brightness slider changes
     * Expose AutoBrightness button
     * Add tunables
     * Use vector drawables for brightness icons
    
    Author: Luca Stefani <luca020400@lineageos.org>
    Date:   Thu Apr 20 18:00:52 2017 +0200
    
        SystemUI: Use AOSP AutoBrightness implementation
    
        Change-Id: I3e3660065407d460f30c6e936d951d4d5eae30c6
    
    Author: Michael Bestas <mikeioannina@gmail.com>
    Date:   Thu Apr 27 14:57:49 2017 +0300
    
        SystemUI: Use already existing overlay to show QS auto brightness
    
        * No need for a new overlay when one exists for years
    
        Change-Id: I9e09a1770f8e645093e880cf1a48597405e0bcf2
    
    Author: Alberto97 <albertop2197@gmail.com>
    Date:   Mon May 29 21:07:05 2017 +0200
    
        SystemUI: Specify user while changing brightness mode
    
        Allow any user changing brightness mode,
        without this only the main one is able to do that.
    
        BUGBASH-598
    
        Change-Id: Ib51e9a45c42837cabb1cfb0d48334165b023a827
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sun Apr 30 13:45:39 2017 +0300
    
        SystemUI: Add tunable for auto brightness icon
    
        Change-Id: I02fe3f5500f95ba9369b952bbec2a073b7ae8316
    
    Author: Alexander Westphal <westphal.alex94@gmail.com>
    Date:   Wed Apr 26 09:20:53 2017 +0100
    
        SystemUI: use vector drawables for brightness icons
    
        Change-Id: I8d22f8de22040d68ed1bfab0da3a78aec717fa62
    
    Author: qjohn <townex22@gmail.com>
    Date:   Sun Jun 18 16:28:10 2017 +0700
    
        SystemUI: Remove unnecessary brightness icons
    
        Change-Id: I7e60ff73125bd0304f9ca14ad5c5a9ce11ffceda
    
    Author: Michael W <baddaemon87@gmail.com>
    Date:   Mon May 25 15:38:33 2020 +0200
    
        SystemUI: Brightness slider: Always show the brightness icon
    
        * Right now, we reserve the space for the icon but it's not displayed
          (reserved space is there so the slider doesn't jump)
        * Always display the icon (if automatic brightness is available)
          so it's more consistent
    
        Change-Id: I5238dc6aad51407349a8d42fae840ea76841dc3d
    
    Author: Michael W <baddaemon87@gmail.com>
    Date:   Fri May 29 20:09:18 2020 +0200
    
        SystemUI: Fix for auto brightness icon being shown when disabled
    
        * With commit 9d42c48c16bba2fcc836cbb68e2979ed9c0d1b23 the case of a
          manually disabled auto brightness icon got lost
        * Re-implement it by also respecting the Settings value instead of just
          the build time flag
    
        Change-Id: I991977be7e1f7c33947073dbd3ce7a800fb158bf
    
    Change-Id: Ic72b0084a4108fb4fa26e893bd6d1eaca96cbb6c

--- packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
@@ -40,6 +40,8 @@ import android.service.vr.IVrManager;
 import android.service.vr.IVrStateCallbacks;
 import android.util.Log;
 import android.util.MathUtils;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.android.internal.BrightnessSynchronizer;
 import com.android.internal.logging.MetricsLogger;
@@ -54,6 +56,7 @@ public class BrightnessController implements ToggleSlider.Listener {
     private static final String TAG = "StatusBar.BrightnessController";
     private static final int SLIDER_ANIMATION_DURATION = 3000;
 
+    private static final int MSG_UPDATE_ICON = 0;
     private static final int MSG_UPDATE_SLIDER = 1;
     private static final int MSG_SET_CHECKED = 2;
     private static final int MSG_ATTACH_LISTENER = 3;
@@ -76,6 +79,7 @@ public class BrightnessController implements ToggleSlider.Listener {
     private final float mMaximumBacklightForVr;
     private final float mDefaultBacklightForVr;
 
+    private final ImageView mIcon;
     private final Context mContext;
     private final ToggleSlider mControl;
     private final boolean mAutomaticAvailable;
@@ -223,8 +227,10 @@ public class BrightnessController implements ToggleSlider.Listener {
                         Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL,
                         UserHandle.USER_CURRENT);
                 mAutomatic = automatic != Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL;
+                mHandler.obtainMessage(MSG_UPDATE_ICON, mAutomatic ? 1 : 0).sendToTarget();
             } else {
                 mHandler.obtainMessage(MSG_SET_CHECKED, 0).sendToTarget();
+                mHandler.obtainMessage(MSG_UPDATE_ICON, 0 /* automatic */).sendToTarget();
             }
         }
     };
@@ -268,6 +274,9 @@ public class BrightnessController implements ToggleSlider.Listener {
             mExternalChange = true;
             try {
                 switch (msg.what) {
+                    case MSG_UPDATE_ICON:
+                        updateIcon(msg.arg1 != 0);
+                        break;
                     case MSG_UPDATE_SLIDER:
                         updateSlider(Float.intBitsToFloat(msg.arg1), msg.arg2 != 0);
                         break;
@@ -292,9 +301,10 @@ public class BrightnessController implements ToggleSlider.Listener {
         }
     };
 
-    public BrightnessController(Context context, ToggleSlider control,
+    public BrightnessController(Context context, ImageView icon, ToggleSlider control,
             BroadcastDispatcher broadcastDispatcher) {
         mContext = context;
+        mIcon = icon;
         mControl = control;
         mControl.setMax(GAMMA_SPACE_MAX);
         mBackgroundHandler = new Handler((Looper) Dependency.get(Dependency.BG_LOOPER));
@@ -327,6 +337,17 @@ public class BrightnessController implements ToggleSlider.Listener {
         mDisplayManager = context.getSystemService(DisplayManager.class);
         mVrManager = IVrManager.Stub.asInterface(ServiceManager.getService(
                 Context.VR_SERVICE));
+
+        mIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Settings.System.putIntForUser(mContext.getContentResolver(),
+                        Settings.System.SCREEN_BRIGHTNESS_MODE, mAutomatic ?
+                            Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL :
+                            Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC,
+                        UserHandle.USER_CURRENT);
+            }
+        });
     }
 
     public void addStateChangedCallback(BrightnessStateChangeCallback cb) {
@@ -355,6 +376,7 @@ public class BrightnessController implements ToggleSlider.Listener {
     @Override
     public void onChanged(ToggleSlider toggleSlider, boolean tracking, boolean automatic,
             int value, boolean stopTracking) {
+        updateIcon(mAutomatic);
         if (mExternalChange) return;
 
         if (mSliderAnimator != null) {
@@ -423,6 +445,14 @@ public class BrightnessController implements ToggleSlider.Listener {
 
     private void setBrightness(float brightness) {
         mDisplayManager.setTemporaryBrightness(brightness);
+    }
+
+    private void updateIcon(boolean automatic) {
+        if (mIcon != null) {
+            mIcon.setImageResource(mAutomatic ?
+                    com.android.systemui.R.drawable.ic_qs_brightness_auto_on :
+                    com.android.systemui.R.drawable.ic_qs_brightness_auto_off);
+        }
     }
 
     private void updateVrMode(boolean isEnabled) {
