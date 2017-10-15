8202906727d05a0afb067b47d70f85cdce75c494
Adrian DC <radian.dc@gmail.com>
2017-10-15 20:26:29 +0200

    frameworks/base: Lights notifications brightness support
    Implement the support of an overall brightness control for the LEDs.
    
    The setting is deactivated by default
    and will be ignored by the unimplemented phones.
    Current LibLights will simply not use the new variable.
    
    Changes includes :
      frameworks/base
      packages/Apps/Settings
    
    Change-Id: I1c0de01b1c6a2a2cf1432028a2e69f90b2373b2c
    Signed-off-by: AdrianDC <radian.dc@gmail.com>
    
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Mon, 9 Nov 2015 16:26:00 -0800
    fw: Move battery light settings to CMSettings.
    Change-Id: I28e60473356b2a9af152df82d34ad7abc9918682
    
    Author: Adrian DC <radian.dc@gmail.com>
    Date:   Sat Oct 14 23:08:47 2017 +0200
    fw: Rebrand to LineageOS and cleanup for Android Oreo
    Change-Id: I845f866891386aee808a4e7e80f4ab7c3ad48830
    
    Author: Gabriele M <moto.falcon.git@gmail.com>
    Date:   Mon, 31 Aug 2015 19:38:16 +0100
    Let liblights adjust the brightness of LEDs while previewing it
    Each device might use the information about the brightness of LEDs
    in a unique way. Currently the slider to adjust the brightness of
    LEDs is using an already scaled RGB color to preview the intensity
    of the light. Instead of doing this, use a white colored light and
    add to it the information about the brightness.
    Change-Id: I4dd35944debc744ea6c7f1fcc5dd7a909e8863fa
    
    Author: AdrianDC <radian.dc@gmail.com>
    Date:   Wed, 11 Nov 2015 21:06:22 +0100
    LEDs Brightness: Update the slider for M
     * Add notification details needed for M
     * Sync with some changes made in the Display Brightness dialog
     * Clean ic_settings indents with Settings ic_settings_24dp.xml
    Change-Id: I8b6ac1920704f43f4776cbd3bdfb3d0ed8d223dc
    Signed-off-by: AdrianDC <radian.dc@gmail.com>
    
    Change-Id: I1c0de01b1c6a2a2cf1432028a2e69f90b2373b2c

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -7114,6 +7114,7 @@ public class NotificationManagerService extends SystemService {
         if (!ledValues.isEnabled()) {
             mNotificationLight.turnOff();
         } else {
+            mNotificationLight.setModes(ledValues.getBrightness());
             mNotificationLight.setFlashing(ledValues.getColor(), Light.LIGHT_FLASH_TIMED,
                     ledValues.getOnMs(), ledValues.getOffMs());
         }
