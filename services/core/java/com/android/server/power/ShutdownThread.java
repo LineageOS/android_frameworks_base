0869f93534d1f5655e0283e69a0b14c3d61bb894
Bernhard Thoben <berni-the-bear@web.de>
2017-11-05 16:29:03 +0100

    Added a shutdown confirmation
    Change-Id: I2a1a41d25831cf470b5a9cf780378e6d822cd571

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -196,7 +196,7 @@ public final class ShutdownThread extends Thread {
                 : (longPressBehavior == 2
                         ? com.android.internal.R.string.shutdown_confirm_question
                         : com.android.internal.R.string.shutdown_confirm);
-        if (showRebootOption && !mRebootSafeMode) {
+        if (showRebootOption && !mRebootSafeMode && mReboot) {
             resourceId = com.android.internal.R.string.reboot_confirm;
         }
 
