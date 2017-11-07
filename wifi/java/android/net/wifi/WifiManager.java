eb79fb08e3af5f7f2d29997148ba0df3ee9fba50
Aaron Kling <webgeek1234@gmail.com>
2017-11-07 14:12:45 -0600

    Expose getChannelList [1/2]
    Change-Id: I0c3faaf770f2924076dbe91c237ce83aaf6a938b

--- wifi/java/android/net/wifi/WifiManager.java
+++ wifi/java/android/net/wifi/WifiManager.java
@@ -3765,4 +3765,14 @@ public class WifiManager {
     public WifiAppPropertyManager getWifiAppPropertyManager() {
         return new WifiAppPropertyManager();
     }
+
+    /** @hide */
+    public List<WifiChannel> getChannelList() {
+        try {
+            return mService.getChannelList();
+        }
+        catch (RemoteException dummy) {
+            return null;
+        }
+    }
 }
