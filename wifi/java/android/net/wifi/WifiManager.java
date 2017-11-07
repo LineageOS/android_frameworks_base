14b49544e240863336324b18bb65959dc7954677
Aaron Kling <webgeek1234@gmail.com>
2017-11-07 14:14:17 -0600

    Expose updateChannelList [1/2]
    Change-Id: I15966109bc322e2a4dadb43c9714b25ef7fab163

--- wifi/java/android/net/wifi/WifiManager.java
+++ wifi/java/android/net/wifi/WifiManager.java
@@ -2853,4 +2853,12 @@ public class WifiManager {
             return null;
         }
     }
+
+    /** @hide */
+    public void updateChannelList() {
+        try {
+            mService.updateChannelList();
+        }
+        catch (RemoteException localRemoteException) {}
+    }
 }
