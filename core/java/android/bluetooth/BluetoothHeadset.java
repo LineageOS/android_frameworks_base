54818a3b405fcfe9199d055a6db1fcfac93878ad
Bandari Ramesh <ramban@codeaurora.org>
2017-11-08 18:51:04 +0530

    Bluetooth: Fix for Global reference table overflow issue[1/2]
     -  To avoid RemoteCallbackList object leaks in Bluetooth Manager
        Service, Added unregister API in BluetoothAdapter.java which
        internally calls unregisterAdapter of Manager Service
    
    CRs-Fixed: 2138560
    Change-Id: I58ce5f5e1b86d87e0ff602b453abb74d0b5e1982

--- core/java/android/bluetooth/BluetoothHeadset.java
+++ core/java/android/bluetooth/BluetoothHeadset.java
@@ -488,6 +488,10 @@ public final class BluetoothHeadset implements BluetoothProfile {
         close();
     }
 
+    public void finalize() {
+        close();
+    }
+
     /**
      * Initiate connection to a profile of the remote bluetooth device.
      *
