c024e215ca489b1782f1cd042932b9a03614098b
Dan Pasanen <dan.pasanen@gmail.com>
2017-11-11 19:08:03 -0600

    InputMethodManagerService: adjust grip mode for input enable/disable
    * This is useful for mata (Essential PH-1) due to the super small bezel.
    
    Change-Id: I9b8c9a8c6b866d03e1c3ff0975cb1c83f69fcaa0

--- services/core/java/com/android/server/InputMethodManagerService.java
+++ services/core/java/com/android/server/InputMethodManagerService.java
@@ -90,6 +90,7 @@ import android.os.RemoteException;
 import android.os.ResultReceiver;
 import android.os.ServiceManager;
 import android.os.SystemClock;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
@@ -207,6 +208,7 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
     final HandlerCaller mCaller;
     final boolean mHasFeature;
     private InputMethodFileManager mFileManager;
+    private boolean mGripStatus;
     private final HardKeyboardListener mHardKeyboardListener;
     private final AppOpsManager mAppOpsManager;
     private final UserManager mUserManager;
@@ -2211,6 +2213,10 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
             }
         }
 
+        if (mInputShown && mGripStatus) {
+            SystemProperties.set("sys.grip.status", "off");
+            mGripStatus = false;
+        }
         return res;
     }
 
@@ -2289,6 +2295,10 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
         mShowRequested = false;
         mShowExplicitlyRequested = false;
         mShowForced = false;
+        if (res && !mGripStatus) {
+            SystemProperties.set("sys.grip.status", "on");
+            mGripStatus = true;
+        }
         return res;
     }
 
