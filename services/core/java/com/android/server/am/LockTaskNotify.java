60b7012fc3cb5ab0e56f33cc163f79c226a65a2f
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2017-12-28 21:55:45 +0100

    Allow screen unpinning on devices without navbar
    Change-Id: Iedfc08f4d95bbee3c8578c0d2450b90739e63603
    
    Screen Pinning: Show correct text for on screen nav.
    
      Similar to I09c2ef661bff272cb4f7ca43bac0e45f4b20a4d4,
      we're not getting an instance of PhoneWindowManager which
      we can rely on to update dynamically.
    
      TICKET: OPO-393
    
    Change-Id: Iacf8221066461fb6940dd88432e665812545c3ff

--- services/core/java/com/android/server/am/LockTaskNotify.java
+++ services/core/java/com/android/server/am/LockTaskNotify.java
@@ -20,9 +20,12 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
+import android.os.RemoteException;
 import android.os.SystemClock;
 import android.util.Slog;
+import android.view.IWindowManager;
 import android.view.WindowManager;
+import android.view.WindowManagerGlobal;
 import android.widget.Toast;
 
 import com.android.internal.R;
@@ -37,12 +40,14 @@ public class LockTaskNotify {
 
     private final Context mContext;
     private final H mHandler;
+    private final IWindowManager mWindowManagerService;
     private Toast mLastToast;
     private long mLastShowToastTime;
 
     public LockTaskNotify(Context context) {
         mContext = context;
         mHandler = new H();
+        mWindowManagerService = WindowManagerGlobal.getWindowManagerService();
     }
 
     public void showToast(int lockTaskModeState) {
@@ -54,7 +59,8 @@ public class LockTaskNotify {
         if (lockTaskModeState == ActivityManager.LOCK_TASK_MODE_LOCKED) {
             text = mContext.getString(R.string.lock_to_app_toast_locked);
         } else if (lockTaskModeState == ActivityManager.LOCK_TASK_MODE_PINNED) {
-            text = mContext.getString(R.string.lock_to_app_toast);
+            text = mContext.getString(hasNavigationBar()
+                      ? R.string.lock_to_app_toast : R.string.lock_to_app_toast_no_navbar);
         }
         if (text == null) {
             return;
@@ -85,6 +91,15 @@ public class LockTaskNotify {
                 WindowManager.LayoutParams.PRIVATE_FLAG_SHOW_FOR_ALL_USERS;
         toast.show();
         return toast;
+    }
+
+    private boolean hasNavigationBar() {
+        try {
+            return mWindowManagerService.hasNavigationBar();
+        } catch (RemoteException e) {
+            // ignore
+        }
+        return false;
     }
 
     private final class H extends Handler {
