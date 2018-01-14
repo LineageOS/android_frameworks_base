5aaf67a2e60b22f93b11f4caa204c39bdc7ea70d
Sam Mortimer <sam@mortimer.me.uk>
2018-01-13 23:26:48 -0800

    SystemUI: Network Traffic [1/3]
    *) Add lineage-sdk internal NetworkTraffic to the statusbar
       using the LineageStatusBarItemHolder helper.
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Thu May 6 13:50:05 2021 +0200
    
        SystemUI: Force left-to-right for network traffic monitor holder
    
        Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/3445
        Change-Id: I19ea852cedfd2a69a47006977f1b74a172008058
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sun Apr 11 09:58:09 2021 +0200
    
        SystemUI: Hide centered_area when showing heads up
    
        Needed for network traffic monitor.
    
        Change-Id: Ib96a75a936cf4d71e23a575e012741b812ed2aa1
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sun Apr 11 09:47:17 2021 +0200
    
        SystemUI: Move network traffic monitor under same layout as clock
    
        This effectively fixes wrongly network monitor layout on
        devices with side notch / hole punch.
    
        Change-Id: I217b886d8da75eab7636066f4822f5fd6dbc9aff
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Sat Jan 13 23:26:48 2018 -0800
    
        SystemUI: Network Traffic [1/3]
    
        *) Add lineage-sdk internal NetworkTraffic to the statusbar
           using the LineageStatusBarItemHolder helper.
    
        Change-Id: Ib947832860970a3bccfac70d27a4761f6164d3d5
    
    Change-Id: Ib947832860970a3bccfac70d27a4759f6164d3d5
    (cherry picked from commit 9f9166d5d5d942f0a8da95acde4d1867959028a3)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -110,6 +110,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private LinearLayout mSystemIconArea;
     private View mOngoingCallChip;
     private View mNotificationIconAreaInner;
+    private View mNetworkTrafficHolder;
+    private View mCenteredIconArea;
     private int mDisabled1;
     private int mDisabled2;
     private DarkIconManager mDarkIconManager;
@@ -239,6 +241,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         updateBlockedIcons();
         mStatusBarIconController.addIconGroup(mDarkIconManager);
         mSystemIconArea = mStatusBar.findViewById(R.id.system_icon_area);
+        mNetworkTrafficHolder = mStatusBar.findViewById(R.id.network_traffic_holder);
         mClockController = mStatusBar.getClockController();
         mOngoingCallChip = mStatusBar.findViewById(R.id.ongoing_call_chip);
         showSystemIconArea(false);
@@ -500,6 +503,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     private void hideSystemIconArea(boolean animate) {
         animateHide(mSystemIconArea, animate);
+        animateHide(mNetworkTrafficHolder, animate);
     }
 
     private void showSystemIconArea(boolean animate) {
@@ -507,6 +511,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         int state = mAnimationScheduler.getAnimationState();
         if (state == IDLE || state == SHOWING_PERSISTENT_DOT) {
             animateShow(mSystemIconArea, animate);
+            animateShow(mNetworkTrafficHolder, animate);
         }
     }
 
