532cdcf239b72482cafcccbfdcd8abd18a585de0
Arne Coucheron <arco68@gmail.com>
2018-01-21 11:17:51 +0100

    Fix double action performed when pressing menu key
    Setting a custom menu key action will do both the custom action
    and the default action at the same time. Comparing code with 14.1
    we're missing the return here.
    
    Change-Id: I2f188ec78fae5068ffa1a5c80a8afa5ee2f167b5

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3924,6 +3924,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     performKeyAction(mMenuPressAction, event);
                 }
             }
+            return -1;
         } else if (keyCode == KeyEvent.KEYCODE_SEARCH) {
             if (down) {
                 if (repeatCount == 0) {
