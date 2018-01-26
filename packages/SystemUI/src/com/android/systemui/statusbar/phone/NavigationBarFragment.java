e95e1fc52e8c6db0c90a1fd3a56839ebc41657a3
Sam Mortimer <sam@mortimer.me.uk>
2018-01-25 18:53:39 -0800

    frameworks/base: Allow customisation of navbar app switch long press action
    Change-Id: Idfb12a7bae6d921d207b5becd69b1005ce3d2b92

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarFragment.java
@@ -522,7 +522,13 @@ public class NavigationBarFragment extends Fragment implements Callbacks {
                     mNavigationBarView.setDisabledFlags(mDisabledFlags1, true);
                     return true;
                 } else if (v.getId() == R.id.recent_apps) {
-                    return onLongPressRecents();
+                    // Send long press key event so that Lineage button handling can intercept
+                    KeyButtonView keyButtonView = (KeyButtonView) v;
+                    keyButtonView.sendEvent(KeyEvent.ACTION_DOWN, KeyEvent.FLAG_LONG_PRESS);
+                    keyButtonView.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_LONG_CLICKED);
+                    return true;
+                    // Original AOSP handling:
+                    //return onLongPressRecents();
                 }
             }
             if (sendBackLongPress) {
