e99e3720edb4c8e7f0b13a396cef768e640cddbc
Bruno Martins <bgcngm@gmail.com>
2018-02-02 18:10:44 +0000

    BluetoothTile: Avoid NPE during boot up phase
     * Listening handling can only take place once mController
       is properly initialized.
    
    Change-Id: Ice0c19321d8632bd55e1d403de95914ad53bad30

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -80,6 +80,7 @@ public class BluetoothTile extends QSTileImpl<BooleanState> {
 
     @Override
     public void handleSetListening(boolean listening) {
+        if (mController == null) return;
         if (listening) {
             mController.addCallback(mCallback);
         } else {
