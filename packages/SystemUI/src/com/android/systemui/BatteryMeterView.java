08bb22aabc1ff73a2f2a8c4167745ca757f8feee
ezio84 <brabus84@gmail.com>
2018-02-14 00:24:18 +0100

    Text battery style: show a tilde when charging
    Change-Id: I74ad0ceef7f8617526e157ebd61747e3fe1a2042

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -223,7 +223,9 @@ public class BatteryMeterView extends LinearLayout implements
 
     private void updatePercentText() {
         if (mBatteryPercentView != null) {
-            mBatteryPercentView.setText(
+            CharSequence mChargeIndicator =
+                    mCharging && mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT ? "~" : "";
+            mBatteryPercentView.setText(mChargeIndicator +
                     NumberFormat.getPercentInstance().format(mLevel / 100f));
         }
     }
@@ -363,6 +365,7 @@ public class BatteryMeterView extends LinearLayout implements
             mForceShowPercent = false;
         }
         updateShowPercent();
+        updatePercentText();
         onDensityOrFontScaleChanged();
     }
 }
