bd756bbf7c2a41b19ddbaecbaa7a966d18548cd5
Kyrylo Mikos <kiril.mik.os@gmail.com>
2018-03-03 11:04:52 +0100

    SystemUI: change back button to hide ime button with rotate animation
    Squashed in: "SystemUI: fix incorrectly sized nav bar icons"
    (Change-Id: I90c533a10830c641e657f48ee6f8407d4c67c830)
    
    Adapted for Oreo by blunden
    
    Change-Id: Ia9a7716a6797d0442670451e59857d44b579b5f6

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonDrawable.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonDrawable.java
@@ -27,6 +27,12 @@ import android.view.Gravity;
  */
 public class KeyButtonDrawable extends LayerDrawable {
 
+    private float mRotation;
+    private Animator mCurrentAnimator;
+    private boolean mIsBackButton;
+
+    private static final int ANIMATION_DURATION = 200;
+
     private final boolean mHasDarkDrawable;
 
     public static KeyButtonDrawable create(Drawable lightDrawable,
@@ -56,5 +62,64 @@ public class KeyButtonDrawable extends LayerDrawable {
         getDrawable(0).setAlpha((int) ((1 - intensity) * 255f));
         getDrawable(1).setAlpha((int) (intensity * 255f));
         invalidateSelf();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        if (mIsBackButton) {
+            final Rect bounds = getBounds();
+            final int boundsCenterX = bounds.width() / 2;
+            final int boundsCenterY = bounds.height() / 2;
+
+            canvas.save();
+            canvas.translate(boundsCenterX, boundsCenterY);
+            canvas.rotate(mRotation);
+            canvas.translate(-boundsCenterX, -boundsCenterY);
+
+            super.draw(canvas);
+            canvas.restore();
+        } else {
+            super.draw(canvas);
+        }
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        super.setAlpha(alpha);
+        if (mCurrentAnimator != null) {
+            mCurrentAnimator.end();
+        }
+    }
+
+    public void setRotation(float rotation) {
+        mRotation = rotation;
+        invalidateSelf();
+    }
+
+    public float getRotation() {
+        return mRotation;
+    }
+
+    public void setImeVisible(boolean ime) {
+        if (mCurrentAnimator != null) {
+            mCurrentAnimator.cancel();
+        }
+
+        final float nextRotation = ime ? -90 : 0;
+        if (mRotation == nextRotation) {
+            return;
+        }
+
+        if (isVisible() && ActivityManager.isHighEndGfx()) {
+            mCurrentAnimator = ObjectAnimator.ofFloat(this, "rotation", nextRotation)
+                    .setDuration(ANIMATION_DURATION);
+            mCurrentAnimator.start();
+        } else {
+            setRotation(nextRotation);
+        }
+    }
+
+    public void setIsBackButton(boolean isBackButton) {
+        mIsBackButton = isBackButton;
     }
 }
