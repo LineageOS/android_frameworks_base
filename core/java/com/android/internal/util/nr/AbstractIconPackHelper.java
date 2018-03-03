AICP/frameworks_base

Slim recent app sidebar icon pack support [1/2]
Change-Id: I727c700dc0d19cb53d408de78508611a0a709ea8

core/java/com/android/internal/util/aicp/AbstractIconPackHelper.java
@@ -1,0 +1,10 @@
+package com.android.internal.util.aicp;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Resources;
+
+public interface AbstractIconPackHelper {
+    boolean isIconPackLoaded();
+    int getResourceIdForActivityIcon(ActivityInfo info);
+    Resources getIconPackResources();
+}

LineageOS/android_frameworks_base

Conflicts:
	packages/SystemUI/res/values/nr_strings.xml

core/java/com/android/internal/util/nr/AbstractIconPackHelper.java
@@ -1,0 +1,10 @@
+package com.android.internal.util.nr;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Resources;
+
+public interface AbstractIconPackHelper {
+    boolean isIconPackLoaded();
+    int getResourceIdForActivityIcon(ActivityInfo info);
+    Resources getIconPackResources();
+}