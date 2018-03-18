omnirom/android_frameworks_base_old

[1/2] base: battery styles
Change-Id: I32741c92d4a3a10d3beeffd42b80313e607b629c

packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -40,6 +40,7 @@ import com.android.systemui.BatteryMeterView;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.R.id;
+import com.android.systemui.omni.BatteryViewManager;
 import com.android.systemui.omni.StatusBarHeaderMachine;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.qs.QSDetail.Callback;
@@ -64,6 +65,7 @@ public class QuickStatusBarHeader extends FrameLayout implements StatusBarHeader
     private HorizontalScrollView mQuickQsPanelScroller;
     private ImageView mBackgroundImage;
     private Drawable mCurrentBackground;
+    private BatteryViewManager mBatteryViewManager;
 
     public QuickStatusBarHeader(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -81,6 +83,9 @@ public class QuickStatusBarHeader extends FrameLayout implements StatusBarHeader
 
         updateResources();
 
+        ViewGroup batteryContainer = (ViewGroup) findViewById(R.id.battery_container);
+        mBatteryViewManager = new BatteryViewManager(mContext, batteryContainer);
+
         // Set the light/dark theming on the header status UI to match the current theme.
         int colorForeground = Utils.getColorAttr(getContext(), android.R.attr.colorForeground);
         float intensity = colorForeground == Color.WHITE ? 0 : 1;
@@ -88,6 +93,7 @@ public class QuickStatusBarHeader extends FrameLayout implements StatusBarHeader
 
         applyDarkness(R.id.battery, tintArea, intensity, colorForeground);
         applyDarkness(R.id.clock, tintArea, intensity, colorForeground);
+        applyDarkness(R.id.battery_style, tintArea, intensity, colorForeground);
 
         BatteryMeterView battery = findViewById(R.id.battery);
         battery.setForceShowPercent(true);

packages/SystemUI/src/com/android/systemui/omni/BatteryPercentView.java
@@ -1,0 +1,117 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.omni;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.view.View;
+import android.util.Log;
+import android.util.DisplayMetrics;
+
+import com.android.systemui.R;
+import com.android.systemui.statusbar.policy.BatteryController;
+
+import java.text.NumberFormat;
+
+public class BatteryPercentView extends AbstractBatteryView {
+    public static final String TAG = BatteryPercentView.class.getSimpleName();
+
+    private int mPercentOffsetY;
+    private int mHeight;
+    private int mWidth;
+
+    public BatteryPercentView(Context context) {
+        this(context, null, 0);
+    }
+
+    public BatteryPercentView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public BatteryPercentView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        loadDimens();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mWidth = mTextWidth;
+        mHeight = getMeasuredHeight();
+        setMeasuredDimension(mWidth, mHeight);
+    }
+
+    @Override
+    public void draw(Canvas c) {
+        final int level = mLevel;
+
+        updatePercentFontSize();
+        mTextPaint.setColor(getCurrentColor(level));
+
+        float textHeight = 0f;
+        float textOffset = 0f;
+        RectF bounds = null;
+        String percentage = NumberFormat.getPercentInstance().format((double) level / 100.0);
+        textHeight = mTextPaint.descent() - mTextPaint.ascent();
+        textOffset = (textHeight / 2) - mTextPaint.descent() + mPercentOffsetY;
+        bounds = new RectF(0, 0, mWidth, mHeight);
+
+        if (percentage != null) {
+            c.drawText(percentage, mWidth, bounds.centerY() + textOffset, mTextPaint);
+        }
+    }
+
+    @Override
+    public void applyStyle() {
+        final int level = mLevel;
+        mTextSize = getResources().getDimensionPixelSize(level == 100 ?
+                R.dimen.omni_battery_level_text_size_small : R.dimen.omni_battery_level_text_size);
+        mTextPaint.setTextSize(mTextSize);
+        Typeface font = Typeface.create("sans-serif-medium", Typeface.NORMAL);
+        mTextPaint.setTypeface(font);
+        mTextPaint.setTextAlign(Paint.Align.RIGHT);
+        Rect bounds = new Rect();
+        String text = level == 100 ? "100%" : ".00%";
+        mTextPaint.getTextBounds(text, 0, text.length(), bounds);
+        mTextWidth = bounds.width();
+    }
+
+    private void updatePercentFontSize() {
+        updateExtraPercentFontSize();
+    }
+
+    @Override
+    public void loadDimens() {
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        mPercentOffsetY = (int) (0.8 * metrics.density + 0.5f);
+    }
+}

LineageOS/android_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -30,6 +30,10 @@ import com.android.systemui.BatteryMeterView;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.R.id;
+
+import com.android.systemui.statusbar.BatteryViewManager;
+import com.android.systemui.statusbar.StatusBarHeaderMachine;
+
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.qs.QSDetail.Callback;
 import com.android.systemui.statusbar.SignalClusterView;
@@ -48,6 +52,8 @@ public class QuickStatusBarHeader extends RelativeLayout {
     protected QuickQSPanel mHeaderQsPanel;
     protected QSTileHost mHost;
 
+    private BatteryViewManager mBatteryViewManager;
+
     public QuickStatusBarHeader(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
@@ -64,6 +70,9 @@ public class QuickStatusBarHeader extends RelativeLayout {
 
         updateResources();
 
+        LinearLayout batteryContainer = (LinearLayout) findViewById(R.id.battery_container);
+        mBatteryViewManager = new BatteryViewManager(mContext, batteryContainer);
+
         // Set the light/dark theming on the header status UI to match the current theme.
         int colorForeground = Utils.getColorAttr(getContext(), android.R.attr.colorForeground);
         float intensity = colorForeground == Color.WHITE ? 0 : 1;
@@ -71,9 +80,7 @@ public class QuickStatusBarHeader extends RelativeLayout {
 
         applyDarkness(R.id.battery, tintArea, intensity, colorForeground);
         applyDarkness(R.id.clock, tintArea, intensity, colorForeground);
-
-        BatteryMeterView battery = findViewById(R.id.battery);
-        battery.setForceShowPercent(true);
+        applyDarkness(R.id.battery_style, tintArea, intensity, colorForeground);
 
         mActivityStarter = Dependency.get(ActivityStarter.class);
     }

packages/SystemUI/src/com/android/systemui/statusbar/BatteryPercentView.java
@@ -1,0 +1,118 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.statusbar;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.view.View;
+import android.util.Log;
+import android.util.DisplayMetrics;
+
+import com.android.systemui.R;
+import com.android.systemui.statusbar.policy.BatteryController;
+
+import java.text.NumberFormat;
+
+public class BatteryPercentView extends AbstractBatteryView {
+    public static final String TAG = BatteryPercentView.class.getSimpleName();
+
+    private int mPercentOffsetY;
+    private int mHeight;
+    private int mWidth;
+
+    public BatteryPercentView(Context context) {
+        this(context, null, 0);
+    }
+
+    public BatteryPercentView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public BatteryPercentView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        loadDimens();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mWidth = mTextWidth;
+        mHeight = getMeasuredHeight();
+        setMeasuredDimension(mWidth, mHeight);
+    }
+
+    @Override
+    public void draw(Canvas c) {
+        final int level = mLevel;
+        if (level == -1) return;
+
+        updatePercentFontSize();
+        mTextPaint.setColor(getCurrentColor(level));
+
+        float textHeight = 0f;
+        float textOffset = 0f;
+        RectF bounds = null;
+        String percentage = NumberFormat.getPercentInstance().format((double) level / 100.0);
+        textHeight = mTextPaint.descent() - mTextPaint.ascent();
+        textOffset = (textHeight / 2) - mTextPaint.descent() + mPercentOffsetY;
+        bounds = new RectF(0, 0, mWidth, mHeight);
+
+        if (percentage != null) {
+            c.drawText(percentage, mWidth, bounds.centerY() + textOffset, mTextPaint);
+        }
+    }
+
+    @Override
+    public void applyStyle() {
+        final int level = mLevel;
+        mTextSize = getResources().getDimensionPixelSize(level == 100 ?
+                R.dimen.omni_battery_level_text_size_small : R.dimen.omni_battery_level_text_size);
+        mTextPaint.setTextSize(mTextSize);
+        Typeface font = Typeface.create("sans-serif-medium", Typeface.NORMAL);
+        mTextPaint.setTypeface(font);
+        mTextPaint.setTextAlign(Paint.Align.RIGHT);
+        Rect bounds = new Rect();
+        String text = level == 100 ? "100%" : ".00%";
+        mTextPaint.getTextBounds(text, 0, text.length(), bounds);
+        mTextWidth = bounds.width();
+    }
+
+    private void updatePercentFontSize() {
+        updateExtraPercentFontSize();
+    }
+
+    @Override
+    public void loadDimens() {
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        mPercentOffsetY = (int) (0.8 * metrics.density + 0.5f);
+    }
+}