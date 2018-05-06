89d5c0c4f7490098a6287f30ec5542e896b4866f
dongwan0605.kim <dongwan0605.kim@lge.com>
2018-05-06 14:57:00 +0900

    Bind app name to menu row when notification updated
    Fix descriptions of notification menu items was reset after notification updated.
    When notification updated, menu row will be reinflated.
    So we need to bind app name again.
    
    Test: Talkback ON > Update existing notification > Swipe notification
          > Touch snooze or gear button and check content descriptions
    
    Change-Id: I070712e42f9405b11446b20ac030d3e2cefa1fa3

--- packages/SystemUI/src/com/android/systemui/statusbar/ExpandableNotificationRow.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/ExpandableNotificationRow.java
@@ -360,6 +360,7 @@ public class ExpandableNotificationRow extends ActivatableNotificationView
         updateNotificationColor();
         if (mMenuRow != null) {
             mMenuRow.onNotificationUpdated(mStatusBarNotification);
+            mMenuRow.setAppName(mAppName);
         }
         if (mIsSummaryWithChildren) {
             mChildrenContainer.recreateNotificationHeader(mExpandClickListener);
