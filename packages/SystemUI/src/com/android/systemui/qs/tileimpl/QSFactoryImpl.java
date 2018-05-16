79be63274ca148e6158f469d80b8457dc7175804
Joey <joey@lineageos.org>
2018-05-16 17:16:06 +0200

    SystemUI: Add reading mode tile
    Author: Joey <joey@lineageos.org>
    Date:   Wed May 16 17:16:06 2018 +0200
    
        base: add reading mode tile
    
        Change-Id: I1c515cbeff0dd5a1fa0c00bc9390a66eb104c7f0
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sat May 26 00:58:57 2018 +0300
    
        SystemUI: Fix reading mode tile accessibility strings
    
        Change-Id: I4f0af56e4947b037e1e2dda79132523d13458818
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Wed Aug 8 18:59:25 2018 +0200
    
        SystemUI: Update reading mode tile for new API
    
        Change-Id: I9fbf1b4de30079c987b75e37e52e234fa2f86c5d
    
    Change-Id: I365ee54b2b2adc52d804a5fc1d35690ef5636071

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
- * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2017-2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use mHost file
  * except in compliance with the License. You may obtain a copy of the License at
@@ -55,6 +55,7 @@ import com.android.systemui.qs.tiles.OneHandedModeTile;
 import com.android.systemui.qs.tiles.PowerShareTile;
 import com.android.systemui.qs.tiles.QRCodeScannerTile;
 import com.android.systemui.qs.tiles.QuickAccessWalletTile;
+import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.ReduceBrightColorsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
@@ -108,6 +109,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<AODTile> mAODTileProvider;
     private final Provider<CaffeineTile> mCaffeineTileProvider;
     private final Provider<HeadsUpTile> mHeadsUpTileProvider;
+    private final Provider<ReadingModeTile> mReadingModeTileProvider;
     private final Provider<SyncTile> mSyncTileProvider;
     private final Provider<PowerShareTile> mPowerShareTileProvider;
     private final Provider<UsbTetherTile> mUsbTetherTileProvider;
@@ -153,6 +155,7 @@ public class QSFactoryImpl implements QSFactory {
             Provider<CaffeineTile> caffeineTileProvider,
             Provider<HeadsUpTile> headsUpTileProvider,
             Provider<PowerShareTile> powerShareTileProvider,
+            Provider<ReadingModeTile> readingModeTileProvider,
             Provider<SyncTile> syncTileProvider,
             Provider<UsbTetherTile> usbTetherTileProvider) {
         mQsHostLazy = qsHostLazy;
@@ -191,6 +194,7 @@ public class QSFactoryImpl implements QSFactory {
         mAODTileProvider = aodTileProvider;
         mCaffeineTileProvider = caffeineTileProvider;
         mHeadsUpTileProvider = headsUpTileProvider;
+        mReadingModeTileProvider = readingModeTileProvider;
         mSyncTileProvider = syncTileProvider;
         mPowerShareTileProvider = powerShareTileProvider;
         mUsbTetherTileProvider = usbTetherTileProvider;
@@ -276,6 +280,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mCaffeineTileProvider.get();
             case "heads_up":
                 return mHeadsUpTileProvider.get();
+            case "reading_mode":
+                return mReadingModeTileProvider.get();
             case "sync":
                 return mSyncTileProvider.get();
             case "powershare":
