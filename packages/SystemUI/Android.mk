fb0c7259fae9f6f88570cdcbc914dac907d75dad
Paul Keith <javelinanddart@gmail.com>
2018-05-28 04:23:00 +0200

    [BACKPORT] SignalClusterView: Hide signal icons for disabled SIMs
    Contains:
    SignalClusterView: Fix compilation & function [BadDaemon]
    SignalClusterView: Fix sim2 icon not being hidden properly [BadDaemon]
    
    Change-Id: If79814cb6bd77150640f977764dfd59b87448c64

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -34,7 +34,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     org.cyanogenmod.platform.internal \
     uicommon
 
-LOCAL_JAVA_LIBRARIES := telephony-common
+LOCAL_JAVA_LIBRARIES := telephony-common telephony-ext
 LOCAL_FULL_LIBS_MANIFEST_FILES := $(LOCAL_PATH)/AndroidManifest_cm.xml
 
 LOCAL_PACKAGE_NAME := SystemUI
