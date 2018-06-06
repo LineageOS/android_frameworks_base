6a2f12067b7b40fabf6a16d00e28aa99fbef1632
Gabriele M <moto.falcon.git@gmail.com>
2018-06-06 15:28:46 +0200

    Revert "Prevent deadlock when using always-on legacy VPNs"
    A different fix will be applied with follow-up changes. This also
    doesn't appear to completely fix the problem (REGRESSION-1963).
    
    This reverts commit ac496f4a070a5b050217335f512e0bb44cd90f32.
    
    Change-Id: Ie77e2d4c58ce8c6be68f125d3311ecced6e1e7ca

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -3241,16 +3241,14 @@ public class ConnectivityService extends IConnectivityManager.Stub
             log("reportNetworkConnectivity(" + nai.network.netId + ", " + hasConnectivity +
                     ") by " + uid);
         }
-        synchronized (mVpns) {
-            synchronized (nai) {
-                // Validating a network that has not yet connected could result in a call to
-                // rematchNetworkAndRequests() which is not meant to work on such networks.
-                if (!nai.everConnected) return;
+        synchronized (nai) {
+            // Validating a network that has not yet connected could result in a call to
+            // rematchNetworkAndRequests() which is not meant to work on such networks.
+            if (!nai.everConnected) return;
 
-                if (isNetworkWithLinkPropertiesBlocked(nai.linkProperties, uid, false)) return;
+            if (isNetworkWithLinkPropertiesBlocked(nai.linkProperties, uid, false)) return;
 
-                nai.networkMonitor.sendMessage(NetworkMonitor.CMD_FORCE_REEVALUATION, uid);
-            }
+            nai.networkMonitor.sendMessage(NetworkMonitor.CMD_FORCE_REEVALUATION, uid);
         }
     }
 
