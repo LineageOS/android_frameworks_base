0c3551013831c4dbeb52a05710957715070a4e87
phhusson <phh@phh.me>
2018-07-03 11:28:09 +0000

    Telephony: Stop using rssnr, it falsly shows wrong signal bars Pixel and other devices drop this
    Change-Id: Iaa54351bfcb8b7abe61a9cc6efef567a999e8198

--- telephony/java/android/telephony/SignalStrength.java
+++ telephony/java/android/telephony/SignalStrength.java
@@ -867,6 +867,9 @@ public class SignalStrength implements Parcelable {
         if (DBG) log("getLTELevel - rsrp:" + mLteRsrp + " snr:" + mLteRssnr + " rsrpIconLevel:"
                 + rsrpIconLevel + " snrIconLevel:" + snrIconLevel
                 + " lteRsrpBoost:" + mLteRsrpBoost);
+	
+	/* Ignore RSSNR for now */
+	if(rsrpIconLevel != -1) return rsrpIconLevel;
 
         /* Choose a measurement type to use for notification */
         if (snrIconLevel != -1 && rsrpIconLevel != -1) {
