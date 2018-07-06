5ae51b271e8e40bbeb33583da29bb093e08b8800
Gurpreet Ghai <gghai@codeaurora.org>
2018-07-06 11:20:44 +0530

    BT: Adding aptX-Adaptive codec entry to Framework
    Adding entry for aptX Adaptive into Framework for
    indexing and UI handling.
    
     Conflicts:
    	core/java/android/bluetooth/BluetoothCodecConfig.java
    	packages/SettingsLib/res/values-en-rGB/arrays.xml
    	packages/SettingsLib/res/values/arrays.xml
    
    CRs-Fixed: 2280216
    
    Change-Id: I94627eb3188b6c69d48f982d4e6c41f78e5e81fa
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>

--- packages/SettingsLib/tests/robotests/src/com/android/settingslib/bluetooth/A2dpProfileTest.java
+++ packages/SettingsLib/tests/robotests/src/com/android/settingslib/bluetooth/A2dpProfileTest.java
@@ -130,7 +130,7 @@ public class A2dpProfileTest {
     private static String KNOWN_CODEC_LABEL = "Use high quality audio: %1$s";
     private static String UNKNOWN_CODEC_LABEL = "Use high quality audio";
     private static String[] CODEC_NAMES =
-            new String[]{"Default", "SBC", "AAC", "aptX", "aptX HD", "LDAC"};
+            new String[]{"Default", "SBC", "AAC", "aptX", "aptX HD", "LDAC", "aptX Adaptive"};
 
     /**
      * Helper for setting up several tests of getHighQualityAudioOptionLabel
