0aa4cc9c0e3d44018cbad37aa9117935c5d1805e
Gurpreet Ghai <gghai@codeaurora.org>
2018-07-06 11:20:44 +0530

    BT: Adding aptX-Adaptive codec entry to Framework
    Adding entry for aptX Adaptive into Framework for
    indexing and UI handling.
    
     Conflicts:
    	core/java/android/bluetooth/BluetoothCodecConfig.java
    	packages/SettingsLib/res/values-en-rGB/arrays.xml
    	packages/SettingsLib/res/values/arrays.xml
    
    CRs-Fixed: 2280216
    
    Change-Id: I94627eb3188b6c69d48f982d4e6c41f78e5e81fa
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>

--- core/java/android/bluetooth/BluetoothCodecConfig.java
+++ core/java/android/bluetooth/BluetoothCodecConfig.java
@@ -59,9 +59,11 @@ public final class BluetoothCodecConfig implements Parcelable {
 
     public static final int SOURCE_CODEC_TYPE_APTX_HD = 3;
 
-    public static final int SOURCE_CODEC_TYPE_LDAC = 4;
+    public static final int SOURCE_CODEC_TYPE_APTX_ADAPTIVE = 4;
 
-    public static final int SOURCE_CODEC_TYPE_MAX = 5;
+    public static final int SOURCE_CODEC_TYPE_LDAC = 5;
+
+    public static final int SOURCE_CODEC_TYPE_MAX = 6;
 
 
     public static final int SOURCE_CODEC_TYPE_INVALID = 1000 * 1000;
@@ -377,6 +379,8 @@ public final class BluetoothCodecConfig implements Parcelable {
                 return "aptX HD";
             case SOURCE_CODEC_TYPE_LDAC:
                 return "LDAC";
+            case SOURCE_CODEC_TYPE_APTX_ADAPTIVE:
+                return "aptX Adaptive";
             case SOURCE_CODEC_TYPE_INVALID:
                 return "INVALID CODEC";
             default:
