bcf76fbafa4e881284f7ce50e9b75926d7ef5705
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-15 17:09:54 +0200

    SystemUI: Add tunables for clock position
    Author: Hendrik Hagendorn <git@finnq.de>
    Date:   Sun Jan 15 10:00:12 2017 +0100
        Tuner: Move clock seconds option to status bar settings
    
        Change-Id: I532c179f06b934009d48433e50b69660247ee295
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Sun Jul 29 14:19:08 2018 +0200
        SystemUI: Fix default clock blacklist value
    
        Change-Id: Ibdc4f6a707569ddb417854760cf579daca989ebb
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Wed Aug 1 18:10:39 2018 +0200
        SystemUI: Initialize mClockVisibleByUser to android:visibility
    
        * Fixes ~ triple clock ~ after leaving from Google SUW.
    
        Change-Id: I3ceea4400b3057e2b6b1f221f9074b1d451133ea
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Thu Sep 27 12:24:19 2018 +0200
        SystemUI: Fix clock icon blacklist handling
    
        * This makes sure clock isn't on blacklist before
          calling showClock(animate);
    
        Change-Id: I7f77a83aa6c109e1682d764fb22701375ae5ba1e
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Tue Apr 2 20:56:25 2019 +0200
        SystemUI: Don't hide right clock when heads up is visible
    
        * https://gitlab.com/LineageOS/issues/android/issues/436
    
        Change-Id: I85a6cd2dff79e6b97d8a7db552c22d611bf25dbd
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Wed Oct 30 15:48:38 2019 +0100
        SystemUI: Fix up right clock handling when showing heads up
    
        * Oops, we somehow forgot to import this code from 16.0 ;-;
    
        Change-Id: Ic4195a30da2870f569f07fc323e4b939320ab6c8
    
    Author: Michael W <baddaemon87@gmail.com>
    Date:   Mon Jun 1 14:43:31 2020 +0200
        SystemUI: Make clock truly invisible
    
        * When the clock is set to left and then hidden, the notification
          icons, which would appear on the left first transition to the
          right to make place for the clock then finally snap into place
        * The same happens when the clock was set to show right and then
          is hidden: The battery, wifi, etc. icons do leave space for the
          (not displayed) clock and then snap to the right
        * This patch fixes the behavior
    
        Change-Id: Ic68b1001becb91da6f1aeaca95a7312b36e1d2a1
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sat Apr 10 22:52:25 2021 +0200
        SystemUI: Make center clock layout safe insets aware
    
        This effectively fixes wrongly centered clock layout on
        devices with side notch / hole punch.
    
        Change-Id: I7cb8cbbe628397c50d37e23ba1853368e61a1fc9
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sun Apr 11 09:47:17 2021 +0200
        SystemUI: Move network traffic monitor under same layout as clock
    
        This effectively fixes wrongly network monitor layout on
        devices with side notch / hole punch.
    
        Change-Id: I217b886d8da75eab7636066f4822f5fd6dbc9aff
    
    Author: Volodymyr Zhdanov <wight554@gmail.com>
    Date:   Tue Nov 9 20:53:56 2021 +0200
        SystemUI: fix clock dark/light mode switching
    
        * move ClockController init to PhoneStatusBarView
        * move add/remove dark receiver logic to ClockController
    
        Change-Id: I6464af6e8aa130ea54fe7bfa19858c8930a29b1e
    
    Change-Id: Icdc039d13fd2676c1eb979a81fb77f9a6ec59b4a
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -51,7 +51,6 @@ import com.android.systemui.plugins.DarkIconDispatcher;
 import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
 import com.android.systemui.settings.CurrentUserTracker;
 import com.android.systemui.statusbar.CommandQueue;
-import com.android.systemui.statusbar.phone.StatusBarIconController;
 import com.android.systemui.statusbar.policy.ConfigurationController.ConfigurationListener;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.tuner.TunerService.Tunable;
@@ -84,7 +83,7 @@ public class Clock extends TextView implements
     private int mCurrentUserId;
 
     private boolean mClockVisibleByPolicy = true;
-    private boolean mClockVisibleByUser = true;
+    private boolean mClockVisibleByUser = getVisibility() == View.VISIBLE;
 
     private boolean mAttached;
     private boolean mScreenReceiverRegistered;
@@ -193,8 +192,7 @@ public class Clock extends TextView implements
             // The receiver will return immediately if the view does not have a Handler yet.
             mBroadcastDispatcher.registerReceiverWithHandler(mIntentReceiver, filter,
                     Dependency.get(Dependency.TIME_TICK_HANDLER), UserHandle.ALL);
-            Dependency.get(TunerService.class).addTunable(this, CLOCK_SECONDS,
-                    StatusBarIconController.ICON_HIDE_LIST);
+            Dependency.get(TunerService.class).addTunable(this, CLOCK_SECONDS);
             mCommandQueue.addCallback(this);
             mCurrentUserTracker.startTracking();
             mCurrentUserId = mCurrentUserTracker.getCurrentUserId();
@@ -283,7 +281,7 @@ public class Clock extends TextView implements
         updateClockVisibility();
     }
 
-    private boolean shouldBeVisible() {
+    public boolean shouldBeVisible() {
         return mClockVisibleByPolicy && mClockVisibleByUser;
     }
 
@@ -337,10 +335,6 @@ public class Clock extends TextView implements
         if (CLOCK_SECONDS.equals(key)) {
             mShowSeconds = TunerService.parseIntegerSwitch(newValue, false);
             updateShowSeconds();
-        } else if (StatusBarIconController.ICON_HIDE_LIST.equals(key)) {
-            setClockVisibleByUser(!StatusBarIconController.getIconHideList(getContext(), newValue)
-                    .contains("clock"));
-            updateClockVisibility();
         }
     }
 
