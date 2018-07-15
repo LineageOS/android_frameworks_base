cd4ab712282076b75eaf8ce79727d6326e2e37f1
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-15 17:09:54 +0200

    SystemUI: Add tunables for clock position
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Wed Aug 1 18:10:39 2018 +0200
    
        SystemUI: Initialize mClockVisibleByUser to android:visibility
    
        * Fixes ~ triple clock ~ after leaving from Google SUW.
    
        Change-Id: I3ceea4400b3057e2b6b1f221f9074b1d451133ea
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Sun Jul 29 14:19:08 2018 +0200
    
        SystemUI: Fix default clock blacklist value
    
        Change-Id: Ibdc4f6a707569ddb417854760cf579daca989ebb
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Tue Apr 2 20:56:25 2019 +0200
    
        SystemUI: Don't hide right clock when heads up is visible
    
        * https://gitlab.com/LineageOS/issues/android/issues/436
    
        Change-Id: I85a6cd2dff79e6b97d8a7db552c22d611bf25dbd
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Thu Sep 27 12:24:19 2018 +0200
    
        SystemUI: Fix clock icon blacklist handling
    
        * This makes sure clock isn't on blacklist before
          calling showClock(animate);
    
        Change-Id: I7f77a83aa6c109e1682d764fb22701375ae5ba1e
    
    Author: Hendrik Hagendorn <git@finnq.de>
    Date:   Sun Jan 15 10:00:12 2017 +0100
    
        Tuner: Move clock seconds option to status bar settings
    
        Change-Id: I532c179f06b934009d48433e50b69660247ee295
    
    Change-Id: Icdc039d13fd2676c1eb979a81fb77f9a6ec59b4a

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -61,6 +61,7 @@ import androidx.lifecycle.LifecycleRegistry;
 import com.android.internal.logging.UiEventLogger;
 import com.android.settingslib.Utils;
 import com.android.systemui.BatteryMeterView;
+import com.android.systemui.Dependency;
 import com.android.systemui.DualToneHandler;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
@@ -84,6 +85,7 @@ import com.android.systemui.statusbar.policy.DateView;
 import com.android.systemui.statusbar.policy.NextAlarmController;
 import com.android.systemui.statusbar.policy.ZenModeController;
 import com.android.systemui.util.RingerModeTracker;
+import com.android.systemui.tuner.TunerService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -100,7 +102,7 @@ import javax.inject.Named;
  */
 public class QuickStatusBarHeader extends RelativeLayout implements
         View.OnClickListener, NextAlarmController.NextAlarmChangeCallback,
-        ZenModeController.Callback, LifecycleOwner {
+        ZenModeController.Callback, LifecycleOwner, TunerService.Tunable {
     private static final String TAG = "QuickStatusBarHeader";
     private static final boolean DEBUG = false;
 
@@ -284,6 +286,9 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 
         mAllIndicatorsEnabled = mPrivacyItemController.getAllIndicatorsAvailable();
         mMicCameraIndicatorsEnabled = mPrivacyItemController.getMicCameraAvailable();
+
+        Dependency.get(TunerService.class).addTunable(this,
+                StatusBarIconController.ICON_BLACKLIST);
     }
 
     public QuickQSPanel getHeaderQsPanel() {
@@ -771,5 +776,11 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 
     private boolean getChipEnabled() {
         return mMicCameraIndicatorsEnabled || mAllIndicatorsEnabled;
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        mClockView.setClockVisibleByUser(!StatusBarIconController.getIconBlacklist(
+                mContext, newValue).contains("clock"));
     }
 }
