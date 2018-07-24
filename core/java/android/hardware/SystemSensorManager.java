0619868d4b753c1cd1c1fe6636faa1c4fc71b179
T H <socialentry@gmail.com>
2018-07-24 12:40:59 -0400

    SystemSensorManager: Fix accel/rotation issue
    Disable batching on accelerometer for now.
    
    On certain devices (kltedv tested) calling registerListener with
    maxReportLatencyUs greater than samplingPeriodUs, causes the
    accelerometer to stop reporting until the listener is unregistered.
    
    Opengapps has a background service that causes this problem,
    breaking rotation intermittently.
    
    Change-Id: I0d8e5f906e39f11f98695de79d71ded2177de561

--- core/java/android/hardware/SystemSensorManager.java
+++ core/java/android/hardware/SystemSensorManager.java
@@ -99,6 +99,9 @@ public class SystemSensorManager extends SensorManager {
     private final Context mContext;
     private final long mNativeInstance;
 
+    // Config
+    private boolean mDenyAccelBatch = false;
+
     /** {@hide} */
     public SystemSensorManager(Context context, Looper mainLooper) {
         synchronized(sLock) {
@@ -112,6 +115,9 @@ public class SystemSensorManager extends SensorManager {
         mTargetSdkLevel = context.getApplicationInfo().targetSdkVersion;
         mContext = context;
         mNativeInstance = nativeCreate(context.getOpPackageName());
+
+        mDenyAccelBatch = context.getResources().getBoolean(
+            com.android.internal.R.bool.config_denyAccelBatch);
 
         // initialize the sensor list
         for (int index = 0;;++index) {
@@ -160,6 +166,11 @@ public class SystemSensorManager extends SensorManager {
             throw new IllegalStateException("register failed, " +
                 "the sensor listeners size has exceeded the maximum limit " +
                 MAX_LISTENER_COUNT);
+        }
+
+        if (mDenyAccelBatch && (sensor.getType() == Sensor.TYPE_ACCELEROMETER) && (maxBatchReportLatencyUs > 0)) {
+            Log.e(TAG, "batch disabled for this sensor");
+            maxBatchReportLatencyUs = 0;
         }
 
         // Invariants to preserve:
