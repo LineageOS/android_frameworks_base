ccc09b6811de6b4d62f5c50b1907eee9e593e114
T H <socialentry@gmail.com>
2018-07-24 12:40:59 -0400

    SystemSensorManager: Fix accel/rotation issue
    Add config to cap the accelerometer report latency.
    
    On certain devices (kltedv tested) calling registerListener with
    maxReportLatencyUs greater than samplingPeriodUs causes the
    accelerometer to stop reporting until the listener is unregistered.
    
    Opengapps has a background service that causes this problem,
    breaking rotation intermittently.
    
    Change-Id: I0d8e5f906e39f11f98695de79d71ded2177de561

--- core/java/android/hardware/SystemSensorManager.java
+++ core/java/android/hardware/SystemSensorManager.java
@@ -99,6 +99,9 @@ public class SystemSensorManager extends SensorManager {
     private final Context mContext;
     private final long mNativeInstance;
 
+    // Config
+    private final boolean mCapAccelBatch;
+
     /** {@hide} */
     public SystemSensorManager(Context context, Looper mainLooper) {
         synchronized(sLock) {
@@ -112,6 +115,9 @@ public class SystemSensorManager extends SensorManager {
         mTargetSdkLevel = context.getApplicationInfo().targetSdkVersion;
         mContext = context;
         mNativeInstance = nativeCreate(context.getOpPackageName());
+
+        mCapAccelBatch = context.getResources().getBoolean(
+            com.android.internal.R.bool.config_capAccelBatch);
 
         // initialize the sensor list
         for (int index = 0;;++index) {
@@ -160,6 +166,12 @@ public class SystemSensorManager extends SensorManager {
             throw new IllegalStateException("register failed, " +
                 "the sensor listeners size has exceeded the maximum limit " +
                 MAX_LISTENER_COUNT);
+        }
+
+        if (mCapAccelBatch && (sensor.getType() == Sensor.TYPE_ACCELEROMETER) &&
+            (maxBatchReportLatencyUs > delayUs)) {
+            Log.i(TAG, "capping batch value for accelerometer");
+            maxBatchReportLatencyUs = delayUs;
         }
 
         // Invariants to preserve:
