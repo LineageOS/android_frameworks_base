edf532e7fcdd48a42e4036860efa764495c7822e
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-09-04 15:05:26 +0200

    Remove "Avoid NPE during boot phase"
    * Change 8c4a19c8 fixed them all
    
    Revert "WifiTile: Avoid NPE during boot phase"
    
    This reverts commit 9f03a4ef72bfdb1be85469e47036172364922992.
    
    Revert "DndTile: Avoid yet another NPE during boot up phase"
    
    This reverts commit 8b802eecf4b7e570803e51466afe2f66e0c4c3db.
    
    Revert "DndTile: Avoid NPE during boot phase"
    
    This reverts commit 79ddcfbadc53d42ee8108f6d53ce24b42a747b1c.
    
    Revert "BluetoothTile: Avoid NPE during boot up phase"
    
    This reverts commit 04cfb47d55e1b2171712b932876b4d3e1d9d1070.
    
    Change-Id: Id830df7c67682c7bba290ec89257f70f62bf4c81

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DndTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DndTile.java
@@ -215,7 +215,6 @@ public class DndTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        if (mController == null) return;
         final int zen = arg instanceof Integer ? (Integer) arg : mController.getZen();
         final boolean newValue = zen != ZEN_MODE_OFF;
         final boolean valueChanged = state.value != newValue;
@@ -278,7 +277,6 @@ public class DndTile extends QSTileImpl<BooleanState> {
     public void handleSetListening(boolean listening) {
         if (mListening == listening) return;
         mListening = listening;
-        if (mController == null) return;
         if (mListening) {
             mController.addCallback(mZenCallback);
             Prefs.registerListener(mContext, mPrefListener);
