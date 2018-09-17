b40e54dc2bc6f344402260a027ff370701ce936e
Bruno Martins <bgcngm@gmail.com>
2018-09-17 11:52:04 +0100

    Add support for runtime toggle of navbar
    This is a refactor of the following changes, so to make use of tuner API.
    
    Author: Timo Wendt <timo@tjwendt.de>
    Date:   Thu Aug 30 12:18:41 2012 +0300
    
        Runtime toggle of navbar
    
        This adds the framework support for enabling the Navigation bar
        on devices with hardware keys. It is toggled from Settings, and
        depends on device-specific support for the KeyDisabler hardware
        control
    
        Change-Id: I88fecb2ca1e8613591c327a93f53909b00239cd8
    
        wm: Nullify hardkey function assignments if enabling the navbar
    
        This caused erroneous (and sometimes duplicate) events being generated
        due to the regular key function assignments. The navbar does its
        own action management, so don't try to derive from the actions
        usually present in hard keys.
    
        Change-Id: I82866e24547f8145cac4f07820ae90aacce09281
    
        Update DEV_FORCE_SHOW_NAVBAR constant.
    
        Change-Id: Ie5b4317162c514d22276956f81007e064a3d0f32
    
        Settings: Move DEV_FORCE_SHOW_NAVBAR load to loadSecureSettings.
    
        Change-Id: I6ac53b8c9f7fce6f9ca6b4ad7bf31a1c1e896863
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Tue Jan 16 15:47:07 2018 +0100
    
        PhoneWindowManager: Make sure KeyDisabler is always called on boot
    
        * Otherwise, some KeyDisabler classes are left in a weird state
        * Because we don't keep track of whether an initial state was ever set,
          we never call KeyDisabler on boot if the setting is set to 0
        * To remedy this, keep track of whether an initial state was set
    
        Change-Id: Ib432ed3278dd8f4f4cba3ba488879b3c1cd9c8f4
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sat Sep 29 20:42:04 2018 +0200
    
        PhoneWindowManager: Fix issues introduced with runtime navbar
    
        * With system settings we need to pass UserHandle.USER_CURRENT
          to make sure we are getting proper value, otherwise we always
          end up getting '0'. Also we need to make sure to set valid
          mHasNavigationBar in setInitialDisplaySize().
    
        Change-Id: I3efd614e735f9a602f13263a742ce858a9d14769
    
    Change-Id: I4a6d3f89bc171c3921875b24c077cb78c03517ad

--- packages/SystemUI/src/com/android/systemui/statusbar/NavigationBarController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NavigationBarController.java
@@ -22,6 +22,7 @@ import static com.android.systemui.Dependency.MAIN_HANDLER_NAME;
 import static com.android.systemui.SysUiServiceProvider.getComponent;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.hardware.display.DisplayManager;
 import android.os.Handler;
 import android.os.RemoteException;
@@ -61,6 +62,17 @@ public class NavigationBarController implements Callbacks {
     private final Handler mHandler;
     private final DisplayManager mDisplayManager;
 
+    public class SystemUiVisibility {
+        public int displayId;
+        public int vis;
+        public int fullscreenStackVis;
+        public int dockedStackVis;
+        public int mask;
+        public Rect fullscreenStackBounds;
+        public Rect dockedStackBounds;
+        public boolean navbarColorManagedByIme;
+    }
+
     /** A displayId - nav bar maps. */
     @VisibleForTesting
     SparseArray<NavigationBarFragment> mNavigationBars = new SparseArray<>();
@@ -83,8 +95,16 @@ public class NavigationBarController implements Callbacks {
 
     @Override
     public void onDisplayReady(int displayId) {
+        onDisplayReady(displayId, null);
+    }
+
+    public void onDisplayReady(int displayId, SystemUiVisibility systemUiVisibility) {
         Display display = mDisplayManager.getDisplay(displayId);
-        createNavigationBar(display, null);
+        createNavigationBar(display, null, systemUiVisibility);
+    }
+
+    public SystemUiVisibility createSystemUiVisibility() {
+        return new SystemUiVisibility();
     }
 
     // TODO(b/117478341): I use {@code includeDefaultDisplay} to make this method compatible to
@@ -112,6 +132,11 @@ public class NavigationBarController implements Callbacks {
      */
     @VisibleForTesting
     void createNavigationBar(Display display, RegisterStatusBarResult result) {
+        createNavigationBar(display, result, null);
+    }
+
+    void createNavigationBar(Display display, RegisterStatusBarResult result,
+            SystemUiVisibility systemUiVisibility) {
         if (display == null) {
             return;
         }
@@ -153,7 +178,18 @@ public class NavigationBarController implements Callbacks {
                     ? Dependency.get(AutoHideController.class)
                     : new AutoHideController(context, mHandler);
             navBar.setAutoHideController(autoHideController);
-            navBar.restoreSystemUiVisibilityState();
+
+            if (systemUiVisibility != null) {
+                navBar.setSystemUiVisibility(systemUiVisibility.displayId,
+                        systemUiVisibility.vis,
+                        systemUiVisibility.fullscreenStackVis,
+                        systemUiVisibility.dockedStackVis,
+                        systemUiVisibility.mask,
+                        systemUiVisibility.fullscreenStackBounds,
+                        systemUiVisibility.dockedStackBounds,
+                        systemUiVisibility.navbarColorManagedByIme);
+            }
+
             mNavigationBars.append(displayId, navBar);
 
             if (result != null) {
