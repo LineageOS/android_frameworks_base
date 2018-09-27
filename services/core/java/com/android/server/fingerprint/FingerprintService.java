4e58e3b909061a112182d3d2d8f0a8b630083fe8
Nikita <megaelebrus@ya.ru>
2018-09-27 15:20:30 +0300

    Fixing Goodix reset

--- services/core/java/com/android/server/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/fingerprint/FingerprintService.java
@@ -101,7 +101,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class FingerprintService extends SystemService implements IHwBinder.DeathRecipient {
     static final String TAG = "FingerprintService";
     static final boolean DEBUG = true;
-    private static final boolean CLEANUP_UNUSED_FP = true;
+    private static final boolean CLEANUP_UNUSED_FP = false;
     private static final String FP_DATA_DIR = "fpdata";
     private static final int MSG_USER_SWITCHING = 10;
     private static final String ACTION_LOCKOUT_RESET =
