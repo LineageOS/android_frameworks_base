4739407c1018a1b5874ec1f7e9d6c3477bcfe60b
LuK1337 <priv.luk@gmail.com>
2018-09-29 20:42:04 +0200

    PhoneWindowManager: Fix issues introduced with runtime navbar
    * With system settings we need to pass UserHandle.USER_CURRENT
      to make sure we are getting proper value, otherwise we always
      end up getting '0'. Also we need to make sure to set valid
      mHasNavigationBar in setInitialDisplaySize().
    
    Change-Id: I3efd614e735f9a602f13263a742ce858a9d14769

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2711,6 +2711,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         mNeedsNavigationBar = mHasNavigationBar;
+        mHasNavigationBar |= mForceNavbar == 1;
 
         // For demo purposes, allow the rotation of the HDMI display to be controlled.
         // By default, HDMI locks rotation to landscape.
@@ -2843,8 +2844,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 updateWakeGestureListenerLp();
             }
 
-            int forceNavbar = LineageSettings.System.getInt(resolver,
-                    LineageSettings.System.FORCE_SHOW_NAVBAR, 0);
+            int forceNavbar = LineageSettings.System.getIntForUser(resolver,
+                    LineageSettings.System.FORCE_SHOW_NAVBAR, 0,
+                    UserHandle.USER_CURRENT);
             if (forceNavbar != mForceNavbar) {
                 mForceNavbar = forceNavbar;
                 if (mLineageHardware.isSupported(LineageHardwareManager.FEATURE_KEY_DISABLE)) {
