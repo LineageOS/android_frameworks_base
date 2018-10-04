854332a5a19ea44f69d49ea9fb40662ba56980d1
DroidFreak32 <rushabshah32@gmail.com>
2018-10-04 11:33:07 +0530

    Screenshot: Append app name after screenshot date.
    Change-Id: I2ee5d65006ff22dfa381aae50e5757b8e7596e91

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -165,7 +165,7 @@ class SaveImageInBackgroundTask extends AsyncTask<Void, Void, Void> {
             // Replace all spaces and special chars with an underscore
             String appNameString = appName.toString().replaceAll("[\\\\/:*?\"<>|\\s]+", "_");
             mImageFileName = String.format(SCREENSHOT_FILE_NAME_TEMPLATE_APPNAME,
-                    appNameString, imageDate);
+                    imageDate, appNameString);
         } else {
             mImageFileName = String.format(SCREENSHOT_FILE_NAME_TEMPLATE, imageDate);
         }
