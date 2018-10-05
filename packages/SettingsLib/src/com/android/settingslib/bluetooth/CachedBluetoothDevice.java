9ff2a4037232c3fcb87eecb9f2eb4c837c31984e
Kiran Kelageri <kirankelageri@codeaurora.org>
2018-10-04 17:52:31 -0700

    Bluetooth-TWS: Disable outgoing connection to 2nd earbud
    Disbaling outgoing connection to 2nd earbud and removed
    vendor specific property.
    
    Change-Id: I912c3acd84f449b6f967d6ef2c7beaebca4ef05c
    CRs-fixed: 2326015

--- packages/SettingsLib/src/com/android/settingslib/bluetooth/CachedBluetoothDevice.java
+++ packages/SettingsLib/src/com/android/settingslib/bluetooth/CachedBluetoothDevice.java
@@ -55,6 +55,7 @@ public class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> 
     // Some Hearing Aids (especially the 2nd device) needs more time to do service discovery
     private static final long MAX_HEARING_AIDS_DELAY_FOR_AUTO_CONNECT = 15000;
     private static final long MAX_HOGP_DELAY_FOR_AUTO_CONNECT = 30000;
+    private static final boolean mIsTwsConnectEnabled = false;
 
     private final Context mContext;
     private final BluetoothAdapter mLocalAdapter;
@@ -675,7 +676,7 @@ public class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> 
         if (bondState == BluetoothDevice.BOND_BONDED) {
             if (mDevice.isBluetoothDock()) {
                 onBondingDockConnect();
-            } else if (SystemProperties.getBoolean("persist.vendor.btstack.connect.peer_earbud", false)) {
+            } else if (mIsTwsConnectEnabled) {
                 Log.d(TAG, "Initiating connection to" + mDevice);
                 if (mDevice.isBondingInitiatedLocally() || mDevice.isTwsPlusDevice()) {
                     connect(false);
