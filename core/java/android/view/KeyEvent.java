594acf53f02213292996608781698a05ace746a7
Jens Lody <jens@jenslody.de>
2018-10-15 17:09:50 +0200

    Add the home-key to the KeyEvent.isWakeKey-function, so it can be used to wakeup the device.
    Change-Id: If0e808d1fe531827142e44887bc392ec95db8ee0

--- core/java/android/view/KeyEvent.java
+++ core/java/android/view/KeyEvent.java
@@ -1870,6 +1870,7 @@ public class KeyEvent extends InputEvent implements Parcelable {
     /** @hide */
     public static final boolean isWakeKey(int keyCode) {
         switch (keyCode) {
+            case KeyEvent.KEYCODE_HOME:
             case KeyEvent.KEYCODE_BACK:
             case KeyEvent.KEYCODE_MENU:
             case KeyEvent.KEYCODE_WAKEUP:
