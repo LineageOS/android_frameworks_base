a3421e98a9ca19117c90f89b2bd1579a61f8fbd1
dianlujitao <dianlujitao@lineageos.org>
2018-11-15 11:33:20 +0800

    TunerServiceImpl: Blacklist Lineage settings from tuner reset
     * They use the tunable infrastructure but are real user settings and
       shouldn't be reset with tuner settings. Moreover, setting them to
       null would cause a IllegalArgumentException.
    
    Change-Id: Ie05910efaee867632a3bd2e84625d7735259130e

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -37,7 +37,11 @@ import com.android.systemui.DemoMode;
 import com.android.systemui.Dependency;
 import com.android.systemui.qs.QSTileHost;
 import com.android.systemui.settings.CurrentUserTracker;
+import com.android.systemui.statusbar.phone.ClockController;
+import com.android.systemui.statusbar.phone.NotificationPanelView;
+import com.android.systemui.statusbar.phone.StatusBar;
 import com.android.systemui.statusbar.phone.StatusBarIconController;
+import com.android.systemui.statusbar.policy.Clock;
 import com.android.systemui.util.leak.LeakDetector;
 
 import lineageos.providers.LineageSettings;
@@ -56,6 +60,13 @@ public class TunerServiceImpl extends TunerService {
     // Things that use the tunable infrastructure but are now real user settings and
     // shouldn't be reset with tuner settings.
     private static final String[] RESET_BLACKLIST = new String[] {
+            Clock.CLOCK_STYLE,
+            ClockController.CLOCK_POSITION,
+            StatusBar.FORCE_SHOW_NAVBAR,
+            StatusBar.SCREEN_BRIGHTNESS_MODE,
+            StatusBar.STATUS_BAR_BRIGHTNESS_CONTROL,
+            NotificationPanelView.DOUBLE_TAP_SLEEP_GESTURE,
+            NotificationPanelView.STATUS_BAR_QUICK_QS_PULLDOWN,
             QSTileHost.TILES_SETTING,
             Settings.Secure.DOZE_ALWAYS_ON
     };
