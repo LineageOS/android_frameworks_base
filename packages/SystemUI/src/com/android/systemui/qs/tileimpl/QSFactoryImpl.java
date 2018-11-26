f10a9c3756240fb5314482a086233c3219ece3ae
Dan Pasanen <dan.pasanen@gmail.com>
2018-11-26 08:16:37 -0600

    qs: add mono audio tile
    Change-Id: Idaf6cd84f82707ecb08d19a8c0080935ece790de

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -41,6 +41,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.MonoAudioTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.ProfilesTile;
@@ -120,6 +121,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new HeadsUpTile(mHost);
             case "livedisplay":
                 return new LiveDisplayTile(mHost);
+            case "mono_audio":
+                return new MonoAudioTile(mHost);
             case "profiles":
                 return new ProfilesTile(mHost);
             case "reading_mode":
