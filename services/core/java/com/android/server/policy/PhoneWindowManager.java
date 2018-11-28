0002a94b46fa602af0307a493e292b5e722ea4c1
chillger <alexander.loskutov@gmail.com>
2018-11-28 22:00:04 +0100

    Button action pull status bar. Expands the status bar when it is collapsed otherwise it gets collapsed.
    Dependent changes:
    Change-Id: I415b8b135de3feeefc7e4ca744c1590eacf107eb
    Change-Id: Iabab0d70b3bf06c07807aa62ed5d6f997e8e4dbd
    
    Change-Id: I54bf889935b62c06b10ae95f0578619e86807e33

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2092,9 +2092,18 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case SPLIT_SCREEN:
                 toggleSplitScreen();
                 break;
+            case PULL_STATUS_BAR:
+                toggleStatusBar();
+                break;
             default:
                 break;
         }
+    }
+
+    private void toggleStatusBar() {
+        StatusBarManager statusBar = (StatusBarManager) mContext.getSystemService(Context.STATUS_BAR_SERVICE);
+        if (statusBar != null)
+            statusBar.togglePanel();
     }
 
     private final Runnable mHomeDoubleTapTimeoutRunnable = new Runnable() {
