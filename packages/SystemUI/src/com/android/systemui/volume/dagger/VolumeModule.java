0ec0d6f0ffd253bb8ac7077b51d953da26061e16
a.derendyaev <a.derendyaev@magdv.com>
2018-12-19 21:57:45 +0800

    SystemUI: runtime configurable audio panel location
    * Updates for twelve (bgcngm / Arian)
      * Adapt to new UI
    
    * Updates for eleven (LuK1337):
      * Disable on TVs
      * Simplify changes once again.
    
    * Updates for ten (sam3000):
      * Squash in tuner service settings reset blacklist.
      * Remove layout xml changes that aren't needed.
      * Apply slide in/out animations to landscape too
        (they work fine).
      * Few other simplifications.
    
    * Placement is now determined at runtime by a lineage setting.
      Use TunerService to track settings change.
      Various cleanup and fix formatting.
      Fixed left/right gravity on expand arrow and ringer.
      Fix commit author.
      (sam3000)
    
    This is a squashed and adapted commit of the following patches:
    
    ----
    
      From cf187bd54fbd807e16b0ac3593e839008efd302e Mon Sep 17 00:00:00 2001
      From: "a.derendyaev" <a.derendyaev@magdv.com>
      Date: Wed, 19 Dec 2018 21:57:45 +0800
      Subject: [PATCH] SystemUI: allow devices override audio panel location
    
      Some devices have volume buttons on left side and it not fancy if volume panel will be at right side.
      You can override panel location using overlay for SystemUI:
    
      <!-- Allow devices override audio panel location to the left side -->
      <bool name="config_audioPanelOnLeftSide">true</bool>
    
      Change-Id: I8a2302867010b0e6a02efa68df57e534ce7bbf46
      Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>
      Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    
    ----
    
      From 9d0ff1efd26961a4f14d0821f50ebe80c2a65ff9 Mon Sep 17 00:00:00 2001
      From: Alex Cruz <alex@dirtyunicorns.com>
      Date: Fri, 21 Dec 2018 22:08:52 -0600
      Subject: [PATCH] Volume panel: Do the same with less
    
      Use common volume_dialog layout for both left- and right-aligned panel
    
      [@TheDorkKnightRises - POSP]
      - Tweak padding values to make them uniform across both alignments
      - Remove redundant comment
    
      Change-Id: If41456f71ffd18466166e7b4120ff34d9e6f5a46
      Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    ----
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Sun Jun 9 13:33:05 2019 -0700
    
        SystemUI: Blacklist volume panel on left setting from tuner reset
    
        Change-Id: I47ddb3b818cfa3548fef42fa686d0991e2a35605
    
    Author: programminghoch10 <hoch10@kabelbw.de>
    Date:   Mon Jul 12 17:55:28 2021 +0200
    
        SystemUI: VolumeDialogImpl: Fix volume panel permanently visible
    
        * The Volume Panel is permanently visible when changing the panel side while the panel is open until SystemUI is restarted
        * This change removes the Panel View when changing the panel side
    
        Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/3482
    
        Change-Id: Ic3ca5a196315e96024121c995cc6282fb74f6e4c
    
    Change-Id: I666420e97f0c7b4435c69ca773631e2d9e738a14
    (cherry picked from commit 232b17f1c507625c643a1616e9f12db828bd151c)

--- packages/SystemUI/src/com/android/systemui/volume/dagger/VolumeModule.java
+++ packages/SystemUI/src/com/android/systemui/volume/dagger/VolumeModule.java
@@ -27,6 +27,7 @@ import com.android.systemui.plugins.VolumeDialogController;
 import com.android.systemui.statusbar.policy.AccessibilityManagerWrapper;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
+import com.android.systemui.tuner.TunerService;
 import com.android.systemui.volume.VolumeComponent;
 import com.android.systemui.volume.VolumeDialogComponent;
 import com.android.systemui.volume.VolumeDialogImpl;
@@ -53,7 +54,8 @@ public interface VolumeModule {
             ConfigurationController configurationController,
             MediaOutputDialogFactory mediaOutputDialogFactory,
             ActivityStarter activityStarter,
-            InteractionJankMonitor interactionJankMonitor) {
+            InteractionJankMonitor interactionJankMonitor,
+            TunerService tunerService) {
         VolumeDialogImpl impl = new VolumeDialogImpl(
                 context,
                 volumeDialogController,
@@ -62,7 +64,8 @@ public interface VolumeModule {
                 configurationController,
                 mediaOutputDialogFactory,
                 activityStarter,
-                interactionJankMonitor);
+                interactionJankMonitor,
+                tunerService);
         impl.setStreamImportant(AudioManager.STREAM_SYSTEM, false);
         impl.setAutomute(true);
         impl.setSilentMode(false);
