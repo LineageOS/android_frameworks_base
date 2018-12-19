c5a75ad077da7d2ba2d45add88f2e8e2e30c9c07
a.derendyaev <a.derendyaev@magdv.com>
2018-12-19 21:57:45 +0800

    SystemUI: runtime configurable audio panel location
    * Updates for eleven (LuK1337):
      * Simplify changes once again.
    
    * Updates for ten (sam3000):
      * Squash in tuner service settings reset blacklist.
      * Remove layout xml changes that aren't needed.
      * Apply slide in/out animations to landscape too
        (they work fine).
      * Few other simplifications.
    
    * Placement is now determined at runtime by a lineage setting.
      Use TunerService to track settings change.
      Various cleanup and fix formatting.
      Fixed left/right gravity on expand arrow and ringer.
      Fix commit author.
      (sam3000)
    
    This is a squashed and adapted commit of the following patches:
    
    ----
    
      From cf187bd54fbd807e16b0ac3593e839008efd302e Mon Sep 17 00:00:00 2001
      From: "a.derendyaev" <a.derendyaev@magdv.com>
      Date: Wed, 19 Dec 2018 21:57:45 +0800
      Subject: [PATCH] SystemUI: allow devices override audio panel location
    
      Some devices have volume buttons on left side and it not fancy if volume panel will be at right side.
      You can override panel location using overlay for SystemUI:
    
      <!-- Allow devices override audio panel location to the left side -->
      <bool name="config_audioPanelOnLeftSide">true</bool>
    
      Change-Id: I8a2302867010b0e6a02efa68df57e534ce7bbf46
      Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>
      Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    
    ----
    
      From 9d0ff1efd26961a4f14d0821f50ebe80c2a65ff9 Mon Sep 17 00:00:00 2001
      From: Alex Cruz <alex@dirtyunicorns.com>
      Date: Fri, 21 Dec 2018 22:08:52 -0600
      Subject: [PATCH] Volume panel: Do the same with less
    
      Use common volume_dialog layout for both left- and right-aligned panel
    
      [@TheDorkKnightRises - POSP]
      - Tweak padding values to make them uniform across both alignments
      - Remove redundant comment
    
      Change-Id: If41456f71ffd18466166e7b4120ff34d9e6f5a46
      Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    ----
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Sun Jun 9 13:33:05 2019 -0700
    
        SystemUI: Blacklist volume panel on left setting from tuner reset
    
        Change-Id: I47ddb3b818cfa3548fef42fa686d0991e2a35605
    
    ----
    
    Change-Id: Ie4e2a6660f76a45e4b8959471165655c0f06b18d
    
    extras
    
    Change-Id: If22c97eb1ce4cfb13396d182d90786f4b7acaee7

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -65,6 +65,7 @@ import android.util.Log;
 import android.util.Slog;
 import android.util.SparseBooleanArray;
 import android.view.ContextThemeWrapper;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.AccessibilityDelegate;
@@ -96,6 +97,9 @@ import com.android.systemui.plugins.VolumeDialogController.StreamState;
 import com.android.systemui.statusbar.policy.AccessibilityManagerWrapper;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
+import com.android.systemui.tuner.TunerService;
+
+import lineageos.providers.LineageSettings;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -114,6 +118,9 @@ public class VolumeDialogImpl implements VolumeDialog,
 
     private static final long USER_ATTEMPT_GRACE_PERIOD = 1000;
     private static final int UPDATE_ANIMATION_DURATION = 80;
+
+    public static final String SETTING_VOLUME_PANEL_ON_LEFT =
+            "lineagesecure:" + LineageSettings.Secure.VOLUME_PANEL_ON_LEFT;
 
     static final int DIALOG_TIMEOUT_MILLIS = 3000;
     static final int DIALOG_SAFETYWARNING_TIMEOUT_MILLIS = 5000;
@@ -164,6 +171,9 @@ public class VolumeDialogImpl implements VolumeDialog,
     private ViewStub mODICaptionsTooltipViewStub;
     private View mODICaptionsTooltipView = null;
 
+    // Volume panel placement left or right
+    private boolean mVolumePanelOnLeft;
+
     public VolumeDialogImpl(Context context) {
         mContext =
                 new ContextThemeWrapper(context, R.style.qs_theme);
@@ -175,6 +185,7 @@ public class VolumeDialogImpl implements VolumeDialog,
         mShowActiveStreamOnly = showActiveStreamOnly();
         mHasSeenODICaptionsTooltip =
                 Prefs.getBoolean(context, Prefs.Key.HAS_SEEN_ODI_CAPTIONS_TOOLTIP, false);
+        Dependency.get(TunerService.class).addTunable(mTunable, SETTING_VOLUME_PANEL_ON_LEFT);
     }
 
     @Override
@@ -223,6 +234,8 @@ public class VolumeDialogImpl implements VolumeDialog,
         lp.setTitle(VolumeDialogImpl.class.getSimpleName());
         lp.windowAnimations = -1;
         lp.gravity = mContext.getResources().getInteger(R.integer.volume_dialog_gravity);
+        lp.gravity &= ~(Gravity.LEFT | Gravity.RIGHT);
+        lp.gravity |= mVolumePanelOnLeft ? Gravity.LEFT : Gravity.RIGHT;
         mWindow.setAttributes(lp);
         mWindow.setLayout(WRAP_CONTENT, WRAP_CONTENT);
 
@@ -306,6 +319,27 @@ public class VolumeDialogImpl implements VolumeDialog,
         initSettingsH();
         initODICaptionsH();
     }
+
+    private float getAnimatorX() {
+        final float x = mDialogView.getWidth() / 2.0f;
+        return mVolumePanelOnLeft ? -x : x;
+    }
+
+    private final TunerService.Tunable mTunable = new TunerService.Tunable() {
+        @Override
+        public void onTuningChanged(String key, String newValue) {
+            if (key.equals(SETTING_VOLUME_PANEL_ON_LEFT)) {
+                final boolean volumePanelOnLeft = TunerService.parseIntegerSwitch(newValue, false);
+                if (mVolumePanelOnLeft != volumePanelOnLeft) {
+                    mVolumePanelOnLeft = volumePanelOnLeft;
+                    mHandler.post(() -> {
+                        // Trigger panel rebuild on next show
+                        mConfigChanged = true;
+                    });
+                }
+            }
+        }
+    };
 
     protected ViewGroup getDialogView() {
         return mDialogView;
@@ -758,7 +792,7 @@ public class VolumeDialogImpl implements VolumeDialog,
                     tryToRemoveCaptionsTooltip();
                     mIsAnimatingDismiss = false;
                 }, 50));
-        if (!isLandscape()) animator.translationX(mDialogView.getWidth() / 2.0f);
+        animator.translationX(getAnimatorX());
         animator.start();
         checkODICaptionsTooltip(true);
         mController.notifyVisible(false);
