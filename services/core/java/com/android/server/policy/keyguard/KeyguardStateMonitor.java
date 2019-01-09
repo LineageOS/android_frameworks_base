937fdc935b800400f1bf367b01f8feb8de91dd37
Joey <joey@lineageos.org>
2019-01-09 22:22:46 +0100

    base: add Trust usb restrictor
    This is a squash of following commits:
    
    Author: Joey <joey@lineageos.org>
    Date:   Wed Jan 9 22:22:46 2019 +0100
    
        base: add Trust usb restrictor
    
        Change-Id: Ie2cb8e91d0f13f0d1473a48d22839b378701c756
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Han Wang <416810799@qq.com>
    Date:   Fri Jun 7 08:30:54 2019 +0200
    
        KeyguardStateMonitor: Don't get IUsbRestrict service in constructor
    
         * If boot up is "too fast" and Trust HAL starts a bit slowly,
           we may end up getting a NULL IUsbRestrict service in
           constructor. This causes a NPE later in onShowingStateChanged().
    
         * To avoid this, we just try to get IUsbRestrict service in
           onShowingStateChanged(). This adds a little overhead, but prevents
           possible crashes.
    
        Change-Id: Idc09cc1700aad8753244789a1f14ea1d5267f77c
    
    Change-Id: Idc09cc1700aad8233244789a1f14ea1d5267f223

--- services/core/java/com/android/server/policy/keyguard/KeyguardStateMonitor.java
+++ services/core/java/com/android/server/policy/keyguard/KeyguardStateMonitor.java
@@ -18,6 +18,7 @@ package com.android.server.policy.keyguard;
 
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.os.RemoteException;
 import android.util.Slog;
 
@@ -25,7 +26,11 @@ import com.android.internal.policy.IKeyguardService;
 import com.android.internal.policy.IKeyguardStateCallback;
 import com.android.internal.widget.LockPatternUtils;
 
+import lineageos.providers.LineageSettings;
+import vendor.lineage.trust.V1_0.IUsbRestrict;
+
 import java.io.PrintWriter;
+import java.util.NoSuchElementException;
 
 /**
  * Maintains a cached copy of Keyguard's state.
@@ -51,10 +56,14 @@ public class KeyguardStateMonitor extends IKeyguardStateCallback.Stub {
     private final LockPatternUtils mLockPatternUtils;
     private final StateCallback mCallback;
 
+    private IUsbRestrict mUsbRestrictor;
+    private ContentResolver mContentResolver;
+
     public KeyguardStateMonitor(Context context, IKeyguardService service, StateCallback callback) {
         mLockPatternUtils = new LockPatternUtils(context);
         mCurrentUserId = ActivityManager.getCurrentUser();
         mCallback = callback;
+        mContentResolver = context.getContentResolver();
 
         try {
             service.addStateMonitorCallback(this);
@@ -88,6 +97,28 @@ public class KeyguardStateMonitor extends IKeyguardStateCallback.Stub {
         mIsShowing = showing;
 
         mCallback.onShowingChanged();
+
+        if (mUsbRestrictor == null) {
+            try {
+                mUsbRestrictor = IUsbRestrict.getService();
+                if (mUsbRestrictor == null) {
+                    // Ignore, the hal is not available
+                    return;
+                }
+            } catch (NoSuchElementException | RemoteException ignored) {
+                return;
+            }
+        }
+
+        boolean shouldRestrictUsb = LineageSettings.Secure.getInt(mContentResolver,
+                LineageSettings.Secure.TRUST_RESTRICT_USB_KEYGUARD, 0) == 1;
+        if (shouldRestrictUsb) {
+            try {
+                mUsbRestrictor.setEnabled(showing);
+            } catch (RemoteException ignored) {
+                // This feature is not supported
+            }
+        }
     }
 
     @Override // Binder interface
