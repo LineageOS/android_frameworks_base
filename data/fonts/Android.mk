678d6a804963eb3aee31c88eeace01b70b842050
Akianonymus <anonymus.aki@gmail.com>
2019-01-11 01:16:09 +0530

    fonts: Build different fonts.xml if EXCLUDE_SERIF_FONTS is true
    * Google introduced a commit to exclude serif fonts ( for saving some system space)
    https://android.googlesource.com/platform/external/noto-fonts/+/c1fe8849cbec6ccc0d315e88936485bb53fe34a3
    
    Now if the flag is used, then it will not build the fallback serif fonts but the fonts.xml still have the fallback serif font file mapping which results in below logspam
    
    E/Typeface: Error mapping font file /system/fonts/NotoSerifEthiopic-Regular.otf
    E/Typeface: Error mapping font file /system/fonts/NotoSerifEthiopic-Bold.otf
    E/Typeface: Error mapping font file /system/fonts/NotoSerifCJK-Regular.ttc and so on......
    
    So, add a check for EXCLUDE_SERIF_FONTS flag and copy respective fonts.xml
    
    * Also, Fix compiling generic fonts
    
    Co-authored-by: neobuddy89 <neobuddy89@gmail.com>
    
    Signed-off-by: Akianonymus <anonymus.aki@gmail.com>
    Change-Id: Icef8903c2f5b5bb928c1cea47dca8ef5714b154b

--- data/fonts/Android.mk
+++ data/fonts/Android.mk
@@ -90,7 +90,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := fonts.xml
 LOCAL_MODULE_CLASS := ETC
 
+ifeq ($(filter true,$(EXCLUDE_SERIF_FONTS) $(SMALLER_FONT_FOOTPRINT)),)
+AOSP_FONTS_FILE := frameworks/base/data/fonts/exclude_serif/fonts.xml
+else
 AOSP_FONTS_FILE := frameworks/base/data/fonts/fonts.xml
+endif
 
 ifdef ADDITIONAL_FONTS_FILE
 ADDITIONAL_FONTS_SCRIPT := frameworks/base/tools/fonts/add_additional_fonts.py
