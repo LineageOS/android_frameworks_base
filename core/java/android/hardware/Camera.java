99242fe3f2f5a6a1cfd869d2ff3aa42d8f0596ef
Nvertigo <nvertigo67@gmail.com>
2019-02-18 19:32:23 +0100

    Fix: "Camera: Simplify code for Aux/Mono white and black lists".
      Without importing java.util.Arrays 241215 breaks the build with:
    
    frameworks/base/core/java/android/hardware/Camera.java:302: error: cannot find symbol
                if (Arrays.asList(packageList.split(",")).contains(packageName)) {
                    ^
      symbol:   variable Arrays
      location: class Camera
    frameworks/base/core/java/android/hardware/Camera.java:307: error: cannot find symbol
                if (Arrays.asList(packageBlacklist.split(",")).contains(packageName)) {
                    ^
      symbol:   variable Arrays
      location: class Camera
    2 errors
    
    Change-Id: I1fef3ea2472e8b92412afbab7ca36f5648e1a4ec

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -54,6 +54,7 @@ import com.android.internal.app.IAppOpsService;
 
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
