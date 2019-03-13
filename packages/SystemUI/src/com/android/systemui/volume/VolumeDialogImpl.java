de8c184b599c668f2330ead2c9876bf329beadeb
Giuseppe Maggio <jertlok@protonmail.com>
2019-03-13 22:31:57 +0100

    VolumeDialog: Track the ally stream
    - Introduce the concept of tracking ally streams, useful
      when you are still on a call, for instance, but you also
      want to change the media volume for a future use. When
      you tap on the media row the calling row will disappear
      because of AOSP logic. With this patch, we track this
      ally and make it available on the expanded panel.
    
    Change-Id: I23792369cf1fd8e0b4d6913c67d02bd4c09f1ac7
    Co-authored-by: Arian <arian.kulmer@web.de>
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -166,6 +166,7 @@ public class VolumeDialogImpl implements VolumeDialog,
     private boolean mShowA11yStream;
 
     private int mActiveStream;
+    private int mAllyStream;
     private int mPrevActiveStream;
     private boolean mAutomute = VolumePrefs.DEFAULT_ENABLE_AUTOMUTE;
     private boolean mSilentMode = VolumePrefs.DEFAULT_ENABLE_SILENT_MODE;
@@ -353,6 +354,8 @@ public class VolumeDialogImpl implements VolumeDialog,
         initRingerH();
         initSettingsH();
         initODICaptionsH();
+
+        mAllyStream = -1;
     }
 
     // Helper to set layout gravity.
@@ -575,6 +578,10 @@ public class VolumeDialogImpl implements VolumeDialog,
             });
         }
 
+        if (mAllyStream == -1) {
+            mAllyStream = mActiveStream;
+        }
+
         if (mExpandRowsView != null) {
             mExpandRowsView.setVisibility(
                     mDeviceProvisionedController.isCurrentUserSetup() &&
@@ -588,7 +595,8 @@ public class VolumeDialogImpl implements VolumeDialog,
                 if (!isNotificationVolumeLinked()) Util.setVisOrGone(
                         findRow(AudioManager.STREAM_NOTIFICATION).view, !mExpanded);
 
-                if (mExpanded) mController.setActiveStream(AudioManager.STREAM_MUSIC);
+                Util.setVisOrGone(findRow(mAllyStream).view, true);
+                if (mExpanded) mController.setActiveStream(mAllyStream);
                 mExpanded = !mExpanded;
                 mExpandRows.setExpanded(mExpanded);
             });
@@ -876,6 +884,7 @@ public class VolumeDialogImpl implements VolumeDialog,
                     tryToRemoveCaptionsTooltip();
                     mExpanded = false;
                     mExpandRows.setExpanded(mExpanded);
+                    mAllyStream = -1;
                 }, 50));
         animator.translationX(getAnimatorX());
         animator.start();
