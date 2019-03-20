c02403a55063b9c4bef73755a76cf2f3a62950e9
Naresh Tanniru <ntanniru@codeaurora.org>
2019-03-20 19:35:03 +0530

    audio: Add support for audio extended codecs
    - Add support for APTX adaptive and CELT
      formats
    
    Change-Id: I735a297acc4d3d4c7f8d2cc9302a13ebcf4e4c3d
    CRs-Fixed: 2433041

--- services/core/java/com/android/server/audio/BtHelper.java
+++ services/core/java/com/android/server/audio/BtHelper.java
@@ -1005,6 +1005,10 @@ public class BtHelper {
                 return AudioSystem.AUDIO_FORMAT_APTX_HD;
             case BluetoothCodecConfig.SOURCE_CODEC_TYPE_LDAC:
                 return AudioSystem.AUDIO_FORMAT_LDAC;
+            case BluetoothCodecConfig.SOURCE_CODEC_TYPE_CELT:
+                return AudioSystem.AUDIO_FORMAT_CELT;
+            case BluetoothCodecConfig.SOURCE_CODEC_TYPE_APTX_ADAPTIVE:
+                return AudioSystem.AUDIO_FORMAT_APTX_ADAPTIVE;
             default:
                 return AudioSystem.AUDIO_FORMAT_DEFAULT;
         }
