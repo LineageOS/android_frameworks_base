1cc8e0e5f120cfbc528fd5287852709c6183f509
Eran Messeri <eranm@google.com>
2019-03-25 14:31:04 +0000

    Permission Check For DPM.getPermittedAccessibilityServices
    Bug: 128599660
    Test: com.android.server.devicepolicy.DevicePolicyManagerTest
    Test: com.google.android.gts.devicepolicy.DeviceOwnerTest
    Change-Id: I8be915bd6a4ff99884d23005a4c6f0100806dbe8
    Merged-In: I8ee3f876fcaffa63636645f0f59709cd147254ef
    (cherry picked from commit 4fd13eefcf99d9b9b0d5f5ea99fdc7c799c83d23)

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -7831,6 +7831,7 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
         if (!mHasFeature) {
             return null;
         }
+        enforceManageUsers();
         synchronized (this) {
             List<String> result = null;
             // If we have multiple profiles we return the intersection of the
