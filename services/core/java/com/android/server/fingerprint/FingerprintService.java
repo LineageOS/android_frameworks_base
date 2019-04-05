ee2f2dacbb14f24dd5d773c078d7d0312bdc1868
Peter Cai <peter@typeblog.net>
2019-04-05 16:44:18 +0200

    Initial support for in-display fingerprint sensors
    Change-Id: I52beaf6492b9daa012b5d42b858d552d1e378a2c

--- services/core/java/com/android/server/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/fingerprint/FingerprintService.java
@@ -91,6 +91,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import vendor.lineage.biometrics.fingerprint.inscreen.V1_0.IFingerprintInscreen;
+
 /**
  * A service to manage multiple clients that want to access the fingerprint HAL API.
  * The service is responsible for maintaining a list of clients and dispatching all
@@ -150,6 +152,8 @@ public class FingerprintService extends SystemService implements IHwBinder.Death
 
     private IBinder mToken = new Binder(); // used for internal FingerprintService enumeration
     private ArrayList<UserFingerprint> mUnknownFingerprints = new ArrayList<>(); // hw fingerprints
+
+    private IFingerprintInscreen mExtDaemon;
 
     private class UserFingerprint {
         Fingerprint f;
@@ -397,6 +401,22 @@ public class FingerprintService extends SystemService implements IHwBinder.Death
     }
 
     protected void handleError(long deviceId, int error, int vendorCode) {
+        if (mExtDaemon == null) {
+            try {
+                mExtDaemon = IFingerprintInscreen.getService();
+            } catch (RemoteException e) {
+                // do nothing
+            }
+        }
+
+        try {
+            if (mExtDaemon != null && !mExtDaemon.shouldHandleError(error)) {
+                return;
+            }
+        } catch (RemoteException e) {
+            // do nothing
+        }
+
         ClientMonitor client = mCurrentClient;
         if (client instanceof InternalRemovalClient || client instanceof InternalEnumerateClient) {
             clearEnumerateState();
@@ -955,7 +975,7 @@ public class FingerprintService extends SystemService implements IHwBinder.Death
         final int groupId = userId; // default group for fingerprint enrollment
 
         EnrollClient client = new EnrollClient(getContext(), mHalDeviceId, token, receiver,
-                userId, groupId, cryptoToken, restricted, opPackageName) {
+                userId, groupId, cryptoToken, restricted, opPackageName, mStatusBarService) {
 
             @Override
             public IBiometricsFingerprint getFingerprintDaemon() {
