a8fc228a4dc0982c15b394a7a95fb8aa55dc1994
Marko Man <darkobas@gmail.com>
2019-04-11 14:32:14 +0200

    fod: differentiate dreaming and pulse
    add pulse callback, change icons accordingly
    
    Change-Id: I76758379059b846c187a7efe11835382872074e0

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitorCallback.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitorCallback.java
@@ -296,4 +296,9 @@ public class KeyguardUpdateMonitorCallback {
      */
     public void onLogoutEnabledChanged() { }
 
+    /**
+     * Called when a pulsing is received.
+     */
+    public void onPulsing(boolean pulsing) { }
+
 }
