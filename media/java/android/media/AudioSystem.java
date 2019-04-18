526de74cad6bd78ce980874e793c0df0d90dd002
Dhananjay Kumar <dhakumar@codeaurora.org>
2019-04-18 21:52:36 +0530

    audio: Add support for APTX TWSP audio codec
    - Add support for APTX TWSP audio format for BT.
    
    CRs-Fixed: 2433041
    Change-Id: I19e256ae981516e9054b6e6eed8372ae2929b006

--- media/java/android/media/AudioSystem.java
+++ media/java/android/media/AudioSystem.java
@@ -153,6 +153,7 @@ public class AudioSystem
     public static final int AUDIO_FORMAT_LDAC           = 0x23000000;
     public static final int AUDIO_FORMAT_CELT           = 0x26000000;
     public static final int AUDIO_FORMAT_APTX_ADAPTIVE  = 0x27000000;
+    public static final int AUDIO_FORMAT_APTX_TWSP      = 0x2A000000;
 
     /**
      * Convert audio format enum values to Bluetooth codec values
@@ -167,6 +168,8 @@ public class AudioSystem
             case AUDIO_FORMAT_CELT: return BluetoothCodecConfig.SOURCE_CODEC_TYPE_CELT;
             case AUDIO_FORMAT_APTX_ADAPTIVE:
                      return BluetoothCodecConfig.SOURCE_CODEC_TYPE_APTX_ADAPTIVE;
+            case AUDIO_FORMAT_APTX_TWSP:
+                     return BluetoothCodecConfig.SOURCE_CODEC_TYPE_APTX_TWSP;
             default: return BluetoothCodecConfig.SOURCE_CODEC_TYPE_INVALID;
         }
     }
