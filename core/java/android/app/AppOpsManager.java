b67a3a3a2ede93b7b1370d08787d8123371559ec
ccc <cadappl@gmail.com>
2019-04-30 17:47:16 +0800

    appops: put @hide back to opToDefaultMode
    The annotation @hide to opToDefaultMode was removed in
    0ef3e79d92943bb480515914f1f5e105f1feafb6 and caused the update of
    api/current.txt, which breaks down official SDK. Here it's to add
    the annotation back to avoid the problem.
    
    Change-Id: I17bc20bfa2357d94caffb3f8ed83b9abbdc6f7d4

--- core/java/android/app/AppOpsManager.java
+++ core/java/android/app/AppOpsManager.java
@@ -1749,6 +1749,10 @@ public class AppOpsManager {
         return sOpDefaultMode[op];
     }
 
+    /**
+     * Retrieve the default mode for the operation.
+     * @hide
+     */
     public static int opToDefaultMode(int op) {
         return opToDefaultMode(op, false);
     }
