8ad0ae72446750ae8d666bba081d62014f2a4f24
Vachounet <vachounet@live.fr>
2019-05-08 14:20:07 +0200

    AudioService: Remove Analog Dock from fixed-volume devices
    * Many devices identifying as "Analog Docks" have not built in volume controls
      and rely on software volume (See: All Motorola Mods, older 3.5mm docks,
      etc.)
    
    Change-Id: Ie9b97c7b17b882a5d804db398d149bd777ccf5a4
    (cherry picked from commit 24ce6c3ea58f339cc8a537205e30d2516d2b073b)

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -18,7 +18,7 @@ package com.android.server.audio;
 
 import static android.Manifest.permission.REMOTE_AUDIO_PLAYBACK;
 import static android.media.AudioManager.RINGER_MODE_NORMAL;
-import static android.media.AudioManager.RINGER_MODE_SILENT;
+import static android.media.AudioManager.RINGER_MODE_SIDOCKLENT;
 import static android.media.AudioManager.RINGER_MODE_VIBRATE;
 import static android.media.AudioManager.STREAM_SYSTEM;
 import static android.os.Process.FIRST_APPLICATION_UID;
@@ -542,7 +542,6 @@ public class AudioService extends IAudioService.Stub
     // Devices for which the volume is fixed (volume is either max or muted)
     Set<Integer> mFixedVolumeDevices = new HashSet<>(Arrays.asList(
             AudioSystem.DEVICE_OUT_DGTL_DOCK_HEADSET,
-            AudioSystem.DEVICE_OUT_ANLG_DOCK_HEADSET,
             AudioSystem.DEVICE_OUT_HDMI_ARC,
             AudioSystem.DEVICE_OUT_SPDIF,
             AudioSystem.DEVICE_OUT_AUX_LINE));
@@ -553,9 +552,9 @@ public class AudioService extends IAudioService.Stub
     // by the audio mode (e.g. media playback in MODE_NORMAL, and phone calls in MODE_IN_CALL).
     Set<Integer> mAbsVolumeMultiModeCaseDevices = new HashSet<>(
             Arrays.asList(AudioSystem.DEVICE_OUT_HEARING_AID));
-
+              
     private final boolean mMonitorRotation;
-
+              
     private boolean mDockAudioMediaEnabled = true;
 
     private int mDockState = Intent.EXTRA_DOCK_STATE_UNDOCKED;
