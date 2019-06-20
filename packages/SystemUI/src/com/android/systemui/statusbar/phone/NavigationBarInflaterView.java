3c02709c09758010726c22a3f42ffd4edf74c206
Songer Travis <tsonger20@gmail.com>
2019-06-21 01:06:00 +0200

    Revert "SystemUI: add left and right virtual buttons while typing"
    This reverts commit 481f96bde7128b34ca4a02efcfd73e94cf3b6ca8.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: Id6323abb87c52c8d993f6ec456ccd1df214bdc02

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java
@@ -475,9 +475,7 @@ public class NavigationBarInflaterView extends FrameLayout
 
     private void clearAllChildren(ViewGroup group) {
         for (int i = 0; i < group.getChildCount(); i++) {
-            if (group.getChildAt(i).getId() != R.id.dpad_group) {
-                ((ViewGroup) group.getChildAt(i)).removeAllViews();
-            }
+            ((ViewGroup) group.getChildAt(i)).removeAllViews();
         }
     }
 
