4c49718f0d2278cf7dcab4c2eb12c48492177db4
Aaron Kling <webgeek1234@gmail.com>
2019-08-15 20:13:24 -0500

    Add Nvidia ShieldTech hooks [1/2]
    These are used to handle certain input related features
    
    This patch was released by Nvidia, but without history or
    authorship. It was then modified slightly to be less invasive on
    devices not using this functionality.
    
    Change-Id: I04e55b4a2d6193d54accf6cbfc01590c35b2e8bb

--- services/core/java/com/android/server/wm/InputMonitor.java
+++ services/core/java/com/android/server/wm/InputMonitor.java
@@ -368,6 +368,9 @@ final class InputMonitor {
         ProtoLog.d(WM_DEBUG_FOCUS_LIGHT, "Input focus has changed to %s", newWindow);
 
         if (newWindow != mInputFocus) {
+            String packageName = (newWindow != null && newWindow.mAttrs != null) ? newWindow.mAttrs.packageName : null;
+            com.nvidia.shieldtech.NvHookHelper.notifyInputFocusChange(packageName);
+
             if (newWindow != null && newWindow.canReceiveKeys()) {
                 // Displaying a window implicitly causes dispatching to be unpaused.
                 // This is to protect against bugs if someone pauses dispatching but
