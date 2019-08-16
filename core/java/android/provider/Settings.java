d33dd728aa977ccc2d9d3293d873843b70042437
Oliver Scott <olivercscott@gmail.com>
2019-08-15 19:40:55 -0700

    fw/b: Add setting to allow tethering to use VPN upstreams
    Change-Id: I77ed0142e653f4993486eea44e4dac21e3f67f17

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13935,6 +13935,17 @@ public final class Settings {
                 "extra_automatic_power_save_mode";
 
         /**
+         * Whether tethering is allowed to use VPN upstreams. (0 = false, 1 = true)
+         * Any changes here must also be reflected in:
+         * packages/modules/Connectivity/Tethering/src/com/android/networkstack/tethering/
+         * Tethering.java
+         * packages/modules/Connectivity/Tethering/src/com/android/networkstack/tethering/
+         * UpstreamNetworkMonitor.java
+         * @hide
+         */
+        public static final String TETHERING_ALLOW_VPN_UPSTREAMS = "tethering_allow_vpn_upstreams";
+
+        /**
          * Whether contextual screen timeout is enabled.
          *
          * @hide
--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -309,6 +309,7 @@ public class SecureSettings {
         Settings.Secure.ON_DEVICE_INFERENCE_UNBIND_TIMEOUT_MS,
         Settings.Secure.ON_DEVICE_INTELLIGENCE_IDLE_TIMEOUT_MS,
         Settings.Secure.FIRST_ON_DEVICE_MODELS_DOWNLOADED_TIME,
+        Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
         Settings.Secure.CONTENT_SAFETY_IDLE_TIMEOUT_MS,
         Settings.Secure.CONTENT_SAFETY_UNBIND_TIMEOUT_MS,
         Settings.Secure.CONTENT_SAFETY_SANDBOXED_UNBIND_TIMEOUT_MS,
