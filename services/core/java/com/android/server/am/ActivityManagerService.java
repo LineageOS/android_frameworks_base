a0385ab4e9c49152fe1da310414f643b1e592d4a
Christopher Tate <ctate@google.com>
2019-08-19 16:16:20 -0700

    DO NOT MERGE - Kill apps outright for API contract violations
    ...rather than relying on in-app code to perform the shutdown.
    
    Backport of security fix.
    
    Bug: 128649910
    Bug: 140108616
    Test: manual
    Test: atest OsHostTests#testForegroundServiceBadNotification
    Change-Id: I94d9de50bb03c33666471e3dbd9c721e9278f7cb
    Merged-In: I94d9de50bb03c33666471e3dbd9c721e9278f7cb
    (cherry picked from commit a79b6ba5c59dc6aaa8adbe1ffa3ee4b761f45e7f)

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -5853,7 +5853,7 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     @Override
     public void crashApplication(int uid, int initialPid, String packageName, int userId,
-            String message) {
+            String message, boolean force) {
         if (checkCallingPermission(android.Manifest.permission.FORCE_STOP_PACKAGES)
                 != PackageManager.PERMISSION_GRANTED) {
             String msg = "Permission Denial: crashApplication() from pid="
@@ -5865,7 +5865,8 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
 
         synchronized(this) {
-            mAppErrors.scheduleAppCrashLocked(uid, initialPid, packageName, userId, message);
+            mAppErrors.scheduleAppCrashLocked(uid, initialPid, packageName, userId,
+                    message, force);
         }
     }
 
