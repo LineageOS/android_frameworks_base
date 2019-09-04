796bd751ead1e2bb4235f12fa5626f1745e6168a
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-04 17:42:29 +0200

    PrivacyItemController: Enable permission hub by default
    Change-Id: I7f3642602c6c5f16dae99dd5608d758fe0f246b4

--- packages/SystemUI/src/com/android/systemui/privacy/PrivacyItemController.kt
+++ packages/SystemUI/src/com/android/systemui/privacy/PrivacyItemController.kt
@@ -81,7 +81,7 @@ class PrivacyItemController @Inject constructor(
 
     fun isAllIndicatorsEnabled(): Boolean {
         return deviceConfigProxy.getBoolean(DeviceConfig.NAMESPACE_PRIVACY,
-                ALL_INDICATORS, false)
+                ALL_INDICATORS, true)
     }
 
     private fun isMicCameraEnabled(): Boolean {
