d98e9c9ad1dc0c698530aef88f68f3c6cd8d9c4f
TheScarastic <warabhishek@gmail.com>
2019-09-10 11:07:39 +0530

    FODCircleView: Add MIUI FP icon
    Change-Id: If357b8eb0da17b1ea97a2ab5ca880d465c33adc9

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -282,7 +282,7 @@ public class FODCircleView extends ImageView {
         updateAlpha();
 
         mPaintFingerprint.setColor(mColor);
-        setImageDrawable(null);
+        setImageResource(R.drawable.fod_icon_pressed);
         invalidate();
     }
 
