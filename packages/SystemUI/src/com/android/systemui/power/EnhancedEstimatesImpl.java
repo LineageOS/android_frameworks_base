9eab75af8aa68d53f90b7dab1f08b216826e26cf
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-12 15:38:15 -0300

    SystemUI: Add method for getting estimated battery time
    Adds an estimated time remaining beside the battery icon in the QS.
    
    Change-Id: I8eda81c0cf7035b88e1a2adcdd5fdedb8d35c038

--- packages/SystemUI/src/com/android/systemui/power/EnhancedEstimatesImpl.java
+++ packages/SystemUI/src/com/android/systemui/power/EnhancedEstimatesImpl.java
@@ -1,6 +1,16 @@
 package com.android.systemui.power;
 
+import android.content.Context;
+import android.os.BatteryStats;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.os.UserManager;
+
+import com.android.internal.os.BatteryStatsHelper;
+
 import com.android.settingslib.fuelgauge.Estimate;
+import com.android.settingslib.fuelgauge.EstimateKt;
+import com.android.settingslib.utils.PowerUtil;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -8,17 +18,34 @@ import javax.inject.Singleton;
 @Singleton
 public class EnhancedEstimatesImpl implements EnhancedEstimates {
 
+    private BatteryStatsHelper mBatteryStatsHelper;
+    private UserManager mUserManager;
+
     @Inject
-    public EnhancedEstimatesImpl() {
+    public EnhancedEstimatesImpl(Context context) {
+        mBatteryStatsHelper = new BatteryStatsHelper(context);
+        mUserManager = context.getSystemService(UserManager.class);
     }
 
     @Override
     public boolean isHybridNotificationEnabled() {
-        return false;
+        return true;
     }
 
     @Override
     public Estimate getEstimate() {
+        try {
+            BatteryStats stats = mBatteryStatsHelper.getStats();
+            if (stats != null) {
+                long remaining = stats.computeBatteryTimeRemaining(PowerUtil.convertMsToUs(
+                        SystemClock.elapsedRealtime()));
+                if (remaining != -1) {
+                    return new Estimate(PowerUtil.convertUsToMs(remaining), false,
+                            EstimateKt.AVERAGE_TIME_TO_DISCHARGE_UNKNOWN);
+                }
+            }
+        } catch (Exception e) {
+        }
         return null;
     }
 
