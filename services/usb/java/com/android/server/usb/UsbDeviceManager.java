05a430ab9db708f452d3b244bc9afd4c2de7533b
Dan Sandler <dsandler@android.com>
2019-09-13 17:37:22 -0400

    Updates to various icons:
     - updated android head in launcher icons
     - USB notifications now use a USB icon instead of the
       platform logo (ADB still uses the platform logo however)
    
    Fixes: 138661761
    Test: attach to adb; confirm that one notification uses the
          Q and one the USB logo
    Test: view system apps in Settings>Apps that have no proper icon
    
    Change-Id: Ia11736c010b4862fea8d0e73c424cfc5c51badfa

--- services/usb/java/com/android/server/usb/UsbDeviceManager.java
+++ services/usb/java/com/android/server/usb/UsbDeviceManager.java
@@ -61,6 +61,7 @@ import android.hardware.usb.gadget.V1_2.UsbSpeed;
 import android.hidl.manager.V1_0.IServiceManager;
 import android.hidl.manager.V1_0.IServiceNotification;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Environment;
 import android.os.FileUtils;
 import android.os.Handler;
@@ -1275,7 +1276,7 @@ public class UsbDeviceManager implements ActivityTaskManagerInternal.ScreenObser
                     }
 
                     Notification.Builder builder = new Notification.Builder(mContext, channel)
-                            .setSmallIcon(com.android.internal.R.drawable.stat_sys_adb)
+                            .setSmallIcon(com.android.internal.R.drawable.stat_sys_data_usb)
                             .setWhen(0)
                             .setOngoing(true)
                             .setTicker(title)
@@ -1285,6 +1286,7 @@ public class UsbDeviceManager implements ActivityTaskManagerInternal.ScreenObser
                                             .system_notification_accent_color))
                             .setContentTitle(title)
                             .setContentText(message)
+                            .setSubText(Build.ID)
                             .setContentIntent(pi)
                             .setVisibility(Notification.VISIBILITY_PUBLIC);
 
