67bdea4c6bef15c885f29e98d16e2147c6b8fb68
Aaron Kling <webgeek1234@gmail.com>
2019-10-23 17:49:08 -0500

    Add nvidia profile manager
    Change-Id: I774ac62c8867151f21712cfeee25f96a591415ad

---
+++ core/java/com/nvidia/NvConstants.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2013, NVIDIA Corporation.  All rights reserved.
+ *
+ * NVIDIA Corporation and its licensors retain all intellectual property and
+ * proprietary rights in and to this software and related documentation.  Any
+ * use, reproduction, disclosure or distribution of this software and related
+ * documentation without an express license agreement from NVIDIA Corporation
+ * is strictly prohibited.
+ */
+
+package com.nvidia;
+
+public class NvConstants {
+    public static final String NvAppClose = "com.nvidia.app.close";
+    public static final String NvBatteryMonitor = "com.nvidia.NvCPLSvc.BatteryMonitor";
+    public static final String NvCPLService = "com.nvidia.NvCPLSvc.NvCPLService";
+    public static final String NvCPLSvc = "com.nvidia.NvCPLSvc";
+    public static final String NvMaxGpuMode = "com.nvidia.NvCPLSvc.MaxGpuMode";
+    public static final String NvOrigin = "com.nvidia.NvCPLSvc.Origin";
+    public static final String NvPowerMode = "com.nvidia.NvCPLSvc.NV_POWER_MODE";
+    public static final String NvPowerModeProperty = "persist.sys.NV_POWER_MODE";
+    public static final String NvStateId = "com.nvidia.NvCPLSvc.StateID";
+    public static final String NvTCPDivisor = "com.nvidia.NvCPLSvc.NV_TCPDIVISOR";
+    public static final String NvThermalStats = "com.nvidia.peripheralservice.NvThermalStats";
+    public static final int maxPerfMode = 0;
+}
