cb5ea3c8386a2b78d82e35b1694c1de89796b68e
Aaron Kling <webgeek1234@gmail.com>
2019-10-23 17:49:08 -0500

    Add nvidia profile manager
    Change-Id: I774ac62c8867151f21712cfeee25f96a591415ad

---
+++ core/java/com/nvidia/profilemanager/ProfileTypeId.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2014, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This is automatically generated file. Do not edit manually.
+ *
+ * To add new application profile types, edit
+ *      //sw/mobile/app_profiles/profile_types.xml
+ * and use
+ *      [path-to-cpl-repo]/tools/settings_codegen.py
+ * to regenerate this file.
+ */
+
+package com.nvidia.profilemanager;
+
+public class ProfileTypeId {
+    public static int LAST_RESERVED_TYPE = 9999;
+
+    public static int DEFAULT            =     0;
+    public static int OEM_OVERRIDE       =     1;
+    public static int DEV_OVERRIDE       =     2;
+    public static int USER               = 10000;
+    public static int OPTIMIZED          = 10001;
+}
