aa30cd1778eac5339b904b2a77fc9a1a88db4436
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-30 13:41:05 +0100

    SystemUI: Bring back good ol' circle battery style
    Co-authored-by: althafvly <althafvly@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: If9c55253c8e8786b4998632241a872104b7e7e8e

--- packages/SystemUI/tests/src/com/android/systemui/theme/ThemeOverlayControllerTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/theme/ThemeOverlayControllerTest.java
@@ -66,6 +66,7 @@ import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController.DeviceProvisionedListener;
 import com.android.systemui.util.settings.SecureSettings;
+import com.android.systemui.util.settings.SystemSettings;
 
 import com.google.common.util.concurrent.MoreExecutors;
 
@@ -102,6 +103,8 @@ public class ThemeOverlayControllerTest extends SysuiTestCase {
     private ThemeOverlayApplier mThemeOverlayApplier;
     @Mock
     private SecureSettings mSecureSettings;
+    @Mock
+    private SystemSettings mSystemSettings;
     @Mock
     private WallpaperManager mWallpaperManager;
     @Mock
@@ -152,7 +155,8 @@ public class ThemeOverlayControllerTest extends SysuiTestCase {
                 .thenReturn(Color.BLACK);
         mThemeOverlayController = new ThemeOverlayController(mContext,
                 mBroadcastDispatcher, mBgHandler, mMainExecutor, mBgExecutor, mThemeOverlayApplier,
-                mSecureSettings, mWallpaperManager, mUserManager, mDeviceProvisionedController,
+                mSecureSettings, mSystemSettings, mWallpaperManager, mUserManager,
+                mDeviceProvisionedController,
                 mUserTracker, mDumpManager, mFeatureFlags, mResources, mWakefulnessLifecycle,
                 mUiModeManager) {
             @VisibleForTesting
@@ -734,7 +738,8 @@ public class ThemeOverlayControllerTest extends SysuiTestCase {
 
         mThemeOverlayController = new ThemeOverlayController(mContext,
                 mBroadcastDispatcher, mBgHandler, executor, executor, mThemeOverlayApplier,
-                mSecureSettings, mWallpaperManager, mUserManager, mDeviceProvisionedController,
+                mSecureSettings, mSystemSettings, mWallpaperManager, mUserManager,
+                mDeviceProvisionedController,
                 mUserTracker, mDumpManager, mFeatureFlags, mResources, mWakefulnessLifecycle,
                 mUiModeManager) {
             @VisibleForTesting
@@ -774,7 +779,8 @@ public class ThemeOverlayControllerTest extends SysuiTestCase {
         Executor executor = MoreExecutors.directExecutor();
         mThemeOverlayController = new ThemeOverlayController(mContext,
                 mBroadcastDispatcher, mBgHandler, executor, executor, mThemeOverlayApplier,
-                mSecureSettings, mWallpaperManager, mUserManager, mDeviceProvisionedController,
+                mSecureSettings, mSystemSettings, mWallpaperManager, mUserManager,
+                mDeviceProvisionedController,
                 mUserTracker, mDumpManager, mFeatureFlags, mResources, mWakefulnessLifecycle,
                 mUiModeManager) {
             @VisibleForTesting
