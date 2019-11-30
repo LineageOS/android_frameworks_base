787013cf96f20777f6cb6e179aa504364a68bca3
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-30 13:41:05 +0100

    SystemUI: Bring back good ol' circle battery style
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   2019-11-30 13:41:05 +0100
        SystemUI: Bring back good ol' circle battery style
    
        * This code is based on the old Java implementation and converted to Kotlin
    
        * Instead of creating different style within the same drawable
          create one on its own, this makes the code more readable and future proof
    
        Change-Id: I23798426e603b277408fb63f4898b385027e7eeb
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   2021-04-08 21:41:24 +0100
        SystemUI: Fix status bar battery icon title
    
        The wrong string was being picked up which is not capitalized
        and not even localized. Use the already localized string
        from SystemUI package.
    
        Change-Id: I4be427c95a0fb98c77d450e0791bb8e3442ed3a1
    
    Change-Id: I025811006cf6088d94b6a2e2e5520543dedd7f98

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -82,7 +82,6 @@ public class KeyguardStatusBarView extends RelativeLayout implements
 
     private final Rect mEmptyRect = new Rect(0, 0, 0, 0);
 
-    private boolean mShowPercentAvailable;
     private boolean mBatteryCharging;
     private boolean mBatteryListening;
 
@@ -198,8 +197,6 @@ public class KeyguardStatusBarView extends RelativeLayout implements
                 R.dimen.system_icons_super_container_avatarless_margin_end);
         mCutoutSideNudge = getResources().getDimensionPixelSize(
                 R.dimen.display_cutout_margin_consumption);
-        mShowPercentAvailable = getContext().getResources().getBoolean(
-                com.android.internal.R.bool.config_battery_percentage_setting_available);
         mRoundedCornerPadding = res.getDimensionPixelSize(
                 R.dimen.rounded_corner_content_padding);
     }
@@ -237,7 +234,7 @@ public class KeyguardStatusBarView extends RelativeLayout implements
                 mMultiUserAvatar.setVisibility(View.GONE);
             }
         }
-        mBatteryView.setForceShowPercent(mBatteryCharging && mShowPercentAvailable);
+        mBatteryView.setForceShowPercent(mBatteryCharging);
     }
 
     private void updateSystemIconsLayoutParams() {
