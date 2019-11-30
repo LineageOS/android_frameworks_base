db8a4e8d68d3a6c6c149ec0243e4869c37db1e37
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-30 13:41:05 +0100

    SystemUI: Bring back good ol' circle battery style
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   2019-11-30 13:41:05 +0100
        SystemUI: Bring back good ol' circle battery style
    
        * This code is based on the old Java implementation and converted to Kotlin
    
        * Instead of creating different style within the same drawable
          create one on its own, this makes the code more readable and future proof
    
        Change-Id: I23798426e603b277408fb63f4898b385027e7eeb
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   2021-04-08 21:41:24 +0100
        SystemUI: Fix status bar battery icon title
    
        The wrong string was being picked up which is not capitalized
        and not even localized. Use the already localized string
        from SystemUI package.
    
        Change-Id: I4be427c95a0fb98c77d450e0791bb8e3442ed3a1
    
    Author: althafvly <althafvly@gmail.com>
    Date:   Mon Jul 11 15:13:40 2022 +0530
    
        SystemUI: Redo circle battery style
    
        - Now shows up on QuickSettings panel.
        - Fixes issues with battery circle style on theme and
          ui mode change and statusbar icon toggle.
        - Fixes wrong tint color with circle battery icon.
        - Re-evaluating system theme to avoid delay in circle
          battery tint color change.
    
        Change-Id: Id90f85e47ce4b9811e9bb67b91169997020300d8
    
    Change-Id: If9c55253c8e8786b4998632241a872104b7e7e8e

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -22,6 +22,7 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Rect;
+import android.os.UserHandle;
 import android.util.AttributeSet;
 import android.util.Pair;
 import android.view.DisplayCutout;
@@ -48,6 +49,8 @@ import com.android.systemui.statusbar.policy.Clock;
 import com.android.systemui.statusbar.policy.VariableDateView;
 import com.android.systemui.util.LargeScreenUtils;
 import com.android.systemui.tuner.TunerService;
+
+import lineageos.providers.LineageSettings;
 
 import java.util.List;
 
@@ -274,7 +277,11 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
 
         int textColor = Utils.getColorAttrDefaultColor(mContext, android.R.attr.textColorPrimary);
         if (textColor != mTextColorPrimary) {
+            boolean isCircleBattery = LineageSettings.System.getIntForUser(
+                    mContext.getContentResolver(), LineageSettings.System.STATUS_BAR_BATTERY_STYLE,
+                    0, UserHandle.USER_CURRENT) == 1;
             int textColorSecondary = Utils.getColorAttrDefaultColor(mContext,
+                    isCircleBattery ? android.R.attr.textColorHint :
                     android.R.attr.textColorSecondary);
             mTextColorPrimary = textColor;
             mClockView.setTextColor(textColor);
