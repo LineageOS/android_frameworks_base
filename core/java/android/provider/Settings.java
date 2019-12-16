bef9ac47e03b041c8f56ce56389ae77c4beb34e0
beanstown106 <nbenis106@gmail.com>
2019-12-16 01:32:26 -0700

    incall vibration options [1/3]
    *allow setting vibration when
    
     call is connected *allow setting vibration when call is disconnected *allow
     setting vibration for call waiting *this works with google and aosp dialer :)
    
    Change-Id: If5b9842960e60299a85e7f6d76da38f1375e85df
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4521,6 +4521,22 @@ public final class Settings {
         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * Whether the phone vibrates on call connect
+         * @hide
+         */
+        public static final String VIBRATE_ON_CONNECT = "vibrate_on_connect";
+         /**
+         * Whether the phone vibrates on call waiting
+         * @hide
+         */
+        public static final String VIBRATE_ON_CALLWAITING = "vibrate_on_callwaiting";
+         /**
+         * Whether the phone vibrates on disconnect
+         * @hide
+         */
+        public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
