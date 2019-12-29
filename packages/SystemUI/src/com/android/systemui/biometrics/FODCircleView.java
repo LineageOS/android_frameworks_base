a196e1644d221533e145a38aa468347241a3f042
Demon000 <demonsingur@gmail.com>
2019-12-29 13:17:58 +0100

    FODCircleView: remove unused outside touch flag
    We don't use any of the ACTION_OUTSIDE events enabled by this flag.
    
    Change-Id: I2036dd5f71fecdc69442eee8b19ae035837089c7

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -405,7 +405,6 @@ public class FODCircleView extends ImageView implements OnTouchListener {
         mParams.packageName = "android";
         mParams.type = WindowManager.LayoutParams.TYPE_SECURE_SYSTEM_OVERLAY;
         mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH |
                 WindowManager.LayoutParams.FLAG_DIM_BEHIND |
                 WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
         mParams.gravity = Gravity.TOP | Gravity.LEFT;
