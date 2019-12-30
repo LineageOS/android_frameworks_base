e285ea0894d4a9f16cf4236d5fef83a0cd5c6327
Noctis Lucis Sora (Qais) <54539834+YeQais@users.noreply.github.com>
2019-12-30 17:34:48 +0000

    use permanent fingerprint lockout immediately

--- services/core/java/com/android/server/biometrics/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/biometrics/fingerprint/FingerprintService.java
@@ -103,7 +103,7 @@ public class FingerprintService extends BiometricServiceBase {
     private static final String ACTION_LOCKOUT_RESET =
             "com.android.server.biometrics.fingerprint.ACTION_LOCKOUT_RESET";
     private static final int MAX_FAILED_ATTEMPTS_LOCKOUT_TIMED = 5;
-    private static final int MAX_FAILED_ATTEMPTS_LOCKOUT_PERMANENT = 20;
+    private static final int MAX_FAILED_ATTEMPTS_LOCKOUT_PERMANENT = 5;
     private static final long FAIL_LOCKOUT_TIMEOUT_MS = 30 * 1000;
     private static final String KEY_LOCKOUT_RESET_USER = "lockout_reset_user";
 
