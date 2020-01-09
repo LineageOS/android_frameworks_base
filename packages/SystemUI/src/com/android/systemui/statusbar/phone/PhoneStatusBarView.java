2e2b53f27d3924ca4d75366e393de29a83f66573
Michal Pražák <Michal.prazak1@gmail.com>
2020-01-09 11:37:29 +0100

    SystemUI: add FloatingRotationButton for hw-key devices
    This is a squash of the following commits:
    
    Author: Michal Pražák <Michal.prazak1@gmail.com>
    Date:   Thu Jan 9 11:37:29 2020 +0100
    
        SystemUI: add FloatingRotationButton for hw-key devices
    
        Add floating rotation button for hardware key devices.
        Screenshot: https://imgur.com/a/KPNGD43
    
        Co-authored-by: Timi <timi.rautamaki@gmail.com>
        Change-Id: I181d0b94cf1b740c20313ee556308f237cc3fd3f
    
    Author: Timi Rautamäki <timi.rautamaki@gmail.com>
    Date:   Thu Jan 6 12:01:10 2022 +0000
    
        SystemUI: don't show FRB when IME is visible
    
        Change-Id: I97a99cc1b98ac9ee1e623fe8f2782434c76f9ce0
    
    Change-Id: I26953cb83edc28483b88cad61affade526647cbe

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -21,24 +21,36 @@ import android.annotation.Nullable;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Rect;
+import android.inputmethodservice.InputMethodService;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.Pair;
+import android.view.ContextThemeWrapper;
+import android.view.Display;
 import android.view.DisplayCutout;
+import android.view.IWindowManager;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowInsets;
+import android.view.WindowManagerGlobal;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 import com.android.internal.policy.SystemBarUtils;
+import com.android.settingslib.Utils;
 import com.android.systemui.Dependency;
 import com.android.systemui.Gefingerpoken;
 import com.android.systemui.R;
 import com.android.systemui.plugins.DarkIconDispatcher;
 import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
+import com.android.systemui.shared.rotation.FloatingRotationButton;
+import com.android.systemui.shared.rotation.RotationButtonController;
+import com.android.systemui.statusbar.CommandQueue;
+import com.android.systemui.statusbar.CommandQueue.Callbacks;
 import com.android.systemui.statusbar.phone.userswitcher.StatusBarUserSwitcherContainer;
 import com.android.systemui.statusbar.policy.Clock;
 import com.android.systemui.user.ui.binder.StatusBarUserChipViewBinder;
@@ -47,13 +59,15 @@ import com.android.systemui.util.leak.RotationUtils;
 
 import java.util.Objects;
 
-public class PhoneStatusBarView extends FrameLayout {
+public class PhoneStatusBarView extends FrameLayout implements Callbacks {
     private static final String TAG = "PhoneStatusBarView";
+    private final CommandQueue mCommandQueue;
     private final StatusBarContentInsetsProvider mContentInsetsProvider;
 
     private DarkReceiver mBattery;
     private Clock mClock;
     private int mRotationOrientation = -1;
+    private RotationButtonController mRotationButtonController;
     @Nullable
     private View mCutoutSpace;
     @Nullable
@@ -71,7 +85,52 @@ public class PhoneStatusBarView extends FrameLayout {
 
     public PhoneStatusBarView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mCommandQueue = Dependency.get(CommandQueue.class);
         mContentInsetsProvider = Dependency.get(StatusBarContentInsetsProvider.class);
+
+        // Only create FRB here if there is no navbar
+        if (!hasNavigationBar()) {
+            final Context lightContext = new ContextThemeWrapper(context,
+                    Utils.getThemeAttr(context, R.attr.lightIconTheme));
+            final Context darkContext = new ContextThemeWrapper(context,
+                    Utils.getThemeAttr(context, R.attr.darkIconTheme));
+            final int lightIconColor =
+                    Utils.getColorAttrDefaultColor(lightContext, R.attr.singleToneColor);
+            final int darkIconColor =
+                    Utils.getColorAttrDefaultColor(darkContext, R.attr.singleToneColor);
+            final FloatingRotationButton floatingRotationButton = new FloatingRotationButton(
+                    context,
+                    R.string.accessibility_rotate_button, R.layout.rotate_suggestion,
+                    R.id.rotate_suggestion, R.dimen.floating_rotation_button_min_margin,
+                    R.dimen.rounded_corner_content_padding,
+                    R.dimen.floating_rotation_button_taskbar_left_margin,
+                    R.dimen.floating_rotation_button_taskbar_bottom_margin,
+                    R.dimen.floating_rotation_button_diameter, R.dimen.key_button_ripple_max_width,
+                    R.bool.floating_rotation_button_position_left);
+
+            mRotationButtonController = new RotationButtonController(lightContext, lightIconColor,
+                    darkIconColor, R.drawable.ic_sysbar_rotate_button_ccw_start_0,
+                    R.drawable.ic_sysbar_rotate_button_ccw_start_90,
+                    R.drawable.ic_sysbar_rotate_button_cw_start_0,
+                    R.drawable.ic_sysbar_rotate_button_cw_start_90,
+                    () -> getDisplay().getRotation());
+            mRotationButtonController.setRotationButton(floatingRotationButton, null);
+        }
+    }
+
+    @Override
+    public void onRotationProposal(final int rotation, boolean isValid) {
+        if (mRotationButtonController != null && !hasNavigationBar()) {
+            mRotationButtonController.onRotationProposal(rotation, isValid);
+        }
+    }
+
+    private boolean hasNavigationBar() {
+        try {
+            IWindowManager windowManager = WindowManagerGlobal.getWindowManagerService();
+            return windowManager.hasNavigationBar(Display.DEFAULT_DISPLAY);
+        } catch (RemoteException ex) { }
+        return false;
     }
 
     void setTouchEventHandler(Gefingerpoken handler) {
@@ -102,6 +161,10 @@ public class PhoneStatusBarView extends FrameLayout {
         if (updateDisplayParameters()) {
             updateLayoutForCutout();
         }
+
+        if (mRotationButtonController != null && !hasNavigationBar()) {
+            mCommandQueue.addCallback(this);
+        }
     }
 
     @Override
@@ -110,6 +173,10 @@ public class PhoneStatusBarView extends FrameLayout {
         Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(mBattery);
         Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(mClock);
         mDisplayCutout = null;
+
+        if (mRotationButtonController != null) {
+            mCommandQueue.removeCallback(this);
+        }
     }
 
     // Per b/300629388, we let the PhoneStatusBarView detect onConfigurationChanged to
@@ -201,6 +268,15 @@ public class PhoneStatusBarView extends FrameLayout {
     public boolean onInterceptTouchEvent(MotionEvent event) {
         mTouchEventHandler.onInterceptTouchEvent(event);
         return super.onInterceptTouchEvent(event);
+    }
+
+    @Override
+    public void setImeWindowStatus(int displayId, IBinder token, int vis, int backDisposition,
+            boolean showImeSwitcher) {
+        if (mRotationButtonController != null) {
+            final boolean imeShown = (vis & InputMethodService.IME_VISIBLE) != 0;
+            mRotationButtonController.getRotationButton().setCanShowRotationButton(!imeShown);
+        }
     }
 
     public void updateResources() {
