c0bc51cbb7e757b2b9bde99a755c588e4d76bfcb
Michal Pražák <Michal.prazak1@gmail.com>
2020-01-09 11:37:29 +0100

    SystemUI: add FloatingRotationButton for hw-key devices
    Add floating rotation button for hardware key devices.
    Screenshot: https://imgur.com/a/KPNGD43
    
    Co-authored-by: Timi <timi.rautamaki@gmail.com>
    Change-Id: I181d0b94cf1b740c20313ee556308f237cc3fd3f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -25,32 +25,41 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.os.RemoteException;
 import android.util.AttributeSet;
 import android.util.EventLog;
 import android.util.Pair;
+import android.view.ContextThemeWrapper;
+import android.view.Display;
 import android.view.DisplayCutout;
 import android.view.Gravity;
+import android.view.IWindowManager;
 import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowInsets;
+import android.view.WindowManagerGlobal;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.LinearLayout;
 
 import com.android.settingslib.Utils;
 import com.android.systemui.Dependency;
 import com.android.systemui.EventLogTags;
+import com.android.systemui.navigationbar.gestural.FloatingRotationButton;
+import com.android.systemui.navigationbar.RotationButtonController;
 import com.android.systemui.R;
 import com.android.systemui.plugins.DarkIconDispatcher;
 import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
 import com.android.systemui.statusbar.CommandQueue;
+import com.android.systemui.statusbar.CommandQueue.Callbacks;
 import com.android.systemui.util.leak.RotationUtils;
 
+import java.util.function.Consumer;
 import java.util.List;
 import java.util.Objects;
 
-public class PhoneStatusBarView extends PanelBar {
+public class PhoneStatusBarView extends PanelBar implements Callbacks {
     private static final String TAG = "PhoneStatusBarView";
     private static final boolean DEBUG = StatusBar.DEBUG;
     private static final boolean DEBUG_GESTURES = false;
@@ -73,6 +82,7 @@ public class PhoneStatusBarView extends PanelBar {
     private DarkReceiver mBattery;
     private ClockController mClockController;
     private int mRotationOrientation = -1;
+    private RotationButtonController mRotationButtonController;
     @Nullable
     private View mCenterIconSpace;
     @Nullable
@@ -93,6 +103,43 @@ public class PhoneStatusBarView extends PanelBar {
         super(context, attrs);
         mCommandQueue = Dependency.get(CommandQueue.class);
         mContentInsetsProvider = Dependency.get(StatusBarContentInsetsProvider.class);
+
+        // Only create FRB here if there is no navbar
+        if (!hasNavigationBar()) {
+            final Context lightContext = new ContextThemeWrapper(context,
+                    Utils.getThemeAttr(context, R.attr.lightIconTheme));
+            final Context darkContext = new ContextThemeWrapper(context,
+                    Utils.getThemeAttr(context, R.attr.darkIconTheme));
+            final int lightIconColor =
+                    Utils.getColorAttrDefaultColor(lightContext, R.attr.singleToneColor);
+            final int darkIconColor =
+                    Utils.getColorAttrDefaultColor(darkContext, R.attr.singleToneColor);
+            final FloatingRotationButton floatingRotationButton =
+                    new FloatingRotationButton(context);
+            mRotationButtonController = new RotationButtonController(lightContext,
+                    lightIconColor, darkIconColor);
+            mRotationButtonController.setRotationButton(floatingRotationButton,
+                    mRotationButtonListener);
+        }
+    }
+
+    private final Consumer<Boolean> mRotationButtonListener = (visible) -> {};
+    private final Consumer<Integer> mRotationWatcher = rotation -> {};
+
+    @Override
+    public void onRotationProposal(final int rotation, boolean isValid) {
+        if (mRotationButtonController != null && !hasNavigationBar()) {
+            final int winRotation = getDisplay().getRotation();
+            mRotationButtonController.onRotationProposal(rotation, winRotation, isValid);
+        }
+    }
+
+    private boolean hasNavigationBar() {
+        try {
+            IWindowManager windowManager = WindowManagerGlobal.getWindowManagerService();
+            return windowManager.hasNavigationBar(Display.DEFAULT_DISPLAY);
+        } catch (RemoteException ex) {}
+        return false;
     }
 
     public void setBar(StatusBar bar) {
@@ -127,6 +174,11 @@ public class PhoneStatusBarView extends PanelBar {
         if (updateOrientationAndCutout()) {
             updateLayoutForCutout();
         }
+
+        if (mRotationButtonController != null && !hasNavigationBar()) {
+            mRotationButtonController.registerListeners();
+            mRotationButtonController.setRotationCallback(mRotationWatcher);
+        }
     }
 
     @Override
@@ -135,6 +187,11 @@ public class PhoneStatusBarView extends PanelBar {
         Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(mBattery);
         mClockController.removeDarkReceiver();
         mDisplayCutout = null;
+
+        if (mRotationButtonController != null) {
+            mRotationButtonController.unregisterListeners();
+            mRotationButtonController.setRotationCallback(null);
+        }
     }
 
     @Override
