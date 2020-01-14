0a8086e03b9ab557f2d35ee13cf1e6662d924a23
Aaron Kling <webgeek1234@gmail.com>
2020-01-14 12:26:59 -0600

    ATV: Display battery notifications
    This is for handheld gaming devices that have batteries and are
    using Android TV.
    
    Change-Id: I22592699214d40aac2a733203b05c076b2a1ae70

--- packages/SystemUI/src/com/android/systemui/power/PowerNotificationWarnings.java
+++ packages/SystemUI/src/com/android/systemui/power/PowerNotificationWarnings.java
@@ -21,6 +21,7 @@ import static android.content.DialogInterface.BUTTON_POSITIVE;
 
 import android.app.KeyguardManager;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.ActivityNotFoundException;
@@ -28,6 +29,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.media.AudioAttributes;
 import android.net.Uri;
 import android.os.Handler;
@@ -67,6 +69,8 @@ import java.util.Objects;
 public class PowerNotificationWarnings implements PowerUI.WarningsUI {
     private static final String TAG = PowerUI.TAG + ".Notification";
     private static final boolean DEBUG = PowerUI.DEBUG;
+
+    private static final String BATTERY_NOTIFICATION_CHANNEL_ID_TV = "powernotifications.battery.tv";
 
     private static final String TAG_BATTERY = "low_battery";
     private static final String TAG_TEMPERATURE = "high_temp";
@@ -151,6 +155,16 @@ public class PowerNotificationWarnings implements PowerUI.WarningsUI {
         mPowerMan = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
         mKeyguard = mContext.getSystemService(KeyguardManager.class);
         mReceiver.init();
+
+        if (isTv()) {
+            // TV-specific notification channel
+            mNoMan.createNotificationChannel(
+                    new NotificationChannel(BATTERY_NOTIFICATION_CHANNEL_ID_TV,
+                            mContext.getString(
+                                    com.android.internal.R.string
+                                            .battery_notification_channel_tv),
+                            NotificationManager.IMPORTANCE_HIGH));
+        }
     }
 
     @Override
@@ -235,6 +249,10 @@ public class PowerNotificationWarnings implements PowerUI.WarningsUI {
         }
     }
 
+    private boolean isTv() {
+        return mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_LEANBACK);
+    }
+
     private void showInvalidChargerNotification() {
         final Notification.Builder nb =
                 new Notification.Builder(mContext, NotificationChannels.ALERTS)
@@ -276,7 +294,9 @@ public class PowerNotificationWarnings implements PowerUI.WarningsUI {
                         .setOnlyAlertOnce(true)
                         .setDeleteIntent(pendingBroadcast(ACTION_DISMISSED_WARNING))
                         .setStyle(new Notification.BigTextStyle().bigText(contentText))
-                        .setVisibility(Notification.VISIBILITY_PUBLIC);
+                        .setVisibility(Notification.VISIBILITY_PUBLIC)
+                        .extend(new Notification.TvExtender()
+                                .setChannelId(BATTERY_NOTIFICATION_CHANNEL_ID_TV));
         if (hasBatterySettings()) {
             nb.setContentIntent(pendingBroadcast(ACTION_SHOW_BATTERY_SETTINGS));
         }
