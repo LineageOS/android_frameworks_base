db4a21230a2082dec3a86ccf847ad7f053eae0f6
Aaron Kling <webgeek1234@gmail.com>
2020-01-14 12:26:59 -0600

    ATV: Display battery notifications
    This is for handheld gaming devices that have batteries and are
    using Android TV.
    
    Change-Id: I22592699214d40aac2a733203b05c076b2a1ae70

--- packages/SystemUI/src/com/android/systemui/power/PowerNotificationWarnings.java
+++ packages/SystemUI/src/com/android/systemui/power/PowerNotificationWarnings.java
@@ -68,6 +68,8 @@ public class PowerNotificationWarnings implements PowerUI.WarningsUI {
     private static final String TAG = PowerUI.TAG + ".Notification";
     private static final boolean DEBUG = PowerUI.DEBUG;
 
+    private static final String BATTERY_NOTIFICATION_CHANNEL_ID_TV = "powernotifications.battery.tv";
+
     private static final String TAG_BATTERY = "low_battery";
     private static final String TAG_TEMPERATURE = "high_temp";
     private static final String TAG_AUTO_SAVER = "auto_saver";
@@ -151,6 +153,16 @@ public class PowerNotificationWarnings implements PowerUI.WarningsUI {
         mPowerMan = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
         mKeyguard = mContext.getSystemService(KeyguardManager.class);
         mReceiver.init();
+
+        if (isTv()) {
+            // TV-specific notification channel
+            mNoMan.createNotificationChannel(
+                    new NotificationChannel(BATTERY_NOTIFICATION_CHANNEL_ID_TV,
+                            mContext.getString(
+                                    com.android.internal.R.string
+                                            .battery_notification_channel_tv),
+                            NotificationManager.IMPORTANCE_HIGH));
+        }
     }
 
     @Override
@@ -276,7 +288,9 @@ public class PowerNotificationWarnings implements PowerUI.WarningsUI {
                         .setOnlyAlertOnce(true)
                         .setDeleteIntent(pendingBroadcast(ACTION_DISMISSED_WARNING))
                         .setStyle(new Notification.BigTextStyle().bigText(contentText))
-                        .setVisibility(Notification.VISIBILITY_PUBLIC);
+                        .setVisibility(Notification.VISIBILITY_PUBLIC)
+                        .extend(new Notification.TvExtender()
+                                .setChannelId(BATTERY_NOTIFICATION_CHANNEL_ID_TV));
         if (hasBatterySettings()) {
             nb.setContentIntent(pendingBroadcast(ACTION_SHOW_BATTERY_SETTINGS));
         }
