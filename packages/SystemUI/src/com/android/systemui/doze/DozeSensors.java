1a919735ed741e533acf1514d880dac18f9b718f
Demon000 <demonsingur@gmail.com>
2020-01-15 02:47:34 +0000

    DozeSensors: do not use binned brightness sensor for proximity if not supported
    Change-Id: Ia6a0fae64a409359d036a2e2f2a42e48de95491a

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -175,6 +175,21 @@ public class DozeSensors {
         return null;
     }
 
+    private Sensor findBrightnessSensorForProximity() {
+        return findBrightnessSensorForProximity(mContext, mSensorManager);
+    }
+
+    static Sensor findBrightnessSensorForProximity(Context context, SensorManager sensorManager) {
+        boolean brightnessSensorReportsProximity =
+                context.getResources().getBoolean(R.bool.doze_brightness_sensor_reports_proximity);
+        if (brightnessSensorReportsProximity) {
+            return findSensorWithType(sensorManager,
+                    context.getString(R.string.doze_brightness_sensor_type));
+        }
+
+        return null;
+    }
+
     /**
      * If sensors should be registered and sending signals.
      */
@@ -297,8 +312,8 @@ public class DozeSensors {
 
             // The default prox sensor can be noisy, so let's use a prox gated brightness sensor
             // if available.
-            Sensor sensor = DozeSensors.findSensorWithType(mSensorManager,
-                    mContext.getString(R.string.doze_brightness_sensor_type));
+            Sensor sensor = DozeSensors.findBrightnessSensorForProximity(mContext,
+                    mSensorManager);
             mUsingBrightnessSensor = sensor != null;
             if (sensor == null) {
                 sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
