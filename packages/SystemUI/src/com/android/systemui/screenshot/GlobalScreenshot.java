b213dbd4cd64e12fb80d8d2c5adfddd3c61d27f6
Michael W <baddaemon87@gmail.com>
2020-02-05 19:03:21 +0100

    SystemUI: Fix shutter sound
    * When shutter sound for camera is forced on as required in some states,
      (config_camera_sound_forced, set via mcc/mnc), we also want to
      (or should) play it when a screenshot is taken from the preview
      instead of an actual picture
    * This change is loosely based on
      https://android-review.googlesource.com/c/platform/frameworks/base/+/1517742/
      but uses publicly available APIs
    
    Testing:
    - Set config_camera_sound_forced to true and push a build to device
    - Turn down all stream volumes to muted
    - Take screenshot of any normal screen
      -> No sound played
    - Open camera, take screenshot
      -> Sound played
    - Turn up volume and repeat the screenshots
      -> Sound played in all cases
    
    Change-Id: Iacbb577c64f73b79cdfae7fb0487fc6a34ffe41c

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -52,6 +52,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.InsetDrawable;
 import android.graphics.drawable.LayerDrawable;
+import android.hardware.camera2.CameraManager;
 import android.media.AudioManager;
 import android.media.MediaActionSound;
 import android.net.Uri;
@@ -60,6 +61,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.provider.Settings;
@@ -230,6 +232,9 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
     private AudioManager mAudioManager;
     private Vibrator mVibrator;
 
+    private CameraManager mCameraManager;
+    private int mCamsInUse = 0;
+
     private int mNavMode;
     private int mLeftInset;
     private int mRightInset;
@@ -329,6 +334,9 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
         // Grab system services needed for screenshot sound
         mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
         mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+        mCameraManager.registerAvailabilityCallback(mCamCallback,
+                new Handler(Looper.getMainLooper()));
 
         // Store UI background executor
         mUiBgExecutor = uiBgExecutor;
@@ -1255,6 +1263,8 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
     }
 
     private void playShutterSound() {
+        boolean playSound = readCameraSoundForced() && mCamsInUse > 0;
+
         switch (mAudioManager.getRingerMode()) {
             case AudioManager.RINGER_MODE_SILENT:
                 // do nothing
@@ -1266,9 +1276,34 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
                 }
                 break;
             case AudioManager.RINGER_MODE_NORMAL:
-                // Play the shutter sound to notify that we've taken a screenshot
-                mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+                // in this case we want to play sound even if not forced on
+                playSound = true;
                 break;
         }
+
+        // We want to play the shutter sound when it's either forced or
+        // when we use normal ringer mode
+        if (playSound) {
+            mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+        }
+    }
+
+    private CameraManager.AvailabilityCallback mCamCallback =
+            new CameraManager.AvailabilityCallback() {
+        @Override
+        public void onCameraOpened(String cameraId, String packageId) {
+            mCamsInUse++;
+        }
+
+        @Override
+        public void onCameraClosed(String cameraId) {
+            mCamsInUse--;
+        }
+    };
+
+    private boolean readCameraSoundForced() {
+        return SystemProperties.getBoolean("audio.camerasound.force", false) ||
+                mContext.getResources().getBoolean(
+                        com.android.internal.R.bool.config_camera_sound_forced);
     }
 }
