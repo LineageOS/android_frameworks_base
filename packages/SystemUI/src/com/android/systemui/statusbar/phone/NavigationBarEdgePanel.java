18ec8379d59924e5ccb5d4f7c19c0a8a598b9c03
Nico <nicorg2529@gmail.com>
2020-03-08 11:39:47 +0000

    Implement long edge swipe gesture
    The gesture will activate if user executes edge swipe for a long
    horizontal motion. The triggered action is screen off in this
    initial commit.
    
    Change-Id: Ie1bbe6645b6a00d346af60d6bb5e4d584997d6e9

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarEdgePanel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarEdgePanel.java
@@ -136,6 +136,7 @@ public class NavigationBarEdgePanel extends View {
     private final float mMinDeltaForSwitch;
 
     private final float mSwipeThreshold;
+    private final float mLongSwipeThreshold;
     private final Path mArrowPath = new Path();
 
     private final SpringAnimation mAngleAnimation;
@@ -180,6 +181,7 @@ public class NavigationBarEdgePanel extends View {
     private boolean mArrowsPointLeft;
     private float mMaxTranslation;
     private boolean mTriggerBack;
+    private boolean mTriggerLongSwipe;
     private float mPreviousTouchTranslation;
     private float mTotalTouchDelta;
     private float mVerticalTranslation;
@@ -316,6 +318,7 @@ public class NavigationBarEdgePanel extends View {
 
         mSwipeThreshold = context.getResources()
                 .getDimension(R.dimen.navigation_edge_action_drag_threshold);
+        mLongSwipeThreshold = mSwipeThreshold * 2.5f;
         setVisibility(GONE);
     }
 
@@ -326,6 +329,10 @@ public class NavigationBarEdgePanel extends View {
 
     public boolean shouldTriggerBack() {
         return mTriggerBack;
+    }
+
+    public boolean shouldTriggerLongSwipe() {
+        return mTriggerLongSwipe;
     }
 
     public void setIsDark(boolean isDark, boolean animate) {
@@ -436,6 +443,10 @@ public class NavigationBarEdgePanel extends View {
         float x = (polarToCartX(mCurrentAngle) * mArrowLength);
         float y = (polarToCartY(mCurrentAngle) * mArrowLength);
         Path arrowPath = calculatePath(x,y);
+        if (mTriggerLongSwipe) {
+            arrowPath.addPath(calculatePath(x,y), mArrowThickness * 2.0f * (mIsLeftPanel ? 1 : -1), 0.0f);
+        }
+
         if (mShowProtection) {
             canvas.drawPath(arrowPath, mProtectionPaint);
         }
@@ -586,6 +597,7 @@ public class NavigationBarEdgePanel extends View {
         mTranslationAnimation.setSpring(mRegularTranslationSpring);
         // Reset the arrow to the side
         setTriggerBack(false /* triggerBack */, false /* animated */);
+        setTriggerLongSwipe(false /* triggerLongSwipe */, false /* animated */);
         setDesiredTranslation(0, false /* animated */);
         setCurrentTranslation(0);
         updateAngle(false /* animate */);
@@ -660,6 +672,10 @@ public class NavigationBarEdgePanel extends View {
         if (Math.abs(yOffset) > Math.abs(x - mStartX) * 2) {
             triggerBack = false;
         }
+
+        boolean triggerLongSwipe = triggerBack && touchTranslation > mLongSwipeThreshold;
+        setTriggerLongSwipe(triggerLongSwipe, true /* animated */);
+
         setTriggerBack(triggerBack, true /* animated */);
 
         if (!mTriggerBack) {
@@ -722,6 +738,18 @@ public class NavigationBarEdgePanel extends View {
     private void setTriggerBack(boolean triggerBack, boolean animated) {
         if (mTriggerBack != triggerBack) {
             mTriggerBack = triggerBack;
+            mAngleAnimation.cancel();
+            updateAngle(animated);
+            // Whenever the trigger back state changes the existing translation animation should be
+            // cancelled
+            mTranslationAnimation.cancel();
+        }
+    }
+
+    private void setTriggerLongSwipe(boolean triggerLongSwipe, boolean animated) {
+        if (mTriggerLongSwipe != triggerLongSwipe) {
+            mTriggerLongSwipe = triggerLongSwipe;
+            mVibratorHelper.vibrate(VibrationEffect.EFFECT_CLICK);
             mAngleAnimation.cancel();
             updateAngle(animated);
             // Whenever the trigger back state changes the existing translation animation should be
