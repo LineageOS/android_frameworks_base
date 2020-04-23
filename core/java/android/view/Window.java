723432d0570703b0d720916ed8c87a1b25de4ab5
Alexander Martinz <amartinz@shiftphones.com>
2020-04-23 13:06:34 +0200

    Camera: Rename prevent power key permission
    * We must not use the "android" namespace or CTS fails.
    
    Change-Id: I034216efe5d472b0bc72cd293001b518f92aec41
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- core/java/android/view/Window.java
+++ core/java/android/view/Window.java
@@ -1155,7 +1155,7 @@ public abstract class Window {
 
     private void setPrivateFlags(int flags, int mask) {
         if ((flags & mask & WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_POWER_KEY) != 0) {
-            mContext.enforceCallingOrSelfPermission("android.permission.PREVENT_POWER_KEY",
+            mContext.enforceCallingOrSelfPermission("lineage.permission.PREVENT_POWER_KEY",
                     "No permission to prevent power key");
         }
         final WindowManager.LayoutParams attrs = getAttributes();
