6e467ca8bf2c7a4f095d2342e29687a104af393d
Pierre-Hugues Husson <pierre-hugues.husson@softathome.com>
2020-05-20 15:02:12 +0000

    Fix other lights when backlight uses SurfaceController API
    Only backlight should go to SurfaceControler, other lights are still using good old Lights API.
    
    Change-Id: I0c55bae02ec3a9d762b90b4ecf31dc63d29530b5

--- services/core/java/com/android/server/lights/LightsService.java
+++ services/core/java/com/android/server/lights/LightsService.java
@@ -76,7 +76,8 @@ public class LightsService extends SystemService {
                 // right now we just fall back to the old path through Lights brightessMode is
                 // anything but USER or the device shouldBeInLowPersistenceMode().
                 if (brightnessMode == BRIGHTNESS_MODE_USER && !shouldBeInLowPersistenceMode()
-                        && mSurfaceControlMaximumBrightness == 255) {
+                        && mSurfaceControlMaximumBrightness == 255
+                        && mId == LightsManager.LIGHT_ID_BACKLIGHT) {
                     // TODO: the last check should be mSurfaceControlMaximumBrightness != 0; the
                     // reason we enforce 255 right now is to stay consistent with the old path. In
                     // the future, the framework should be refactored so that brightness is a float
