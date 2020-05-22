59b40332650ab5801a21fd3213d3ae0ccc6e417f
Michael W <baddaemon87@gmail.com>
2020-05-22 11:04:29 +0200

    Tuner: Remove most keys from blacklist
    * We ignore Lineage keys now generally, no need to maintain a complete list
      of those
    * Keep Settings.Secure.DOZE_ALWAYS_ON and StatusBar.SCREEN_BRIGHTNESS_MODE
      as they are not lineage keys
    * Make keys private where possible - they are not required outside of their
      classes anymore
    
    List of changes that introduced the keys here initially:
    - SystemUI: Bring back good ol' circle battery style
      Commit 439b220b147dbce17b48f9992a1eb853fded3047
    - SystemUI: Add tunables for clock AM/PM style
      Commit 0772b15a4a7350fcc6f54cc657fae9905f71efde
    - SystemUI: Add tunables for clock position
      Commit 5e0404f6f4ad2377f67da5e5211691c853eee863
    - SystemUI: Blacklist KEY_EDGE_LONG_SWIPE_ACTION for tuner
      Commit 804ed1c7d445dbcf3289ddee8bc1c69ac78d3af5
    - TunerServiceImpl: Add another key to blacklist
      Commit 1629359f0d54d54c1bc53f3816edda57fd995e08
    - TunerServiceImpl: Blacklist notification panel tuners
      Commit 951e53783372dc81fa67766c918ea1aaf1bba917
    - SystemUI: Allow translucent notifications background on lockscreen [1/3]
      Commit 2184236cdd579df840cb144dea60265a0986ec95
    - Add support for runtime toggle of navbar
      Commit 625c820ee2ddb919b51ed8021c6d355900e5e6da
    - SystemUI: Port statusbar brightness control
      Commit 8ed65f135efc79046462786423c428f88843b74b
    - SystemUI: runtime configurable audio panel location
      Commit d5ff45112792494041c285ccdda0b40a3c24ac9d
    
    Change-Id: Ia811a17c0194fc501585ff9ffbed00682985daf3

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -72,21 +72,11 @@ public class TunerServiceImpl extends TunerService {
 
     // Things that use the tunable infrastructure but are now real user settings and
     // shouldn't be reset with tuner settings.
+    // Do not add Lineage specific keys here as they are blacklisted automatically
     private static final String[] RESET_BLACKLIST = new String[] {
-            BatteryMeterView.STATUS_BAR_BATTERY_STYLE,
-            Clock.CLOCK_STYLE,
-            ClockController.CLOCK_POSITION,
-            EdgeBackGestureHandler.KEY_EDGE_LONG_SWIPE_ACTION,
-            NavigationBarView.NAVIGATION_BAR_MENU_ARROW_KEYS,
-            NotificationPanelView.DOUBLE_TAP_SLEEP_GESTURE,
-            NotificationPanelView.STATUS_BAR_QUICK_QS_PULLDOWN,
-            NotificationStackScrollLayout.LOCKSCREEN_TRANSLUCENT_NOTIFICATIONS_BG_ENABLED,
             QSTileHost.TILES_SETTING,
             Settings.Secure.DOZE_ALWAYS_ON,
-            StatusBar.FORCE_SHOW_NAVBAR,
             StatusBar.SCREEN_BRIGHTNESS_MODE,
-            StatusBar.STATUS_BAR_BRIGHTNESS_CONTROL,
-            VolumeDialogImpl.SETTING_VOLUME_PANEL_ON_LEFT,
     };
 
     private final Observer mObserver = new Observer();
