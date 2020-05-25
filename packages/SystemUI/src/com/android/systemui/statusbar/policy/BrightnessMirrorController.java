b57845ff5abd5e03c60ebcfd1297add1220ea5fa
Michael W <baddaemon87@gmail.com>
2020-05-25 15:38:33 +0200

    SystemUI: Brightness slider: Always show the brightness icon
    * Right now, we reserve the space for the icon but it's not displayed
      (reserved space is there so the slider doesn't jump)
    * Always display the icon (if automatic brightness is available)
      so it's more consistent
    
    Change-Id: I5238dc6aad51407349a8d42fae840ea76841dc3d

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BrightnessMirrorController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BrightnessMirrorController.java
@@ -18,10 +18,13 @@ package com.android.systemui.statusbar.policy;
 
 import android.annotation.NonNull;
 import android.content.res.Resources;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.ArraySet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.android.systemui.R;
 import com.android.systemui.statusbar.NotificationShadeDepthController;
@@ -64,6 +67,7 @@ public class BrightnessMirrorController
         mVisibilityCallback.accept(true);
         mNotificationPanel.setPanelAlpha(0, true /* animate */);
         mDepthController.setBrightnessMirrorVisible(true);
+        updateIcon();
     }
 
     public void hideMirror() {
@@ -139,5 +143,23 @@ public class BrightnessMirrorController
 
     public interface BrightnessMirrorListener {
         void onBrightnessMirrorReinflated(View brightnessMirror);
+    }
+
+    private void updateIcon() {
+        ImageView iv = mBrightnessMirror.findViewById(R.id.brightness_icon);
+        boolean autoBrightnessAvailable = mBrightnessMirror.getContext().getResources().getBoolean(
+                com.android.internal.R.bool.config_automatic_brightness_available);
+        if (autoBrightnessAvailable) {
+            int automatic = Settings.System.getIntForUser(mBrightnessMirror.getContext()
+                            .getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS_MODE,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL,
+                    UserHandle.USER_CURRENT);
+            boolean isAutomatic = automatic != Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL;
+            iv.setImageResource(isAutomatic
+                    ? com.android.systemui.R.drawable.ic_qs_brightness_auto_on
+                    : com.android.systemui.R.drawable.ic_qs_brightness_auto_off);
+            iv.setVisibility(View.VISIBLE);
+        }
     }
 }
