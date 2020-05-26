23c6dc73a919ba8ee21e5f537434dbe982ebcb21
althafvly <althafvly@gmail.com>
2020-05-26 21:17:59 +0800

    SystemUI: Re-evaluate system theme on UI mode change
    - Need for power menu to set accurate colors
    
    Change-Id: I05d41eaf8ea19ce3b6ce659d01da33cf55de3b7e

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -68,6 +68,7 @@ import com.android.systemui.monet.ColorScheme;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController.DeviceProvisionedListener;
+import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.settings.SecureSettings;
 
 import org.json.JSONException;
@@ -109,6 +110,7 @@ public class ThemeOverlayController extends SystemUI implements Dumpable {
     private final SecureSettings mSecureSettings;
     private final Executor mMainExecutor;
     private final Handler mBgHandler;
+    private final TunerService mTunerService;
     private final boolean mIsMonetEnabled;
     private final UserTracker mUserTracker;
     private final DeviceProvisionedController mDeviceProvisionedController;
@@ -342,7 +344,7 @@ public class ThemeOverlayController extends SystemUI implements Dumpable {
             SecureSettings secureSettings, WallpaperManager wallpaperManager,
             UserManager userManager, DeviceProvisionedController deviceProvisionedController,
             UserTracker userTracker, DumpManager dumpManager, FeatureFlags featureFlags,
-            WakefulnessLifecycle wakefulnessLifecycle) {
+            WakefulnessLifecycle wakefulnessLifecycle, TunerService tunerService) {
         super(context);
 
         mIsMonetEnabled = featureFlags.isMonetEnabled();
@@ -357,8 +359,16 @@ public class ThemeOverlayController extends SystemUI implements Dumpable {
         mWallpaperManager = wallpaperManager;
         mUserTracker = userTracker;
         mWakefulnessLifecycle = wakefulnessLifecycle;
+        mTunerService = tunerService;
         dumpManager.registerDumpable(TAG, this);
     }
+
+    private final TunerService.Tunable mTunable = new TunerService.Tunable() {
+        @Override
+        public void onTuningChanged(String key, String newValue) {
+            reevaluateSystemTheme(true /* forceReload */);
+        }
+    };
 
     @Override
     public void start() {
@@ -393,6 +403,10 @@ public class ThemeOverlayController extends SystemUI implements Dumpable {
                     }
                 },
                 UserHandle.USER_ALL);
+
+        mTunerService.addTunable(mTunable, Settings.Secure.UI_NIGHT_MODE,
+                Settings.Secure.UI_NIGHT_MODE_OVERRIDE_ON,
+                Settings.Secure.UI_NIGHT_MODE_OVERRIDE_OFF);
 
         if (!mIsMonetEnabled) {
             return;
