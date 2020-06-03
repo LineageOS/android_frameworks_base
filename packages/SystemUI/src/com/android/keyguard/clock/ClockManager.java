d40bf9ace22d1deb2d6d7b20727ea8bb05cd06c4
gnumdk <gnumdk@gmail.com>
2020-06-03 14:07:19 +0000

    Add a LineageOS clock
        Here a custom clock for ClockPlugin showing LineageOS logo
        https://i.ibb.co/4N8gZrT/clock.png
    
    Change-Id: I7e29eba21357ab740dae20a738ef34c18f1a594e

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -148,6 +148,7 @@ public final class ClockManager {
         LayoutInflater layoutInflater = injectionInflater.injectable(LayoutInflater.from(context));
 
         addBuiltinClock(() -> new DefaultClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new LineageClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new BubbleClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new AnalogClockController(res, layoutInflater, colorExtractor));
 
