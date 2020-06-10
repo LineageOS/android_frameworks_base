d41cf50b5816c11dcbba3bb56396243c789001b4
Kshitij Gupta <kshitijgm@gmail.com>
2020-06-10 14:19:12 +0000

    WifiManager: Add StaState API [1/2]
    Wifi stastate callback functions are refrenced by mediatek ims blobs when they init to register ims-service.
    
    test: IMS services (voLTE and voWIFI) works fine on mtk67xx (begonia) and crashes due to missing stastate callback functions is no more.
    
    - Reversed out from miui_BEGONIAININGlobal_V11.0.2.0.QGGINXM_7e11a54a70_10.0
    
    Change-Id: I7309906d775f45c7ea42b3324679a0f7b776a1c9

--- wifi/java/com/android/server/wifi/BaseWifiService.java
+++ wifi/java/com/android/server/wifi/BaseWifiService.java
@@ -25,6 +25,7 @@ import android.net.wifi.IDppCallback;
 import android.net.wifi.INetworkRequestMatchCallback;
 import android.net.wifi.IOnWifiUsabilityStatsListener;
 import android.net.wifi.ISoftApCallback;
+import android.net.wifi.IStaStateCallback;
 import android.net.wifi.ITrafficStateCallback;
 import android.net.wifi.IWifiManager;
 import android.net.wifi.ScanResult;
@@ -479,6 +480,17 @@ public class BaseWifiService extends IWifiManager.Stub {
 
     @Override
     public void updateWifiUsabilityScore(int seqNum, int score, int predictionHorizonSec) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void registerStaStateCallback(
+            IBinder binder, IStaStateCallback callback, int callbackIdentifier) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void unregisterStaStateCallback(int callbackIdentifier) {
         throw new UnsupportedOperationException();
     }
 }
