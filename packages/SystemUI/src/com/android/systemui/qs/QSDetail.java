be772fe10b984801518beb4608a87e7eaa691160
Michael W <baddaemon87@gmail.com>
2020-06-16 12:13:38 +0200

    SystemUI: Fix QS Detail Header
    * Commit eff2c740e55668a4fdf427e64a8970a751830991 introduced some padding
      in case of an existing corner cutout (e.g. a camera in the corner or a
      display), so applies the size of the cutout as padding left/right
    * it tries to keep top/bottom paddings the same, but the values are
      (at the time of calling - not yet?) correct, so the final result ends
      up wrong (top: 0, bottom: 22)
    * Apply the padding values that are used in the layout instead if relying
      on get[...]Padding methods
    * Keep the whole padding untouched in case there is no cutout
    
    Test: QS Detail looks like before r37 merge
    Change-Id: I5ba57deff1784fc4ea16649256965ad42dd78239

--- packages/SystemUI/src/com/android/systemui/qs/QSDetail.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSDetail.java
@@ -159,16 +159,11 @@ public class QSDetail extends LinearLayout {
         DisplayCutout cutout = insets.getDisplayCutout();
         Pair<Integer, Integer> padding = PhoneStatusBarView.cornerCutoutMargins(
                 cutout, getDisplay());
-        if (padding == null) {
-            mQsDetailHeader.setPaddingRelative(
-                    getResources().getDimensionPixelSize(R.dimen.qs_detail_header_padding),
-                    getPaddingTop(),
-                    getResources().getDimensionPixelSize(R.dimen.qs_detail_header_padding),
-                    getPaddingBottom()
-            );
-        } else {
-            mQsDetailHeader.setPadding(padding.first, getPaddingTop(),
-                    padding.second, getPaddingBottom());
+        int paddingTopBottom = getResources().getDimensionPixelSize(
+                R.dimen.qs_detail_items_padding_top);
+        if (padding != null) {
+            mQsDetailHeader.setPadding(padding.first, paddingTopBottom,
+                    padding.second, paddingTopBottom);
         }
         return super.onApplyWindowInsets(insets);
     }
