856f49b01e68888fc440ded60ce52f62d002e738
Paul Keith <javelinanddart@gmail.com>
2020-06-22 19:43:00 +0200

    Revert "Add a LineageOS clock"
    * This is a contrived usage of branding that is bad practice
      for a first-party implementation.
    
    This reverts commit 257a6821cbc2802265b20c3631fdae5505cd983a.
    
    Change-Id: Id7e8e5a425baba5826663ab53d0fa5ff3160415e

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -150,7 +150,6 @@ public final class ClockManager {
         addBuiltinClock(() -> new DefaultClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new BubbleClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new AnalogClockController(res, layoutInflater, colorExtractor));
-        addBuiltinClock(() -> new LineageClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
