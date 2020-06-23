db4daf7ff4ac802c1341e29b0fa7495331d1f416
Jesse Chan <jc@lineageos.org>
2020-06-24 06:08:19 +0800

    VolumeDialog: notifyVisible after the volume panel is fully hidden
    notifyVisible(false) leads to reset of active stream. As a result,
    the current active row will be untinted. Currently, this happens
    immediately after the dismissal is requested, before the volume panel
    is fully hidden.
    
    User can notice the row has been untinted before the panel is hidden.
    
    This changes moves notifyVisible to the end of animation so the volume
    panel appearance will be consistent in between.
    
    Change-Id: Iacd83d48634a11bc248c9a2a45b68b53256fdcb2
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -816,11 +816,11 @@ public class VolumeDialogImpl implements VolumeDialog,
                     mDialog.dismiss();
                     tryToRemoveCaptionsTooltip();
                     mIsAnimatingDismiss = false;
+                    mController.notifyVisible(false);
                 }, 50));
         animator.translationX(getAnimatorX());
         animator.start();
         checkODICaptionsTooltip(true);
-        mController.notifyVisible(false);
         synchronized (mSafetyWarningLock) {
             if (mSafetyWarning != null) {
                 if (D.BUG) Log.d(TAG, "SafetyWarning dismissed");
