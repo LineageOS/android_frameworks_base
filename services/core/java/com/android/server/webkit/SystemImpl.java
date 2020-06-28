777707984db27141578b41a823736e3040b13643
LuK1337 <priv.luk@gmail.com>
2020-06-29 00:31:59 +0200

    webkit: SystemImpl: Make systemIsDebuggable() check IS_ENG as well
    * Since we are shipping userdebug builds by default
      it'd be nice to keep signature and version downgrade
      checks intact.
    * Fixes : https://gitlab.com/LineageOS/issues/android/-/issues/2192
    
    Change-Id: Ib46ccb50ac091469caf99a73a5a08942cbc457f6

--- services/core/java/com/android/server/webkit/SystemImpl.java
+++ services/core/java/com/android/server/webkit/SystemImpl.java
@@ -244,7 +244,7 @@ public class SystemImpl implements SystemInterface {
 
     @Override
     public boolean systemIsDebuggable() {
-        return Build.IS_DEBUGGABLE;
+        return Build.IS_DEBUGGABLE && Build.IS_ENG;
     }
 
     @Override
