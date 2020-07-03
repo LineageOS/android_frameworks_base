bbf3029802ac749cf58ab42ced67c7532319874e
Sagar Verma <sagarv@codeaurora.org>
2020-07-03 22:08:04 +0530

    BT-Audio: Keep track of Active TWS+ EB device in BTHelper
    Keep track of active TWS+ EB device in MM Audio module
    based on device connect/disconnect events as part of
    Active device update. This device tracking will help us
    to make decision for Audio updates regarding reconfig
    should be done OR not based on EBs transition use-case.
    
    CRs-Fixed: 2717923
    Change-Id: I2df8447acb1d8e1b843a49906f2f3e3993dee5d6

--- services/core/java/com/android/server/audio/AudioDeviceInventory.java
+++ services/core/java/com/android/server/audio/AudioDeviceInventory.java
@@ -846,6 +846,7 @@ public class AudioDeviceInventory {
           if (state == BluetoothProfile.STATE_DISCONNECTED) {
               mDeviceBroker.postBluetoothA2dpDeviceConnectionStateSuppressNoisyIntent(
                              device, state, profile, suppressNoisyIntent, a2dpVolume);
+              BtHelper.SetA2dpActiveDevice(null);
               return;
           }
           // state == BluetoothProfile.STATE_CONNECTED
@@ -870,6 +871,7 @@ public class AudioDeviceInventory {
                                  AudioSystem.DEVICE_OUT_BLUETOOTH_A2DP, BtHelper.getName(device),
                                  address, a2dpCodec));
                       if (BtHelper.isTwsPlusSwitch(device, existingDevice.getValue().mDeviceAddress)) {
+                          BtHelper.SetA2dpActiveDevice(device);
                           if (AudioService.DEBUG_DEVICES) {
                               Log.d(TAG,"TWS+ device switch");
                           }
