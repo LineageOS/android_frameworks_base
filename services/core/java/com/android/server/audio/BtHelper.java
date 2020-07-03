9a44270f368169cc1c0d6735252e905bd76a1a6a
Sagar Verma <sagarv@codeaurora.org>
2020-07-03 22:08:04 +0530

    BT-Audio: Keep track of Active TWS+ EB device in BTHelper
    Keep track of active TWS+ EB device in MM Audio module
    based on device connect/disconnect events as part of
    Active device update. This device tracking will help us
    to make decision for Audio updates regarding reconfig
    should be done OR not based on EBs transition use-case.
    
    CRs-Fixed: 2717923
    Change-Id: I2df8447acb1d8e1b843a49906f2f3e3993dee5d6

--- services/core/java/com/android/server/audio/BtHelper.java
+++ services/core/java/com/android/server/audio/BtHelper.java
@@ -72,6 +72,7 @@ public class BtHelper {
 
     // Reference to BluetoothA2dp to query for AbsoluteVolume.
     static private @Nullable BluetoothA2dp mA2dp;
+    static private @Nullable BluetoothDevice mBluetoothA2dpActiveDevice;
 
     // If absolute volume is supported in AVRCP device
     private boolean mAvrcpAbsVolSupported = false;
@@ -219,6 +220,11 @@ public class BtHelper {
         return deviceName;
     }
 
+    /*packages*/ static void SetA2dpActiveDevice(BluetoothDevice device) {
+        Log.w(TAG,"SetA2dpActiveDevice for TWS+ pair as " + device);
+        mBluetoothA2dpActiveDevice = device;
+    }
+
     /*packages*/ @NonNull static boolean isTwsPlusSwitch(@NonNull BluetoothDevice device,
                                                                  String address) {
         BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
@@ -230,8 +236,8 @@ public class BtHelper {
         if (device.isTwsPlusDevice() &&
             connDevice.isTwsPlusDevice() &&
             device.getTwsPlusPeerAddress().equals(address)) {
-            if(mA2dp.getConnectionState(connDevice) != BluetoothProfile.STATE_CONNECTED) {
-                Log.w(TAG,"Active earbud is already disconnected");
+            if (mBluetoothA2dpActiveDevice == null) {
+                Log.w(TAG,"Not a TwsPlusSwitch as previous active device was null");
                 return false;
             }
             Log.i(TAG,"isTwsPlusSwitch true");
