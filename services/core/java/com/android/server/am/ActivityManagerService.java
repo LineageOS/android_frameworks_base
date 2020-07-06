2ebd49d5dd17752f926c526ccd740f6e923c65a7
John Reck <jreck@google.com>
2020-07-06 16:10:49 -0700

    Add missing isShellUser check
    Bug: 160390416
    Test: verified command still works from shell
    
    [basilgello: Back-port to 14.1:
     - {ROOT,SHELL}_UID -> Process.{ROOT,SHELL}_UID]
    Signed-off-by: Vasyl Gello <vasek.gello@gmail.com>
    
    Change-Id: I23bb06e00f1623e4f27c02d7eb2c0d273b40771b
    (cherry picked from commit 03542611973e4ce3ddca522ee12bcc85e59ce901)
    Merged-In: I23bb06e00f1623e4f27c02d7eb2c0d273b40771b
    (cherry picked from commit 5e2931c6569aa8084be9d0690a1ca30534f49c46)
    (cherry picked from commit 4a31000e6072c14608ec1c59321481c8aa330313)

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -4306,9 +4306,18 @@ public final class ActivityManagerService extends ActivityManagerNative
         return procState;
     }
 
+    private boolean isCallerShell() {
+        final int callingUid = Binder.getCallingUid();
+        return callingUid == Process.SHELL_UID || callingUid == Process.ROOT_UID;
+    }
+
     @Override
     public boolean setProcessMemoryTrimLevel(String process, int userId, int level) {
         synchronized (this) {
+            if (!isCallerShell()) {
+                EventLog.writeEvent(0x534e4554, 160390416, Binder.getCallingUid(), "");
+                throw new SecurityException("Only shell can call it");
+            }
             final ProcessRecord app = findProcessLocked(process, userId, "setProcessMemoryTrimLevel");
             if (app == null) {
                 return false;
