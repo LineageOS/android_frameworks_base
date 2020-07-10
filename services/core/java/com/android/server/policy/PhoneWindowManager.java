fa0acf56703c59b10fd7d5bdbbceea5a3be2861a
Aaron Kling <webgeek1234@gmail.com>
2020-07-10 16:08:56 -0500

    Allow forcing landscape mode with an overlay
    Partial backport of I770cf207c08ba0d36de6e3b6ab378e94498a02bd
    
    Change-Id: I35abd3433d3e95e4d62671fe15a59cb6ddbb33d0

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2727,7 +2727,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         int longSizeDp = longSize * DisplayMetrics.DENSITY_DEFAULT / density;
 
         // Allow the navigation bar to move on non-square small devices (phones).
-        mNavigationBarCanMove = width != height && shortSizeDp < 600;
+        mNavigationBarCanMove = width != height && res.getBoolean(R.bool.config_navBarCanMove);
 
         mHasNavigationBar = res.getBoolean(com.android.internal.R.bool.config_showNavigationBar);
 
