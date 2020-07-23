2fbb544549bd031e8997c9d0cdcf19cb7b53387d
zhangyupeng1 <zhangyupeng1@xiaomi.com>
2020-07-23 21:56:37 +0800

    [BugFix][Fingerprint]fix client was canceled by service by mistake
    issue:
    After the call of client A started in the lockout state returns,
    after client B calls the fp service, A is dead,
    a death notification will be sent to the service, and the service will stop clent B.
    
    solution:
    Each time onAuthentication sets a flag mAlreadyDone to true.
    Judge this flag as true in binderDie, and keep the current
    fingerprint operation to avoid affecting the current user
    
    Signed-off-by: zhangyupeng1 <zhangyupeng1@xiaomi.com>
    Change-Id: I56ce99505febbb48dc13ccfc3c2d120123da4fa3
    Merged-In: I56ce99505febbb48dc13ccfc3c2d120123da4fa3

--- services/core/java/com/android/server/biometrics/BiometricServiceBase.java
+++ services/core/java/com/android/server/biometrics/BiometricServiceBase.java
@@ -954,6 +954,7 @@ public abstract class BiometricServiceBase extends SystemService
             if (!client.onError(getHalDeviceId(), errorCode, 0 /* vendorCode */)) {
                 Slog.w(getTag(), "Cannot send permanent lockout message to client");
             }
+
             return;
         }
         startClient(client, true /* initiatedByClient */);
