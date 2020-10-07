399f53d299856325c1a0ff9d786722677439ecb4
pramod kotreshappa <kpramod@codeaurora.org>
2020-10-06 22:15:16 -0700

    Add LC3 codec type.
    - Add LC3 codec type
    - Add additional sample rate and channel mode
    
    CRs-Fixed: 2789473
    
    Change-Id: I13d0c6944a12422c13e2069697817c0f26330179

--- core/java/android/bluetooth/BluetoothCodecConfig.java
+++ core/java/android/bluetooth/BluetoothCodecConfig.java
@@ -82,6 +82,9 @@ public final class BluetoothCodecConfig implements Parcelable {
     public static final int SOURCE_CODEC_TYPE_CELT = 8;
 
     @UnsupportedAppUsage
+    public static final int SOURCE_CODEC_TYPE_LC3 = 9;
+
+    @UnsupportedAppUsage
     public static final int SOURCE_CODEC_TYPE_INVALID = 1000 * 1000;
 
     /** @hide */
@@ -137,6 +140,14 @@ public final class BluetoothCodecConfig implements Parcelable {
     @UnsupportedAppUsage
     public static final int SAMPLE_RATE_192000 = 0x1 << 5;
 
+    public static final int SAMPLE_RATE_16000 = 0x1 << 6;
+
+    public static final int SAMPLE_RATE_24000 = 0x1 << 7;
+
+    public static final int SAMPLE_RATE_32000 = 0x1 << 8;
+
+    public static final int SAMPLE_RATE_8000 = 0x1 << 9;
+
 
     /** @hide */
     @IntDef(prefix = "BITS_PER_SAMPLE_", value = {
@@ -178,6 +189,7 @@ public final class BluetoothCodecConfig implements Parcelable {
 
     @UnsupportedAppUsage
     public static final int CHANNEL_MODE_STEREO = 0x1 << 1;
+    public static final int CHANNEL_MODE_JOINT_STEREO = 0x1 << 2;
 
     private final @SourceCodecType int mCodecType;
     private @CodecPriority int mCodecPriority;
