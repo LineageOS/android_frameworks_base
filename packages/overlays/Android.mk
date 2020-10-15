22d6bfe68f28c447f0373f51f5ed6584b174aab0
DarkJoker360 <simoespo159@gmail.com>
2020-10-15 09:02:59 -0700

    Revert "Remove two-button overlay from default overlays"
    This reverts commit 2aa347798732f8ac1a87f0316af7bc345f2c184a.
    
    Change-Id: I7d06b06a3bdbcdd0b0d667693200f341a6bbafac

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -52,6 +52,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconShapeTeardropOverlay \
 	IconShapeVesselOverlay \
 	NavigationBarMode3ButtonOverlay \
+	NavigationBarMode2ButtonOverlay \
 	NavigationBarModeGesturalOverlay \
 	NavigationBarModeGesturalOverlayNarrowBack \
 	NavigationBarModeGesturalOverlayWideBack \
