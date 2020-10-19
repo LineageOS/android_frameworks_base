3cc991193ddb41ccc17e7731ec50f4fec35358b6
ganeshi4u <ganeshvarmai4u@gmail.com>
2020-10-19 17:23:14 +0000

    SystemUI: screenshot: Add delete action chip intent
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Change-Id: Ia55ac91c2db2f44062674a699f8df63c96e1de5c

--- packages/SystemUI/src/com/android/systemui/dagger/DefaultBroadcastReceiverBinder.java
+++ packages/SystemUI/src/com/android/systemui/dagger/DefaultBroadcastReceiverBinder.java
@@ -23,6 +23,7 @@ import com.android.systemui.accessibility.hearingaid.HearingDevicesDialogReceive
 import com.android.systemui.media.dialog.MediaOutputDialogReceiver;
 import com.android.systemui.people.widget.PeopleSpaceWidgetPinnedReceiver;
 import com.android.systemui.people.widget.PeopleSpaceWidgetProvider;
+import com.android.systemui.screenshot.DeleteScreenshotReceiver;
 import com.android.systemui.screenshot.SmartActionsReceiver;
 
 import dagger.Binds;
@@ -35,6 +36,15 @@ import dagger.multibindings.IntoMap;
  */
 @Module
 public abstract class DefaultBroadcastReceiverBinder {
+    /**
+     *
+     */
+    @Binds
+    @IntoMap
+    @ClassKey(DeleteScreenshotReceiver.class)
+    public abstract BroadcastReceiver bindDeleteScreenshotReceiver(
+            DeleteScreenshotReceiver broadcastReceiver);
+
     /**
      *
      */
