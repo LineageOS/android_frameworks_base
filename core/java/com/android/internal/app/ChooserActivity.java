1a243d26de8d8d33ea50d9dd544b5580421c4d04
LuK1337 <priv.luk@gmail.com>
2020-10-28 23:59:20 +0100

    Sharesheet: Display two rows of max ranked targets
    This change makes sharesheet way more useful by increasing the amount of
    visible ranked apps.
    
    Change-Id: Ic092f1d1784259c9f3c0870eda1dd1ae8544c697

--- core/java/com/android/internal/app/ChooserActivity.java
+++ core/java/com/android/internal/app/ChooserActivity.java
@@ -2855,7 +2855,7 @@ public class ChooserActivity extends ResolverActivity implements
 
     @Override // ChooserListCommunicator
     public int getMaxRankedTargets() {
-        return mMaxTargetsPerRow;
+        return mMaxTargetsPerRow * 2;
     }
 
     @Override // ChooserListCommunicator
