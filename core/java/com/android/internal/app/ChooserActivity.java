14f6cdeeacd9139a95693b72257f10151d9646e3
LuK1337 <priv.luk@gmail.com>
2020-10-28 23:59:20 +0100

    Sharesheet: Increase max ranked targets to 8
    This change makes sharesheet way more useful by increasing
    amount of visible pinned apps in portrait mode from 4 to 8.
    
    Change-Id: Ic092f1d1784259c9f3c0870eda1dd1ae8544c697

--- core/java/com/android/internal/app/ChooserActivity.java
+++ core/java/com/android/internal/app/ChooserActivity.java
@@ -2800,7 +2800,7 @@ public class ChooserActivity extends ResolverActivity implements
 
     @Override // ChooserListCommunicator
     public int getMaxRankedTargets() {
-        return getMaxTargetsPerRow();
+        return MAX_TARGETS_PER_ROW_LANDSCAPE;
     }
 
     @Override // ChooserListCommunicator
