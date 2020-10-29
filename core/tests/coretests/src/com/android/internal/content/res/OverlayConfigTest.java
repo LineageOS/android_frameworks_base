01f9bce7f226fe499769cb44e1cd72f95d7e8c09
LuK1337 <priv.luk@gmail.com>
2020-10-29 12:46:05 +0100

    Allow lineage resources package to be overlayed by RRO packages
    Change-Id: I152ef9a87b16338e14af508e0f1f42830688c5ea

--- core/tests/coretests/src/com/android/internal/content/res/OverlayConfigTest.java
+++ core/tests/coretests/src/com/android/internal/content/res/OverlayConfigTest.java
@@ -430,7 +430,7 @@ public class OverlayConfigTest {
         final OverlayConfig overlayConfig = createConfigImpl();
         if (mScannerRule.getIteration() == OverlayConfigIterationRule.Iteration.ZYGOTE) {
             final ArrayList<IdmapInvocation> idmapInvocations =
-                    overlayConfig.getImmutableFrameworkOverlayIdmapInvocations();
+                    overlayConfig.getImmutableFrameworkOverlayIdmapInvocations("android");
             assertEquals(2, idmapInvocations.size());
 
             final IdmapInvocation i0 = idmapInvocations.get(0);
@@ -467,7 +467,7 @@ public class OverlayConfigTest {
 
         if (mScannerRule.getIteration() == OverlayConfigIterationRule.Iteration.ZYGOTE) {
             final ArrayList<IdmapInvocation> idmapInvocations =
-                    overlayConfig.getImmutableFrameworkOverlayIdmapInvocations();
+                    overlayConfig.getImmutableFrameworkOverlayIdmapInvocations("android");
             assertEquals(3, idmapInvocations.size());
 
             final IdmapInvocation i0 = idmapInvocations.get(0);
