6f1a8b35e6f3c5f85157c445249e6aa4cfcb0a3c
LuK1337 <priv.luk@gmail.com>
2020-11-12 14:49:51 +0100

    fixup! SystemUI: implement better partial screenshot
    * On R sys ui flags no longer disable edge swipe...
    
    Change-Id: I767997a82ee0fc819bbaa7bc906e89127e53a17c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -173,6 +173,8 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
     private int mRightInset;
     private int mSysUiFlags;
 
+    private boolean mBlockedGesturalNavigation;
+
     private final GestureNavigationSettingsObserver mGestureNavigationSettingsObserver;
 
     private final NavigationEdgeBackPlugin.BackCallback mBackCallback =
@@ -650,6 +652,10 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
         return topActivity != null && mGestureBlockingActivities.contains(topActivity);
     }
 
+    public void setBlockedGesturalNavigation(boolean blocked) {
+        mBlockedGesturalNavigation = blocked;
+    }
+
     @Override
     public void writeToProto(SystemUiTraceProto proto) {
         if (proto.edgeBackGestureHandler == null) {
@@ -664,7 +670,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
         }
 
         public void onInputEvent(InputEvent event) {
-            EdgeBackGestureHandler.this.onInputEvent(event);
+            if (!mBlockedGesturalNavigation) {
+                EdgeBackGestureHandler.this.onInputEvent(event);
+            }
             finishInputEvent(event, true);
         }
     }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -872,6 +872,7 @@ public class NavigationBarView extends FrameLayout implements
 
     public void setBlockedGesturalNavigation(boolean blocked) {
         mBlockedGesturalNavigation = blocked;
+        mEdgeBackGestureHandler.setBlockedGesturalNavigation(blocked);
         updateDisabledSystemUiStateFlags();
         updatePanelSystemUiStateFlags();
     }
