0aa8430e787d9a08eaa0391b5358f4085bb7959a
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-27 18:23:28 +0000

    FODCircleView: ensure that when we check for bouncer, we're on keyguard.
    Bouncer doesn't seem to exist anywhere else.
    
    Tests: - have phone unlocked by trust agent, open any third party app with biometric unlock, ensure FOD is visible
    Change-Id: Ic3895853f7461afcbc816c6155c8db13011bb668

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -381,7 +381,7 @@ public class FODCircleView extends ImageView {
             return;
         }
 
-        if (mUpdateMonitor.getUserCanSkipBouncer(mUpdateMonitor.getCurrentUser())) {
+        if (mIsKeyguard && mUpdateMonitor.getUserCanSkipBouncer(mUpdateMonitor.getCurrentUser())) {
             // Ignore show calls if user can skip bouncer
             return;
         }
