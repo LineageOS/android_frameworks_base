265c4a3a79e71fdf02ebb8fc387a8ed0fa622ea7
Bhakthavatsala Raghavendra <braghave@codeaurora.org>
2020-12-22 16:38:39 -0800

    Bluetooth: Add BC profile entry
    Add  BC profile entry and override constructor for CachedBluetoothDevice
    
    CRs-fixed: 2853618
    
    Change-Id: I750139a482280ce3409989582ffad50aed20e081

--- packages/SettingsLib/src/com/android/settingslib/bluetooth/BluetoothEventManager.java
+++ packages/SettingsLib/src/com/android/settingslib/bluetooth/BluetoothEventManager.java
@@ -43,7 +43,8 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.UUID;
-
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 /**
  * BluetoothEventManager receives broadcasts and callbacks from the Bluetooth
  * API and dispatches the event on the UI thread to the right class in the
@@ -63,6 +64,8 @@ public class BluetoothEventManager {
     private final android.os.Handler mReceiverHandler;
     private final UserHandle mUserHandle;
     private final Context mContext;
+    private final String ACT_BROADCAST_SOURCE_INFO =
+          "android.bluetooth.BroadcastAudioSAManager.action.BROADCAST_SOURCE_INFO";
 
     interface Handler {
         void onReceive(Context context, Intent intent, BluetoothDevice device);
@@ -130,6 +133,23 @@ public class BluetoothEventManager {
         addHandler(BluetoothDevice.ACTION_ACL_CONNECTED, new AclStateChangedHandler());
         addHandler(BluetoothDevice.ACTION_ACL_DISCONNECTED, new AclStateChangedHandler());
         addHandler(BluetoothA2dp.ACTION_CODEC_CONFIG_CHANGED, new A2dpCodecConfigChangedHandler());
+        Object sourceInfoHandler = null;
+        try {
+           Class<?> classSourceInfoHandler =
+               Class.forName("com.android.settingslib.bluetooth.BroadcastSourceInfoHandler");
+           Constructor ctor;
+           ctor = classSourceInfoHandler.getDeclaredConstructor(
+                      new Class[] {CachedBluetoothDeviceManager.class});
+           sourceInfoHandler = ctor.newInstance(mDeviceManager);
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException
+                  | InstantiationException | InvocationTargetException e) {
+              e.printStackTrace();
+        }
+        if (sourceInfoHandler != null) {
+           Log.d(TAG, "adding SourceInfo Handler");
+           addHandler(ACT_BROADCAST_SOURCE_INFO,
+                    (Handler)sourceInfoHandler);
+        }
 
         registerAdapterIntentReceiver();
     }
