f7da5b5f8d4165d81a674781c31f8649d4e1d0f7
Aaron Kling <webgeek1234@gmail.com>
2020-12-31 21:38:11 +0000

    Add nvidia profile manager
    This implements and API for apps to interface with NvCPL and by
    extension the PowerHAL.
    
    Reverse engineered from the Shield Experience 9.1.1 update then cleaned
    up to match Android standards.
    
    Co-authored-by: nift4 <nift4@protonmail.com>
    Change-Id: I774ac62c8867151f21712cfeee25f96a591415ad

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -131,6 +131,8 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.os.SomeArgs;
 import com.android.internal.util.UserIcons;
 
+import com.nvidia.NvAppProfileService;
+
 import dalvik.system.VMRuntime;
 
 import libcore.util.EmptyArray;
@@ -184,6 +186,7 @@ public class ApplicationPackageManager extends PackageManager {
     private volatile UserManager mUserManager;
     private volatile PermissionManager mPermissionManager;
     private volatile PackageInstaller mInstaller;
+    private volatile NvAppProfileService mAppProfileService;
     private volatile ArtManager mArtManager;
     private volatile DevicePolicyManager mDevicePolicyManager;
     private volatile String mPermissionsControllerPackageName;
@@ -493,6 +496,15 @@ public class ApplicationPackageManager extends PackageManager {
     public boolean isWirelessConsentModeEnabled() {
         return mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_wirelessConsentRequired);
+    }
+
+    /** @hide */
+    @Override
+    public NvAppProfileService getAppProfileService() {
+        if (mAppProfileService == null) {
+            mAppProfileService = new NvAppProfileService(mContext);
+        }
+        return mAppProfileService;
     }
 
     @Override
