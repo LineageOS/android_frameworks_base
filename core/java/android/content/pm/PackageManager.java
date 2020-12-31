e2a49f8d5d48b64016e7acb985164295122d47ad
Aaron Kling <webgeek1234@gmail.com>
2020-12-31 21:38:11 +0000

    Add nvidia profile manager
    This implements and API for apps to interface with NvCPL and by
    extension the PowerHAL.
    
    Reverse engineered from the Shield Experience 8.2.0 update then cleaned
    up to match Android standards.
    
    Co-authored-by: nift4 <nift4@protonmail.com>
    
    Change-Id: I774ac62c8867151f21712cfeee25f96a591415ad
    (cherry picked from commit e5b84fdd3e5a9a13a6727922afc5ff01d3d16b86)

--- core/java/android/content/pm/PackageManager.java
+++ core/java/android/content/pm/PackageManager.java
@@ -89,6 +89,8 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.util.ArrayUtils;
 import com.android.internal.util.DataClass;
 
+import com.nvidia.NvAppProfileService;
+
 import dalvik.system.VMRuntime;
 
 import java.io.File;
@@ -5705,6 +5707,11 @@ public abstract class PackageManager {
     @NonNull
     public abstract List<PermissionGroupInfo> getAllPermissionGroups(
             @PermissionGroupInfoFlags int flags);
+
+    /** @hide */
+    public NvAppProfileService getAppProfileService() {
+        return null;
+    }
 
     /**
      * Get the platform-defined permissions which belong to a particular permission group.
