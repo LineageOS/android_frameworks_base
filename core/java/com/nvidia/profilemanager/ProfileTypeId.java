5754162049ce68545381a5dea631e1e81c29b5d7
Aaron Kling <webgeek1234@gmail.com>
2020-12-31 21:38:11 +0000

    Add nvidia profile manager
    This implements and API for apps to interface with NvCPL and by
    extension the PowerHAL.
    
    Reverse engineered from the Shield Experience 8.2.0 update then cleaned
    up to match Android standards.
    
    Change-Id: I774ac62c8867151f21712cfeee25f96a591415ad

---
+++ core/java/com/nvidia/profilemanager/ProfileTypeId.java
@@ -0,0 +1,11 @@
+package com.nvidia.profilemanager;
+
+public class ProfileTypeId {
+    public static int LAST_RESERVED_TYPE = 9999;
+
+    public static int DEFAULT = 0;
+    public static int OEM_OVERRIDE = 1;
+    public static int DEV_OVERRIDE = 2;
+    public static int USER = 10000;
+    public static int OPTIMIZED = 10001;
+}
