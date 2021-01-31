2d13ca60c5466c8cba71b0bd305e55854e065c20
goodway.wang_cp <sz755cp2369@gmail.com>
2021-01-31 10:29:31 +0800

    cutout:check if not config config_mainBuiltInDisplayCutoutRectApproximation
    Change-Id: I19ee4bfaa5d8126233a00cd11e5a6e157e05130e

--- core/java/android/view/DisplayCutout.java
+++ core/java/android/view/DisplayCutout.java
@@ -38,6 +38,7 @@ import android.os.Parcelable;
 import android.text.TextUtils;
 import android.util.Pair;
 import android.util.proto.ProtoOutputStream;
+import android.util.Log;
 
 import com.android.internal.R;
 import com.android.internal.annotations.GuardedBy;
@@ -681,6 +682,11 @@ public final class DisplayCutout {
      * @hide
      */
     public static DisplayCutout fromResourcesRectApproximation(Resources res, int displayWidth, int displayHeight) {
+        final String mDisplayCutout = res.getString(R.string.config_mainBuiltInDisplayCutoutRectApproximation);
+        final String mDisplayIcon = res.getString(R.string.config_icon_mask);
+        if (mDisplayCutout.equals(mDisplayIcon)) {
+            Log.w(TAG,"Dont't Forget to config config_mainBuiltInDisplayCutoutRectApproximation!!!");
+        }
         return fromSpec(res.getString(R.string.config_mainBuiltInDisplayCutoutRectApproximation),
                 displayWidth, displayHeight, DENSITY_DEVICE_STABLE / (float) DENSITY_DEFAULT,
                 loadWaterfallInset(res));
