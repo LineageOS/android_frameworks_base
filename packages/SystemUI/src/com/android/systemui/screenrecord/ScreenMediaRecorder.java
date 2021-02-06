2807f50ea5f5263bb23039c42fa0c9dc1d736bf6
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-02-06 21:54:25 +0300

    SystemUI: allow to limit the max framerate of built-in screen recorder
    Some devices seem to have problems when running the screen recorder at
    native resolution @ max fps (I'm looking at you surya). The symptoms may
    include severe lagging in the recording or stuff like crashes when
    stopping in a landscape orientation. Allow limiting the recorder fps to
    fix these issues.
    
    Test: record screen on surya with config_screenRecorderMaxFramerate==90
    while screen is running @ 120Hz, observe a smooth video. Stop the
    recording while in full screen landscape youtube video, observe no crash
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Change-Id: I361c7ae4bf74f2dd67b86e960f8d2d6ef63f5b8f

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
@@ -48,6 +48,8 @@ import android.util.Size;
 import android.view.Surface;
 import android.view.WindowManager;
 
+import com.android.systemui.R;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -79,6 +81,7 @@ public class ScreenMediaRecorder {
     private ScreenRecordingMuxer mMuxer;
     private ScreenInternalAudioRecorder mAudio;
     private ScreenRecordingAudioSource mAudioSource;
+    private int mMaxRefreshRate;
 
     private Context mContext;
     MediaRecorder.OnInfoListener mListener;
@@ -90,6 +93,8 @@ public class ScreenMediaRecorder {
         mUser = user;
         mListener = listener;
         mAudioSource = audioSource;
+        mMaxRefreshRate = mContext.getResources().getInteger(
+                R.integer.config_screenRecorderMaxFramerate);
     }
 
     private void prepare() throws IOException, RemoteException {
@@ -127,6 +132,7 @@ public class ScreenMediaRecorder {
         int screenWidth = metrics.widthPixels;
         int screenHeight = metrics.heightPixels;
         int refereshRate = (int) wm.getDefaultDisplay().getRefreshRate();
+        if (mMaxRefreshRate != 0 && refereshRate > mMaxRefreshRate) refereshRate = mMaxRefreshRate;
         int vidBitRate = screenHeight * screenWidth * refereshRate / VIDEO_FRAME_RATE
                 * VIDEO_FRAME_RATE_TO_RESOLUTION_RATIO;
         mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
