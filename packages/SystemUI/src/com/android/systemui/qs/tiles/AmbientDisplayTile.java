cf2ef51694c0dd2cbfd2d416774e51df17170e30
Luca Stefani <luca.stefani.ge1@gmail.com>
2021-03-17 22:34:17 +0100

    SystemUI: AmbientTile: Don't hardcode display settings intent
    Change-Id: Ifaa69c2f235b85ae847524c81f0ba59e2a51cf01

--- packages/SystemUI/src/com/android/systemui/qs/tiles/AmbientDisplayTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/AmbientDisplayTile.java
@@ -39,9 +39,6 @@ import javax.inject.Inject;
 public class AmbientDisplayTile extends QSTileImpl<BooleanState> {
 
     private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_ambient_display);
-
-    private static final Intent DISPLAY_SETTINGS = new Intent("android.settings.DISPLAY_SETTINGS");
-
     private final SecureSetting mSetting;
 
     @Inject
@@ -78,7 +75,7 @@ public class AmbientDisplayTile extends QSTileImpl<BooleanState> {
 
     @Override
     public Intent getLongClickIntent() {
-        return DISPLAY_SETTINGS;
+        return new Intent(Settings.ACTION_DISPLAY_SETTINGS);
     }
 
     private void setEnabled(boolean enabled) {
