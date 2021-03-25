55219d15d80fcdf6284b77de1231a6e551600c17
Beth Thibodeau <ethibodeau@google.com>
2021-03-25 17:19:30 -0400

    Increase maximum allowed size for status bar icons
    The previous size was causing some apps to crash which otherwise worked
    fine. This more closely matches the hard limit in RecordingCanvas
    (which we need to stay below to prevent SystemUI from crashing).
    
    Fixes: 182891864
    Fixes: 182232777
    Bug: 169255797
    Test: atest StatusBarIconViewTest
    Test: manual - posting notifications with different drawable sizes
    Change-Id: I8deacc651e05a202ec980eeb8bcdf4f92daea8eb
    (cherry picked from commit 5cd7976f7d2b702f803f0628f61f02491834cd41)
    (cherry picked from commit 8875da5207678f41b6ec90800d1bf2d03327bd05)

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
@@ -25,6 +25,7 @@ import android.database.ContentObserver;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
 import android.graphics.Typeface;
@@ -49,8 +50,15 @@ import java.util.ArrayList;
 import cyanogenmod.providers.CMSettings;
 
 public class StatusBarIconView extends AnimatedImageView {
-    /** Maximum allowed width or height for an icon drawable */
-    private static final int MAX_IMAGE_SIZE = 500;
+    /**
+     * Maximum allowed byte count for an icon bitmap
+     * @see android.graphics.RecordingCanvas.MAX_BITMAP_SIZE
+     */
+    private static final int MAX_BITMAP_SIZE = 100 * 1024 * 1024; // 100 MB
+    /**
+     * Maximum allowed width or height for an icon drawable, if we can't get byte count
+     */
+    private static final int MAX_IMAGE_SIZE = 5000;
 
     private static final String TAG = "StatusBarIconView";
     private boolean mAlwaysScaleIcon;
@@ -227,9 +235,18 @@ public class StatusBarIconView extends AnimatedImageView {
             return false;
         }
 
-        if (drawable.getIntrinsicWidth() > MAX_IMAGE_SIZE
+        if (drawable instanceof BitmapDrawable && ((BitmapDrawable) drawable).getBitmap() != null) {
+            // If it's a bitmap we can check the size directly
+            int byteCount = ((BitmapDrawable) drawable).getBitmap().getByteCount();
+            if (byteCount > MAX_BITMAP_SIZE) {
+                Log.w(TAG, "Drawable is too large (" + byteCount + " bytes) " + mIcon);
+                return false;
+            }
+        } else if (drawable.getIntrinsicWidth() > MAX_IMAGE_SIZE
                 || drawable.getIntrinsicHeight() > MAX_IMAGE_SIZE) {
-            Log.w(TAG, "Drawable is too large " + mIcon);
+            // Otherwise, check dimensions
+            Log.w(TAG, "Drawable is too large (" + drawable.getIntrinsicWidth() + "x"
+                    + drawable.getIntrinsicHeight() + ") " + mIcon);
             return false;
         }
 
