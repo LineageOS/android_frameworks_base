2508ea3fd28e4e9699579c9cfaf082c0bc7fb499
Agrabren <agrabren@yahoo.com>
2021-03-25 19:47:15 +0000

    Don't forcibly consume joystick inputs
    This is required by RsMouse in order to tell if an application has
    consumed a joystick input.
    
    Change-Id: I084588414c236a28a301492b26e9f15731c98999

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -6596,7 +6596,6 @@ public final class ViewRootImpl implements ViewParent,
                     return FINISH_HANDLED;
                 } else if ((source & InputDevice.SOURCE_CLASS_JOYSTICK) != 0) {
                     mJoystick.process(event);
-                    return FINISH_HANDLED;
                 } else if ((source & InputDevice.SOURCE_TOUCH_NAVIGATION)
                         == InputDevice.SOURCE_TOUCH_NAVIGATION) {
                     mTouchNavigation.process(event);
