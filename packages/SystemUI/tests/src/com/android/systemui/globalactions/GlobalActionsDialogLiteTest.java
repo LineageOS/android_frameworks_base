5e7672f4ddfa49aa533d4a2b7aec01c77a6d0602
Gegham Zakaryan <zakaryan.2004@outlook.com>
2021-03-29 21:59:00 +0400

    frameworks: Add more power menu customizations
    * Forward-port the rest of power menu customizations (airplane mode, screenshot, lockdown, bugreport, user switcher).
    * Screenshot option will always show by default and can be turned off in Settings.
    * Use the new LineageGlobalActionsService to get user's configuration
    
    Squashed with the following changes:
    
    Author: Michael Bestas <mikeioannina@gmail.com>
    Date:   Sat Jan 31 06:06:38 2015 +0200
    
        GlobalActions: Use circular user avatars
    
        * Use circular & smaller avatars, logic copied from SystemUI
        * Improve current user indication (thanks to maxwen)
    
        Change-Id: I81e6c4afd604f5cb8b22e2c4db9b174bbe7fb3f0
        Ticket-Id: OPO-456, CYNGNOS-1901
    
    Author: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Date:   Mon Mar 29 20:17:47 2021 +0400
    
        frameworks: Add tunable for GlobalActions configuration
    
        Before, the power menu may have not updated when switching users.
        With tunables, we can be sure each user will only see their own settings.
    
        Remove the ACTION_UPDATE_POWER_MENU broadcast as well.
    
        Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
        Change-Id: I1d7dbe1eac41a83910c9c6db28162212971b42fb
    
    Author: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Date:   Mon Mar 29 23:33:17 2021 +0400
    
        frameworks: New Users dialog for GlobalActions
    
        Take advantage of the new R-style dialog used in PowerOptions
        for showing the user switcher. All of the previous functionalities
        have been retained.
    
        Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
        Change-Id: Ib8ba9276d85b0ddcb3a188e72235eb77a4c70a82
    
    Author: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Date:   Thu Apr 1 13:23:57 2021 +0200
    
        GlobalActionsDialog: Remove broken PhoneStateListener for airplane mode toggle
    
        The Airplane Mode toggle in GlobalActions is completely broken,
        being stuck in TurningOn transition state when pressing it when
        Airplane Mode is off. This happens because the PhoneStateListener
        doesn't trigger onServiceStateChanged when Airplane Mode turns on.
        This commit removes the redundant PhoneStateListener and makes the
        logic work for Telephony and non-Telephony devices pretty much the
        same way by allowing onAirplaneModeChanged() to handle both cases.
    
        Change-Id: Ifd1e06ff144addf8dd2c8494769b2d646599ae0a
        Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Fri Feb 25 23:49:46 2022 +0000
    
        GlobalActionsDialog: Never show emergency without telephony support
    
        Change-Id: I7a4cd340d69fa6e3e2e6a2ba0529276f3b04155b
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Change-Id: I225037422937602940aafb5d20acea8c0bcff6ba

--- packages/SystemUI/tests/src/com/android/systemui/globalactions/GlobalActionsDialogLiteTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/globalactions/GlobalActionsDialogLiteTest.java
@@ -63,7 +63,6 @@ import com.android.systemui.statusbar.VibratorHelper;
 import com.android.systemui.statusbar.phone.CentralSurfaces;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
-import com.android.systemui.telephony.TelephonyListenerManager;
 import com.android.systemui.util.RingerModeLiveData;
 import com.android.systemui.util.RingerModeTracker;
 import com.android.systemui.util.settings.GlobalSettings;
@@ -91,7 +90,6 @@ public class GlobalActionsDialogLiteTest extends SysuiTestCase {
     @Mock private DevicePolicyManager mDevicePolicyManager;
     @Mock private LockPatternUtils mLockPatternUtils;
     @Mock private BroadcastDispatcher mBroadcastDispatcher;
-    @Mock private TelephonyListenerManager mTelephonyListenerManager;
     @Mock private GlobalSettings mGlobalSettings;
     @Mock private SecureSettings mSecureSettings;
     @Mock private Resources mResources;
@@ -137,7 +135,6 @@ public class GlobalActionsDialogLiteTest extends SysuiTestCase {
                 mDevicePolicyManager,
                 mLockPatternUtils,
                 mBroadcastDispatcher,
-                mTelephonyListenerManager,
                 mGlobalSettings,
                 mSecureSettings,
                 mVibratorHelper,
