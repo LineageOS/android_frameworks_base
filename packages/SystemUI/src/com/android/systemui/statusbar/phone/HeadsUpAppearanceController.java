648dc0f35eb363defadd0e7784a1ce37dd327f3f
LuK1337 <priv.luk@gmail.com>
2021-04-11 09:58:09 +0200

    SystemUI: Hide centered_area when showing heads up
    Needed for network traffic monitor.
    
    Change-Id: Ib96a75a936cf4d71e23a575e012741b812ed2aa1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
@@ -54,6 +54,7 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
     private final HeadsUpManagerPhone mHeadsUpManager;
     private final NotificationStackScrollLayout mStackScroller;
     private final HeadsUpStatusBarView mHeadsUpStatusBarView;
+    private final View mCenteredView;
     private final View mCenteredIconView;
     private final ClockController mClockController;
     private final View mOperatorNameView;
@@ -108,6 +109,7 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
                 statusBarView.findViewById(R.id.clock),
                 new ClockController(statusBarView.getContext(), statusBarView),
                 statusBarView.findViewById(R.id.operator_name_frame),
+                statusBarView.findViewById(R.id.centered_area),
                 statusBarView.findViewById(R.id.centered_icon_area));
     }
 
@@ -126,11 +128,13 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
             View clockView,
             ClockController clockController,
             View operatorNameView,
+            View centeredView,
             View centeredIconView) {
         mNotificationIconAreaController = notificationIconAreaController;
         mHeadsUpManager = headsUpManager;
         mHeadsUpManager.addListener(this);
         mHeadsUpStatusBarView = headsUpStatusBarView;
+        mCenteredView = centeredView;
         mCenteredIconView = centeredIconView;
         headsUpStatusBarView.setOnDrawingRectChangedListener(
                 () -> updateIsolatedIconLocation(true /* requireUpdate */));
@@ -287,6 +291,9 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
                 if (!isRightClock) {
                     hide(clockView, View.INVISIBLE);
                 }
+                if (mCenteredView.getVisibility() != View.GONE) {
+                    hide(mCenteredView, View.INVISIBLE);
+                }
                 if (mCenteredIconView.getVisibility() != View.GONE) {
                     hide(mCenteredIconView, View.INVISIBLE);
                 }
@@ -296,6 +303,9 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
             } else {
                 if (!isRightClock) {
                     show(clockView);
+                }
+                if (mCenteredView.getVisibility() != View.GONE) {
+                    show(mCenteredView);
                 }
                 if (mCenteredIconView.getVisibility() != View.GONE) {
                     show(mCenteredIconView);
