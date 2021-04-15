7fd09b8030b6edab5c0e90762e057bbc1c87e7f4
Oliver Scott <olivercscott@gmail.com>
2021-04-15 10:55:16 -0400

    Implement backup/restore for network policy
    Modify network policy XML parser helper functions for backup/restore scenarios
    
    Issue: calyxos#384
    Change-Id: I77158b344a47138a17fbce72d3ecb3be6927b870

--- services/core/java/com/android/server/backup/SystemBackupAgent.java
+++ services/core/java/com/android/server/backup/SystemBackupAgent.java
@@ -24,6 +24,7 @@ import android.app.backup.FullBackup;
 import android.app.backup.FullBackupDataOutput;
 import android.app.backup.WallpaperBackupHelper;
 import android.content.Context;
+import android.net.INetworkPolicyManager;
 import android.os.Environment;
 import android.os.ParcelFileDescriptor;
 import android.os.RemoteException;
@@ -56,6 +57,7 @@ public class SystemBackupAgent extends BackupAgentHelper {
     private static final String ACCOUNT_MANAGER_HELPER = "account_manager";
     private static final String SLICES_HELPER = "slices";
     private static final String PEOPLE_HELPER = "people";
+    private static final String NETWORK_POLICY_HELPER = "network_policy";
 
     // These paths must match what the WallpaperManagerService uses.  The leaf *_FILENAME
     // are also used in the full-backup file format, so must not change unless steps are
@@ -101,6 +103,7 @@ public class SystemBackupAgent extends BackupAgentHelper {
         addHelper(ACCOUNT_MANAGER_HELPER, new AccountManagerBackupHelper());
         addHelper(SLICES_HELPER, new SliceBackupHelper(this));
         addHelper(PEOPLE_HELPER, new PeopleBackupHelper(mUserId));
+        addHelper(NETWORK_POLICY_HELPER, new NetworkPolicyBackupHelper());
     }
 
     @Override
