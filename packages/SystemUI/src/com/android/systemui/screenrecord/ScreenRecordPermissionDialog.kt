c2ea8eee1fd459b7fa284bd6aa7c36ae1dd1dadb
Ido Ben-Hur <idoybh2@gmail.com>
2021-05-13 18:48:20 +0300

    Screenrecord: Add an option to record for longer
    Unlimited time-wise. Only limit file size to 15GiB
    
    Change-Id: Ia16045dde478f65d1bee4a5167684c403f018906

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionDialog.kt
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionDialog.kt
@@ -62,6 +62,7 @@ class ScreenRecordPermissionDialog(
     private lateinit var options: Spinner
     private lateinit var stopDotSwitch: Switch
     private lateinit var lowQualitySwitch: Switch
+    private lateinit var longerDurationSwitch: Switch
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setDialogTitle(R.string.screenrecord_permission_dialog_title)
@@ -110,6 +111,7 @@ class ScreenRecordPermissionDialog(
         options = requireViewById(R.id.screen_recording_options)
         stopDotSwitch = requireViewById(R.id.screenrecord_stopdot_switch)
         lowQualitySwitch = requireViewById(R.id.screenrecord_lowquality_switch)
+        longerDurationSwitch = requireViewById(R.id.screenrecord_longer_timeout_switch)
         val a: ArrayAdapter<*> =
             ScreenRecordingAdapter(context, android.R.layout.simple_spinner_dropdown_item, MODES)
         a.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -142,6 +144,7 @@ class ScreenRecordPermissionDialog(
             else ScreenRecordingAudioSource.NONE
         val showStopDot = stopDotSwitch.isChecked
         val lowQuality = lowQualitySwitch.isChecked
+        val longerDuration = longerDurationSwitch.isChecked
         val startIntent =
             PendingIntent.getForegroundService(
                 userContext,
@@ -153,7 +156,8 @@ class ScreenRecordPermissionDialog(
                     showTaps,
                     captureTarget,
                     showStopDot,
-                    lowQuality
+                    lowQuality,
+                    longerDuration
                 ),
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
             )
