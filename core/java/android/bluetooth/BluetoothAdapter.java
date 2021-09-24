c8580e2b839b0c05806f90bc260ecc37223a563d
pramod kotreshappa <kpramod@codeaurora.org>
2021-09-24 00:15:12 -0700

    Use BLUETOOTH_CONNECT permission instead of legacy permission
    CRs-Fixed: 3043842
    
    Change-Id: I70f3c109140bf9634e2d0a293a323cae3c9cd476

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -2008,12 +2008,12 @@ public final class BluetoothAdapter {
     }
 
     /** @hide */
-    @RequiresPermission(Manifest.permission.BLUETOOTH)
+    @RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)
     public boolean isBroadcastActive() {
         try {
             mServiceLock.readLock().lock();
             if (mService != null) {
-                return mService.isBroadcastActive();
+                return mService.isBroadcastActive(mAttributionSource);
             }
         } catch (RemoteException e) {
             Log.e(TAG, "", e);
