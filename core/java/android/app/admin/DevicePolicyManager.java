7cf616ab4a8fd30a8be342f2b9e20c1bbd8b8bf7
Alex Johnston <acjohnston@google.com>
2021-10-05 11:44:37 +0000

    Stop managed profile owner granting READ_SMS
    Reason: There is only one telephony stack shared
    between the personal and work profile.
    
    Bug: 194382185
    Bug: 189942529
    Test: build
    Change-Id: If0d27a317a7c0ee46af371b30208327e5636c7cf
    (cherry picked from commit 87f37319bf7ee22c6e7c29432b6c9bbce0fdb591)
    Merged-In: If0d27a317a7c0ee46af371b30208327e5636c7cf

--- core/java/android/app/admin/DevicePolicyManager.java
+++ core/java/android/app/admin/DevicePolicyManager.java
@@ -5861,6 +5861,15 @@ public class DevicePolicyManager {
      * Permissions can be granted or revoked only for applications built with a
      * {@code targetSdkVersion} of {@link android.os.Build.VERSION_CODES#M} or later.
      *
+     * Control over the following permissions are restricted for managed profile owners:
+     * <ul>
+     *  <li>Manifest.permission.READ_SMS</li>
+     * </ul>
+     * <p>
+     * A managed profile owner may not grant these permissions (i.e. call this method with any of
+     * the permissions listed above and {@code grantState} of
+     * {@code #PERMISSION_GRANT_STATE_GRANTED}), but may deny them.
+     *
      * @param admin Which profile or device owner this request is associated with.
      * @param packageName The application to grant or revoke a permission to.
      * @param permission The permission to grant or revoke.
