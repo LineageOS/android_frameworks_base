4feee1013dfc65c9df36a555f58636377d1915d2
TheScarastic <warabhishek@gmail.com>
2021-10-06 07:04:50 +0000

    udfps: change window type to TYPE_DISPLAY_OVERLAY
     * TYPE_KEYGUARD_DIALOG was below volume and navbar and would have caused issues
       when HBM was enabled
    
     * BiometricPrompt uses different layer which again is below navbar and causes
       bright volume and navbar
    
    Change-Id: Ia37930ffd7a28fc8785f26ada3b69a02df0d4891

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -568,7 +568,7 @@ public class UdfpsController implements DozeReceiver, UdfpsHbmProvider {
         checkArgument(mSensorProps != null);
 
         mCoreLayoutParams = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG,
+                WindowManager.LayoutParams.TYPE_DISPLAY_OVERLAY,
                 getCoreLayoutParamFlags(),
                 PixelFormat.TRANSLUCENT);
         mCoreLayoutParams.setTitle(TAG);
