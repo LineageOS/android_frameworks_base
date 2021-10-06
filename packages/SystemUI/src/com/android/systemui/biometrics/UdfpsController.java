1e6b8b128be9c21cee5345467f4021343f4e2453
TheScarastic <warabhishek@gmail.com>
2021-10-06 06:44:46 +0000

    udfps: Implement UdfpsHbmProvider
     * Google implements it in SystemUIGoogle for their special logic
    
    Change-Id: If8da96c71855585bd13396c6f609b526b995b559

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -58,6 +58,7 @@ import android.view.accessibility.AccessibilityManager;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.systemui.R;
+import com.android.systemui.biometrics.UdfpsHbmTypes.HbmType;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.doze.DozeReceiver;
@@ -97,7 +98,7 @@ import kotlin.Unit;
  */
 @SuppressWarnings("deprecation")
 @SysUISingleton
-public class UdfpsController implements DozeReceiver {
+public class UdfpsController implements DozeReceiver, UdfpsHbmProvider {
     private static final String TAG = "UdfpsController";
     private static final long AOD_INTERRUPT_TIMEOUT_MILLIS = 1000;
 
@@ -754,7 +755,7 @@ public class UdfpsController implements DozeReceiver {
                 mView = (UdfpsView) mInflater.inflate(R.layout.udfps_view, null, false);
                 mOnFingerDown = false;
                 mView.setSensorProperties(mSensorProps);
-                mView.setHbmProvider(mHbmProvider);
+                mView.setHbmProvider(this);
                 UdfpsAnimationViewController animation = inflateUdfpsAnimation(reason);
                 mAttemptedToDismissKeyguard = false;
                 animation.init();
@@ -1019,5 +1020,22 @@ public class UdfpsController implements DozeReceiver {
          * Called onFingerDown events.
          */
         void onFingerDown();
+    }
+
+    @Override
+    public void enableHbm(@HbmType int hbmType, @Nullable Surface surface,
+            @Nullable Runnable onHbmEnabled) {
+        // TO-DO send call to lineage biometric hal and/or add dummy jni that device could override
+        if (onHbmEnabled != null) {
+            onHbmEnabled.run();
+        }
+    }
+
+    @Override
+    public void disableHbm(@Nullable Runnable onHbmDisabled) {
+        // TO-DO send call to lineage biometric hal and/or add dummy jni that device could override
+        if (onHbmDisabled != null) {
+            onHbmDisabled.run();
+        }
     }
 }
