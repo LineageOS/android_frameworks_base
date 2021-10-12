a573fcd653d5a270ba682ab71c260a24eb8252e8
Danny Lin <danny@kdrag0n.dev>
2021-10-11 19:25:11 -0700

    SystemUI: Remove nav bar background in QS customizer
    This looks outdated, since most apps are edge-to-edge nowadays.
    
    Change-Id: Ia2281fc27cff326a061ad42575f845f30604b9ee
    Signed-off-by: althafvly <althafvly@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -105,12 +105,6 @@ public class QSCustomizer extends LinearLayout {
         if (enabled != mSceneContainerEnabled) {
             mSceneContainerEnabled = enabled;
             updateTransparentViewHeight();
-            if (mSceneContainerEnabled) {
-                findViewById(R.id.nav_bar_background).setVisibility(View.GONE);
-            } else {
-                findViewById(R.id.nav_bar_background)
-                        .setVisibility(mIsShowingNavBackdrop ? View.VISIBLE : View.GONE);
-            }
         }
     }
 
@@ -120,13 +114,8 @@ public class QSCustomizer extends LinearLayout {
     }
 
     void updateNavBackDrop(Configuration newConfig, LightBarController lightBarController) {
-        View navBackdrop = findViewById(R.id.nav_bar_background);
         mIsShowingNavBackdrop = newConfig.smallestScreenWidthDp >= 600
                 || newConfig.orientation != Configuration.ORIENTATION_LANDSCAPE;
-        if (navBackdrop != null) {
-            navBackdrop.setVisibility(
-                    mIsShowingNavBackdrop && !mSceneContainerEnabled ? View.VISIBLE : View.GONE);
-        }
         updateNavColors(lightBarController);
     }
 
