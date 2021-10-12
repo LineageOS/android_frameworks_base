d6acae6996abbe49dda14ca314c30c4bd6b7071c
Danny Lin <danny@kdrag0n.dev>
2021-10-11 19:59:51 -0700

    Alter model name to avoid SafetyNet HW attestation enforcement
    As of September 2, Google is enforcing SafetyNet's previously
    opportunistic hardware-backed attestation based on device information.
    Append a space to the device model name in order to avoid such
    enforcement.
    
    Also contains:
    	Spoof build fingerprint for Google Play Services
    
    	SafetyNet's CTS profile attestation checks whether Build.FINGERPRINT
    	matches that of the device's stock OS, which has passed CTS testing.
    	Spoof the fingerprint for Google Play Services to help pass SafetyNet.
    
    	We used to set the real system build fingerprint to the stock one, but
    	Android relies on each build having a unique fingerprint in order to
    	clear the correct caches and update persistent state for system changes.
    	On devices that no longer receive updates from the OEM, the build
    	fingerprint never changes and Android doesn't account for updates
    	correctly, which causes issues when updating without wiping data.
    	Only spoofing the fingerprint for Google Play Services fixes this issue.
    
    	Corresponding vendor commit:
    	"Only use stock build fingerprint for Google Play Services"
    
    	NB: This code is under the gmscompat package, but it does not depend on
    	any code from gmscompat.
    
    	Change-Id: I26a2498eb2e2163933303b03f6d516e5fb30fe51
    
    * We don't need to spoof the fingerprint here since we do it globally, but we
      use the Build field spoofing code it added for model
    
    Change-Id: Ib7779e0aae40cab3730a56785e9231896917ab0a

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -57,6 +57,8 @@ import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
 
+import com.android.internal.gmscompat.AttestationHooks;
+
 import java.io.File;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -1188,6 +1190,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
+        AttestationHooks.initApplicationBeforeOnCreate(app);
         return app;
     }
     
@@ -1205,6 +1208,7 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
+        AttestationHooks.initApplicationBeforeOnCreate(app);
         return app;
     }
 
