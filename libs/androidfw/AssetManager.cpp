bf0cee3b5b0370ff6f759160f3bd6c5b482059d2
Luca Stefani <luca.stefani.ge1@gmail.com>
2021-10-28 23:56:16 +0200

    androidfw: Add Lineage res to default assets
    Change-Id: I815a78e645a2eaf65667fea3789d0a68149ee075

--- libs/androidfw/AssetManager.cpp
+++ libs/androidfw/AssetManager.cpp
@@ -63,6 +63,7 @@ static const bool kIsDebug = false;
 static const char* kAssetsRoot = "assets";
 static const char* kAppZipName = NULL; //"classes.jar";
 static const char* kSystemAssets = "framework/framework-res.apk";
+static const char* kLineageAssets = "framework/org.lineageos.platform-res.apk";
 static const char* kResourceCache = "resource-cache";
 
 static const char* kExcludeExtension = ".EXCLUDE";
@@ -367,10 +368,18 @@ bool AssetManager::addDefaultAssets()
     const char* root = getenv("ANDROID_ROOT");
     LOG_ALWAYS_FATAL_IF(root == NULL, "ANDROID_ROOT not set");
 
-    String8 path(root);
-    appendPath(path, kSystemAssets);
-
-    return addAssetPath(path, NULL, false /* appAsLib */, true /* isSystemAsset */);
+    bool success = true;
+    {
+       String8 path(root);
+       appendPath(path, kSystemAssets);
+       success &= addAssetPath(path, NULL, false /* appAsLib */, true /* isSystemAsset */);
+    }
+    {
+       String8 path(root);
+       appendPath(path, kLineageAssets);
+       success &= addAssetPath(path, NULL, false /* appAsLib */, true /* isSystemAsset */);
+    }
+    return success;
 }
 
 int32_t AssetManager::nextAssetPath(const int32_t cookie) const
