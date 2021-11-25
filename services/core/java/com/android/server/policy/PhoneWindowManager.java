53c606e2514c6946a928084f3342b7d7c2855fa2
LuK1337 <priv.luk@gmail.com>
2021-11-25 17:39:29 +0100

    Use custom flag for edge long swipe gesture
    Previously we were using same flags that'd be used for regular back key
    long press thus being unable to determine whether the event was sent
    from EdgeBackGestureHandle or not.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/4194
    Change-Id: I5c4fd455f581ac5c9c5e3a146095be33e82e8d6e

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -692,8 +692,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private PendingIntent mTorchOffPendingIntent;
 
     private boolean mLongSwipeDown;
-    private static final int LONG_SWIPE_FLAGS = KeyEvent.FLAG_LONG_PRESS
-            | KeyEvent.FLAG_FROM_SYSTEM | KeyEvent.FLAG_VIRTUAL_HARD_KEY;
 
     private class PolicyHandler extends Handler {
         @Override
@@ -4024,7 +4022,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         // Handle special keys.
         switch (keyCode) {
             case KeyEvent.KEYCODE_BACK: {
-                boolean isLongSwipe = (event.getFlags() & LONG_SWIPE_FLAGS) == LONG_SWIPE_FLAGS;
+                boolean isLongSwipe = (event.getFlags() & KeyEvent.FLAG_LONG_SWIPE) != 0;
                 if (mLongSwipeDown && isLongSwipe && !down) {
                     // Trigger long swipe action
                     performKeyAction(mEdgeLongSwipeAction, event);
