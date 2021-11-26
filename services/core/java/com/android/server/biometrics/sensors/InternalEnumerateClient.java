a445c38ee9cc52ae14d96e0309b2301b4557c3dc
Pierre-Hugues Husson <phh@phh.me>
2021-11-26 01:28:01 -0800

    Biometrics: Allow disabling of fingerprint cleanups
    [SamarV-121 - Adapt to S]
    [DarkJoker360 - Switch to overlays]
    [neobuddy89 - Fixed up compilation issues]
    
    Change-Id: I8a486e707712b81711fb1a691faec029499fa897
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/biometrics/sensors/InternalEnumerateClient.java
+++ services/core/java/com/android/server/biometrics/sensors/InternalEnumerateClient.java
@@ -44,6 +44,8 @@ public abstract class InternalEnumerateClient<T> extends HalClientMonitor<T>
     // List of templates to remove from the HAL
     private List<BiometricAuthenticator.Identifier> mUnknownHALTemplates = new ArrayList<>();
 
+    private boolean mCleanup;
+
     protected InternalEnumerateClient(@NonNull Context context, @NonNull LazyDaemon<T> lazyDaemon,
             @NonNull IBinder token, int userId, @NonNull String owner,
             @NonNull List<? extends BiometricAuthenticator.Identifier> enrolledList,
@@ -55,6 +57,8 @@ public abstract class InternalEnumerateClient<T> extends HalClientMonitor<T>
                 BiometricsProtoEnums.CLIENT_UNKNOWN);
         mEnrolledList = enrolledList;
         mUtils = utils;
+        mCleanup = context.getResources().getBoolean(
+                 com.android.internal.R.bool.config_cleanupUnusedFingerprints);
     }
 
     @Override
@@ -114,8 +118,10 @@ public abstract class InternalEnumerateClient<T> extends HalClientMonitor<T>
             BiometricAuthenticator.Identifier identifier = mEnrolledList.get(i);
             Slog.e(TAG, "doTemplateCleanup(): Removing dangling template from framework: "
                     + identifier.getBiometricId() + " " + identifier.getName());
-            mUtils.removeBiometricForUser(getContext(),
-                    getTargetUserId(), identifier.getBiometricId());
+            if (mCleanup) {
+               mUtils.removeBiometricForUser(getContext(),
+                        getTargetUserId(), identifier.getBiometricId());
+            }
             FrameworkStatsLog.write(FrameworkStatsLog.BIOMETRIC_SYSTEM_HEALTH_ISSUE_DETECTED,
                     mStatsModality,
                     BiometricsProtoEnums.ISSUE_UNKNOWN_TEMPLATE_ENROLLED_FRAMEWORK,
