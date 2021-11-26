b535f63e89315431d86b8253f99077cc4f0e31e2
Oliver Scott <olivercscott@gmail.com>
2021-11-26 13:59:21 -0500

    Migrate from network isolation to restricted networking mode
    * Pre-12, we used to implement network-isolation with POLICY_REJECT_ALL
    * Starting from 12, we've switched to AOSP's restricted-networking-mode
    * Read uids which were isolated, and migrate them to restricted networking,
      for both upgrades (which may be from 11) and restores (same)
    * Remove POLICY_REJECT_ALL for migrated uids to avoid future conflicts
    
    Co-authored-by: Chirayu Desai <chirayudesai1@gmail.com>
    Change-Id: I01d21abb9084eb1f61ccefd8a6da33e82697294e

--- services/core/java/com/android/server/net/NetworkPolicyManagerService.java
+++ services/core/java/com/android/server/net/NetworkPolicyManagerService.java
@@ -86,6 +86,7 @@ import static android.net.NetworkPolicyManager.MASK_METERED_NETWORKS;
 import static android.net.NetworkPolicyManager.MASK_RESTRICTED_MODE_NETWORKS;
 import static android.net.NetworkPolicyManager.POLICY_ALLOW_METERED_BACKGROUND;
 import static android.net.NetworkPolicyManager.POLICY_NONE;
+import static android.net.NetworkPolicyManager.POLICY_REJECT_ALL;
 import static android.net.NetworkPolicyManager.POLICY_REJECT_CELLULAR;
 import static android.net.NetworkPolicyManager.POLICY_REJECT_METERED_BACKGROUND;
 import static android.net.NetworkPolicyManager.POLICY_REJECT_VPN;
@@ -2410,6 +2411,25 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
         }
     }
 
+    private void migrateNetworkIsolation() {
+        // Get pre-12 network-isolation uids
+        final int[] uidsWithPolicy = getUidsWithPolicy(POLICY_REJECT_ALL);
+        final Set<Integer> uidsToDeny =
+                Arrays.stream(uidsWithPolicy).boxed().collect(Collectors.toSet());
+
+        // Remove the POLICY_REJECT_ALL uids from the allowlist
+        Set<Integer> uidsAllowedOnRestrictedNetworks =
+                ConnectivitySettingsManager.getUidsAllowedOnRestrictedNetworks(mContext);
+        uidsAllowedOnRestrictedNetworks.removeAll(uidsToDeny);
+        ConnectivitySettingsManager.setUidsAllowedOnRestrictedNetworks(mContext,
+                uidsAllowedOnRestrictedNetworks);
+
+        // Clear policy to avoid future conflicts
+        for (int uid : uidsToDeny) {
+            removeUidPolicy(uid, POLICY_REJECT_ALL);
+        }
+    }
+
     @GuardedBy({"mUidRulesFirstLock", "mNetworkPoliciesSecondLock"})
     private void readPolicyAL() {
         if (LOGV) Slog.v(TAG, "readPolicyAL()");
@@ -2432,6 +2452,9 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
         } finally {
             IoUtils.closeQuietly(fis);
         }
+
+        // Migrate from pre-12 network-isolation to restricted-networking-mode
+        migrateNetworkIsolation();
     }
 
     private void readPolicyXml(InputStream inputStream, boolean forRestore) throws IOException,
@@ -2888,6 +2911,9 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
         } catch (IOException | XmlPullParserException e) {
             Slog.w(TAG, "applyRestore: error reading payload", e);
         }
+
+        // Migrate from pre-12 network-isolation to restricted-networking-mode
+        migrateNetworkIsolation();
     }
 
     @Override
