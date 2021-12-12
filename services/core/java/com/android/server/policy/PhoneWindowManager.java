1975ad87c3f596af3004656169f2af1290828500
LuK1337 <priv.luk@gmail.com>
2021-12-12 15:12:23 +0100

    Don't pass repeated back key events to app if custom action is set up
    Some apps may react to back key long press and it doesn't make sense to
    allow them to do that if 'Back long press action' is set to anything
    other than 'No action'.
    
    Change-Id: Iacac909d6a288cacf964c89d9586d572d14d1871

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -4292,6 +4292,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_BACK: {
                 if (down) {
                     mBackKeyHandled = false;
+
+                    // Don't pass repeated events to app if user has custom long press action
+                    // set up in settings
+                    if (event.getRepeatCount() > 0 && hasLongPressOnBackBehavior()) {
+                        result &= ~ACTION_PASS_TO_USER;
+                    }
                 } else {
                     if (!hasLongPressOnBackBehavior()) {
                         mBackKeyHandled |= backKeyPress();
