d564c29734f29480377e251a76f1c0cc0ce22a5b
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-26 16:08:13 +0530

    NetworkPolicyManager: Add POLICY_REJECT_ALL constant
    * This was used by network-isolation in 11 and earlier versions
    * We've switched to restricted-networking-mode for 12, however
      we should still keep this around for usage with migration, and
      as a reminder that this value should not be re-used to avoid
      future complications
    
    Change-Id: I1e51cc610ecc8cd0cb8f86b25f9f990f348b4097

--- core/java/android/net/NetworkPolicyManager.java
+++ core/java/android/net/NetworkPolicyManager.java
@@ -96,6 +96,13 @@ public class NetworkPolicyManager {
      * @hide
      */
     public static final int POLICY_REJECT_WIFI = 0x8000;
+    /** Reject network usage on all networks
+     * Not used since 12, since we now use restricted-networking-mode
+     * However, since this was present in earlier builds, keep it around
+     * to help with migration, and as a reminder that this value should not be re-used.
+     * @hide
+     */
+    public static final int POLICY_REJECT_ALL = 0x40000;
 
     /*
      * Rules defining whether an uid has access to a network given its type (metered / non-metered).
