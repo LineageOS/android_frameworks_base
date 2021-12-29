38475e94086cefbd634200dfc1ac783125ef343d
Oliver Scott <olivercscott@gmail.com>
2021-12-29 12:35:50 +0100

    Increase maximum password length to 64
    Change-Id: I9570db3ecb4e160eb759d48ae1c4aa70436b624f

--- core/java/android/app/admin/DevicePolicyManager.java
+++ core/java/android/app/admin/DevicePolicyManager.java
@@ -3630,7 +3630,7 @@ public class DevicePolicyManager {
      * Maximum supported password length. Kind-of arbitrary.
      * @hide
      */
-    public static final int MAX_PASSWORD_LENGTH = 16;
+    public static final int MAX_PASSWORD_LENGTH = 64;
 
     /**
      * Service Action: Service implemented by a device owner or profile owner supervision app to
