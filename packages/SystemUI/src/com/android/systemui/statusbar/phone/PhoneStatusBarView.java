6941ebee6f90384048b6c80d1afdfa18a558c3eb
Timi Rautamäki <timi.rautamaki@gmail.com>
2022-01-06 12:01:10 +0000

    SystemUI: don't show FRB when IME is visible
    Change-Id: I97a99cc1b98ac9ee1e623fe8f2782434c76f9ce0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -25,6 +25,8 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.inputmethodservice.InputMethodService;
+import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.AttributeSet;
 import android.util.EventLog;
@@ -351,6 +353,15 @@ public class PhoneStatusBarView extends PanelBar implements Callbacks {
             for (StatusBar.ExpansionChangedListener listener : mExpansionChangedListeners) {
                 listener.onExpansionChanged(frac, expanded);
             }
+        }
+    }
+
+    @Override
+    public void setImeWindowStatus(int displayId, IBinder token, int vis, int backDisposition,
+            boolean showImeSwitcher) {
+        if (mRotationButtonController != null) {
+            final boolean imeShown = (vis & InputMethodService.IME_VISIBLE) != 0;
+            mRotationButtonController.getRotationButton().setCanShowRotationButton(!imeShown);
         }
     }
 
