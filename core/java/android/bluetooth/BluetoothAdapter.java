f606ff9c46f6e54b287c692e6b2eee9f7e8fc02c
Pranav Vashi <neobuddy89@gmail.com>
2022-01-27 00:54:42 +0530

    fixup! DeviceGroup: Framework changes for Group Device operations.
    fixup for change-Id: I0a8186e800e9d2701319db1adc97bdcf0441cc12
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Id67a9bc3e4a0e411b7377f111324465ca447b2a3

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -3278,6 +3278,7 @@ public final class BluetoothAdapter {
             case BluetoothProfile.LE_AUDIO:
                 BluetoothLeAudio leAudio = (BluetoothLeAudio) proxy;
                 leAudio.close();
+                break;
             case BluetoothProfile.GROUP_CLIENT:
                 BluetoothDeviceGroup groupClient = (BluetoothDeviceGroup) proxy;
                 groupClient.close();
