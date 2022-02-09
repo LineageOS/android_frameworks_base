7e6ead6fb84a7e5612cd7200aa4273980fabe459
maitreya29 <maitreyapatni30@gmail.com>
2022-02-09 10:26:45 +0530

    Screenrecord: Allow to reduce 3 second screen record timer
    Change-Id: Ia487395c733ba609f89d84fdf934801174a40505

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionContentManager.kt
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionContentManager.kt
@@ -110,6 +110,7 @@ class ScreenRecordPermissionContentManager(
     private lateinit var audioSwitch: Switch
     private lateinit var lowQualitySwitch: Switch
     private lateinit var longerDurationSwitch: Switch
+    private lateinit var skipTimeSwitch: Switch
     private lateinit var tapsView: View
     private lateinit var options: Spinner
 
@@ -162,6 +163,7 @@ class ScreenRecordPermissionContentManager(
         lowQualitySwitch = containerView.requireViewById(R.id.screenrecord_lowquality_switch)
         longerDurationSwitch =
             containerView.requireViewById(R.id.screenrecord_longer_timeout_switch)
+        skipTimeSwitch = containerView.requireViewById(R.id.screenrecord_skip_time_switch)
 
         tapsView = containerView.requireViewById(R.id.show_taps)
         updateTapsViewVisibility()
@@ -172,6 +174,7 @@ class ScreenRecordPermissionContentManager(
         tapsSwitch.setOnTouchListener { _, event -> event.action == ACTION_MOVE }
         lowQualitySwitch.setOnTouchListener { _, event -> event.action == ACTION_MOVE }
         longerDurationSwitch.setOnTouchListener { _, event -> event.action == ACTION_MOVE }
+        skipTimeSwitch.setOnTouchListener { _, event -> event.action == ACTION_MOVE }
 
         options = containerView.requireViewById(R.id.screen_recording_options)
         val a: ArrayAdapter<*> =
@@ -227,10 +230,11 @@ class ScreenRecordPermissionContentManager(
             else ScreenRecordingAudioSource.NONE
         val lowQuality = lowQualitySwitch.isChecked
         val longerDuration = longerDurationSwitch.isChecked
+        val skipTime = skipTimeSwitch.isChecked
 
         controller.startCountdown(
-            DELAY_MS,
-            INTERVAL_MS,
+            if (skipTime) NO_DELAY else DELAY_MS,
+            if (skipTime) NO_DELAY else INTERVAL_MS,
             {
                 screenRecordingStartStopRepository.startRecording(
                     ScreenRecordingParameters(
@@ -272,6 +276,7 @@ class ScreenRecordPermissionContentManager(
             )
 
         private const val DELAY_MS: Long = 3000
+        private const val NO_DELAY: Long = 100
         private const val INTERVAL_MS: Long = 1000
 
         fun createOptionList(displayManager: DisplayManager): List<ScreenShareOption> {
