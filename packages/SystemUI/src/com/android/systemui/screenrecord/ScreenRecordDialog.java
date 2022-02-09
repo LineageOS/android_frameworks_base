7e62dd5d16f877a67292ba24dd1ae10732ae9655
maitreya29 <maitreyapatni30@gmail.com>
2022-02-09 10:26:45 +0530

    Screenrecord: Allow to reduce 3 second screen record timer
    Change-Id: Ia487395c733ba609f89d84fdf934801174a40505

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordDialog.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordDialog.java
@@ -60,6 +60,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
     private static final List<ScreenRecordingAudioSource> MODES = Arrays.asList(INTERNAL, MIC,
             MIC_AND_INTERNAL);
     private static final long DELAY_MS = 3000;
+    private static final long NO_DELAY = 100;
     private static final long INTERVAL_MS = 1000;
     private static final String TAG = "ScreenRecordDialog";
     private static final String PREFS = "screenrecord_";
@@ -69,6 +70,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
     private static final String PREF_LONGER = "use_longer_timeout";
     private static final String PREF_AUDIO = "use_audio";
     private static final String PREF_AUDIO_SOURCE = "audio_source";
+    private static final String PREF_SKIP = "skip_timer";
 
     private final RecordingController mController;
     private final UserContextProvider mUserContextProvider;
@@ -79,6 +81,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
     private Switch mLowQualitySwitch;
     private Switch mLongerSwitch;
     private Switch mAudioSwitch;
+    private Switch mSkipSwitch;
     private Spinner mOptions;
 
     public ScreenRecordDialog(Context context,
@@ -121,6 +124,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
 
         mAudioSwitch = findViewById(R.id.screenrecord_audio_switch);
         mTapsSwitch = findViewById(R.id.screenrecord_taps_switch);
+        mSkipSwitch = findViewById(R.id.screenrecord_skip_switch);
         mStopDotSwitch = findViewById(R.id.screenrecord_stopdot_switch);
         mLowQualitySwitch = findViewById(R.id.screenrecord_lowquality_switch);
         mLongerSwitch = findViewById(R.id.screenrecord_longer_timeout_switch);
@@ -160,6 +164,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
         boolean showStopDot = mStopDotSwitch.isChecked();
         boolean lowQuality = mLowQualitySwitch.isChecked();
         boolean longerDuration = mLongerSwitch.isChecked();
+        boolean skipTime = mSkipSwitch.isChecked();
         ScreenRecordingAudioSource audioMode = mAudioSwitch.isChecked()
                 ? (ScreenRecordingAudioSource) mOptions.getSelectedItem()
                 : NONE;
@@ -174,7 +179,8 @@ public class ScreenRecordDialog extends SystemUIDialog {
                 RecordingService.REQUEST_CODE,
                 RecordingService.getStopIntent(userContext),
                 PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-        mController.startCountdown(DELAY_MS, INTERVAL_MS, startIntent, stopIntent);
+        mController.startCountdown(skipTime ? NO_DELAY : DELAY_MS, INTERVAL_MS, startIntent,
+                stopIntent);
     }
 
     private class CaptureTargetResultReceiver extends ResultReceiver {
@@ -203,6 +209,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
         Prefs.putInt(userContext, PREFS + PREF_LONGER, mLongerSwitch.isChecked() ? 1 : 0);
         Prefs.putInt(userContext, PREFS + PREF_AUDIO, mAudioSwitch.isChecked() ? 1 : 0);
         Prefs.putInt(userContext, PREFS + PREF_AUDIO_SOURCE, mOptions.getSelectedItemPosition());
+        Prefs.putInt(userContext, PREFS + PREF_SKIP, mSkipSwitch.isChecked() ? 1 : 0);
     }
 
     private void loadPrefs() {
@@ -213,5 +220,6 @@ public class ScreenRecordDialog extends SystemUIDialog {
         mLongerSwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_LONGER, 0) == 1);
         mAudioSwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_AUDIO, 0) == 1);
         mOptions.setSelection(Prefs.getInt(userContext, PREFS + PREF_AUDIO_SOURCE, 0));
+        mSkipSwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_SKIP, 0) == 1);
     }
 }
