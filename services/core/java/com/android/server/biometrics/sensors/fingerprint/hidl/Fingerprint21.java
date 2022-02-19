87f04da03724b26f64b78e628f171e4bc4b20673
Quallenauge <Hamsi2k@freenet.de>
2022-02-19 22:28:20 +0100

    Biometrics: Allow disabling of fingerprint cleanups
    Don't schedule cleanups at all if the driver
    doesn't support enumerate function.
    
    Change-Id: If9e1b82bc551e2fd06218b1720f5986633c1ab55

--- services/core/java/com/android/server/biometrics/sensors/fingerprint/hidl/Fingerprint21.java
+++ services/core/java/com/android/server/biometrics/sensors/fingerprint/hidl/Fingerprint21.java
@@ -124,6 +124,8 @@ public class Fingerprint21 implements IHwBinder.DeathRecipient, ServiceProvider 
     private final int mSensorId;
     private final boolean mIsPowerbuttonFps;
 
+    private boolean mCleanup;
+
     private final class BiometricTaskStackListener extends TaskStackListener {
         @Override
         public void onTaskStackChanged() {
@@ -345,6 +347,9 @@ public class Fingerprint21 implements IHwBinder.DeathRecipient, ServiceProvider 
             mDaemon = null;
             mCurrentUserId = UserHandle.USER_NULL;
         });
+
+        mCleanup = context.getResources().getBoolean(
+                org.lineageos.platform.internal.R.bool.config_cleanupUnusedFingerprints);
 
         try {
             ActivityManager.getService().registerUserSwitchObserver(mUserSwitchObserver, TAG);
@@ -694,6 +699,9 @@ public class Fingerprint21 implements IHwBinder.DeathRecipient, ServiceProvider 
 
     private void scheduleInternalCleanup(int userId,
             @Nullable BaseClientMonitor.Callback callback) {
+        if (!mCleanup) {
+            return;
+        }
         mHandler.post(() -> {
             scheduleUpdateActiveUserWithoutHandler(userId);
 
