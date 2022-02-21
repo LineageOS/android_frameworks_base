47958b6f54fbabb8fce63522c539d3d53443f1f6
LuK1337 <priv.luk@gmail.com>
2022-02-21 17:05:38 +0100

    SystemUI: Don't store pending ControlAction if the device is locked
    Passing control action to activityStarter.dismissKeyguardThenExecute()
    is enough to toggle it post device unlock.
    
    Test: Use a control action from locked device, notice that it's no
          longer toggled twice.
    Change-Id: I48cb9e0ad2425d35063fddefc738ab36c01690b8

--- packages/SystemUI/src/com/android/systemui/controls/ui/ControlActionCoordinatorImpl.kt
+++ packages/SystemUI/src/com/android/systemui/controls/ui/ControlActionCoordinatorImpl.kt
@@ -64,7 +64,6 @@ class ControlActionCoordinatorImpl @Inject constructor(
         private val featureFlags: FeatureFlags,
 ) : ControlActionCoordinator {
     private var dialog: Dialog? = null
-    private var pendingAction: Action? = null
     private var actionsInProgress = mutableSetOf<String>()
     private val isLocked: Boolean
         get() = !keyguardStateController.isUnlocked()
@@ -166,15 +165,6 @@ class ControlActionCoordinatorImpl @Inject constructor(
         )
     }
 
-    override fun runPendingAction(controlId: String) {
-        if (isLocked) return
-        if (pendingAction?.controlId == controlId) {
-            showSettingsDialogIfNeeded(pendingAction!!)
-            pendingAction?.invoke()
-            pendingAction = null
-        }
-    }
-
     @MainThread
     override fun enableActionOnTouch(controlId: String) {
         actionsInProgress.remove(controlId)
@@ -196,17 +186,11 @@ class ControlActionCoordinatorImpl @Inject constructor(
         val authRequired = action.authIsRequired || !allowTrivialControls
 
         if (keyguardStateController.isShowing() && authRequired) {
-            if (isLocked) {
-                broadcastSender.closeSystemDialogs()
-
-                // pending actions will only run after the control state has been refreshed
-                pendingAction = action
-            }
             activityStarter.dismissKeyguardThenExecute({
                 Log.d(ControlsUiController.TAG, "Device unlocked, invoking controls action")
                 action.invoke()
                 true
-            }, { pendingAction = null }, true /* afterKeyguardGone */)
+            }, null, true /* afterKeyguardGone */)
         } else {
             showSettingsDialogIfNeeded(action)
             action.invoke()
