b18c7eb724d588b0edab12c76e923a374401fb5e
Bruno Martins <bgcngm@gmail.com>
2022-02-25 23:49:46 +0000

    GlobalActionsDialogLite: Never show emergency without telephony
    Change-Id: I7a4cd340d69fa6e3e2e6a2ba0529276f3b04155b

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -956,6 +956,11 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         public boolean showBeforeProvisioning() {
             return true;
         }
+
+        @Override
+        public boolean shouldShow() {
+            return mHasTelephony;
+        }
     }
 
     private class EmergencyAffordanceAction extends EmergencyAction {
