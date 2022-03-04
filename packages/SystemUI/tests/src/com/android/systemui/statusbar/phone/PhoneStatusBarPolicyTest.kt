ce5d29cd0a39aae717d7ca0fbbd98abf70d82598
Oliver Scott <olivercscott@gmail.com>
2022-03-05 00:08:35 +0100

    Implement firewall status bar indicator
    * Add API to get the last resumed activity's UID
    * Show blocked status indicator based on the last resumed activity's UID blocked status
    
    Also includes squashed changes:
    
    Author: Michael Bestas <mkbestas@gmail.com>
    Date:   Thu Mar 2 01:01:37 2023 +0200
    
        fixup! Implement firewall status bar indicator
    
        Adding new core symbols is a no go, we can still make this work
        by moving the string to SystemUI. config_statusBarIcons is
        only used in StatusBarShellCommand for "get-status-icons" command.
    
        Change-Id: Ic4daaacb2b0db0b94fcd4aee0d7880a6b12c97b0
    
    Author: Oliver Scott <olivercscott@gmail.com>
    Date:   Tue Mar 14 15:32:06 2023 -0400
    
        fixup! Implement firewall status bar indicator
    
        Check UID network access when its default network changes.
    
        Issue: calyxos#1041
        Requires: I2729b61c349ec2812a74d7d1c04b90a58b0f5b88
        Change-Id: Ied0f465f485a4415ae50f4ffc50a4ce9b6caa25e
    
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I7aa405e7f5a2f70e6cf2b6156a29d220617e4ae5

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicyTest.kt
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicyTest.kt
@@ -170,6 +170,7 @@ class PhoneStatusBarPolicyTest : SysuiTestCase() {
 
     private fun createStatusBarPolicy(): PhoneStatusBarPolicy {
         return PhoneStatusBarPolicy(
+            context,
             iconController,
             commandQueue,
             broadcastDispatcher,
