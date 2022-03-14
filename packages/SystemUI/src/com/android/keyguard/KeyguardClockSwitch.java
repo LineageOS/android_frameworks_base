b5379d3a1e08321a111727db185dd4f1fa0fdc30
Timi Rautamäki <timi.rautamaki@gmail.com>
2022-03-14 08:50:25 +0000

    Keyguard: allow large clock on tablets
    This effectively allows large clock to be shown on tablets,
    while keeping it disabled for small screen devices.
    
    Change-Id: I5bd1cdead55bd11dd4d0684ff95986cbcfdf4757

--- packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
@@ -1,5 +1,7 @@
 package com.android.keyguard;
 
+import static com.android.systemui.shared.recents.utilities.Utilities.isTablet;
+
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
@@ -100,7 +102,8 @@ public class KeyguardClockSwitch extends RelativeLayout {
 
         if (mDisplayedClockSize != null) {
             boolean landscape = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE;
-            boolean useLargeClock = mDisplayedClockSize == LARGE && !landscape;
+            boolean useLargeClock = mDisplayedClockSize == LARGE &&
+                    (!landscape || isTablet(mContext));
             animateClockChange(useLargeClock);
         }
     }
@@ -291,7 +294,7 @@ public class KeyguardClockSwitch extends RelativeLayout {
         }
         boolean landscape = getResources().getConfiguration().orientation
                 == Configuration.ORIENTATION_LANDSCAPE;
-        boolean useLargeClock = clockSize == LARGE && !landscape;
+        boolean useLargeClock = clockSize == LARGE && (!landscape || isTablet(mContext));
 
         // let's make sure clock is changed only after all views were laid out so we can
         // translate them properly
