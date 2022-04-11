20d44b35c2fb26b322e8dd21524565b612d3cb42
Danny Lin <danny@kdrag0n.dev>
2022-04-10 18:35:11 -0700

    SystemUI: Add API for runtime taskbar config
    Allow Launcher3 to override the taskbar state at runtime based on user
    preference.
    
    Change-Id: Ic2616ff974e98e03edbcdbdf677fa40b40b37abb

--- packages/SystemUI/src/com/android/systemui/recents/OverviewProxyService.java
+++ packages/SystemUI/src/com/android/systemui/recents/OverviewProxyService.java
@@ -275,6 +275,12 @@ public class OverviewProxyService implements CallbackController<OverviewProxyLis
         }
 
         @Override
+        public void setTaskbarEnabled(boolean enabled) {
+            verifyCallerAndClearCallingIdentityPostMain("setTaskbarEnabled", () ->
+                    onTaskbarEnabled(enabled));
+        }
+
+        @Override
         public void notifyTaskbarStatus(boolean visible, boolean stashed) {
             verifyCallerAndClearCallingIdentityPostMain("notifyTaskbarStatus", () ->
                     onTaskbarStatusUpdated(visible, stashed));
@@ -872,6 +878,12 @@ public class OverviewProxyService implements CallbackController<OverviewProxyLis
         }
     }
 
+    private void onTaskbarEnabled(boolean enabled) {
+        for (int i = mConnectionCallbacks.size() - 1; i >= 0; --i) {
+            mConnectionCallbacks.get(i).onTaskbarEnabled(enabled);
+        }
+    }
+
     private void onTaskbarStatusUpdated(boolean visible, boolean stashed) {
         for (int i = mConnectionCallbacks.size() - 1; i >= 0; --i) {
             mConnectionCallbacks.get(i).onTaskbarStatusUpdated(visible, stashed);
@@ -1058,6 +1070,7 @@ public class OverviewProxyService implements CallbackController<OverviewProxyLis
         /** Notify the recents app (overview) is started by 3-button navigation. */
         default void onToggleRecentApps() {}
         default void onHomeRotationEnabled(boolean enabled) {}
+        default void onTaskbarEnabled(boolean enabled) {}
         default void onTaskbarStatusUpdated(boolean visible, boolean stashed) {}
         default void onTaskbarAutohideSuspend(boolean suspend) {}
         default void onAssistantProgress(@FloatRange(from = 0.0, to = 1.0) float progress) {}
