86f74dbb21b3361c1ca79b5368f9f8a7bb82fb9d
Timi Rautamäki <timi.rautamaki@gmail.com>
2022-04-22 21:01:37 +0000

    SystemUI: screenshot: open the screenshot instead of edit
    Open the taken screenshot in gallery application when clicking on the
    preview. There is a seperate edit button, so the preview imageview
    should open gallery instead.
    
    Test: atest com.android.systemui.screenshot.ScreenshotNotificationSmartActionsTest
    Change-Id: I8f882ddd0da47ca50acc37d25ee0866ce5698e4f

--- packages/SystemUI/src/com/android/systemui/screenshot/ActionProxyReceiver.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ActionProxyReceiver.java
@@ -18,6 +18,7 @@ package com.android.systemui.screenshot;
 
 import static com.android.systemui.screenshot.ScreenshotController.ACTION_TYPE_EDIT;
 import static com.android.systemui.screenshot.ScreenshotController.ACTION_TYPE_SHARE;
+import static com.android.systemui.screenshot.ScreenshotController.ACTION_TYPE_VIEW;
 import static com.android.systemui.screenshot.ScreenshotController.EXTRA_ACTION_INTENT;
 import static com.android.systemui.screenshot.ScreenshotController.EXTRA_DISALLOW_ENTER_PIP;
 import static com.android.systemui.screenshot.ScreenshotController.EXTRA_ID;
@@ -97,9 +98,15 @@ public class ActionProxyReceiver extends BroadcastReceiver {
                 true /* deferred */);
 
         if (intent.getBooleanExtra(EXTRA_SMART_ACTIONS_ENABLED, false)) {
-            String actionType = Intent.ACTION_EDIT.equals(intent.getAction())
-                    ? ACTION_TYPE_EDIT
-                    : ACTION_TYPE_SHARE;
+            String action = intent.getAction();
+            String actionType;
+            if (Intent.ACTION_VIEW.equals(action)) {
+                actionType = ACTION_TYPE_VIEW;
+            } else if (Intent.ACTION_EDIT.equals(action)) {
+                actionType = ACTION_TYPE_EDIT;
+            } else {
+                actionType = ACTION_TYPE_SHARE;
+            }
             mScreenshotSmartActions.notifyScreenshotAction(
                     intent.getStringExtra(EXTRA_ID), actionType, false, null);
         }
