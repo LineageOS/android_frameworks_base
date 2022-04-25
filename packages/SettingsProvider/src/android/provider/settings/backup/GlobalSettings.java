CalyxOS/platform_frameworks_base

Partially revert "Settings.Global: Add UIDS_ALLOWED_ON_RESTRICTED_NETWORKS"
UIDs do not persist across installations

This partially reverts commit a2362a0fb9de2f863a848e19e24511b1b06802f2.

Change-Id: If489c43f4f866ab5ced009f410cf2ef78024af04

packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
@@ -79,7 +79,6 @@ public class GlobalSettings {
         Settings.Global.AUTOMATIC_POWER_SAVE_MODE,
         Settings.Global.ADVANCED_BATTERY_USAGE_AMOUNT,
         Settings.Global.POWER_BUTTON_LONG_PRESS_DURATION_MS,
-        Settings.Global.WIFI_OFF_TIMEOUT,
-        Settings.Global.UIDS_ALLOWED_ON_RESTRICTED_NETWORKS
+        Settings.Global.WIFI_OFF_TIMEOUT
     };
 }

LineageOS/android_frameworks_base

packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
@@ -78,7 +78,6 @@ public class GlobalSettings {
         Settings.Global.POWER_BUTTON_LONG_PRESS,
         Settings.Global.AUTOMATIC_POWER_SAVE_MODE,
         Settings.Global.ADVANCED_BATTERY_USAGE_AMOUNT,
-        Settings.Global.POWER_BUTTON_LONG_PRESS_DURATION_MS,
-        Settings.Global.UIDS_ALLOWED_ON_RESTRICTED_NETWORKS
+        Settings.Global.POWER_BUTTON_LONG_PRESS_DURATION_MS
     };
 }