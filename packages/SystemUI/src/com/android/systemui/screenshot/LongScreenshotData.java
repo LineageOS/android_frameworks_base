f149074aa6c06825ec8736820a6f671faaa2865b
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-26 01:39:00 +0300

    SystemUI: screenshot: disable magnification for partial screenshots
    Partial screenshots are screen-size and don't need a magnifying glass.
    
    Change-Id: I472103352411d1482423230a4ff08ddda45cc53d
    (cherry picked from commit 92abcecfe2bd5a84a601a5eafd3ddf75c3968d38)

--- packages/SystemUI/src/com/android/systemui/screenshot/LongScreenshotData.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/LongScreenshotData.java
@@ -31,6 +31,8 @@ public class LongScreenshotData {
     private final AtomicReference<ScrollCaptureController.LongScreenshot> mLongScreenshot;
     private final AtomicReference<ScreenshotController.TransitionDestination>
             mTransitionDestinationCallback;
+    private String mForegroundAppName;
+    private boolean mNeedsMagnification;
 
     @Inject
     public LongScreenshotData() {
@@ -72,5 +74,27 @@ public class LongScreenshotData {
      */
     public ScreenshotController.TransitionDestination takeTransitionDestinationCallback() {
         return mTransitionDestinationCallback.getAndSet(null);
+    }
+
+    /**
+     * Set the holder's foreground app name.
+     */
+    public void setForegroundAppName(String foregroundAppName) {
+        mForegroundAppName = foregroundAppName;
+    }
+
+    /**
+     * Return the current foreground app name.
+     */
+    public String getForegroundAppName() {
+        return mForegroundAppName;
+    }
+
+    public void setNeedsMagnification(boolean status) {
+        mNeedsMagnification = status;
+    }
+
+    public boolean getNeedsMagnification() {
+        return mNeedsMagnification;
     }
 }
--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -725,6 +725,7 @@ public class ScreenshotController {
         ScrollCaptureController.BitmapScreenshot bitmapScreenshot =
             new ScrollCaptureController.BitmapScreenshot(mContext, newScreenshot);
 
+        mLongScreenshotHolder.setNeedsMagnification(false);
         startLongScreenshotActivity(bitmapScreenshot);
     }
 
@@ -781,6 +782,7 @@ public class ScreenshotController {
                             return;
                         }
 
+                        mLongScreenshotHolder.setNeedsMagnification(true);
                         startLongScreenshotActivity(longScreenshot);
                     }, mMainExecutor);
                 });
