a52abae3191c59191f43e88b4f7960a84395f41c
LuK1337 <priv.luk@gmail.com>
2022-06-26 16:09:43 +0200

    SystemUI: screenshot: close QS after launching long screenshot activity
    Change-Id: Ief5510c037b27be714f8f6cc8661d81931f3babd

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -91,6 +91,7 @@ import androidx.concurrent.futures.CallbackToFutureAdapter;
 import com.android.internal.app.ChooserActivity;
 import com.android.internal.logging.UiEventLogger;
 import com.android.internal.policy.PhoneWindow;
+import com.android.internal.statusbar.IStatusBarService;
 import com.android.settingslib.applications.InterestingConfigChanges;
 import com.android.systemui.R;
 import com.android.systemui.broadcast.BroadcastSender;
@@ -265,6 +266,7 @@ public class ScreenshotController {
     private final PhoneWindow mWindow;
     private final DisplayManager mDisplayManager;
     private final ScrollCaptureController mScrollCaptureController;
+    private final IStatusBarService mStatusBarService;
     private final LongScreenshotData mLongScreenshotHolder;
     private final Vibrator mVibrator;
     private final boolean mIsLowRamDevice;
@@ -300,6 +302,7 @@ public class ScreenshotController {
             ImageExporter imageExporter,
             @Main Executor mainExecutor,
             ScrollCaptureController scrollCaptureController,
+            IStatusBarService statusBarService,
             LongScreenshotData longScreenshotHolder,
             ActivityManager activityManager,
             TimeoutHandler timeoutHandler,
@@ -311,6 +314,7 @@ public class ScreenshotController {
         mImageExporter = imageExporter;
         mMainExecutor = mainExecutor;
         mScrollCaptureController = scrollCaptureController;
+        mStatusBarService = statusBarService;
         mLongScreenshotHolder = longScreenshotHolder;
         mIsLowRamDevice = activityManager.isLowRamDevice();
         mBgExecutor = Executors.newSingleThreadExecutor();
@@ -722,6 +726,12 @@ public class ScreenshotController {
                     .overridePendingAppTransitionRemote(runner, DEFAULT_DISPLAY);
         } catch (Exception e) {
             Log.e(TAG, "Error overriding screenshot app transition", e);
+        }
+
+        try {
+            mStatusBarService.collapsePanels();
+        } catch (RemoteException e) {
+            Log.e(TAG, "Error during collapsing panels", e);
         }
     }
 
