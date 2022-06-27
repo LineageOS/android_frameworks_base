6d592153f5c61f167728c6c67a6938a5eebeca84
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-27 16:51:50 +0300

    SystemUI: doze: add config for double tap sensors that need a proximity check
    Change-Id: Id7df987f0e9db4e5e2328534764ded397b6ac48d

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -208,7 +208,8 @@ public class DozeSensors {
                         true /* configured */,
                         DozeLog.REASON_SENSOR_DOUBLE_TAP,
                         dozeParameters.doubleTapReportsTouchCoordinates(),
-                        true /* touchscreen */
+                        true /* touchscreen */,
+                        !dozeParameters.doubleTapNeedsProximityCheck() /* performsProxCheck */
                 ),
                 new TriggerSensor(
                         findSensors(config.tapSensorTypeMapping()),
