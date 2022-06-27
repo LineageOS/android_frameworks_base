cbf44d65c68ab1976edcb6cdd804ffd9b248139d
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-27 16:42:56 +0300

    Partially revert "Assume sensors perform prox check"
    This commit partially reverts f40bd8fbb65c896c824fe3f1a5be857bbe8ae281
    ("Assume sensors perform prox check").
    
    Some devices do not have proximity gated sensors, and still require
    gating in software.
    Partially revert this commit to bring back the ability for sensors to
    be proximity gated in software.
    
    Change-Id: Ic3d5c6e98d1767623ace4ee4eea1c52606fabd4a

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -524,6 +524,7 @@ public class DozeSensors {
         private final boolean mSettingDefault;
         private final boolean mRequiresTouchscreen;
         private final boolean mRequiresProx;
+        private final boolean mPerformsProxCheck;
 
         // Whether the sensor should only register if the device is in AOD
         private final boolean mRequiresAod;
@@ -549,6 +550,26 @@ public class DozeSensors {
             this(
                     sensor,
                     setting,
+                    configured,
+                    pulseReason,
+                    reportsTouchCoordinates,
+                    requiresTouchscreen,
+                    true
+            );
+        }
+
+        TriggerSensor(
+                Sensor sensor,
+                String setting,
+                boolean configured,
+                int pulseReason,
+                boolean reportsTouchCoordinates,
+                boolean requiresTouchscreen,
+                boolean performsProxCheck
+        ) {
+            this(
+                    sensor,
+                    setting,
                     true /* settingDef */,
                     configured,
                     pulseReason,
@@ -556,6 +577,7 @@ public class DozeSensors {
                     requiresTouchscreen,
                     false /* ignoresSetting */,
                     false /* requiresProx */,
+                    performsProxCheck,
                     true /* immediatelyReRegister */,
                     false
             );
@@ -575,6 +597,36 @@ public class DozeSensors {
                 boolean requiresAod
         ) {
             this(
+                    sensor,
+                    setting,
+                    settingDef,
+                    configured,
+                    pulseReason,
+                    reportsTouchCoordinates,
+                    requiresTouchscreen,
+                    ignoresSetting,
+                    requiresProx,
+                    true,
+                    immediatelyReRegister,
+                    requiresAod
+            );
+        }
+
+        TriggerSensor(
+                Sensor sensor,
+                String setting,
+                boolean settingDef,
+                boolean configured,
+                int pulseReason,
+                boolean reportsTouchCoordinates,
+                boolean requiresTouchscreen,
+                boolean ignoresSetting,
+                boolean requiresProx,
+                boolean performsProxCheck,
+                boolean immediatelyReRegister,
+                boolean requiresAod
+        ) {
+            this(
                     new Sensor[]{ sensor },
                     setting,
                     settingDef,
@@ -584,6 +636,7 @@ public class DozeSensors {
                     requiresTouchscreen,
                     ignoresSetting,
                     requiresProx,
+                    performsProxCheck,
                     immediatelyReRegister,
                     DevicePostureController.DEVICE_POSTURE_UNKNOWN,
                     requiresAod
@@ -604,6 +657,38 @@ public class DozeSensors {
                 @DevicePostureController.DevicePostureInt int posture,
                 boolean requiresAod
         ) {
+            this(
+                    sensors,
+                    setting,
+                    settingDef,
+                    configured,
+                    pulseReason,
+                    reportsTouchCoordinates,
+                    requiresTouchscreen,
+                    ignoresSetting,
+                    requiresProx,
+                    true,
+                    immediatelyReRegister,
+                    posture,
+                    requiresAod
+            );
+        }
+
+        TriggerSensor(
+                @NonNull Sensor[] sensors,
+                String setting,
+                boolean settingDef,
+                boolean configured,
+                int pulseReason,
+                boolean reportsTouchCoordinates,
+                boolean requiresTouchscreen,
+                boolean ignoresSetting,
+                boolean requiresProx,
+                boolean performsProxCheck,
+                boolean immediatelyReRegister,
+                @DevicePostureController.DevicePostureInt int posture,
+                boolean requiresAod
+        ) {
             mSensors = sensors;
             mSetting = setting;
             mSettingDefault = settingDef;
@@ -613,6 +698,7 @@ public class DozeSensors {
             mRequiresTouchscreen = requiresTouchscreen;
             mIgnoresSetting = ignoresSetting;
             mRequiresProx = requiresProx;
+            mPerformsProxCheck = performsProxCheck;
             mRequiresAod = requiresAod;
             mPosture = posture;
             mImmediatelyReRegister = immediatelyReRegister;
@@ -740,11 +826,21 @@ public class DozeSensors {
                     screenX = event.values[0];
                     screenY = event.values[1];
                 }
-                mSensorCallback.onSensorPulse(mPulseReason, screenX, screenY, event.values);
+                mSensorCallback.onSensorPulse(mPulseReason, mPerformsProxCheck,
+                        screenX, screenY, event.values);
                 if (!mRegistered && mImmediatelyReRegister) {
                     updateListening();
                 }
             }));
+        }
+
+        /**
+         * If the sensor itself performs proximity checks, to avoid pocket dialing.
+         * Gated sensors don't need to be stopped when the {@link DozeMachine} is
+         * {@link DozeMachine.State#DOZE_AOD_PAUSED}.
+         */
+        public boolean performsProxCheck() {
+            return mPerformsProxCheck;
         }
 
         public void registerSettingsObserver(ContentObserver settingsObserver) {
@@ -840,7 +936,8 @@ public class DozeSensors {
                     mDozeLog.traceSensorEventDropped(mPulseReason, "debounce");
                     return;
                 }
-                mSensorCallback.onSensorPulse(mPulseReason, -1, -1, event.getValues());
+                mSensorCallback.onSensorPulse(mPulseReason, true /* sensorPerformsProxCheck */,
+                        -1, -1, event.getValues());
             }));
         }
     }
@@ -889,11 +986,13 @@ public class DozeSensors {
         /**
          * Called when a sensor requests a pulse
          * @param pulseReason Requesting sensor, e.g. {@link DozeLog#REASON_SENSOR_PICKUP}
+         * @param sensorPerformedProxCheck true if the sensor already checked for FAR proximity.
          * @param screenX the location on the screen where the sensor fired or -1
          *                if the sensor doesn't support reporting screen locations.
          * @param screenY the location on the screen where the sensor fired or -1
          * @param rawValues raw values array from the event.
          */
-        void onSensorPulse(int pulseReason, float screenX, float screenY, float[] rawValues);
+        void onSensorPulse(int pulseReason, boolean sensorPerformedProxCheck,
+                float screenX, float screenY, float[] rawValues);
     }
 }
--- packages/SystemUI/tests/src/com/android/systemui/doze/DozeTriggersTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/doze/DozeTriggersTest.java
@@ -257,10 +257,10 @@ public class DozeTriggersTest extends SysuiTestCase {
     @Test
     public void testProximitySensorNotAvailable() {
         mProximitySensor.setSensorAvailable(false);
-        mTriggers.onSensor(DozeLog.PULSE_REASON_SENSOR_LONG_PRESS, 100, 100, null);
-        mTriggers.onSensor(DozeLog.PULSE_REASON_SENSOR_WAKE_REACH, 100, 100,
+        mTriggers.onSensor(DozeLog.PULSE_REASON_SENSOR_LONG_PRESS, false, 100, 100, null);
+        mTriggers.onSensor(DozeLog.PULSE_REASON_SENSOR_WAKE_REACH, false, 100, 100,
                 new float[]{1});
-        mTriggers.onSensor(DozeLog.REASON_SENSOR_TAP, 100, 100, null);
+        mTriggers.onSensor(DozeLog.REASON_SENSOR_TAP, false, 100, 100, null);
     }
 
     @Test
@@ -277,7 +277,7 @@ public class DozeTriggersTest extends SysuiTestCase {
         ).when(mHost).setPulsePending(boolCaptor.capture());
 
         // WHEN quick pick up is triggered
-        mTriggers.onSensor(DozeLog.REASON_SENSOR_QUICK_PICKUP, 100, 100, null);
+        mTriggers.onSensor(DozeLog.REASON_SENSOR_QUICK_PICKUP, false, 100, 100, null);
 
         // THEN request pulse
         verify(mMachine).requestPulse(anyInt());
@@ -294,7 +294,7 @@ public class DozeTriggersTest extends SysuiTestCase {
 
         // WHEN the pick up gesture is triggered and keyguard isn't occluded
         when(mKeyguardStateController.isOccluded()).thenReturn(false);
-        mTriggers.onSensor(DozeLog.REASON_SENSOR_PICKUP, 100, 100, null);
+        mTriggers.onSensor(DozeLog.REASON_SENSOR_PICKUP, false, 100, 100, null);
 
         // THEN wakeup
         verify(mMachine).wakeUp(DozeLog.REASON_SENSOR_PICKUP);
@@ -302,7 +302,7 @@ public class DozeTriggersTest extends SysuiTestCase {
 
     @Test
     public void test_onSensor_tap() {
-        mTriggers.onSensor(DozeLog.REASON_SENSOR_TAP, 100, 200, null);
+        mTriggers.onSensor(DozeLog.REASON_SENSOR_TAP, false, 100, 200, null);
 
         verify(mHost).onSlpiTap(100, 200);
         verify(mMachine).wakeUp(DozeLog.REASON_SENSOR_TAP);
@@ -310,7 +310,7 @@ public class DozeTriggersTest extends SysuiTestCase {
 
     @Test
     public void test_onSensor_double_tap() {
-        mTriggers.onSensor(DozeLog.REASON_SENSOR_DOUBLE_TAP, 100, 200, null);
+        mTriggers.onSensor(DozeLog.REASON_SENSOR_DOUBLE_TAP, false, 100, 200, null);
 
         verify(mHost).onSlpiTap(100, 200);
         verify(mMachine).wakeUp(DozeLog.REASON_SENSOR_DOUBLE_TAP);
@@ -323,7 +323,7 @@ public class DozeTriggersTest extends SysuiTestCase {
 
         // WHEN the pick up gesture is triggered and keyguard IS occluded
         when(mKeyguardStateController.isOccluded()).thenReturn(true);
-        mTriggers.onSensor(DozeLog.REASON_SENSOR_PICKUP, 100, 100, null);
+        mTriggers.onSensor(DozeLog.REASON_SENSOR_PICKUP, false, 100, 100, null);
 
         // THEN never wakeup
         verify(mMachine, never()).wakeUp(DozeLog.REASON_SENSOR_PICKUP);
@@ -342,7 +342,7 @@ public class DozeTriggersTest extends SysuiTestCase {
         float[] rawValues = new float[]{screenX, screenY, misc, major, minor};
 
         // WHEN longpress gesture is triggered
-        mTriggers.onSensor(reason, screenX, screenY, rawValues);
+        mTriggers.onSensor(reason, false, screenX, screenY, rawValues);
 
         // THEN
         // * don't immediately send interrupt
@@ -368,7 +368,7 @@ public class DozeTriggersTest extends SysuiTestCase {
         when(mHost.isPulsePending()).thenReturn(false);
         when(mMachine.isExecutingTransition()).thenReturn(true);
 
-        mTriggers.onSensor(DozeLog.PULSE_REASON_SENSOR_LONG_PRESS, 100, 100, null);
+        mTriggers.onSensor(DozeLog.PULSE_REASON_SENSOR_LONG_PRESS, false, 100, 100, null);
 
         verify(mDozeLog).tracePulseDropped(anyString(), eq(null));
     }
@@ -379,7 +379,7 @@ public class DozeTriggersTest extends SysuiTestCase {
         when(mMachine.getState()).thenReturn(DozeMachine.State.DOZE_AOD_PAUSED);
 
         // WHEN udfps long-press is triggered
-        mTriggers.onSensor(DozeLog.REASON_SENSOR_UDFPS_LONG_PRESS, 100, 100,
+        mTriggers.onSensor(DozeLog.REASON_SENSOR_UDFPS_LONG_PRESS, false, 100, 100,
                 new float[]{0, 1, 2, 3, 4});
 
         // THEN the pulse is NOT dropped
@@ -398,7 +398,7 @@ public class DozeTriggersTest extends SysuiTestCase {
         when(mMachine.getState()).thenReturn(DozeMachine.State.DOZE_AOD_PAUSING);
 
         // WHEN udfps long-press is triggered
-        mTriggers.onSensor(DozeLog.REASON_SENSOR_UDFPS_LONG_PRESS, 100, 100,
+        mTriggers.onSensor(DozeLog.REASON_SENSOR_UDFPS_LONG_PRESS, false, 100, 100,
                 new float[]{0, 1, 2, 3, 4});
 
         // THEN the pulse is NOT dropped
