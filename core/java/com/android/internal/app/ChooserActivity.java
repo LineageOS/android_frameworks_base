9866c89dc4dbeb4df6d75df9f560afb01f7c04e8
Aayush Gupta <aayushgupta219@gmail.com>
2022-07-04 12:41:30 +0000

    base: ChooserActivity: Don't show image edit button on multiple targets
    Ie52aaab90f678d32d9b67f95d81f86b82d1b9c7c introduced edit button but didn't
    took into account that there can be multiple images being shared. This patch
    adds a check to ensure that this action is not shown in such cases.
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I787cd9cfd873242542b330a522bed5df9deb62a4

--- core/java/com/android/internal/app/ChooserActivity.java
+++ core/java/com/android/internal/app/ChooserActivity.java
@@ -1311,15 +1311,18 @@ public class ChooserActivity extends ResolverActivity implements
 
         final ViewGroup actionRow =
                 (ViewGroup) contentPreviewLayout.findViewById(R.id.chooser_action_row);
+        String action = targetIntent.getAction();
+
         //TODO: addActionButton(actionRow, createCopyButton());
         if (shouldNearbyShareBeIncludedAsActionButton()) {
             addActionButton(actionRow, createNearbyButton(targetIntent));
         }
-        addActionButton(actionRow, createEditButton(targetIntent));
+        if (!Intent.ACTION_SEND_MULTIPLE.equals(action)) {
+            addActionButton(actionRow, createEditButton(targetIntent));
+        }
 
         mPreviewCoord = new ContentPreviewCoordinator(contentPreviewLayout, false);
 
-        String action = targetIntent.getAction();
         if (Intent.ACTION_SEND.equals(action)) {
             Uri uri = targetIntent.getParcelableExtra(Intent.EXTRA_STREAM);
             imagePreview.findViewById(R.id.content_preview_image_1_large)
