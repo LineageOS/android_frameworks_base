33d7eed950027e0ec841adad340b4ed3a98d9fd2
Danny Baumann <dannybaumann@web.de>
2022-07-20 15:53:13 +0200

    SystemUI: Fix QS header clock color
    Now that we're flipping QS colors by theme (dark/light), we can no longer
    rely on wallpaper colors for QS clock. Instead, we now can rely on clock color
    being updated correctly on QS re-inflation (via
    QuickStatusBarHeader.updateResources).
    
    Change-Id: Icdf2484793cb63b7c0ab6ab87e94185e6bdc9ca4

--- packages/SystemUI/tests/src/com/android/systemui/qs/QuickStatusBarHeaderControllerTest.kt
+++ packages/SystemUI/tests/src/com/android/systemui/qs/QuickStatusBarHeaderControllerTest.kt
@@ -23,7 +23,6 @@ import androidx.test.filters.SmallTest
 import com.android.systemui.R
 import com.android.systemui.SysuiTestCase
 import com.android.systemui.battery.BatteryMeterViewController
-import com.android.systemui.colorextraction.SysuiColorExtractor
 import com.android.systemui.demomode.DemoModeController
 import com.android.systemui.flags.FeatureFlags
 import com.android.systemui.flags.Flags
@@ -71,8 +70,6 @@ class QuickStatusBarHeaderControllerTest : SysuiTestCase() {
     @Mock
     private lateinit var qsCarrierGroupController: QSCarrierGroupController
     @Mock
-    private lateinit var colorExtractor: SysuiColorExtractor
-    @Mock
     private lateinit var iconContainer: StatusIconContainer
     @Mock
     private lateinit var qsCarrierGroup: QSCarrierGroup
@@ -118,7 +115,6 @@ class QuickStatusBarHeaderControllerTest : SysuiTestCase() {
                 demoModeController,
                 quickQSPanelController,
                 qsCarrierGroupControllerBuilder,
-                colorExtractor,
                 qsExpansionPathInterpolator,
                 featureFlags,
                 variableDateViewControllerFactory,
