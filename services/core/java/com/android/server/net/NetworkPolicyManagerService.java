9db29164c3b3b38274c92a8877d02c33610f01aa
Tư Mã Tần Quảng <baobaoxich@gmail.com>
2022-07-25 20:56:19 +0000

    Fix "restrict network usage" command that doesn't sync with settings and doesn't restrict other types of data
    Signed-off-by: Tư Mã Tần Quảng <baobaoxich@gmail.com>
    Change-Id: I8d937f467fcd435f0dd0433524efe1e8632be2f6

--- services/core/java/com/android/server/net/NetworkPolicyManagerService.java
+++ services/core/java/com/android/server/net/NetworkPolicyManagerService.java
@@ -2656,10 +2656,6 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
     public void setUidPolicy(int uid, int policy) {
         mContext.enforceCallingOrSelfPermission(MANAGE_NETWORK_POLICY, TAG);
 
-        if (!UserHandle.isApp(uid)) {
-            throw new IllegalArgumentException("cannot apply policy to UID " + uid);
-        }
-
         if (LOGD) {
             Log.d(TAG, "setUidPolicy: uid = " + uid + " policy = " + policy);
         }
@@ -2682,10 +2678,6 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
     public void addUidPolicy(int uid, int policy) {
         mContext.enforceCallingOrSelfPermission(MANAGE_NETWORK_POLICY, TAG);
 
-        if (!UserHandle.isApp(uid)) {
-            throw new IllegalArgumentException("cannot apply policy to UID " + uid);
-        }
-
         if (LOGD) {
             Log.d(TAG, "addUidPolicy: uid = " + uid + " policy = " + policy);
         }
@@ -2703,10 +2695,6 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
     @Override
     public void removeUidPolicy(int uid, int policy) {
         mContext.enforceCallingOrSelfPermission(MANAGE_NETWORK_POLICY, TAG);
-
-        if (!UserHandle.isApp(uid)) {
-            throw new IllegalArgumentException("cannot apply policy to UID " + uid);
-        }
 
         if (LOGD) {
             Log.d(TAG, "removeUidPolicy: uid = " + uid + " policy = " + policy);
