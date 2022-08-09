072da7372e854ff38a9b0a64a49de024cdd35a64
iusmac <iusico.maxim@libero.it>
2022-08-09 07:58:47 +0200

    Screenrecord: Allow to select AVC codec profile level
    Nevertheless we have an option to limit the recording by frame rate, but
    we still can't limit it by resolution. It's not particularly needed for
    the devices of today which are enough powerful and can encode/play any
    profile level, but there are still many old devices for which the
    profile level can make a big difference.
    
    Traditionally the device declares the high quality multimedia profiles
    for video encoding (editing, recording video via camera ecc.), but these
    profiles are also used for screen recording. And here comes the pitfall
    for weak devices: screen recording is a video encoding process +
    highload user tasks on foreground and background, all in total is too
    much for an old device. The recorded video is "laggy", because the
    frames are simply skipped to the detriment of system smoothness. That's
    why some devices will need to limit AVC codec profile level for screen
    recorder only, as it doesn't make sense to do it globally, because it
    will also affect camera and video editing.
    
    Test: record the screen using the closest profile level to the display
    capabilities (resolution@frame rate). Check if video max. resolution and
    frame rate match the selected profile level constraints [1]. In case the
    profile level is the right one, the quality and fluidity of the video
    won't be in any way different from what you normally see on your display
    and will be playable anywhere, including the device that recorded it.
    
    [1] https://en.wikipedia.org/wiki/Advanced_Video_Coding#Levels
    
    [neobuddy89: Added default config to 4.2 and simplified]
    Change-Id: I918ee8f95124a34bda80feb57494b49ed449bf7b

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
@@ -102,6 +102,7 @@ public class ScreenMediaRecorder {
     private final AtomicBoolean mIsStarted = new AtomicBoolean();
 
     private int mMaxRefreshRate;
+    private String mAvcProfileLevel;
 
     private Context mContext;
     ScreenMediaRecorderListener mListener;
@@ -123,6 +124,8 @@ public class ScreenMediaRecorder {
         mDisplayId = displayId;
         mMaxRefreshRate = mContext.getResources().getInteger(
                 com.android.systemui.res.R.integer.config_screenRecorderMaxFramerate);
+        mAvcProfileLevel = mContext.getResources().getString(
+                com.android.systemui.res.R.string.config_screenRecorderAVCProfileLevel);
     }
 
     private void prepare() throws IOException, RemoteException, RuntimeException {
@@ -175,7 +178,7 @@ public class ScreenMediaRecorder {
         mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
         mMediaRecorder.setVideoEncodingProfileLevel(
                 MediaCodecInfo.CodecProfileLevel.AVCProfileMain,
-                MediaCodecInfo.CodecProfileLevel.AVCLevel3);
+                getAvcProfileLevelCodeByName(mAvcProfileLevel));
         mMediaRecorder.setVideoSize(videoParameters.mWidth, videoParameters.mHeight);
         mMediaRecorder.setVideoFrameRate(videoParameters.mRefreshRate);
         mMediaRecorder.setVideoEncodingBitRate(videoParameters.bitrate());
@@ -218,6 +221,21 @@ public class ScreenMediaRecorder {
                     mMediaProjection, mAudioSource == MIC_AND_INTERNAL);
         }
 
+    }
+
+    /**
+     * Match human-readable AVC level name to its constant value.
+     */
+    private int getAvcProfileLevelCodeByName(final String levelName) {
+        switch (levelName) {
+            case "3": return MediaCodecInfo.CodecProfileLevel.AVCLevel3;
+            case "3.1": return MediaCodecInfo.CodecProfileLevel.AVCLevel31;
+            case "3.2": return MediaCodecInfo.CodecProfileLevel.AVCLevel32;
+            case "4": return MediaCodecInfo.CodecProfileLevel.AVCLevel4;
+            case "4.1": return MediaCodecInfo.CodecProfileLevel.AVCLevel41;
+            default:
+            case "4.2": return MediaCodecInfo.CodecProfileLevel.AVCLevel42;
+        }
     }
 
     /**
