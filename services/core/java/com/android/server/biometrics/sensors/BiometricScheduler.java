187212916fa4a8d6606e0faad5cd084e0903840e
SagarMakhar <sagarmakhar@gmail.com>
2022-08-17 13:21:15 +0000

    BiometricScheduler: Cancel operation if not idle
    - some hals fail to report success/failure (for ex. realme fp hals)
    
    [DarkJoker360 - Switch to overlays]
    [timjosten - Adapt to 12.1.0_r8 merge changes]
    [ghostrider-reborn - Simplify code]
    
    Change-Id: I442ce063280af36a04c25fcbc3dd45a90f196988

--- services/core/java/com/android/server/biometrics/sensors/BiometricScheduler.java
+++ services/core/java/com/android/server/biometrics/sensors/BiometricScheduler.java
@@ -71,6 +71,9 @@ import java.util.function.Supplier;
 public class BiometricScheduler<T, U> {
 
     private static final String TAG = "BiometricScheduler";
+
+    private boolean mCancel;
+
     // Number of recent operations to keep in our logs for dumpsys
     protected static final int LOG_NUM_RECENT_OPERATIONS = 50;
 
@@ -278,12 +281,15 @@ public class BiometricScheduler<T, U> {
      * @param gestureAvailabilityDispatcher may be null if the sensor does not support gestures
      *                                      (such as fingerprint swipe).
      */
-    public BiometricScheduler(@SensorType int sensorType,
+    public BiometricScheduler(Context context, @SensorType int sensorType,
             @Nullable GestureAvailabilityDispatcher gestureAvailabilityDispatcher) {
         this(new Handler(Looper.getMainLooper()), sensorType, gestureAvailabilityDispatcher,
                 IBiometricService.Stub.asInterface(
                         ServiceManager.getService(Context.BIOMETRIC_SERVICE)),
                 LOG_NUM_RECENT_OPERATIONS);
+
+        mCancel = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_fpCancelIfNotIdle);
     }
 
     /**
@@ -314,8 +320,13 @@ public class BiometricScheduler<T, U> {
 
     protected void startNextOperation() {
         if (mCurrentOperation != null) {
-            Slog.v(TAG, "Not idle, current operation: " + mCurrentOperation);
-            return;
+            if (mCancel && !mCurrentOperation.isFinished()) {
+                Slog.v(TAG, "Not idle, cancelling current operation: " + mCurrentOperation);
+                mCurrentOperation.cancel(mHandler, mInternalCallback);
+            } else {
+                Slog.v(TAG, "Not idle, current operation: " + mCurrentOperation);
+                return;
+            }
         }
         if (mPendingOperations.isEmpty()) {
             Slog.d(TAG, "No operations, returning to idle");
