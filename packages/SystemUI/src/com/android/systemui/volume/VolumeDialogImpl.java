71cf9effdef806a40866b8d70a94f3d5e775adc8
timjosten <timjosten@gmail.com>
2022-09-05 04:18:06 +0000

    SystemUI: Use App icon for per-app volume
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: LorDClockaN <lordclockan@gmail.com>
    Change-Id: If9b79997bdf8f3613ddd4567bda652685bb9dc3e
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -271,6 +271,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     private ExpandableIndicator mExpandRows;
     private View mAppVolumeView;
     private ImageButton mAppVolumeIcon;
+    private String mAppVolumeActivePackageName;
     private final List<VolumeRow> mRows = new ArrayList<>();
     private ConfigurableTexts mConfigurableTexts;
     private final SparseBooleanArray mDynamic = new SparseBooleanArray();
@@ -1431,17 +1432,30 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         ContentResolver cr = mContext.getContentResolver();
         int showAppVolume = Settings.System.getInt(cr, Settings.System.SHOW_APP_VOLUME, 0);
         boolean ret = showAppVolume == 1;
+        mAppVolumeActivePackageName = null;
         if (ret) {
             ret = false;
             AudioManager audioManager = mController.getAudioManager();
             for (AppVolume av : audioManager.listAppVolumes()) {
                 if (av.isActive()) {
                     ret = true;
-            break;
+                    mAppVolumeActivePackageName = av.getPackageName();
+                    break;
                 }
             }
         }
         return ret;
+    }
+
+    private Drawable getApplicationIcon(String packageName) {
+        PackageManager pm = mContext.getPackageManager();
+        Drawable icon = null;
+        try {
+            icon = pm.getApplicationIcon(packageName);
+        } catch (Exception e) {
+            // nothing to do
+        }
+        return icon;
     }
 
     public void initAppVolumeH() {
@@ -1456,6 +1470,13 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                 mActivityStarter.startActivity(new Intent(Settings.Panel.ACTION_APP_VOLUME),
                         true /* dismissShade */);
             });
+            Drawable icon = mAppVolumeActivePackageName != null ?
+                    getApplicationIcon(mAppVolumeActivePackageName) : null;
+            if (icon != null) {
+                mAppVolumeIcon.setImageTintList(null);
+                mAppVolumeIcon.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                mAppVolumeIcon.setImageDrawable(icon);
+            }
         }
     }
 
