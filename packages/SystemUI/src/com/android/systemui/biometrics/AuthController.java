5b67aee9ae4a1fae1ad9fbdcd463e505b2758d25
LuK1337 <priv.luk@gmail.com>
2022-10-15 21:29:12 +0200

    fixup! SystemUI: Use max Display.Mode in AuthController as well
    * Required after r11 merge
    
    Change-Id: I8081bccff5045218fcefd2f712b359cef970832d

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthController.java
@@ -1058,7 +1058,6 @@ public class AuthController extends CoreStartable implements CommandQueue.Callba
     @Override
     public void dump(@NonNull PrintWriter pw, @NonNull String[] args) {
         final AuthDialog dialog = mCurrentDialog;
-        pw.println("  stableDisplaySize=" + mStableDisplaySize);
         pw.println("  faceAuthSensorLocation=" + mFaceAuthSensorLocation);
         pw.println("  fingerprintLocation=" + mFingerprintLocation);
         pw.println("  udfpsBounds=" + mUdfpsBounds);
