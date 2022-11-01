6ca90e2a61911f3b641bab6765d66dbc3c1da64e
Chris Crump <chriscrawford893@gmail.com>
2022-11-01 17:27:48 -0400

    core: Initial SenseProvider for FaceSense service
    SenseProvider is a custom provider that connects and listens to FaceSense, the service responsible for face unlock on a device.
    
    The provider is loosely based on Android's HIDL provider but instead of using a HAL, we use our own service to handle face data
    
    Change-Id: I1b83429a793a50fba292a38dbf5aa8a54ae5ace3
    [ghostrider-reborn: rename prop to ro.face.sense_service.enabled]
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- services/core/java/com/android/server/biometrics/sensors/face/FaceService.java
+++ services/core/java/com/android/server/biometrics/sensors/face/FaceService.java
@@ -26,6 +26,7 @@ import android.annotation.Nullable;
 import android.content.Context;
 import android.hardware.biometrics.BiometricManager;
 import android.hardware.biometrics.BiometricsProtoEnums;
+import android.hardware.biometrics.SensorProperties;
 import android.hardware.biometrics.IBiometricSensorReceiver;
 import android.hardware.biometrics.IBiometricService;
 import android.hardware.biometrics.IBiometricServiceLockoutResetCallback;
@@ -35,6 +36,7 @@ import android.hardware.biometrics.ITestSessionCallback;
 import android.hardware.biometrics.face.IFace;
 import android.hardware.biometrics.face.SensorProps;
 import android.hardware.face.Face;
+import android.hardware.face.FaceSensorProperties;
 import android.hardware.face.FaceSensorPropertiesInternal;
 import android.hardware.face.FaceServiceReceiver;
 import android.hardware.face.IFaceService;
@@ -63,6 +65,8 @@ import com.android.server.biometrics.sensors.LockoutResetDispatcher;
 import com.android.server.biometrics.sensors.LockoutTracker;
 import com.android.server.biometrics.sensors.face.aidl.FaceProvider;
 import com.android.server.biometrics.sensors.face.hidl.Face10;
+import com.android.server.biometrics.sensors.face.sense.SenseProvider;
+import com.android.server.biometrics.sensors.face.sense.SenseUtils;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -647,6 +651,22 @@ public class FaceService extends SystemService {
             }
         }
 
+        private void addSenseProvider() {
+            if (SenseUtils.canUseProvider()) {
+                FaceSensorPropertiesInternal props = new FaceSensorPropertiesInternal(
+                        SenseProvider.DEVICE_ID,
+                        SensorProperties.STRENGTH_WEAK,
+                        1, /** maxEnrollmentsPerUser **/
+                        new ArrayList(),
+                        FaceSensorProperties.TYPE_RGB,
+                        false, /** supportsFaceDetection **/
+                        false, /** supportsSelfIllumination **/
+                        false); /** resetLockoutRequiresChallenge **/
+                SenseProvider provider = new SenseProvider(getContext(), props, mLockoutResetDispatcher);
+                mServiceProviders.add(provider);
+            }
+        }
+
         @Override // Binder call
         public void registerAuthenticators(
                 @NonNull List<FaceSensorPropertiesInternal> hidlSensors) {
@@ -664,6 +684,7 @@ public class FaceService extends SystemService {
             handler.post(() -> {
                 addHidlProviders(hidlSensors);
                 addAidlProviders();
+                addSenseProvider();
 
                 final IBiometricService biometricService = IBiometricService.Stub.asInterface(
                         ServiceManager.getService(Context.BIOMETRIC_SERVICE));
