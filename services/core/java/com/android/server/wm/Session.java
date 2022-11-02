e99031f792d003f7bf76784ee957d696bb056bbe
HZ <99131470+hustler-not-chatty@users.noreply.github.com>
2022-11-02 20:24:42 +0800

    AlertWindowNotification: Correctly load app label
    Passes User ID to AlertWindowNotification to make sure that we can
    correctly load app label for the ones installed in work profile.
    
    Test: Install an app that can display over other apps in the work
    profile, watch the notification title and content when it's drawing over
    other apps. It should now display app label, rather than its package
    name.
    
    Change-Id: I872c0c0f6870a09c4ef1c372d7a87dec156f91fc

--- services/core/java/com/android/server/wm/Session.java
+++ services/core/java/com/android/server/wm/Session.java
@@ -840,7 +840,8 @@ class Session extends IWindowSession.Stub implements IBinder.DeathRecipient {
             if (mAlertWindows.isEmpty()) {
                 cancelAlertWindowNotification();
             } else if (mAlertWindowNotification == null && !isSatellitePointingUiPackage()) {
-                mAlertWindowNotification = new AlertWindowNotification(mService, mPackageName);
+                mAlertWindowNotification = new AlertWindowNotification(mService, mPackageName,
+                        UserHandle.getUserId(mUid));
                 if (mShowingAlertWindowNotificationAllowed) {
                     mAlertWindowNotification.post();
                 }
