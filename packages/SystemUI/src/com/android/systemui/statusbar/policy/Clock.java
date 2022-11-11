172d8345690809430d4dddfd5fdb6de5bcb3ca8f
LuK1337 <priv.luk@gmail.com>
2022-11-12 00:09:55 +0100

    SystemUI: Disable clock auto hide feature for QS
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/5272
    Change-Id: I1ffe7c24e9e8e4f4f771898197a56fede831c9c3

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -119,6 +119,8 @@ public class Clock extends TextView implements
     private boolean mShowSeconds;
     private Handler mSecondsHandler;
 
+    private boolean mIsStatusBar;
+
     // Fields to cache the width so the clock remains at an approximately constant width
     private int mCharsAtCurrentWidth = -1;
     private int mCachedWidth = -1;
@@ -173,6 +175,7 @@ public class Clock extends TextView implements
                     }
                 }
             };
+            mIsStatusBar = a.getBoolean(R.styleable.Clock_isStatusBar, mIsStatusBar);
             mNonAdaptedColor = getCurrentTextColor();
         } finally {
             a.recycle();
@@ -284,6 +287,10 @@ public class Clock extends TextView implements
     }
 
     private void handleTaskStackListener(boolean register) {
+        if (!mIsStatusBar) {
+            // We don't support clock auto hide for quick settings.
+            return;
+        }
         if (register && !mTaskStackListenerRegistered) {
             TaskStackChangeListeners.getInstance().registerTaskStackListener(mTaskStackListener);
             mTaskStackListenerRegistered = true;
