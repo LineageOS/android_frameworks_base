dbd300b1e954a596a3053a229fced219390da9f4
OnlyTomInSecond <q2781273965@gmail.com>
2022-12-15 01:13:58 +0800

    Screenshot: Unregister task listener on destroy
    This prevents submitting tasks after the threadloop has been shutdown.
    
    Change-Id: Id899ddc9476d045c78c4e85bfac7e7a9b29cee6d

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -491,6 +491,7 @@ public class ScreenshotController {
         removeWindow();
         releaseMediaPlayer();
         releaseContext();
+        TaskStackChangeListeners.getInstance().unregisterTaskStackListener(mTaskListener);
         mBgExecutor.shutdownNow();
     }
 
