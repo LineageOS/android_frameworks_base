d8dca19484a0ce131a1a94629b7c6cb5414c5af7
Wolfram Liebchen <achalmgucker@googlemail.com>
2023-01-23 22:03:20 +0100

    SystemUI: Network traffic with center cutout [1/3]
    Add possible network traffic display locations on the left
    and right side.
    
    Change-Id: Ic209aa1c0816cb22a5cf4ad9f19908746d38fc68

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -24,6 +24,8 @@ import android.view.View;
 
 import com.android.systemui.R;
 
+import org.lineageos.internal.statusbar.NetworkTraffic;
+
 public final class PhoneStatusBarTransitions extends BarTransitions {
     private static final float ICON_ALPHA_WHEN_NOT_OPAQUE = 1;
     private static final float ICON_ALPHA_WHEN_LIGHTS_OUT_BATTERY_CLOCK = 0.5f;
@@ -31,7 +33,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
 
     private final float mIconAlphaWhenOpaque;
 
-    private View mStartSide, mStatusIcons, mNetworkTraffic, mBattery;
+    private View mStartSide, mStatusIcons, mBattery;
+    private NetworkTraffic mNetworkTrafficStart, mNetworkTrafficCenter, mNetworkTrafficEnd;
     private Animator mCurrentAnimation;
 
     /**
@@ -43,8 +46,13 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mIconAlphaWhenOpaque = res.getFraction(R.dimen.status_bar_icon_drawing_alpha, 1, 1);
         mStartSide = statusBarView.findViewById(R.id.status_bar_start_side_except_heads_up);
         mStatusIcons = statusBarView.findViewById(R.id.statusIcons);
-        mNetworkTraffic = statusBarView.findViewById(R.id.network_traffic);
+        mNetworkTrafficStart = statusBarView.findViewById(R.id.network_traffic_start);
+        mNetworkTrafficCenter = statusBarView.findViewById(R.id.network_traffic_center);
+        mNetworkTrafficEnd = statusBarView.findViewById(R.id.network_traffic_end);
         mBattery = statusBarView.findViewById(R.id.battery);
+        mNetworkTrafficStart.setViewPosition(0);        /* start side display */
+        mNetworkTrafficCenter.setViewPosition(1);       /* center display */
+        mNetworkTrafficEnd.setViewPosition(2);          /* end side display */
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -87,7 +95,9 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             anims.playTogether(
                     animateTransitionTo(mStartSide, newAlpha),
                     animateTransitionTo(mStatusIcons, newAlpha),
-                    animateTransitionTo(mNetworkTraffic, newAlpha),
+                    animateTransitionTo(mNetworkTrafficStart, newAlpha),
+                    animateTransitionTo(mNetworkTrafficCenter, newAlpha),
+                    animateTransitionTo(mNetworkTrafficEnd, newAlpha),
                     animateTransitionTo(mBattery, newAlphaBC)
                     );
             if (isLightsOut(mode)) {
@@ -98,7 +108,9 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         } else {
             mStartSide.setAlpha(newAlpha);
             mStatusIcons.setAlpha(newAlpha);
-            mNetworkTraffic.setAlpha(newAlpha);
+            mNetworkTrafficStart.setAlpha(newAlpha);
+            mNetworkTrafficCenter.setAlpha(newAlpha);
+            mNetworkTrafficEnd.setAlpha(newAlpha);
             mBattery.setAlpha(newAlphaBC);
         }
     }
