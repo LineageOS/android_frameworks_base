8129fae3a01a0eb14953e01ccb9e11d9c381407d
Daniel Norman <danielnorman@google.com>
2023-02-09 12:28:26 -0800

    Checks if AccessibilityServiceInfo is within parcelable size.
    - If too large when parsing service XMLs then skip this service.
    - If too large when a service attempts to update its own info
      then throw an error.
    
    Bug: 261589597
    Test: atest AccessibilityServiceInfoTest
    Change-Id: Iffc0cd48cc713f7904d68059e141cb7de5a4b906
    Merged-In: Iffc0cd48cc713f7904d68059e141cb7de5a4b906
    (cherry picked from commit on googleplex-android-review.googlesource.com host: 553232c29079fbeab28f95307d025c1426aa7142)
    Merged-In: Iffc0cd48cc713f7904d68059e141cb7de5a4b906

--- services/accessibility/java/com/android/server/accessibility/AccessibilityManagerService.java
+++ services/accessibility/java/com/android/server/accessibility/AccessibilityManagerService.java
@@ -1069,6 +1069,12 @@ public class AccessibilityManagerService extends IAccessibilityManager.Stub {
             AccessibilityServiceInfo accessibilityServiceInfo;
             try {
                 accessibilityServiceInfo = new AccessibilityServiceInfo(resolveInfo, mContext);
+                if (!accessibilityServiceInfo.isWithinParcelableSize()) {
+                    Slog.e(LOG_TAG, "Skipping service "
+                            + accessibilityServiceInfo.getResolveInfo().getComponentInfo()
+                            + " because service info size is larger than safe parcelable limits.");
+                    continue;
+                }
                 mTempAccessibilityServiceInfoList.add(accessibilityServiceInfo);
             } catch (XmlPullParserException | IOException xppe) {
                 Slog.e(LOG_TAG, "Error while initializing AccessibilityServiceInfo", xppe);
