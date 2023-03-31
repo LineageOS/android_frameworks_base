532ee0af38408eead131c6788b37bb867b8d76df
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-31 04:04:16 +0300

    Screenrecord: Remember last used blinking dot position
    Change-Id: Iaaece0037160df9f659a1db65a7020510dbed0b0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
@@ -47,6 +47,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.systemui.Prefs;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.logging.UiEventLogger;
 import com.android.systemui.dagger.qualifiers.LongRunning;
@@ -89,6 +90,7 @@ public class RecordingService extends Service implements ScreenMediaRecorderList
             "com.android.systemui.screenrecord.STOP_FROM_NOTIF";
     protected static final String ACTION_SHARE = "com.android.systemui.screenrecord.SHARE";
     private static final String PERMISSION_SELF = "com.android.systemui.permission.SELF";
+    private static final String PREF_DOT_RIGHT = "screenrecord_dot_right";
 
     private final RecordingController mController;
     protected final KeyguardDismissUtil mKeyguardDismissUtil;
@@ -536,7 +538,7 @@ public class RecordingService extends Service implements ScreenMediaRecorderList
 
     private void showDot() {
         mDotShowing = true;
-        mIsDotAtRight = true;
+        mIsDotAtRight = Prefs.getInt(this, PREF_DOT_RIGHT, 1) == 1;
         final int size = (int) (this.getResources()
                 .getDimensionPixelSize(R.dimen.screenrecord_dot_size));
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
@@ -546,7 +548,7 @@ public class RecordingService extends Service implements ScreenMediaRecorderList
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE // don't get softkey inputs
                 | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, // allow outside inputs
                 PixelFormat.TRANSLUCENT);
-        params.gravity = Gravity.TOP | Gravity.RIGHT;
+        params.gravity = Gravity.TOP | (mIsDotAtRight ? Gravity.RIGHT : Gravity.LEFT);
         params.width = size;
         params.height = size;
 
@@ -595,6 +597,7 @@ public class RecordingService extends Service implements ScreenMediaRecorderList
             dot.setAnimation(null);
             mWindowManager.removeView(mFrameLayout);
         }
+        Prefs.putInt(this, PREF_DOT_RIGHT, mIsDotAtRight ? 1 : 0);
     }
 
     private Animation getDotAnimation() {
