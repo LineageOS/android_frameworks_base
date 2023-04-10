59bdb34ef57a70ac45004f2c7fcce41dfd826061
Oliver Scott <olivercscott@gmail.com>
2023-04-10 11:15:40 -0400

    Stop domain verification delegated from UIDs blocked by network policy
    Issue: calyxos#889
    Change-Id: I00278802bdf9e9d2ff2c8e94304a14cceb1a2c0d

--- packages/StatementService/src/com/android/statementservice/domain/DomainVerifier.kt
+++ packages/StatementService/src/com/android/statementservice/domain/DomainVerifier.kt
@@ -17,8 +17,12 @@
 package com.android.statementservice.domain
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.content.pm.verify.domain.DomainVerificationManager
+import android.net.ConnectivityManager
 import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkPolicyManager
 import android.util.Log
 import androidx.collection.LruCache
 import com.android.statementservice.network.retriever.StatementRetriever
@@ -91,6 +95,18 @@ class DomainVerifier private constructor(
         val assetMatcher = synchronized(targetAssetCache) { targetAssetCache[packageName] }
             .takeIf { it!!.isPresent }
             ?: return WorkResult.failure() to VerifyStatus.FAILURE_PACKAGE_MANAGER
+        // Only verify hosts if UID's networking is not blocked
+        val networkPolicyManager = appContext.getSystemService(NetworkPolicyManager::class.java)
+        if (networkPolicyManager != null) {
+            val isNetworkMetered = appContext.getSystemService(ConnectivityManager::class.java)
+                    ?.getNetworkCapabilities(network)?.hasCapability(
+                            NetworkCapabilities.NET_CAPABILITY_NOT_METERED) == false
+            val packageUid = appContext.packageManager.getPackageUid(packageName,
+                    PackageManager.PackageInfoFlags.of(0))
+            if (networkPolicyManager.isUidNetworkingBlocked(packageUid, isNetworkMetered)) {
+                return WorkResult.failure() to VerifyStatus.NO_RESPONSE
+            }
+        }
         return verifyHost(host, assetMatcher.get(), network)
     }
 
