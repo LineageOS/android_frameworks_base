e5a16863476169df716405152e0aea905c9de48d
Chirayu Desai <chirayudesai1@gmail.com>
2023-05-04 23:01:25 +0530

    GnssLocationProvider: Disable AGPS by default, it'll be enabled in SetupWizard
    * This way the user can choose to turn it off, and it'll
      never have been enabled
    * If they just keep it as the default (toggle will be on in SuW),
      then it'll be enabled, which is the default we want
    
    Change-Id: I878200a0351d0b0018a975e432602de2badc4eef

--- services/core/java/com/android/server/location/gnss/GnssLocationProvider.java
+++ services/core/java/com/android/server/location/gnss/GnssLocationProvider.java
@@ -1104,7 +1104,7 @@ public class GnssLocationProvider extends AbstractLocationProvider implements
 
             boolean agpsEnabled =
                     (Settings.Global.getInt(mContext.getContentResolver(),
-                            Settings.Global.ASSISTED_GPS_ENABLED, 1) != 0) || isEmergency;
+                            Settings.Global.ASSISTED_GPS_ENABLED, 0) != 0) || isEmergency;
             mPositionMode = getSuplMode(agpsEnabled);
 
             if (DEBUG) {
