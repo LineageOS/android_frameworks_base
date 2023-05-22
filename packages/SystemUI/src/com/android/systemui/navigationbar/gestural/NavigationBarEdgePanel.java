b1edf34c538f1efa3bb39f0ec0a24621b45fe44e
nift4 <nift4@protonmail.com>
2023-05-22 17:24:51 +0200

    SystemUI: fix multi-display gesture arrow
    Change-Id: I58f02631b2e3dbd19e810463374cb41e9f657242

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationBarEdgePanel.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationBarEdgePanel.java
@@ -287,8 +287,38 @@ public class NavigationBarEdgePanel extends View implements NavigationEdgeBackPl
             };
     private BackCallback mBackCallback;
 
-    @Inject
-    public NavigationBarEdgePanel(
+    /**
+     * Injectable instance to create a new NavigationBarEdgePanel.
+     *
+     * Necessary because we don't have good handling of per-display contexts at the moment. With
+     * this, you can pass in a specific context that knows what display it is in.
+     */
+    public static class Factory {
+        private final LatencyTracker mLatencyTracker;
+        private final VibratorHelper mVibratorHelper;
+        private final Executor mBackgroundExecutor;
+
+        @Inject
+        public Factory(
+                LatencyTracker latencyTracker,
+                VibratorHelper vibratorHelper,
+                @Background Executor backgroundExecutor) {
+            mLatencyTracker = latencyTracker;
+            mBackgroundExecutor = backgroundExecutor;
+            mVibratorHelper = vibratorHelper;
+        }
+
+        public NavigationBarEdgePanel create(Context context) {
+            return new NavigationBarEdgePanel(
+                context,
+                mLatencyTracker,
+                mVibratorHelper,
+                mBackgroundExecutor
+            );
+        }
+    }
+
+    NavigationBarEdgePanel(
             Context context,
             LatencyTracker latencyTracker,
             VibratorHelper vibratorHelper,
