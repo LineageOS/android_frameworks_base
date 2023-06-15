4a4f20e13ab2334b0fd5fbc61ee53c8e49f3dfbf
Michael Bestas <mkbestas@lineageos.org>
2023-06-15 15:08:59 +0300

    fixup! Add support for runtime toggle of navbar
    Change-Id: I2da018dd388324b5e0f94c8035f3d4a4cebbf24b

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarController.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarController.java
@@ -486,7 +486,7 @@ public class NavigationBarController implements
      * @return whether there is a soft nav bar on specific display.
      */
     private boolean hasSoftNavigationBar(Context context, int displayId) {
-        if (displayId == DEFAULT_DISPLAY &&
+        if (displayId == mDisplayTracker.getDefaultDisplayId() &&
                 LineageSettings.System.getIntForUser(context.getContentResolver(),
                         LineageSettings.System.FORCE_SHOW_NAVBAR, 0,
                         UserHandle.USER_CURRENT) == 1) {
