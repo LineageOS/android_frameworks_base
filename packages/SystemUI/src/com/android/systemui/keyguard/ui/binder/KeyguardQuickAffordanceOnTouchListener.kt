e1e89fc4d2d4b0da61ab9ba92fd294967dcc2538
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-06-21 18:21:55 +0200

    SystemUI: Check if primitive vibrations are supported in new quick affordances
     * and provide an alternative if not
    
    [SamarV-121: Use HEAVY_CLICK effect on activation and deactivation]
    
    Change-Id: I9b6fafd731c49bf4d875b0ad581ee1b4666daf17

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardQuickAffordanceOnTouchListener.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardQuickAffordanceOnTouchListener.kt
@@ -117,9 +117,17 @@ class KeyguardQuickAffordanceOnTouchListener(
         view.setOnClickListener {
             vibratorHelper?.vibrate(
                 if (viewModel.isActivated) {
-                    KeyguardBottomAreaVibrations.Activated
+                    if (KeyguardBottomAreaVibrations.areAllPrimitivesSupported) {
+                        KeyguardBottomAreaVibrations.Activated
+                    } else {
+                        KeyguardBottomAreaVibrations.ActivatedAlt
+                    }
                 } else {
-                    KeyguardBottomAreaVibrations.Deactivated
+                    if (KeyguardBottomAreaVibrations.areAllPrimitivesSupported) {
+                        KeyguardBottomAreaVibrations.Deactivated
+                    } else {
+                        KeyguardBottomAreaVibrations.DeactivatedAlt
+                    }
                 }
             )
             viewModel.onClicked(
