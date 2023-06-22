c722557c56e3de4df55708b32e5076875a6e9a86
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-22 23:11:13 +0530

    SystemUI: Add FingerprintInteractiveToAuthProvider implementation
    This is required for the "touch to unlock anytime" setting on devices
    with side mounted fingerprint sensor.
    
    Ref: 66a048d646fd816e7757e300c62fa5cebf04761d
    
    Change-Id: If3308860a428d9966f2b0a0024764df95f8f9a8b

--- packages/SystemUI/src/com/android/systemui/biometrics/dagger/BiometricsModule.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/dagger/BiometricsModule.kt
@@ -25,6 +25,8 @@ import com.android.launcher3.icons.IconProvider
 import com.android.systemui.CoreStartable
 import com.android.systemui.biometrics.AuthController
 import com.android.systemui.biometrics.EllipseOverlapDetectorParams
+import com.android.systemui.biometrics.FingerprintInteractiveToAuthProvider
+import com.android.systemui.biometrics.FingerprintInteractiveToAuthProviderImpl
 import com.android.systemui.biometrics.UdfpsUtils
 import com.android.systemui.biometrics.data.repository.BiometricStatusRepository
 import com.android.systemui.biometrics.data.repository.BiometricStatusRepositoryImpl
@@ -44,11 +46,14 @@ import com.android.systemui.biometrics.ui.binder.DeviceEntryUnlockTrackerViewBin
 import com.android.systemui.biometrics.ui.binder.SideFpsOverlayViewBinder
 import com.android.systemui.camera.CameraSensorPrivacyModule
 import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.dagger.qualifiers.Background
 import com.android.systemui.display.dagger.SystemUIDisplaySubcomponent
 import com.android.systemui.display.data.repository.DisplayStateRepository
 import com.android.systemui.keyguard.ui.binder.AlternateBouncerViewBinder
 import com.android.systemui.statusbar.policy.ConfigurationController.ConfigurationListener
+import com.android.systemui.user.domain.interactor.SelectedUserInteractor
 import com.android.systemui.util.concurrency.ThreadFactory
+import com.android.systemui.util.settings.SecureSettings
 import dagger.Binds
 import dagger.BindsOptionalOf
 import dagger.Module
@@ -58,6 +63,7 @@ import dagger.multibindings.IntoMap
 import dagger.multibindings.IntoSet
 import java.util.concurrent.Executor
 import javax.inject.Qualifier
+import kotlinx.coroutines.CoroutineDispatcher
 
 /** Dagger module for all things biometric. */
 @Module(includes = [CameraSensorPrivacyModule::class])
@@ -151,6 +157,20 @@ interface BiometricsModule {
                 BoundingBoxOverlapDetector(values[2])
             }
         }
+
+        @Provides
+        fun providesFingerprintInteractiveToAuth(
+            @Background backgroundDispatcher: CoroutineDispatcher,
+            context: Context,
+            secureSettings: SecureSettings,
+            selectedUserInteractor: SelectedUserInteractor,
+        ): FingerprintInteractiveToAuthProvider =
+            FingerprintInteractiveToAuthProviderImpl(
+                backgroundDispatcher,
+                context,
+                secureSettings,
+                selectedUserInteractor,
+            )
     }
 }
 
