2480196ecc8da5233892a53c3e4092e5ff32ada5
LuK1337 <priv.luk@gmail.com>
2023-06-22 13:49:00 +0200

    SystemUI: Don't require AOD for screen off UDFPS
    Change-Id: I47eceb1e99146c6793f9bd4e3b86d4f6878a11eb

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -251,7 +251,7 @@ public class DozeSensors {
                         dozeParameters.longPressUsesProx(),
                         !dozeParameters.longPressNeedsProximityCheck() /* performsProxCheck */,
                         false /* immediatelyReRegister */,
-                        true /* requiresAod */
+                        !mScreenOffUdfpsEnabled /* requiresAod */
                 ),
                 new PluginSensor(
                         new SensorManagerPlugin.Sensor(TYPE_WAKE_DISPLAY),
