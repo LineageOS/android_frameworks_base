b4885ecfd5d15358b1b53e8fb43c2bdb2c66fc0d
Michael W <baddaemon87@gmail.com>
2023-06-25 17:12:22 +0200

    SystemUI: SideFpsEventHandler: Don't crash without FingerprintManager
    * On devices without FP, no FingerprintManager service is published
      resulting in a crash when accessing it
    * Since notifyPowerPressed is sent unconditionally, make sure we even
      have a FPS
    
    Change-Id: I74dbd6357aac4d3a0fb108018755b08968330156

--- services/core/java/com/android/server/policy/SideFpsEventHandler.java
+++ services/core/java/com/android/server/policy/SideFpsEventHandler.java
@@ -130,6 +130,10 @@ public class SideFpsEventHandler implements View.OnClickListener {
      */
     public void notifyPowerPressed() {
         Log.i(TAG, "notifyPowerPressed");
+        final PackageManager pm = mContext.getPackageManager();
+        if (!pm.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)) {
+            return;
+        }
         if (mFingerprintManager == null && mSideFpsEventHandlerReady.get()) {
             mFingerprintManager = mContext.getSystemService(FingerprintManager.class);
         }
