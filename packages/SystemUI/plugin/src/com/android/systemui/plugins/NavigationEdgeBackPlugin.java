65f0c728720da1e5dfb82655548e276a2932c541
someone5678 <nemui3353@gmail.com>
2023-07-14 17:26:19 +0900

    Implement edge long swipe gesture to new back gesture affordance
    Author: Nico <nicorg2529@gmail.com>
    Date:   Sun Mar 8 11:39:47 2020 +0000
    
        Implement edge long swipe gesture [1/3]
    
        The gesture will activate if user executes edge swipe for a long
        horizontal motion. The triggered action is configurable in Button
        Settings.
    
        Reference:
        https://gerrit.dirtyunicorns.com/q/topic:%22back-longswipe-actions%22
    
        Change-Id: Ie1bbe6645b6a00d346af60d6bb5e4d584997d6e9
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Thu Nov 25 17:39:29 2021 +0100
    
        Use custom flag for edge long swipe gesture
    
        Previously we were using same flags that'd be used for regular back key
        long press thus being unable to determine whether the event was sent
        from EdgeBackGestureHandle or not.
    
        Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/4194
        Change-Id: I5c4fd455f581ac5c9c5e3a146095be33e82e8d6e
    
    Author: someone5678 <nemui3353@gmail.com>
    Date:   Fri, 14 Jul 2023 17:26:19 +0900
    
        Adapt and fix from original long swipe gesture impl
    
        Change-Id: I22c6442299df0cf15d046718fa40df6fb6aee07c
        Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    Author: someone5678 <nemui3353@gmail.com>
    Date:   Sun Jul 30 20:55:13 2023 +0900
    
        Flip back gesture canvas if started from left
    
        * Better than constant right arrow direction
    
        Change-Id: I45108f185130d6f778660ba11f3beb758826ba99
        Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    Author: someone5678 <nemui3353@gmail.com>
    Date:   Tue Aug 1 12:25:00 2023 +0900
    
        Always add second arrow in front on long swipe
    
        * After last commit if long swipe gesture started from left, second arrow is added
          in back instead of front.
    
        * This looks unnatural and causes inconsistency with right.
    
        * Hardcode to -1 so that second arrow always added in front of first one.
    
        Change-Id: Ia043a749218ab40c40520736fa54ffaa26919137
        Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    Change-Id: I6a54d781b64aeec278b9f1be4a42bcfaf681b160

--- packages/SystemUI/plugin/src/com/android/systemui/plugins/NavigationEdgeBackPlugin.java
+++ packages/SystemUI/plugin/src/com/android/systemui/plugins/NavigationEdgeBackPlugin.java
@@ -45,6 +45,9 @@ public interface NavigationEdgeBackPlugin extends Plugin {
     /** Sets the callback that should be invoked when a Back gesture is detected. */
     void setBackCallback(BackCallback callback);
 
+    /** Specifies if the long swipe should be enabled or not. */
+    default void setLongSwipeEnabled(boolean enabled) {}
+
     /** Sets the base LayoutParams for the UI. */
     void setLayoutParams(WindowManager.LayoutParams layoutParams);
 
@@ -57,7 +60,7 @@ public interface NavigationEdgeBackPlugin extends Plugin {
     /** Callback to let the system react to the detected back gestures. */
     interface BackCallback {
         /** Indicates that a Back gesture was recognized and the system should go back. */
-        void triggerBack();
+        void triggerBack(boolean isLongPress);
 
         /** Indicates that the gesture was cancelled and the system should not go back. */
         void cancelBack();
@@ -68,5 +71,12 @@ public interface NavigationEdgeBackPlugin extends Plugin {
          * @param triggerBack if back will be triggered in current state.
          */
         void setTriggerBack(boolean triggerBack);
+
+        /**
+         * Indicates if long swipe will be triggered if committed in current state.
+         *
+         * @param triggerLongSwipe if long swipe will be triggered in current state.
+         */
+        void setTriggerLongSwipe(boolean triggerLongSwipe);
     }
 }
