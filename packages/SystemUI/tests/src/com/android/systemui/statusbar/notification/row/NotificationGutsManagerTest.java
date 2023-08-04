9c0e09e708a704425e0acca6a75341f740c8fa70
Julia Reynolds <juliacr@google.com>
2023-08-04 13:29:29 -0400

    Disable priority conversation widget for secondary users
    Test: NotificationConversationInfoTest.java
    Test: make a conversation priority on the primary user
    Test: make a conversation priority on a secondary user
    Bug: 288896269
    (cherry picked from commit adf620316dcfaf19d7d4a73e2c63322b4a3a4d3a)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:b0522df0a33d0165656797df7edab978cb403bd4)
    Merged-In: I3f3991d2cb7fb9970cc8ada39ceae9a7ff2fcb31
    Change-Id: I3f3991d2cb7fb9970cc8ada39ceae9a7ff2fcb31

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/notification/row/NotificationGutsManagerTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/notification/row/NotificationGutsManagerTest.java
@@ -54,6 +54,7 @@ import android.content.pm.ShortcutManager;
 import android.graphics.Color;
 import android.os.Binder;
 import android.os.Handler;
+import android.os.UserManager;
 import android.provider.Settings;
 import android.service.notification.StatusBarNotification;
 import android.testing.AndroidTestingRunner;
@@ -139,6 +140,8 @@ public class NotificationGutsManagerTest extends SysuiTestCase {
     @Mock private PeopleSpaceWidgetManager mPeopleSpaceWidgetManager;
     @Mock private AssistantFeedbackController mAssistantFeedbackController;
 
+    @Mock private UserManager mUserManager;
+
     @Before
     public void setUp() {
         mTestableLooper = TestableLooper.get(this);
@@ -156,7 +159,8 @@ public class NotificationGutsManagerTest extends SysuiTestCase {
 
         mGutsManager = new NotificationGutsManager(mContext,
                 () -> Optional.of(mStatusBar), mHandler, mHandler, mAccessibilityManager,
-                mHighPriorityProvider, mINotificationManager, mNotificationEntryManager,
+                mHighPriorityProvider, mINotificationManager, mUserManager,
+                mNotificationEntryManager,
                 mPeopleSpaceWidgetManager, mLauncherApps, mShortcutManager,
                 mChannelEditorDialogController, mContextTracker, mAssistantFeedbackController,
                 Optional.of(mBubblesManager), new UiEventLoggerFake(), mOnUserInteractionCallback,
