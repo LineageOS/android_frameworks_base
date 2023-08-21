657826c421e9ea6ca2934be6a04473c2fa71cdff
nift4 <nift4@protonmail.com>
2023-08-21 15:33:19 +0200

    services: fix quickswitch with gesture pill disabled
    Change-Id: Ia2a543eec28c65897a5f3f0b89ebc6b6028a29a1

--- services/core/java/com/android/server/wm/RecentTasks.java
+++ services/core/java/com/android/server/wm/RecentTasks.java
@@ -251,12 +251,13 @@ class RecentTasks {
                     final boolean isAppWindowTouch = FIRST_APPLICATION_WINDOW <= win.mAttrs.type
                             && win.mAttrs.type <= LAST_APPLICATION_WINDOW;
                     if (isAppWindowTouch) {
-                        final Task stack = mService.getTopDisplayFocusedRootTask();
-                        final Task topTask = stack != null ? stack.getTopMostTask() : null;
-                        ProtoLog.i(WM_DEBUG_TASKS, "Resetting frozen recents task list"
-                                + " reason=app touch win=%s x=%d y=%d insetFrame=%s", win, x, y,
-                                mTmpRect);
-                        resetFreezeTaskListReordering(topTask);
+                        // If we quickswitch while having gesture pill disabled, navbar height
+                        // is 0dp, which means the quickswitch start touch is inside app window
+                        // as well. To solve this, we defer resetting the freeze 500ms into the
+                        // future and if Launcher3 sends a freeze notice again, this app touch
+                        // effectively gets ignored when removeCallbacks() removes this runnable.
+                        mService.mH.removeCallbacks(mResetFreezeTaskListOnTimeoutRunnable);
+                        mService.mH.postDelayed(mResetFreezeTaskListOnTimeoutRunnable, 500);
                     }
                 }
             }, null).recycleOnUse());
