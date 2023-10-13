b36b7d8e258b0acacab5ab6ac434d6dc226ba540
Bruno Martins <bgcngm@gmail.com>
2023-10-13 14:04:00 +0100

    SystemUI: Respect status bar DT2S gesture defaults
    Feature can be disabled by default, but if additionally the preference
    is hidden in Settings, it doesn't allow TunerService to set the proper
    default.
    
    Change-Id: I12b37335fba12cf4d5564f1aa19c5e4cf19f8775

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -4521,7 +4521,9 @@ public final class NotificationPanelViewController implements Dumpable {
         @Override
         public void onTuningChanged(String key, String newValue) {
             if (DOUBLE_TAP_SLEEP_GESTURE.equals(key)) {
-                mDoubleTapToSleepEnabled = TunerService.parseIntegerSwitch(newValue, true);
+                mDoubleTapToSleepEnabled = TunerService.parseIntegerSwitch(newValue,
+                        mResources.getBoolean(org.lineageos.platform.internal.R.bool.
+                                config_dt2sGestureEnabledByDefault));
             }
         }
     }
