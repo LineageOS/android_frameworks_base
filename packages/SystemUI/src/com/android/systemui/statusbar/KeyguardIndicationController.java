43ce5a49583c3b4c2347b61a1a787f041c0bff85
Bruno Martins <bgcngm@gmail.com>
2023-10-23 16:22:32 +0100

    Keyguard: Hide battery indication on battery-less devices
    Change-Id: Iaa995505aa580020906f38177ea16713a79997ad

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -497,7 +497,7 @@ public class KeyguardIndicationController {
     }
 
     private void updateLockScreenBatteryMsg(boolean animate) {
-        if (mPowerPluggedIn || mEnableBatteryDefender) {
+        if (mBatteryPresent && (mPowerPluggedIn || mEnableBatteryDefender)) {
             String powerIndication = computePowerIndication();
             if (DEBUG_CHARGING_SPEED) {
                 powerIndication += ",  " + (mChargingWattage / 1000) + " mW";
@@ -513,7 +513,7 @@ public class KeyguardIndicationController {
                     animate);
         } else {
             mKeyguardLogger.log(TAG, LogLevel.DEBUG, "hide battery indication");
-            // don't show the charging information if device isn't plugged in
+            // don't show the charging information
             mRotateTextViewController.hideIndication(INDICATION_TYPE_BATTERY);
         }
     }
