8f063a45f35052b3d1afdf0a8545ddfe1f7aa66d
LuK1337 <priv.luk@gmail.com>
2024-02-19 16:20:04 +0100

    Allow signature spoofing for microG Companion/Services
    This patch enables signature spoofing when the following conditions are
    met:
    * Build is debuggable (userdebug/eng)
    * Package name is com.android.vending or com.google.android.gms
    * Package is signed with microG release keys
    * Fake signature is correct
    
    Additionally, we let these apps be forceQueryable if they so desire.
    
    Change-Id: I8fc82ed266a2cc59636b662c7ea7e29c94f509b5

--- services/core/jni/onload.cpp
+++ services/core/jni/onload.cpp
@@ -58,6 +58,7 @@ int register_android_server_am_LowMemDetector(JNIEnv* env);
 int register_android_server_utils_AnrTimer(JNIEnv *env);
 int register_com_android_server_soundtrigger_middleware_AudioSessionProviderImpl(JNIEnv* env);
 int register_com_android_server_soundtrigger_middleware_ExternalCaptureStateTracker(JNIEnv* env);
+int register_android_server_com_android_server_pm_ComputerEngine(JNIEnv* env);
 int register_android_server_com_android_server_pm_PackageManagerShellCommandDataLoader(JNIEnv* env);
 int register_android_server_AdbDebuggingManager(JNIEnv* env);
 int register_android_server_FaceService(JNIEnv* env);
@@ -122,6 +123,7 @@ extern "C" jint JNI_OnLoad(JavaVM* vm, void* /* reserved */)
     register_android_server_utils_AnrTimer(env);
     register_com_android_server_soundtrigger_middleware_AudioSessionProviderImpl(env);
     register_com_android_server_soundtrigger_middleware_ExternalCaptureStateTracker(env);
+    register_android_server_com_android_server_pm_ComputerEngine(env);
     register_android_server_com_android_server_pm_PackageManagerShellCommandDataLoader(env);
     register_android_server_AdbDebuggingManager(env);
     register_android_server_FaceService(env);
