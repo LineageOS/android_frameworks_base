2a94f200ab1f6c94fc95fef5df4ddfc458c4ddb9
Chirayu Desai <chirayudesai1@gmail.com>
2024-03-07 03:44:46 +0530

    fixup! SystemUI: Add powershare QS tile
    Change-Id: I25c2dedf33f8f036a9d13efafc48d0f617f3a208

--- packages/SystemUI/src/com/android/systemui/qs/tiles/PowerShareTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/PowerShareTile.java
@@ -188,10 +188,6 @@ public class PowerShareTile extends QSTileImpl<BooleanState>
             return;
         }
 
-        if (state.slash == null) {
-            state.slash = new SlashState();
-        }
-
         state.icon = ResourceIcon.get(R.drawable.ic_qs_powershare);
         try {
             state.value = mPowerShare.isEnabled();
@@ -199,7 +195,6 @@ public class PowerShareTile extends QSTileImpl<BooleanState>
             state.value = false;
             ex.printStackTrace();
         }
-        state.slash.isSlashed = state.value;
         state.label = mContext.getString(R.string.quick_settings_powershare_label);
 
         if (mBatteryController.isPowerSave() || getBatteryLevel() < getMinBatteryLevel()) {
