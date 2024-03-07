8acab11ae07403837e36f42a1cff2521a5c02295
Michael Bestas <mikeioannina@gmail.com>
2024-03-07 23:40:00 +0200

    Revert "SystemUI: Allow Wi-Fi/cell tiles to co-exist with provider model"
    This reverts commit 6a568876039fac75d587039ea9bd2930d82b792f.
    
    Change-Id: I4b039dc28daeb1a06edbeda036152892857d441e

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/WifiSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/WifiSignalController.java
@@ -112,16 +112,18 @@ public class WifiSignalController extends SignalController<WifiState, IconGroup>
         }
         IconState statusIcon = new IconState(
                 wifiVisible, getCurrentIconId(), contentDescription);
-        IconState qsIcon = new IconState(mCurrentState.connected,
-                mWifiTracker.isCaptivePortal ? R.drawable.ic_qs_wifi_disconnected
-                        : getQsCurrentIconId(), contentDescription);
-        boolean isDefault = mCurrentState.isDefault || (!mNetworkController.isRadioOn()
-                && !mNetworkController.isEthernetDefault());
+        IconState qsIcon = null;
+        if (mCurrentState.isDefault || (!mNetworkController.isRadioOn()
+                && !mNetworkController.isEthernetDefault())) {
+            qsIcon = new IconState(mCurrentState.connected,
+                    mWifiTracker.isCaptivePortal ? R.drawable.ic_qs_wifi_disconnected
+                            : getQsCurrentIconId(), contentDescription);
+        }
         WifiIndicators wifiIndicators = new WifiIndicators(
                 mCurrentState.enabled, statusIcon, qsIcon,
                 ssidPresent && mCurrentState.activityIn,
                 ssidPresent && mCurrentState.activityOut,
-                wifiDesc, mCurrentState.isTransient, mCurrentState.statusLabel, isDefault
+                wifiDesc, mCurrentState.isTransient, mCurrentState.statusLabel
         );
         callback.setWifiIndicators(wifiIndicators);
     }
@@ -154,8 +156,7 @@ public class WifiSignalController extends SignalController<WifiState, IconGroup>
                 statusIcon, qsIcon, typeIcon, qsTypeIcon,
                 mCurrentState.activityIn, mCurrentState.activityOut, dataContentDescription,
                 dataContentDescriptionHtml, description,
-                mCurrentState.subId, /* roaming= */ false, /* showTriangle= */ true,
-                /* isDefault= */ qsIcon != null
+                mCurrentState.subId, /* roaming= */ false, /* showTriangle= */ true
         );
         callback.setMobileDataIndicators(mobileDataIndicators);
     }
