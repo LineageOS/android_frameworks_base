2399c75a7e8c3cd8827febb43c228a25b69781bc
Michael Bestas <mkbestas@lineageos.org>
2024-03-10 21:49:16 +0200

    Partially revert "Show floating rotation button regardless of ime status"
    Add this back for hw-key devices.
    
    This reverts commit 77c2881156de5f154ab7988a7e5fc7dda2d3cc53.
    
    Change-Id: I26c5bb624fc533f6aaafe6b1052d1ac7b9437d33

--- packages/SystemUI/shared/src/com/android/systemui/shared/rotation/FloatingRotationButton.java
+++ packages/SystemUI/shared/src/com/android/systemui/shared/rotation/FloatingRotationButton.java
@@ -71,6 +71,7 @@ public class FloatingRotationButton implements RotationButton {
 
     private AnimatedVectorDrawable mAnimatedDrawable;
     private boolean mIsShowing;
+    private boolean mCanShow = true;
     private int mDisplayRotation;
 
     private boolean mIsTaskbarVisible = false;
@@ -148,7 +149,7 @@ public class FloatingRotationButton implements RotationButton {
 
     @Override
     public boolean show() {
-        if (mIsShowing) {
+        if (!mCanShow || mIsShowing) {
             return false;
         }
 
@@ -217,6 +218,14 @@ public class FloatingRotationButton implements RotationButton {
     @Override
     public void setDarkIntensity(float darkIntensity) {
         mKeyButtonView.setDarkIntensity(darkIntensity);
+    }
+
+    @Override
+    public void setCanShowRotationButton(boolean canShow) {
+        mCanShow = canShow;
+        if (!mCanShow) {
+            hide();
+        }
     }
 
     @Override
