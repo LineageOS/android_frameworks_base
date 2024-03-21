b4cc41018f5d5bf584a92d72d7087ac9a21696bb
Tommy Webb <tommy@calyxinstitute.org>
2024-03-21 19:58:07 +0000

    UsbDeviceManager: Adapt for data signaling quirks
    Always treat USB as disconnected if USB data signaling is turned off.
    Set USB functions to none, and send a state change event.
    
    The resulting behavior matches the Google Pixel series, which already
    reports USB as disconnected when data signaling is turned off, but the
    Motorola series (and maybe others that use Qualcomm's HAL) do not.
    
    Test: Manual: Set Restrict USB to Always allow. Connect to a computer.
    Observe the USB notification on device. Set Restrict USB to Deny. Check
    that the notification disappears. Search Settings for USB and tap the
    Connected devices > USB result. The "Use USB for" section is disabled.
    
    Issue: calyxos#2154
    Change-Id: I9d8753d4dbb18012e0d40f4991ab4c15bad14de1

--- services/usb/java/com/android/server/usb/UsbDeviceManager.java
+++ services/usb/java/com/android/server/usb/UsbDeviceManager.java
@@ -1172,7 +1172,9 @@ public class UsbDeviceManager implements ActivityTaskManagerInternal.ScreenObser
                     UsbPort port = (UsbPort) args.arg1;
                     UsbPortStatus status = (UsbPortStatus) args.arg2;
 
-                    if (status != null) {
+                    final boolean isDataForcedOff = status != null && status.getUsbDataStatus()
+                            == UsbPortStatus.DATA_STATUS_DISABLED_FORCE;
+                    if (status != null && !isDataForcedOff) {
                         mHostConnected = status.getCurrentDataRole() == DATA_ROLE_HOST;
                         mSourcePower = status.getCurrentPowerRole() == POWER_ROLE_SOURCE;
                         mSinkPower = status.getCurrentPowerRole() == POWER_ROLE_SINK;
@@ -1202,6 +1204,12 @@ public class UsbDeviceManager implements ActivityTaskManagerInternal.ScreenObser
                         mPowerBrickConnectionStatus = UsbPortStatus.POWER_BRICK_STATUS_UNKNOWN;
                     }
 
+                    if (isDataForcedOff) {
+                        mConnected = false;
+                        setEnabledFunctions(UsbManager.FUNCTION_NONE, false,
+                                /* operationId */ sUsbOperationCount.incrementAndGet());
+                    }
+
                     if (mHostConnected) {
                         if (!mUsbAccessoryConnected) {
                             mInHostModeWithNoAccessoryConnected = true;
@@ -1220,7 +1228,7 @@ public class UsbDeviceManager implements ActivityTaskManagerInternal.ScreenObser
                     args.recycle();
                     updateUsbNotification(false);
                     if (mBootCompleted) {
-                        if (mHostConnected || prevHostConnected) {
+                        if (mHostConnected || prevHostConnected || isDataForcedOff) {
                             updateUsbStateBroadcastIfNeeded(getAppliedFunctions(mCurrentFunctions));
                         }
                     } else {
