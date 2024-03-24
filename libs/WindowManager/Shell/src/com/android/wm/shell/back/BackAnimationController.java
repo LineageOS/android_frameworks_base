f84ce9e1cb1b409813eb3f96f16a77feccfe7e84
OnlyTomInSecond <q2781273965@gmail.com>
2024-03-24 16:34:02 +0800

    fixup! Implement edge long swipe gesture
    Change-Id: Ia0e8846eb163f19c41819f1ee7b136df8824dffe

--- libs/WindowManager/Shell/src/com/android/wm/shell/back/BackAnimationController.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/back/BackAnimationController.java
@@ -735,6 +735,13 @@ public class BackAnimationController implements RemoteCallable<BackAnimationCont
                     "onGestureFinished called while no gesture is started");
             return;
         }
+        if (mTriggerLongSwipe) {
+            // Let key event handlers deal with back long swipe gesture
+            sendEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_BACK, KeyEvent.FLAG_LONG_SWIPE);
+            sendEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_BACK, KeyEvent.FLAG_LONG_SWIPE);
+            finishBackNavigation(false);
+            return;
+        }
         boolean triggerBack = activeTouchTracker.getTriggerBack();
         ProtoLog.d(WM_SHELL_BACK_PREVIEW, "onGestureFinished() mTriggerBack == %s", triggerBack);
 
@@ -758,12 +765,6 @@ public class BackAnimationController implements RemoteCallable<BackAnimationCont
                 injectBackKey();
             }
             finishBackNavigation(triggerBack);
-            return;
-        }
-        if (mTriggerLongSwipe) {
-            // Let key event handlers deal with back long swipe gesture
-            sendEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_BACK, KeyEvent.FLAG_LONG_SWIPE);
-            sendEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_BACK, KeyEvent.FLAG_LONG_SWIPE);
             return;
         }
 
