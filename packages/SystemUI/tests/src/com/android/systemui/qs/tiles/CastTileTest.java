f64d96f36b93f9d2234a120f031cde68b1026303
Danny Lin <danny@kdrag0n.dev>
2024-03-25 20:44:21 +0100

    Reapply "SystemUI: Allow Wi-Fi/cell tiles to co-exist with provider model"
    This reverts commit 8acab11ae07403837e36f42a1cff2521a5c02295.
    
    Change-Id: I3e06c54a775f69d134985dc9ce4893808789eadb

--- packages/SystemUI/tests/src/com/android/systemui/qs/tiles/CastTileTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/qs/tiles/CastTileTest.java
@@ -138,7 +138,7 @@ public class CastTileTest extends SysuiTestCase {
         WifiIndicators indicators = new WifiIndicators(
                 false, mock(IconState.class),
                 qsIcon, false, false, "",
-                false, "");
+                false, "", true);
         mSignalCallback.setWifiIndicators(indicators);
         mTestableLooper.processAllMessages();
 
@@ -152,7 +152,7 @@ public class CastTileTest extends SysuiTestCase {
         WifiIndicators indicators = new WifiIndicators(
                 true, mock(IconState.class),
                 qsIcon, false, false, "",
-                false, "");
+                false, "", true);
         mSignalCallback.setWifiIndicators(indicators);
         mTestableLooper.processAllMessages();
 
@@ -164,7 +164,7 @@ public class CastTileTest extends SysuiTestCase {
         WifiIndicators indicators = new WifiIndicators(
                 true, mock(IconState.class),
                 qsIcon, false, false, "",
-                false, "");
+                false, "", true);
         mSignalCallback.setWifiIndicators(indicators);
         mTestableLooper.processAllMessages();
     }
