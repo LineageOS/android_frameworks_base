03e1d2da62c89108e608e30f1140ddd335d924b6
Oliver Scott <olivercscott@gmail.com>
2024-03-26 10:39:24 -0400

    fixup! SystemUI: Add caffeine QS tile
    Change-Id: I4887631f9ed64140444125f20ac2dbdd43d5eae4

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -34,7 +34,7 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 import com.android.internal.logging.MetricsLogger;
-import com.android.systemui.R;
+import com.android.systemui.res.R;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.plugins.ActivityStarter;
