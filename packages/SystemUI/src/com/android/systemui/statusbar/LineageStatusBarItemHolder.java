1b82cf6b201eedb33b3e2df4c3104ad8f962ce0a
Oliver Scott <olivercscott@gmail.com>
2024-03-26 10:46:48 -0400

    fixup! SystemUI: Add Lineage statusbar item holder
    Change-Id: I7bbb229ea43ad8b955ccb9eaf3748822efb603ec

--- packages/SystemUI/src/com/android/systemui/statusbar/LineageStatusBarItemHolder.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/LineageStatusBarItemHolder.java
@@ -20,13 +20,12 @@ import android.content.Context;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.View.OnSystemUiVisibilityChangeListener;
 import android.widget.RelativeLayout;
 
 import com.android.systemui.Dependency;
 import com.android.systemui.plugins.DarkIconDispatcher;
 import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
-import com.android.systemui.R;
+import com.android.systemui.res.R;
 
 import org.lineageos.internal.statusbar.LineageStatusBarItem;
 
