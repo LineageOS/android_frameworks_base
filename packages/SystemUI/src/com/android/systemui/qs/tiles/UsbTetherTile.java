2d6153511135136b5567aab2c3a742784e4728d7
Oliver Scott <olivercscott@gmail.com>
2024-03-26 10:41:25 -0400

    fixup! SystemUI: Add USB Tether tile
    Change-Id: I43a35b382806ca07908a1d64fd759e26f7ab5d19

--- packages/SystemUI/src/com/android/systemui/qs/tiles/UsbTetherTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/UsbTetherTile.java
@@ -35,7 +35,6 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 import com.android.internal.logging.MetricsLogger;
-import com.android.systemui.R;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.plugins.ActivityStarter;
@@ -46,6 +45,7 @@ import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.QsEventLogger;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.res.R;
 
 import javax.inject.Inject;
 
