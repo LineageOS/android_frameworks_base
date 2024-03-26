2e58cfcc4cad40f8ab9a86de0954f314b6489025
Oliver Scott <olivercscott@gmail.com>
2024-03-26 10:40:59 -0400

    fixup! SystemUI: Add Sync tile
    Change-Id: Ie3fb788ae737e365686da246c8c72750fb64c572

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SyncTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SyncTile.java
@@ -30,7 +30,7 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 import com.android.internal.logging.MetricsLogger;
-import com.android.systemui.R;
+import com.android.systemui.res.R;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.plugins.ActivityStarter;
