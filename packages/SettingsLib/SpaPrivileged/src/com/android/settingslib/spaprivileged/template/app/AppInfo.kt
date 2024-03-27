9cdd12a99ce0cb117dbbdc257777be3f2b19b102
Michael Bestas <mkbestas@gmail.com>
2024-03-27 19:31:10 +0200

    SettingsLib: Always show package name in installed app details
    Change-Id: I5c57c63a494bed90bf191234206fa0eb7477c83c

--- packages/SettingsLib/SpaPrivileged/src/com/android/settingslib/spaprivileged/template/app/AppInfo.kt
+++ packages/SettingsLib/SpaPrivileged/src/com/android/settingslib/spaprivileged/template/app/AppInfo.kt
@@ -36,7 +36,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.Dp
-import com.android.settingslib.development.DevelopmentSettingsEnabler
 import com.android.settingslib.spa.framework.compose.rememberDrawablePainter
 import com.android.settingslib.spa.framework.theme.SettingsDimension
 import com.android.settingslib.spa.framework.theme.isSpaExpressiveEnabled
@@ -119,17 +118,15 @@ class AppInfoProvider(private val packageInfo: PackageInfo) {
     }
 
     @Composable
-    fun FooterAppVersion(showPackageName: Boolean = rememberIsDevelopmentSettingsEnabled()) {
+    fun FooterAppVersion() {
         val context = LocalContext.current
         val footer =
-            remember(packageInfo, showPackageName) {
+            remember(packageInfo) {
                 val list = mutableListOf<String>()
                 packageInfo.versionNameBidiWrapped?.let {
                     list += context.getString(R.string.version_text, it)
                 }
-                if (showPackageName) {
-                    list += packageInfo.packageName
-                }
+                list += packageInfo.packageName
                 list.joinToString(separator = System.lineSeparator())
             }
         if (footer.isBlank()) return
@@ -137,12 +134,6 @@ class AppInfoProvider(private val packageInfo: PackageInfo) {
         Box(modifier = Modifier.padding(SettingsDimension.footerPadding)) {
             CopyableBody(footer)
         }
-    }
-
-    @Composable
-    private fun rememberIsDevelopmentSettingsEnabled(): Boolean {
-        val context = LocalContext.current
-        return remember { DevelopmentSettingsEnabler.isDevelopmentSettingsEnabled(context) }
     }
 
     private companion object {
