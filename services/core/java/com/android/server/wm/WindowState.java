fe760134bfa7c0950b3474a3e5abfa0a3b7e53a7
Linus Tufvesson <lus@google.com>
2024-04-29 16:32:15 +0200

    Hide SAW subwindows
    .. when top window is hidden through Window#setHideOverlayWindows
    
    Bug: 318683640
    Test: atest CtsWindowManagerDeviceWindow:HideOverlayWindowsTest
    Flag: EXEMPT securityfix
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:c37bc9147086f497ac7b1595083836014f524d5f)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:89bc634cb534b8e0ffd798ac9f9f89ac1be0f785)
    Merged-In: If19240f5aec2e048de80d75cbbdc00be47622d7f
    Change-Id: If19240f5aec2e048de80d75cbbdc00be47622d7f

--- services/core/java/com/android/server/wm/WindowState.java
+++ services/core/java/com/android/server/wm/WindowState.java
@@ -2635,8 +2635,9 @@ class WindowState extends WindowContainer<WindowState> implements WindowManagerP
     }
 
     void setForceHideNonSystemOverlayWindowIfNeeded(boolean forceHide) {
+        final int baseType = getBaseType();
         if (mOwnerCanAddInternalSystemWindow
-                || (!isSystemAlertWindowType(mAttrs.type) && mAttrs.type != TYPE_TOAST)) {
+                || (!isSystemAlertWindowType(baseType) && baseType != TYPE_TOAST)) {
             return;
         }
         if (mForceHideNonSystemOverlayWindow == forceHide) {
