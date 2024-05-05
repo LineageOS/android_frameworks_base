429210f1c6ecd327c4603228a5111aae1fa5a783
Bruno Martins <bgcngm@gmail.com>
2024-05-05 12:44:41 +0100

    BatteryMeterView: Hide icon on battery-less devices
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Joseph Annareddy <javelinanddart@gmail.com>
    Change-Id: I96a53e019ae7686d1af4ed42b9e1624fd13f4976

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryControllerImpl.java
@@ -91,6 +91,7 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
 
     protected int mLevel;
     protected boolean mPluggedIn;
+    protected boolean mPresent;
     private int mPluggedChargingSource;
     protected boolean mCharging;
     private boolean mStateUnknown = false;
@@ -177,6 +178,7 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
         ipw.print("mHasReceivedBattery="); ipw.println(mHasReceivedBattery);
         ipw.print("mLevel="); ipw.println(mLevel);
         ipw.print("mPluggedIn="); ipw.println(mPluggedIn);
+        ipw.print("mPresent="); ipw.println(mPresent);
         ipw.print("mCharging="); ipw.println(mCharging);
         ipw.print("mCharged="); ipw.println(mCharged);
         ipw.print("mIsBatteryDefender="); ipw.println(mIsBatteryDefender);
@@ -227,6 +229,7 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
         cb.onWirelessChargingChanged(mWirelessCharging);
         cb.onIsBatteryDefenderChanged(mIsBatteryDefender);
         cb.onIsIncompatibleChargingChanged(mIsIncompatibleCharging);
+        cb.onBatteryPresentChanged(mPresent);
     }
 
     @Override
@@ -264,6 +267,11 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
             }
 
             boolean present = intent.getBooleanExtra(EXTRA_PRESENT, true);
+            if (present != mPresent) {
+                mPresent = present;
+                fireBatteryPresentChanged();
+            }
+
             boolean unknown = !present;
             if (unknown != mStateUnknown) {
                 mStateUnknown = unknown;
@@ -334,6 +342,11 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
     @Override
     public boolean isPluggedIn() {
         return mPluggedIn;
+    }
+
+    @Override
+    public boolean isPresent() {
+        return mPresent;
     }
 
     @Override
@@ -483,6 +496,11 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
     private void fireIsIncompatibleChargingChanged() {
         dispatchSafeChange(
                 (callback) -> callback.onIsIncompatibleChargingChanged(mIsIncompatibleCharging));
+    }
+
+    private void fireBatteryPresentChanged() {
+        dispatchSafeChange(
+                (callback) -> callback.onBatteryPresentChanged(mPresent));
     }
 
     @Override
