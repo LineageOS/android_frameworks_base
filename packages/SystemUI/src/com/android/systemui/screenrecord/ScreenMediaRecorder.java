41cbb114463908b80e3dd9ba85c1db9b9a3b0c66
Michael Bestas <mkbestas@lineageos.org>
2024-06-16 03:17:45 +0300

    fixup! Screenrecord: Allow limiting the max framerate
    Change-Id: I44c8d1d3555aecc55bc91204cf9eb8fe871c7a63

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
@@ -55,7 +55,6 @@ import android.view.WindowManager;
 
 import com.android.internal.R;
 import com.android.systemui.mediaprojection.MediaProjectionCaptureTarget;
-import com.android.systemui.res.R;
 
 import java.io.Closeable;
 import java.io.File;
@@ -117,7 +116,7 @@ public class ScreenMediaRecorder extends MediaProjection.Callback {
         mListener = listener;
         mAudioSource = audioSource;
         mMaxRefreshRate = mContext.getResources().getInteger(
-                R.integer.config_screenRecorderMaxFramerate);
+                com.android.systemui.res.R.integer.config_screenRecorderMaxFramerate);
         mAvcProfileLevel = mContext.getResources().getString(
                 R.string.config_screenRecorderAVCProfileLevel);
     }
