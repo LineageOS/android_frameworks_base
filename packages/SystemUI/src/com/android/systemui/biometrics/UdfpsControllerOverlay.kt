35fc2f2a8d91a28263386e0e6ab1d1e6e6e5fbb3
pjgowtham <pjgowtham@gmail.com>
2024-10-15 21:05:25 +0530

    SystemUI: Remove udfps fwk dimming support
    This will be replaced with a superior solution.
    
    Change-Id: I32ddad5d5cd433b9a53d6e2a5880d82c185d7f6d

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
@@ -158,8 +158,6 @@ class UdfpsControllerOverlay @JvmOverloads constructor(
 
     private var overlayTouchListener: TouchExplorationStateChangeListener? = null
 
-    private val frameworkDimming = context.getResources().getBoolean(
-        R.bool.config_udfpsFrameworkDimming)
     private val coreLayoutParams = WindowManager.LayoutParams(
         WindowManager.LayoutParams.TYPE_NAVIGATION_BAR_PANEL,
         0 /* flags set in computeLayoutParams() */,
@@ -171,22 +169,11 @@ class UdfpsControllerOverlay @JvmOverloads constructor(
         layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
         flags = (Utils.FINGERPRINT_OVERLAY_LAYOUT_PARAM_FLAGS or
                 WindowManager.LayoutParams.FLAG_SPLIT_TOUCH)
-        if (frameworkDimming) {
-            flags = flags or WindowManager.LayoutParams.FLAG_DIM_BEHIND
-        }
         privateFlags = WindowManager.LayoutParams.PRIVATE_FLAG_TRUSTED_OVERLAY
-        dimAmount = 0.0f
         // Avoid announcing window title.
         accessibilityTitle = " "
         inputFeatures = WindowManager.LayoutParams.INPUT_FEATURE_SPY
     }
-
-    var dimAmount
-        get() = coreLayoutParams.dimAmount
-        set(value) {
-            coreLayoutParams.dimAmount = value
-            windowManager.updateViewLayout(getTouchOverlay(), coreLayoutParams)
-        }
 
     /** If the overlay is currently showing. */
     val isShowing: Boolean
