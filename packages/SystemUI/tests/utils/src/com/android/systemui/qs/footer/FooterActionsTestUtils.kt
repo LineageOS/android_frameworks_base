00333b7c7154aab04be671a0d8c63bba563024c6
Tommy Webb <tommy@calyxinstitute.org>
2024-12-12 22:13:04 +0000

    Quick Settings power menu uses keyguard show state
    Issue: calyxos#2874
    Change-Id: I6062fb3f66b19f498c0ea4fb7d253615073888c2

--- packages/SystemUI/tests/utils/src/com/android/systemui/qs/footer/FooterActionsTestUtils.kt
+++ packages/SystemUI/tests/utils/src/com/android/systemui/qs/footer/FooterActionsTestUtils.kt
@@ -56,6 +56,7 @@ import com.android.systemui.settings.UserTracker
 import com.android.systemui.statusbar.policy.DeviceProvisionedController
 import com.android.systemui.statusbar.policy.FakeSecurityController
 import com.android.systemui.statusbar.policy.FakeUserInfoController
+import com.android.systemui.statusbar.policy.KeyguardStateController
 import com.android.systemui.statusbar.policy.SecurityController
 import com.android.systemui.statusbar.policy.UserInfoController
 import com.android.systemui.statusbar.policy.UserSwitcherController
@@ -123,6 +124,7 @@ class FooterActionsTestUtils(
         metricsLogger: MetricsLogger = FakeMetricsLogger(),
         uiEventLogger: UiEventLogger = UiEventLoggerFake(),
         deviceProvisionedController: DeviceProvisionedController = mock(),
+        keyguardStateController: KeyguardStateController = mock(),
         qsSecurityFooterUtils: QSSecurityFooterUtils = mock(),
         fgsManagerController: FgsManagerController = mock(),
         userSwitcherInteractor: UserSwitcherInteractor = mock(),
@@ -139,6 +141,7 @@ class FooterActionsTestUtils(
             metricsLogger,
             uiEventLogger,
             deviceProvisionedController,
+            keyguardStateController,
             qsSecurityFooterUtils,
             fgsManagerController,
             userSwitcherInteractor,
