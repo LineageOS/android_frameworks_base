f52a4b716f1cfb03def202a91d10d78d054c568a
LuK1337 <priv.luk@gmail.com>
2024-12-28 17:32:43 +0100

    SystemUI: Don't remove keyguard slice view
    Before this change, keyguard slice view would disappear after rotation,
    breaking notification stack scroll layout alignment while at it.
    
    Change-Id: I78a1b40a6aa6a9bb31ddc774e024b6b356e2a39c

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
@@ -137,8 +137,5 @@ constructor(
         }
     }
 
-    override fun removeViews(constraintLayout: ConstraintLayout) {
-        if (smartspaceController.isEnabled) return
-        constraintLayout.removeView(R.id.keyguard_slice_view)
-    }
+    override fun removeViews(constraintLayout: ConstraintLayout) {}
 }
