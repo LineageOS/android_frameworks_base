0fd2f5b96933fda9d9aac56b0f2ddb4dc5372bc9
Ido Ben-Hur <idoybh2@gmail.com>
2025-04-02 11:35:24 +0300

    SystemUI: ToastUI: Fixup toast theme again
    AOSP fixed the icon view in A15/QPR2 but they still have the theme broken
    Re-apply our old fix
    
    Change-Id: I724eedc81f6c97e0cc98672b4136a0b3bcbcf525

--- packages/SystemUI/src/com/android/systemui/toast/ToastUI.java
+++ packages/SystemUI/src/com/android/systemui/toast/ToastUI.java
@@ -135,7 +135,7 @@ public class ToastUI implements
                 return;
             }
             Context displayContext = context.createDisplayContext(display);
-            mToast = mToastFactory.createToast(mContext, displayContext /* sysuiContext */, text,
+            mToast = mToastFactory.createToast(mContext, mContext /* sysuiContext */, text,
                     packageName, userHandle.getIdentifier(), mOrientation);
 
             if (mToast.getInAnimation() != null) {
