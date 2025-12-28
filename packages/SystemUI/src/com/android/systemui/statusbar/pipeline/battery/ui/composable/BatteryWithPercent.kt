6ae3b76b9aed3c1ac2418fc0c5c6f4c95c72c9ec
LuK1337 <priv.luk@gmail.com>
2025-12-28 22:49:08 +0100

    SystemUI: Bring back good ol' circle battery style once again
    Change-Id: I8547891db0cecfaac703a049148c666b2b9940c8

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/battery/ui/composable/BatteryWithPercent.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/battery/ui/composable/BatteryWithPercent.kt
@@ -37,6 +37,7 @@ import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import com.android.systemui.statusbar.phone.domain.interactor.IsAreaDark
+import com.android.systemui.statusbar.pipeline.battery.data.repository.BatteryRepository
 import com.android.systemui.statusbar.pipeline.battery.shared.ui.BatteryColors
 import com.android.systemui.statusbar.pipeline.battery.ui.viewmodel.BatteryViewModel
 
@@ -45,7 +46,6 @@ fun BatteryWithPercent(
     viewModel: BatteryViewModel,
     isDarkProvider: () -> IsAreaDark,
     modifier: Modifier = Modifier,
-    showIcon: Boolean = true,
     showPercent: Boolean = true,
     showEstimate: Boolean = false,
 ) {
@@ -86,7 +86,7 @@ fun BatteryWithPercent(
         horizontalArrangement = Arrangement.spacedBy(4.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        if (showIcon) {
+        if (viewModel.batteryIconStyle != BatteryRepository.ICON_STYLE_TEXT) {
             UnifiedBattery(
                 viewModel = viewModel,
                 isDarkProvider = isDarkProvider,
