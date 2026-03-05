9cf6f3a1839960f6cf1ec93ef64e0e0a45b00f54
Barış Kaya <bkaya@google.com>
2026-03-05 15:41:02 +0000

    Catch OutOfMemoryError while parsing xml in GameManagerService
    This part of the GameManagerService would fail if the given xml was too big (>1 GB). Catching the OutOfMemoryError allows the process to continue instead of crashing.
    
    Test: Tested on 25q4-release branch with the apk and repro steps provided on the bug
    Bug: b/480975165
    Flag: EXEMPT BUGFIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:d3a1cc8820b17b04723df14b9e5df02473f440d6
    Merged-In: I7142bde41c0583022cacbf076b5ba6acbbcfd3c2
    Change-Id: I7142bde41c0583022cacbf076b5ba6acbbcfd3c2

--- services/core/java/com/android/server/app/GameManagerService.java
+++ services/core/java/com/android/server/app/GameManagerService.java
@@ -619,7 +619,8 @@ public final class GameManagerService extends IGameManagerService.Stub {
                         array.recycle();
                     }
                 }
-            } catch (NameNotFoundException | XmlPullParserException | IOException ex) {
+            } catch (NameNotFoundException | XmlPullParserException | IOException
+                     | OutOfMemoryError ex) {
                 // set flag back to default values when parsing fails
                 mPerfModeOverridden = false;
                 mBatteryModeOverridden = false;
