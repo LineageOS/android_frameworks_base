a4d3e13d5556ab8894074e62f3709595e2c93539
Jiaming Liu <jiamingliu@google.com>
2026-03-27 04:40:17 +0000

    Add Task-level input sink to block cross-task tap pass-through
    When there are TaskFragments that do not fill the entire Task, there
    could be security concerns for touches passing through the unfilled area
    in the Task and reaching the Task below. The risk could also happens in
    adjacent TaskFragments.
    Therefore, the change add a Task-level input sink to avoid the
    cross-task touch pass through.
    
    Bug: 490083213
    Test: atest WmTests:TaskInputSinkTests
    Flag: EXEMPT CVE_FIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:4373cf896dce49ba35ba2351d6bb974415bf9737
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:18db874796f662ff20c49ee9f7cdc3d5dd33e6c6
    Merged-In: Icd0774a5ec642d57a06038e2635cd22ad9ab6cd6
    Change-Id: Icd0774a5ec642d57a06038e2635cd22ad9ab6cd6

--- services/core/java/com/android/server/wm/Task.java
+++ services/core/java/com/android/server/wm/Task.java
@@ -423,6 +423,12 @@ class Task extends TaskFragment {
     @Nullable
     DecorSurfaceContainer mDecorSurfaceContainer;
 
+    // A surface that could intercept touches to avoid cross-Task passthrough. This is applied to
+    // non-organizer-created Task only and created when the first TaskFragment is added to the
+    // children of this Task.
+    @Nullable
+    TaskInputSink mTaskInputSink = null;
+
     static final int LAYER_RANK_INVISIBLE = -1;
     // Ranking (from top) of this task among all visible tasks. (-1 means it's not visible)
     // This number will be assigned when we evaluate OOM scores for all visible tasks.
@@ -1487,6 +1493,11 @@ class Task extends TaskFragment {
         // passed from Task constructor.
         final TaskFragment childTaskFrag = child.asTaskFragment();
         if (childTaskFrag != null && childTaskFrag.asTask() == null) {
+            if (!mCreatedByOrganizer && mTaskInputSink == null) {
+                // The TaskInputSink is created for the Task when the first TaskFragment is added
+                // to the Task's children and the Task is not created by an organizer.
+                mTaskInputSink = new TaskInputSink(this);
+            }
             if (childTaskFrag.mTaskFragmentOrganizerProcessName != null
                     && mTaskFragmentHostProcessName == null) {
                 mTaskFragmentHostUid = childTaskFrag.mTaskFragmentOrganizerUid;
@@ -2740,6 +2751,9 @@ class Task extends TaskFragment {
         if (mDecorSurfaceContainer != null) {
             mDecorSurfaceContainer.release();
         }
+        if (mTaskInputSink != null) {
+            mTaskInputSink.releaseSurfaceControl();
+        }
 
         super.removeImmediately();
         mRemoving = false;
@@ -3284,6 +3298,11 @@ class Task extends TaskFragment {
 
     @Override
     void prepareSurfaces() {
+        // Input sink surface is not a part of animation, so apply in a steady state
+        // (non-sync) with pending transaction.
+        if (mTaskInputSink != null && isVisible() && mSyncState == SYNC_STATE_NONE) {
+            mTaskInputSink.applyChangesToSurfaceIfChanged(getPendingTransaction());
+        }
         mDimmer.resetDimStates();
         super.prepareSurfaces();
 
@@ -3337,7 +3356,6 @@ class Task extends TaskFragment {
         super.dump(pw, prefix, dumpAll);
         mAnimatingActivityRegistry.dump(pw, "AnimatingApps:", prefix);
     }
-
 
     /**
      * Fills in a {@link TaskInfo} with information from this task. Note that the base intent in the
