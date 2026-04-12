d965af9e3ab7bcb77131c0199e435efb73c06c4e
Himanshu Arora <hmarora@google.com>
2026-04-12 09:17:01 +0000

    RESTRICT AUTOMERGE Filter ignorable Unicode codepoints in ExternalStorageProvider
    Normalize directory paths by filtering out ignorable Unicode codepoints before performing block checks.
    
    Bug: 475232460
    Test: atest ExternalStorageProviderTest
    Flag: EXEMPT BUGFIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:31d6e9390fbb33ecdfde504e17562c48e155de22
    Merged-In: Ib955a2884c9c018ab717d0ecbbdd5847b90ef23c
    Change-Id: Ib955a2884c9c018ab717d0ecbbdd5847b90ef23c

--- packages/ExternalStorageProvider/src/com/android/externalstorage/ExternalStorageProvider.java
+++ packages/ExternalStorageProvider/src/com/android/externalstorage/ExternalStorageProvider.java
@@ -25,6 +25,8 @@ import android.content.UriPermission;
 import android.database.Cursor;
 import android.database.MatrixCursor;
 import android.database.MatrixCursor.RowBuilder;
+import android.icu.lang.UCharacter;
+import android.icu.lang.UProperty;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.Bundle;
@@ -61,6 +63,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.Normalizer;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -399,14 +402,15 @@ public class ExternalStorageProvider extends FileSystemProvider {
         // This the root's path will be just an empty string.
         final String path = getPathFromDocId(documentId);
 
+        final String normalizedPath = normalizeAndFilterDefaultIgnorableCodepoints(path);
         // Block the root of the storage
-        if (path.isEmpty()) {
+        if (normalizedPath.isEmpty()) {
             return true;
         }
 
         // Block /Download/ and /Android/ folders from the tree.
-        if (equalIgnoringCase(path, Environment.DIRECTORY_DOWNLOADS) ||
-                equalIgnoringCase(path, Environment.DIRECTORY_ANDROID)) {
+        if (equalIgnoringCase(normalizedPath, Environment.DIRECTORY_DOWNLOADS)
+                || equalIgnoringCase(normalizedPath, Environment.DIRECTORY_ANDROID)) {
             return true;
         }
 
@@ -834,5 +838,48 @@ public class ExternalStorageProvider extends FileSystemProvider {
 
     private static boolean equalIgnoringCase(@NonNull String a, @NonNull String b) {
         return TextUtils.equals(a.toLowerCase(Locale.ROOT), b.toLowerCase(Locale.ROOT));
+    }
+
+    /**
+     * Normalizes the given path to NFD form and removes all default ignorable Unicode characters.
+     * These include characters (e.g., invisible zero-width spaces) that are ignored by the lower
+     * file system, but can be exploited by malicious apps to bypass path-based regex checks.
+     *
+     * <p>Wholesale copied from MediaProvider at the path:
+     * //packages/providers/MediaProvider/src/com/android/providers/media/util/FileUtils.java
+     *
+     * @param path the input file path, possibly containing invisible Unicode characters
+     * @return a normalized path string with ignorable characters removed
+     */
+    @VisibleForTesting
+    static String normalizeAndFilterDefaultIgnorableCodepoints(String path) {
+        // Nothing to normalize.
+        if (path == null || path.isEmpty()) {
+            return path;
+        }
+
+        path = Normalizer.normalize(path, Normalizer.Form.NFD);
+        final int[] codePoints = path.codePoints().toArray();
+
+        boolean hasIgnorableCodepoints = false;
+        for (int codePoint : codePoints) {
+            if (UCharacter.hasBinaryProperty(codePoint, UProperty.DEFAULT_IGNORABLE_CODE_POINT)) {
+                hasIgnorableCodepoints = true;
+                break;
+            }
+        }
+        // Input is already normalized.
+        if (!hasIgnorableCodepoints) {
+            return path;
+        }
+
+        // Remove default ignorable code points.
+        StringBuilder normalizedPath = new StringBuilder(codePoints.length);
+        for (int codePoint : codePoints) {
+            if (!UCharacter.hasBinaryProperty(codePoint, UProperty.DEFAULT_IGNORABLE_CODE_POINT)) {
+                normalizedPath.appendCodePoint(codePoint);
+            }
+        }
+        return normalizedPath.toString();
     }
 }
