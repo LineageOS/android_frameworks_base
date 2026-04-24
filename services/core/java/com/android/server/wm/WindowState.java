b18281074fbfeea81d3d47d857b24c640f36b012
Candice Lo <chihtinglo@google.com>
2026-04-24 07:56:29 +0000

    Fix using the base type for checking
    When a window is a sub-window, its `mAttr.type` won't be captured by `isSystemAlertWindowType`. Therefore, the window could run into the early return condition and skip the hide/show in `setHiddenWhileSuspended` when its top parent window is a system alert window and the app is suspended, which will lead to security concerns. Therefore, we check the base type of the window instead.
    
    Bug: 493714520
    Test: atest WindowStateTests
    Flag: EXEMPT BUGFIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:617b18b15e675266e52fd88b69506ad7d5f86778
    Merged-In: Ia0c154fca56db7c0c89abc2ea09af600d4990323
    Change-Id: Ia0c154fca56db7c0c89abc2ea09af600d4990323

--- services/core/java/com/android/server/wm/WindowState.java
+++ services/core/java/com/android/server/wm/WindowState.java
@@ -3232,8 +3232,9 @@ class WindowState extends WindowContainer<WindowState> implements WindowManagerP
     }
 
     void setHiddenWhileSuspended(boolean hide) {
+        final int baseType = getBaseType();
         if (mOwnerCanAddInternalSystemWindow
-                || (!isSystemAlertWindowType(mAttrs.type) && mAttrs.type != TYPE_TOAST)) {
+                || (!isSystemAlertWindowType(baseType) && baseType != TYPE_TOAST)) {
             return;
         }
         if (mHiddenWhileSuspended == hide) {
