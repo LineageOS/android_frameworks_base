a0ce1ae95c4f98a9c3a57cf322b9d7fc39c26b7a
Duy Truong <duytruong@google.com>
2026-05-04 16:06:15 -0700

    Add bracket checking support to SQLiteTokenizer
    Add OPTION_CHECK_BRACKETS to SQLiteTokenizer to allow checking for
    unbalanced brackets. This can be used to prevent SQL injection where
    user-provided selection strings attempt to break out of parentheses.
    
    Bug: 393377017
    Test: atest SQLiteTokenizerTest
    Flag: EXEMPT BUGFIX
    
    Autogenerated by MendIt (go/androidmendit)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:f80be5110a5652541944503569c9e5d69a867c43
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:89242a44a7bbb503aacdc6b567a92c717589b3c2
    Merged-In: I84dd342b7ee2328413c9aef5734d7ffefafa4d85
    Change-Id: I84dd342b7ee2328413c9aef5734d7ffefafa4d85

--- core/java/android/database/sqlite/SQLiteTokenizer.java
+++ core/java/android/database/sqlite/SQLiteTokenizer.java
@@ -70,6 +70,12 @@ public class SQLiteTokenizer {
     public static final int OPTION_TOKEN_ONLY = 1 << 0;
 
     /**
+     * Checks brackets; an unbalanced expression or one that attempts to close an
+     * earlier bracket will result in an exception.
+     */
+    public static final int OPTION_CHECK_BRACKETS = 1 << 1;
+
+    /**
      * Tokenize the given SQL, returning the list of each encountered token.
      *
      * @throws IllegalArgumentException if invalid SQL is encountered.
@@ -86,11 +92,13 @@ public class SQLiteTokenizer {
      *
      * @throws IllegalArgumentException if invalid SQL is encountered.
      */
-    public static void tokenize(@Nullable String sql, int options, Consumer<String> checker) {
+    public static void tokenize(@Nullable String sql, int options,
+            @Nullable Consumer<String> checker) {
         if (sql == null) {
             return;
         }
         int pos = 0;
+        int level = 0;
         final int len = sql.length();
         while (pos < len) {
             final char ch = peek(sql, pos);
@@ -105,7 +113,9 @@ public class SQLiteTokenizer {
                 final int end = pos;
 
                 final String token = sql.substring(start, end);
-                checker.accept(token);
+                if (checker != null) {
+                    checker.accept(token);
+                }
 
                 continue;
             }
@@ -142,9 +152,11 @@ public class SQLiteTokenizer {
                     } else {
                         token = tokenUnquoted;
                     }
-                    checker.accept(token);
+                    if (checker != null) {
+                        checker.accept(token);
+                    }
                 } else {
-                    if ((options &= OPTION_TOKEN_ONLY) != 0) {
+                    if ((options & OPTION_TOKEN_ONLY) != 0) {
                         throw genException("Non-token detected", sql);
                     }
                 }
@@ -164,10 +176,12 @@ public class SQLiteTokenizer {
 
                 final String token = sql.substring(quoteStart + 1, quoteEnd);
 
-                checker.accept(token);
+                if (checker != null) {
+                    checker.accept(token);
+                }
                 continue;
             }
-            if ((options &= OPTION_TOKEN_ONLY) != 0) {
+            if ((options & OPTION_TOKEN_ONLY) != 0) {
                 throw genException("Non-token detected", sql);
             }
 
@@ -199,10 +213,31 @@ public class SQLiteTokenizer {
                 throw genException("Semicolon is not allowed", sql);
             }
 
+            if (ch == '(') {
+                pos++;
+                level++;
+
+                continue;
+            }
+
+            if (ch == ')') {
+                pos++;
+                level--;
+                if (level < 0 && (options & OPTION_CHECK_BRACKETS) != 0) {
+                    throw genException("Unbalanced brackets", sql);
+                }
+
+                continue;
+            }
+
             // For this purpose, we can simply ignore other characters.
             // (Note it doesn't handle the X'' literal properly and reports this X as a token,
             // but that should be fine...)
             pos++;
+        }
+
+        if (level != 0 && (options & OPTION_CHECK_BRACKETS) != 0) {
+            throw genException("Unbalanced brackets", sql);
         }
     }
 
