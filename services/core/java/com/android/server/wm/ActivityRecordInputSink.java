7578cbf03aab07730dd7b858a9eddeed2c588768
chihtinglo <chihtinglo@google.com>
2026-05-21 10:45:15 +0000

    Move input sink on top of the Activity when needed
    An Activity below the TaskFragments could be exposed to user touches
    since (1) the upper TaskFragments do not fill the entire Task, and (2)
    the bounds of the Activity within the TaskFragment and the Activity
    below in the hierarchy can be different.
    Therefore, we will need an input sink above the Activity to block the
    touches between the TaskFragments. This change could avoid the risk of
    tapjecking within the Task.
    
    Bug: 490083213
    Test: atest WmTests:ActivityRecordInputSinkTests
    Test: atest WmTests:ActivityRecordInputSinkWithTaskFragmentTests
    Test: atest CtsWindowManagerDeviceActivity:ActivityRecordInputSinkTests
    Test: atest CtsWindowManagerDeviceInput:WindowUntrustedTouchTest
    Test: atest CtsWindowManagerSdk35TestCases:ActivityRecordInputSinkTestsSdk35
    Test: atest CtsWindowManagerSdk35TestCases:WindowUntrustedTouchTestSdk35
    Flag: EXEMPT CVE_FIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:42b79155a6b7ffdf1cfc2552d854b9cd9692d381
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:c4d386118c62c352a0189ed48fecfcf8105894aa
    Merged-In: Ia9ea05e190cb809516de5765262b8cbee7676769
    Change-Id: Ia9ea05e190cb809516de5765262b8cbee7676769

--- services/core/java/com/android/server/wm/ActivityRecordInputSink.java
+++ services/core/java/com/android/server/wm/ActivityRecordInputSink.java
@@ -60,6 +60,7 @@ class ActivityRecordInputSink {
 
     private InputWindowHandleWrapper mInputWindowHandleWrapper;
     private SurfaceControl mSurfaceControl;
+    private boolean mBlockTouchesFromTop;
 
     ActivityRecordInputSink(ActivityRecord activityRecord, ActivityRecord sourceRecord,
             boolean appOptInTouchPassThrough) {
@@ -84,30 +85,52 @@ class ActivityRecordInputSink {
     }
 
     public void applyChangesToSurfaceIfChanged(SurfaceControl.Transaction transaction) {
-        InputWindowHandleWrapper inputWindowHandleWrapper = getInputWindowHandleWrapper();
+        final boolean blockTouchesFromTop = isBlockingTouchesFromTopNeeded();
+        final InputWindowHandleWrapper inputWindowHandleWrapper =
+                getInputWindowHandleWrapper(blockTouchesFromTop);
         if (mSurfaceControl == null) {
-            mSurfaceControl = createSurface(transaction);
+            mSurfaceControl = createSurface();
+            updateInputSinkZOrder(transaction, blockTouchesFromTop);
+        } else if (mBlockTouchesFromTop != blockTouchesFromTop) {
+            updateInputSinkZOrder(transaction, blockTouchesFromTop);
         }
         if (inputWindowHandleWrapper.isChanged()) {
             inputWindowHandleWrapper.applyChangesToSurface(transaction, mSurfaceControl);
         }
     }
 
-    private SurfaceControl createSurface(SurfaceControl.Transaction t) {
+    private SurfaceControl createSurface() {
         SurfaceControl surfaceControl = mActivityRecord.makeChildSurface(null)
                 .setName(mName)
                 .setHidden(false)
                 .setCallsite("ActivityRecordInputSink.createSurface")
                 .build();
-        // Put layer below all siblings (and the parent surface too)
-        t.setLayer(surfaceControl, Integer.MIN_VALUE);
         return surfaceControl;
     }
 
-    private InputWindowHandleWrapper getInputWindowHandleWrapper() {
+    private void updateInputSinkZOrder(
+            SurfaceControl.Transaction t, boolean blockTouchesFromTop) {
+        // Put the layer above all siblings if the touches need to be blocked from top, otherwise,
+        // put layer below all siblings (and the parent surface too)
+        t.setLayer(mSurfaceControl, blockTouchesFromTop ? Integer.MAX_VALUE : Integer.MIN_VALUE);
+        mBlockTouchesFromTop = blockTouchesFromTop;
+    }
+
+    private InputWindowHandleWrapper getInputWindowHandleWrapper(
+            boolean blockTouchesFromTop) {
         if (mInputWindowHandleWrapper == null) {
             mInputWindowHandleWrapper = new InputWindowHandleWrapper(createInputWindowHandle());
         }
+        mInputWindowHandleWrapper.setDisplayId(mActivityRecord.getDisplayId());
+
+        if (blockTouchesFromTop) {
+            // Early return if the caller explicitly specifies that an input sink is required on top
+            // of the Activity to intercept the touch events.
+            mInputWindowHandleWrapper.setInputConfigMasked(
+                    InputConfig.DEFAULT, InputConfig.NOT_TOUCHABLE);
+            return mInputWindowHandleWrapper;
+        }
+
         // Don't block touches from passing through to an activity below us in the same task, if
         // that activity is either from the same uid or if that activity has launched an activity
         // in our uid.
@@ -123,9 +146,9 @@ class ActivityRecordInputSink {
                     InputConfig.NOT_TOUCHABLE);
         } else {
             // Set to touchable, so it can block by intercepting the touch events.
-            mInputWindowHandleWrapper.setInputConfigMasked(0, InputConfig.NOT_TOUCHABLE);
+            mInputWindowHandleWrapper.setInputConfigMasked(
+                    InputConfig.DEFAULT, InputConfig.NOT_TOUCHABLE);
         }
-        mInputWindowHandleWrapper.setDisplayId(mActivityRecord.getDisplayId());
         return mInputWindowHandleWrapper;
     }
 
@@ -139,6 +162,58 @@ class ActivityRecordInputSink {
         inputWindowHandle.ownerUid = WindowManagerService.MY_UID;
         inputWindowHandle.inputConfig = InputConfig.NOT_FOCUSABLE | InputConfig.NO_INPUT_CHANNEL;
         return inputWindowHandle;
+    }
+
+    /**
+     * A non-embedded Activity below the TaskFragments could be exposed to user touches since
+     * (1) the upper TaskFragments do not fill the entire Task, and (2) the bounds of the Activity
+     * within the TaskFragment and the Activity below in the hierarchy can be different. Therefore,
+     * we move the input sink above for the top most Activity that is covered partially to block the
+     * touches. This could help avoid tapjacking happening within a Task.
+     *
+     * @return true if the ActivityRecord is visible to users, and it is below a TaskFragment with
+     * a different Uid.
+     */
+    private boolean isBlockingTouchesFromTopNeeded() {
+        if (!mActivityRecord.isVisibleRequested()
+                // Based on the trust model for embedded Activities, return early if this Activity
+                // is embedded.
+                || mActivityRecord.isEmbedded()
+                || mActivityRecord.inTransition()) {
+            return false;
+        }
+
+        final Task task = mActivityRecord.getTask();
+        if (task == null) {
+            return false;
+        }
+        final int indexOfActivityRecord = task.mChildren.indexOf(mActivityRecord);
+        if (indexOfActivityRecord < 0) {
+            return false;
+        }
+
+        // Traversing from bottom to top
+        for (int i = indexOfActivityRecord + 1; i < task.getChildCount(); i++) {
+            WindowContainer child = task.getChildAt(i);
+            if (!child.isVisibleRequested()) {
+                // If the child is not visible, it doesn't need to be considered for blocking
+                // touches from the top.
+                continue;
+            }
+
+            final TaskFragment tf = child.asTaskFragment();
+            if (tf != null) {
+                if (!mActivityRecord.isUid(tf.mTaskFragmentOrganizerUid)
+                        && tf.mTaskFragmentOrganizerUid != mActivityRecord.mAllowedTouchUid) {
+                    return true;
+                }
+            } else {
+                // We rely on the input sink of the visible Activity above to take care of the input
+                // handling. Therefore, returning false once we meet a child of a visible Activity.
+                return false;
+            }
+        }
+        return false;
     }
 
     void releaseSurfaceControl() {
