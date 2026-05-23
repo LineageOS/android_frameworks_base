efcf159d4d47b94a8cd9effe9ec3fdd51e40215b
Duy Truong <duytruong@google.com>
2026-05-23 01:14:32 -0700

    Use byte count rather than string length for NMS component name check
    Bug: 505067993
    Test: atest NotificationManagerServiceTest
    Flag: EXEMPT BUGFIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:9581befdf4db092b4ac5500b5146906c5dbc568c
    Merged-In: Ie1bf232340770e1237f80177bb0b86eabeda3635
    Change-Id: Ie1bf232340770e1237f80177bb0b86eabeda3635

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -5561,7 +5561,7 @@ public class NotificationManagerService extends SystemService {
                 boolean granted, boolean userSet) {
             Objects.requireNonNull(listener);
             checkNotificationListenerAccess();
-            if (granted && listener.flattenToString().length()
+            if (granted && listener.flattenToString().getBytes().length
                     > NotificationManager.MAX_SERVICE_COMPONENT_NAME_LENGTH) {
                 throw new IllegalArgumentException(
                         "Component name too long: " + listener.flattenToString());
