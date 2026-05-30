36949cdd36a6de5b778d865579004ecec9cb6361
LuK1337 <priv.luk@gmail.com>
2026-05-30 12:59:48 +0200

    PhoneWindowManager: Add partial screenshot action
    Change-Id: Ia9a529f5c4db793e76e382d7dd2ec68ee7eb189b

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -87,6 +87,8 @@ import static android.view.WindowManager.LayoutParams.TYPE_VOICE_INTERACTION_STA
 import static android.view.WindowManager.LayoutParams.TYPE_WALLPAPER;
 import static android.view.WindowManager.LayoutParams.isSystemAlertWindowType;
 import static android.view.WindowManager.ScreenshotSource.SCREENSHOT_KEY_OTHER;
+import static android.view.WindowManager.TAKE_SCREENSHOT_FULLSCREEN;
+import static android.view.WindowManager.TAKE_SCREENSHOT_SELECTED_REGION;
 import static android.view.WindowManagerGlobal.ADD_OKAY;
 import static android.view.WindowManagerGlobal.ADD_PERMISSION_DENIED;
 import static android.view.contentprotection.flags.Flags.createAccessibilityOverlayAppOpEnabled;
@@ -2340,7 +2342,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 triggerVirtualKeypress(KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
                 break;
             case SCREENSHOT:
-                takeScreenshot(SCREENSHOT_KEY_OTHER);
+                takeScreenshot(TAKE_SCREENSHOT_FULLSCREEN, SCREENSHOT_KEY_OTHER);
+                notifyKeyGestureCompleted(event, KeyGestureEvent.KEY_GESTURE_TYPE_TAKE_SCREENSHOT);
+                break;
+            case PARTIAL_SCREENSHOT:
+                takeScreenshot(TAKE_SCREENSHOT_SELECTED_REGION, SCREENSHOT_KEY_OTHER);
                 notifyKeyGestureCompleted(event, KeyGestureEvent.KEY_GESTURE_TYPE_TAKE_SCREENSHOT);
                 break;
             default:
@@ -7753,7 +7759,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
-    private void takeScreenshot(int source) {
-        mScreenshotHelper.takeScreenshot(source, mHandler, null);
+    private void takeScreenshot(int type, int source) {
+        mScreenshotHelper.takeScreenshot(type, source, mHandler, null);
     }
 }
