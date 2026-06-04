4dba1f4e9afb76a4cd350a3d41c1bf0372a40b37
Gabriel Roman <gabrielroman@google.com>
2026-06-04 01:55:28 -0700

    Truncate long device admin descriptions
    Limit the length of the description loaded from a device admin's
    manifest to a maximum of 8192 characters. This prevents potential issues
    with excessively long descriptions. Cherry picked the main CL to fix the
    order
    
    Bug: 492093013
    Test: manual
    Flag: EXEMPT BUGFIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:f1ed046d2c48a38b9a800ef6a86cc14d3ab312a3
    Security vulnerability cherry pick for https://b.corp.google.com/issues/492093013
    Topic: https://googleplex-android-review.git.corp.google.com/q/topic:%22b492093013%22
    #b/492093013
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:bace39bacf296b95a71f7bff1f87f68e39178874
    Merged-In: I06b621e37d97fd8554172c2446e2838dfaccc104
    Change-Id: I06b621e37d97fd8554172c2446e2838dfaccc104

--- core/java/android/content/pm/ComponentInfo.java
+++ core/java/android/content/pm/ComponentInfo.java
@@ -16,6 +16,7 @@
 
 package android.content.pm;
 
+import android.annotation.NonNull;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Service;
@@ -24,10 +25,14 @@ import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.res.Resources.NotFoundException;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Parcel;
+import android.text.TextUtils;
 import android.util.Printer;
+
+import java.util.Objects;
 
 /**
  * Base class containing information common to all application components
@@ -44,7 +49,7 @@ public class ComponentInfo extends PackageItemInfo {
      * part of.
      */
     public ApplicationInfo applicationInfo;
-    
+
     /**
      * The name of the process this component should run in.
      * From the "android:process" attribute or, if not set, the same
@@ -83,7 +88,7 @@ public class ComponentInfo extends PackageItemInfo {
      * attribute or, if not set, 0.
      */
     public int descriptionRes;
-    
+
     /**
      * Indicates whether or not this component may be instantiated.  Note that this value can be
      * overridden by the one in its parent {@link ApplicationInfo}.
@@ -104,6 +109,9 @@ public class ComponentInfo extends PackageItemInfo {
      * pattern or PIN).
      */
     public boolean directBootAware = false;
+
+    /** @hide */
+    public static final int MAX_SAFE_DESCRIPTION_LENGTH = 1024;
 
     public ComponentInfo() {
     }
@@ -145,18 +153,33 @@ public class ComponentInfo extends PackageItemInfo {
         return name;
     }
 
+    /** @hide */
+    public CharSequence loadDescription(@NonNull PackageManager pm) {
+        Objects.requireNonNull(pm);
+        if (descriptionRes != 0) {
+            try {
+                return TextUtils.trimToSize(
+                        pm.getText(packageName, descriptionRes, applicationInfo),
+                        MAX_SAFE_DESCRIPTION_LENGTH);
+            } catch (OutOfMemoryError e) {
+                throw new NotFoundException();
+            }
+        }
+        throw new NotFoundException();
+    }
+
     /**
      * Return whether this component and its enclosing application are enabled.
      */
     public boolean isEnabled() {
         return enabled && applicationInfo.enabled;
     }
-    
+
     /**
      * Return the icon resource identifier to use for this component.  If
      * the component defines an icon, that is used; else, the application
      * icon is used.
-     * 
+     *
      * @return The icon associated with this component.
      */
     public final int getIconResource() {
@@ -173,7 +196,7 @@ public class ComponentInfo extends PackageItemInfo {
     public final int getLogoResource() {
         return logo != 0 ? logo : applicationInfo.logo;
     }
-    
+
     /**
      * Return the banner resource identifier to use for this component. If the
      * component defines a banner, that is used; else, the application banner is
@@ -242,7 +265,7 @@ public class ComponentInfo extends PackageItemInfo {
         dest.writeInt(exported ? 1 : 0);
         dest.writeInt(directBootAware ? 1 : 0);
     }
-    
+
     protected ComponentInfo(Parcel source) {
         super(source);
         applicationInfo = ApplicationInfo.CREATOR.createFromParcel(source);
@@ -263,7 +286,7 @@ public class ComponentInfo extends PackageItemInfo {
     public Drawable loadDefaultIcon(PackageManager pm) {
         return applicationInfo.loadIcon(pm);
     }
-    
+
     /**
      * @hide
      */
@@ -280,7 +303,7 @@ public class ComponentInfo extends PackageItemInfo {
     protected Drawable loadDefaultLogo(PackageManager pm) {
         return applicationInfo.loadLogo(pm);
     }
-    
+
     /**
      * @hide
      */
