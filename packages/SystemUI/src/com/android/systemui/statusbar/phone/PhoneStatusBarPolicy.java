a27115f69ebbb8040a57650636a182a0f1f3f5b8
Jis G Jacob <studiokeys@blissroms.org>
2026-07-26 23:14:09 +0530

    SystemUI: Only show statusbar firewall icon for apps requesting INTERNET
    * PhoneStatusBarPolicy checks if an app's default network state is blocked
      and displays a status bar firewall icon. However, for applications that
      do not declare android.permission.INTERNET in their manifest (such as
      DeskClock), ConnectivityManager reports the network status as blocked.
    * This results in false-positive firewall icons whenever offline apps are
       opened while a network connection is active.
    
    * Fix this by verifying that the active package requested INTERNET
      permission in its manifest before displaying the firewall status
      bar icon.
    
    Change-Id: I95eca228ff9056dca168c75b9712f6dd9b342e44
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -29,9 +29,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
+import android.Manifest;
 import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.net.Network;
@@ -630,6 +632,34 @@ public class PhoneStatusBarPolicy
                                 final int uid = ActivityTaskManager.getService()
                                         .getLastResumedActivityUid();
                                 if (uid != Process.INVALID_UID) {
+                                    boolean requestsInternet = false;
+                                    PackageManager pm = mContext.getPackageManager();
+                                    String[] packages = pm.getPackagesForUid(uid);
+                                    if (packages != null) {
+                                        for (String pkg : packages) {
+                                            try {
+                                                PackageInfo pi = pm.getPackageInfo(
+                                                        pkg, PackageManager.GET_PERMISSIONS);
+                                                if (pi != null
+                                                        && pi.requestedPermissions != null) {
+                                                    for (String permission :
+                                                            pi.requestedPermissions) {
+                                                        if (Manifest.permission.INTERNET
+                                                                .equals(permission)) {
+                                                            requestsInternet = true;
+                                                            break;
+                                                        }
+                                                    }
+                                                }
+                                            } catch (PackageManager.NameNotFoundException e) {
+                                                // Ignore
+                                            }
+                                            if (requestsInternet) {
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    final boolean finalRequestsInternet = requestsInternet;
                                     mMainExecutor.execute(() -> {
                                         boolean isLauncher = false;
                                         List<ResolveInfo> homeActivities = mContext
@@ -651,6 +681,7 @@ public class PhoneStatusBarPolicy
                                         final boolean finalIsLauncher = isLauncher;
                                         final boolean showIcon;
                                         if (!finalIsLauncher
+                                                && finalRequestsInternet
                                                 && blocked
                                                 != ConnectivityManager.BLOCKED_REASON_NONE
                                                 && (!mKeyguardStateController.isShowing()
