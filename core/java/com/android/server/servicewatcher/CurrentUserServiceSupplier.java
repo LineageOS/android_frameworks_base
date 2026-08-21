03da1783cbaf5ad30abc86ceaafb6fc6a70ea1af
LuK1337 <priv.luk@gmail.com>
2026-08-21 11:14:15 +0200

    ServiceWatcher: Allow microG location provider to be installed in /data
    Tested with Offi app, it no longer gets stuck waiting for fused location.
    
    Also adb shell dumpsys location now shows two registered microG location
    providers:
        network provider:
          service: ProviderRequest[@+1d0h0m0s0ms, WorkSource{1000 android}]
          listeners:
            1000/android[GnssService]/87624516 Request[PASSIVE, minUpdateInterval=0, hiddenFromAppOps, WorkSource{1000 android}]
            1000/android[twilight]/E10B5C4B Request[@+1d0h0m0s0ms BALANCED, WorkSource{1000 android}]
          last location=null
          enabled=true
          allowed=true
          identity=10184/com.google.android.gms
          properties=ProviderProperties[powerUsage=Low, accuracy=Coarse]
          target service=10184/com.google.android.gms/org.microg.gms.location.provider.NetworkLocationProviderService@2
          connected=true
        fused provider:
          service: ProviderRequest[OFF]
          last location=null
          enabled=true
          allowed=true
          identity=10184/com.google.android.gms
          properties=ProviderProperties[powerUsage=Low, accuracy=Coarse]
          target service=10184/com.google.android.gms/org.microg.gms.location.provider.FusedLocationProviderService@2
          connected=true
    
    Fixes: https://github.com/LineageOS/issues/issues/317
    Change-Id: Ic756cbdc6708a976b5a5b7b408dcb8fc2d652526

--- core/java/com/android/server/servicewatcher/CurrentUserServiceSupplier.java
+++ core/java/com/android/server/servicewatcher/CurrentUserServiceSupplier.java
@@ -17,11 +17,15 @@
 package com.android.server.servicewatcher;
 
 import static android.content.pm.PackageManager.GET_META_DATA;
+import static android.content.pm.PackageManager.GET_SIGNING_CERTIFICATES;
 import static android.content.pm.PackageManager.MATCH_DIRECT_BOOT_AUTO;
 import static android.content.pm.PackageManager.MATCH_DIRECT_BOOT_AWARE;
 import static android.content.pm.PackageManager.MATCH_DIRECT_BOOT_UNAWARE;
 import static android.content.pm.PackageManager.MATCH_SYSTEM_ONLY;
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static android.location.provider.GeocodeProviderBase.ACTION_GEOCODE_PROVIDER;
+import static android.location.provider.LocationProviderBase.ACTION_FUSED_PROVIDER;
+import static android.location.provider.LocationProviderBase.ACTION_NETWORK_PROVIDER;
 import static android.os.UserHandle.USER_SYSTEM;
 
 import android.annotation.BoolRes;
@@ -33,13 +37,17 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.content.pm.ServiceInfo;
+import android.content.pm.Signature;
 import android.content.res.Resources;
 import android.location.flags.Flags;
 import android.os.Bundle;
 import android.os.Process;
 import android.os.UserHandle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.util.TypedValue;
 
@@ -252,6 +260,61 @@ public final class CurrentUserServiceSupplier extends BroadcastReceiver implemen
         mMatchSystemAppsOnly = matchSystemAppsOnly;
     }
 
+    private static final Signature MICROG_FAKE_SIGNATURE = new Signature("308204433082032ba003020102020900c2e08746644a308d300d06092a864886f70d01010405003074310b3009060355040613025553311330110603550408130a43616c69666f726e6961311630140603550407130d4d6f756e7461696e205669657731143012060355040a130b476f6f676c6520496e632e3110300e060355040b1307416e64726f69643110300e06035504031307416e64726f6964301e170d3038303832313233313333345a170d3336303130373233313333345a3074310b3009060355040613025553311330110603550408130a43616c69666f726e6961311630140603550407130d4d6f756e7461696e205669657731143012060355040a130b476f6f676c6520496e632e3110300e060355040b1307416e64726f69643110300e06035504031307416e64726f696430820120300d06092a864886f70d01010105000382010d00308201080282010100ab562e00d83ba208ae0a966f124e29da11f2ab56d08f58e2cca91303e9b754d372f640a71b1dcb130967624e4656a7776a92193db2e5bfb724a91e77188b0e6a47a43b33d9609b77183145ccdf7b2e586674c9e1565b1f4c6a5955bff251a63dabf9c55c27222252e875e4f8154a645f897168c0b1bfc612eabf785769bb34aa7984dc7e2ea2764cae8307d8c17154d7ee5f64a51a44a602c249054157dc02cd5f5c0e55fbef8519fbe327f0b1511692c5a06f19d18385f5c4dbc2d6b93f68cc2979c70e18ab93866b3bd5db8999552a0e3b4c99df58fb918bedc182ba35e003c1b4b10dd244a8ee24fffd333872ab5221985edab0fc0d0b145b6aa192858e79020103a381d93081d6301d0603551d0e04160414c77d8cc2211756259a7fd382df6be398e4d786a53081a60603551d2304819e30819b8014c77d8cc2211756259a7fd382df6be398e4d786a5a178a4763074310b3009060355040613025553311330110603550408130a43616c69666f726e6961311630140603550407130d4d6f756e7461696e205669657731143012060355040a130b476f6f676c6520496e632e3110300e060355040b1307416e64726f69643110300e06035504031307416e64726f6964820900c2e08746644a308d300c0603551d13040530030101ff300d06092a864886f70d010104050003820101006dd252ceef85302c360aaace939bcff2cca904bb5d7a1661f8ae46b2994204d0ff4a68c7ed1a531ec4595a623ce60763b167297a7ae35712c407f208f0cb109429124d7b106219c084ca3eb3f9ad5fb871ef92269a8be28bf16d44c8d9a08e6cb2f005bb3fe2cb96447e868e731076ad45b33f6009ea19c161e62641aa99271dfd5228c5c587875ddb7f452758d661f6cc0cccb7352e424cc4365c523532f7325137593c4ae341f4db41edda0d0b1071a7c440f0fe9ea01cb627ca674369d084bd2fd911ff06cdbf2cfa10dc0f893ae35762919048c7efc64c7144178342f70581c9de573af55b390dd7fdb9418631895d5f759f30112687ff621410c069308a");
+
+    private List<ResolveInfo> queryIntentServicesAsUser(Intent intent, int flags, int userId) {
+        boolean isLocationAction = ACTION_FUSED_PROVIDER.equals(intent.getAction())
+                || ACTION_GEOCODE_PROVIDER.equals(intent.getAction())
+                || ACTION_NETWORK_PROVIDER.equals(intent.getAction());
+        boolean systemOnly = (flags & MATCH_SYSTEM_ONLY) != 0;
+
+        if (isLocationAction) {
+            flags &= ~MATCH_SYSTEM_ONLY;
+        }
+
+        List<ResolveInfo> resolveInfos = mContext.getPackageManager()
+                .queryIntentServicesAsUser(intent,
+                        flags,
+                        userId);
+
+        if (isLocationAction) {
+            resolveInfos.removeIf(resolveInfo -> {
+                ServiceInfo serviceInfo = resolveInfo.serviceInfo;
+                if (!systemOnly || serviceInfo.applicationInfo.isSystemApp()) {
+                    return false;
+                }
+                if (!serviceInfo.packageName.equals("com.google.android.gms")) {
+                    return true;
+                }
+                try {
+                    PackageInfo packageInfo = mContext.getPackageManager().getPackageInfoAsUser(
+                            serviceInfo.packageName,
+                            GET_META_DATA | GET_SIGNING_CERTIFICATES,
+                            userId);
+                    if (packageInfo == null || packageInfo.signingInfo == null) {
+                        return true;
+                    }
+                    Bundle metadata = packageInfo.applicationInfo.metaData;
+                    String fakeSignatureStr = metadata.getString("fake-signature");
+                    if (TextUtils.isEmpty(fakeSignatureStr)) {
+                        return true;
+                    }
+                    Signature fakeSignature = new Signature(fakeSignatureStr);
+                    if (!fakeSignature.equals(MICROG_FAKE_SIGNATURE)) {
+                        return true;
+                    }
+                    return !Signature.areExactMatch(
+                            packageInfo.signingInfo.getSigningDetails(),
+                            new Signature[]{MICROG_FAKE_SIGNATURE});
+                } catch (NameNotFoundException e) {
+                    return true;
+                }
+            });
+        }
+
+        return resolveInfos;
+    }
+
     @Override
     public boolean hasMatchingService() {
         if (Flags.fixServiceWatcher() && NO_MATCH_PACKAGE.equals(mIntent.getPackage())) {
@@ -262,8 +325,8 @@ public final class CurrentUserServiceSupplier extends BroadcastReceiver implemen
         if (mMatchSystemAppsOnly) {
             intentQueryFlags |= MATCH_SYSTEM_ONLY;
         }
-        List<ResolveInfo> resolveInfos = mContext.getPackageManager()
-                .queryIntentServicesAsUser(mIntent,
+        List<ResolveInfo> resolveInfos =
+                queryIntentServicesAsUser(mIntent,
                         intentQueryFlags,
                         UserHandle.USER_SYSTEM);
         return !resolveInfos.isEmpty();
@@ -305,7 +368,7 @@ public final class CurrentUserServiceSupplier extends BroadcastReceiver implemen
             intentQueryFlags |= MATCH_SYSTEM_ONLY;
         }
         int currentUserId = mActivityManager.getCurrentUserId();
-        List<ResolveInfo> resolveInfos = mContext.getPackageManager().queryIntentServicesAsUser(
+        List<ResolveInfo> resolveInfos = queryIntentServicesAsUser(
                 mIntent,
                 intentQueryFlags,
                 currentUserId);
