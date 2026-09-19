5c392b4131400a85fe1d1d209f50906dc7a1bd05
LuK1337 <priv.luk@gmail.com>
2026-09-19 17:12:48 +0200

    SystemUI: Fix torch tile stuck unavailable after boot
    Some devices are booting so fast that camera isn't available after boot,
    so lets reduce reconnect delay for them.
    
    Change-Id: I68d7fbb14b1f37078e22a9dd53f22e6ac287a2c6

--- packages/SystemUI/src/com/android/systemui/flashlight/data/repository/FlashlightRepository.kt
+++ packages/SystemUI/src/com/android/systemui/flashlight/data/repository/FlashlightRepository.kt
@@ -182,6 +182,13 @@ constructor(
     private fun updateReconnect() {
         bgScope.launch {
             delay(RECONNECT_COOLDOWN)
+            RECONNECT_COOLDOWN = when (RECONNECT_COOLDOWN) {
+                2.seconds -> 5.seconds
+                5.seconds -> 10.seconds
+                10.seconds -> 15.seconds
+                15.seconds -> 30.seconds
+                else -> RECONNECT_COOLDOWN
+            }
             canAttemptReconnect.set(true)
         }
     }
@@ -471,7 +478,7 @@ constructor(
 
     private companion object {
         private const val BASE_TORCH_LEVEL = 1
-        private val RECONNECT_COOLDOWN = 30.seconds
+        private var RECONNECT_COOLDOWN = 2.seconds
         private val RECONNECT_TIMEOUT = 2.seconds
     }
 }
